891fb6ba95a1251db17e6521955696a99e06bae4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-20 22:32:42 +0000

    Update archive 69

---
+++ trunk/Archive2/69/12bc01db972456/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+struct Data
+{
+    std::vector<T> data;
+};
+
+template <typename T>
+struct Spec;
+
+template <typename T, typename A>
+struct Spec<std::vector<T, A>>
+{
+    static std::vector<T, A> Get()
+    {
+        return std::vector<T, A>(3);
+    }
+};
+
+template <typename T>
+struct Spec<Data<T>>
+{
+    static Data<T> Get()
+    {
+        return Data<T>{{T{}, T{}}};
+    }
+};
+
+template <typename T>
+struct Spec<Data<Data<T>>>
+{
+    static Data<Data<T>> Get()
+    {
+        return Data<Data<T>>{{{}, {}, {}, {}}};
+    }
+};
+
+template <typename T>
+auto Get() {
+    return Spec<T>::Get();
+}
+
+int main()
+{
+    std::cout << Get<Data<int>>().data.size() << std::endl;
+    std::cout << Get<std::vector<int>>().size() << std::endl;
+    std::cout << Get<Data<Data<int>>>().data.size() << std::endl;
+}
