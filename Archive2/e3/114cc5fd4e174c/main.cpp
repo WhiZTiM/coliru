e6b43257e0bbdeeb43b18b4559a2b04a0527d2a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-21 17:56:12 +0000

    Update archive e3

---
+++ Archive2/e3/114cc5fd4e174c/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+namespace JNI
+{
+    struct JInt 
+    {
+        std::string Name()
+        {
+            return "I";
+        }
+    };
+    
+    struct JVoid
+    {
+        std::string Name()
+        {
+            return "V";
+        }
+    };
+        
+    struct JString
+    {
+        std::string Name()
+        {
+            return "java/lang/String;";
+        }
+    };
+    
+    template <typename IndexedType>
+    struct JArray
+    {
+        std::string Name()
+        {
+            return "[" + IndexedType().Name();
+        }
+    };
+    
+    template <typename>
+    struct JFunction;
+    
+    template <typename Ret, typename Arg1>
+    struct JFunction<Ret(Arg1)>
+    {
+        std::string Sig()
+        {
+            return "(" + Arg1().Name() + ")" + Ret().Name();
+        }
+    };
+    
+    template <typename Ret, typename Arg1, typename Arg2>
+    struct JFunction<Ret(Arg1, Arg2)>
+    {
+        std::string Sig()
+        {
+            return "(" + Arg1().Name() + Arg2().Name() + ")" + Ret().Name();
+        }
+    };
+}
+
+int main()
+{
+    using namespace JNI;
+    std::cout << JFunction<JInt(JInt)>().Sig() << std::endl;
+    std::cout << JFunction<JInt(JArray<JInt>)>().Sig() << std::endl;
+    std::cout << JFunction<JVoid(JInt, JArray<JInt>)>().Sig() << std::endl;
+    std::cout << JFunction<JVoid(JInt, JString)>().Sig() << std::endl;
+}
