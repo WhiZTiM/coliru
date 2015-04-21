b5ade4ed4b8940aa3e1e280414ff4b4783400e9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-21 19:15:23 +0000

    Update archive 68

---
+++ trunk/Archive2/68/eab070e2111aa7/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <array>
+#include <cstdint>
+
+using namespace std;
+
+typedef uint32_t ClientId;
+
+std::array<char, 32> buffer;
+
+void send(ClientId clientId, const char payload[], std::size_t payloadSize)
+{
+    cout << "Non-templated send.\n";
+}
+
+template <typename Func>
+void send(ClientId clientId, Func convFunc, std::size_t payloadSize)
+{
+    convFunc();
+    
+    cout << "Templated send.\n";
+    
+    send(clientId, buffer.data(), payloadSize);
+}
+
+int main()
+{
+    int val = 42;
+    auto convFunc = []() {};
+    
+    send(1, convFunc, 16);
+}
