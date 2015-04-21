273291bfb8cd6a4cd968a5d6900630bf68d8c226
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-21 23:24:25 +0000

    Update archive ad

---
+++ trunk/Archive2/ad/95a48ff7519357/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class FXX {
+    public:
+    FXX() {cout << "FXX default ctor" << endl;}
+    void channel() = 0;
+    virtual void phrase() { cout << "Fearless" << endl;}
+};
+class Archer : public FXX {
+    int shots_taken;
+    public:
+    Archer(int glengoole_blue) : shots_taken(glengoole_blue) {}
+    virtual void phrase() { cout << "Phrasing BOOM!" << endl;}
+};
+
+int main()
+{
+    Archer* sterling = new Archer(10);
+    sterling->phrase();
+}
