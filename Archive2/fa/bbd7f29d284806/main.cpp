50616e18a93e440582d88441ef3c72ad7addb7a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-21 23:23:07 +0000

    Update archive fa

---
+++ Archive2/fa/bbd7f29d284806/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class RGB
+{
+public:
+
+RGB(float r1, float g1, float  b1);
+RGB(RGB const& color); //copy constructor
+RGB();
+
+friend ostream& operator<<(ostream& os, RGB const& color);
+friend istream& operator>>(istream& is, RGB& color);
+
+friend float r();
+friend float g();
+friend float b();
+private:
+    float r, g, b;
+};
+
+//Something something
+
+RGB::RGB(float r1, float g1, float b1 ){
+r = r1;
+g = g1;
+b = b1;
+}
+
+//Something something
+
+ostream& operator<<(ostream& os, const RGB& color){    // << Overloading
+return os<<"Red: "<<color.r<<endl<<"Green: "<<color.g<<endl<<"Blue: "<<color.b<<endl;
+}
+
+int main()
+{
+    RGB mycolor(1,2,3);
+    cout<<mycolor;
+}
