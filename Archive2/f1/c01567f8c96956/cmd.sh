811a5277bbddbeebf1d81964c1aee2f111129396
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-21 22:15:48 +0000

    Update archive f1

---
+++ Archive2/f1/c01567f8c96956/cmd.sh
@@ -0,0 +1,5 @@
+#title demultiplexing using priority_queue
+export num_packets=1000
+clang++ -Dnum_packets=${num_packets} -std=c++14 -stdlib=libc++ -O2 -Wall -pedantic -pthread main.cpp && ./a.out >packets
+printf "Left column shows packets ordered by receive time. Right column shows the result of partial demultiplexing with priority_queue. (Grouping by port number enables batch processing later on):\n"
+head -n${num_packets} packets >a; tail -n${num_packets} packets >b; paste -d '	' a b
