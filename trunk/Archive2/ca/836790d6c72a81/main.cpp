bc47b55fcc214f153a16f5f96dc33ea71d4cfefe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-21 03:06:25 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/836790d6c72a81/main.cpp
@@ -0,0 +1,5 @@
+            Level   Stats   DivLevel    Base    OverBase   Relative
+Feraligatr  43      623     14.488      530     0.027336
+Onix        41      458     11.170      385     0.029014  +6.139%
+Haunter     41      463     11.292      405     0.027883  +1.999%
+Growlithe   38      389     10.236      350     0.029248  +6.992%
