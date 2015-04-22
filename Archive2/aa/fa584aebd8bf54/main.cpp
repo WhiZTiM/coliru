b34bd29c834363e56c7f1c9fe80f85b069c58c15
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-22 14:15:46 +0000

    Update archive aa

---
+++ Archive2/aa/fa584aebd8bf54/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <regex>
+ 
+int main()
+{
+    // Simple regular expression matching
+    std::string fnames[] = {"ftp://127.0.0.1:8080/", "ftp://127.0.0.1/", 
+      "ftp://127.0.0.1/home/amit", "ftp://127.0.0.1:8080/home/amit", "ftp://127.0.0.1"};
+
+    // Extraction of several sub-matches
+    std::regex pieces_regex("([a-z]+)\\://(.*)[\\:*]([0-9]*)[\\/](.*)");
+    std::smatch pieces_match;
+ 
+    for (const auto &fname : fnames) {
+        if (std::regex_match(fname, pieces_match, pieces_regex)) {
+            std::cout << fname << '\n';
+            for (size_t i = 0; i < pieces_match.size(); ++i) {
+                std::ssub_match sub_match = pieces_match[i];
+                std::string piece = sub_match.str();
+                std::cout << "  submatch " << i << ": " << piece << '\n';
+            }   
+        }   
+    }   
+}
