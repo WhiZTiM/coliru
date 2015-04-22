cc07d1851cc8db1f66b7bc1d84391383b3860378
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-22 20:13:01 +0000

    Update archive e9

---
+++ Archive2/e9/58376d70b7a2fe/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+    {
+    int x,y;
+    cin >> x >> y;
+    if ((x>=0 && x<=432) && (y>=-468 && y<=468)){
+        cout << "dentro";
+        }else{
+              cout << "fora";
+              }
+    return 0;
+    }
