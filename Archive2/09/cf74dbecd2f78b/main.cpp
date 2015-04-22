016305ee6e4a35418864239469426b103ffcde8e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-22 21:11:53 +0000

    Update archive 09

---
+++ Archive2/09/cf74dbecd2f78b/main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+using namespace std;
+
+auto foo() {
+    string *acc = new string[2];
+    string some_string;
+
+    for ( int i = 0; i < 2; i ++ ) {
+
+     if ( i = 0 )
+        some_string = "ab";
+     else 
+        some_string = "cd";
+     acc[i] = some_string;
+     some_string = "";
+    }
+    return acc
+}
+
+int main() {
+    delete foo();   
+}
