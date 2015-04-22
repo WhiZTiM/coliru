9a8026d752b9827c2d4fa3843166c2fe018c4e70
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-22 08:23:43 +0000

    Update archive 06

---
+++ trunk/Archive2/06/4d93f05ea39f72/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct PointerCompare {
+    bool operator()(const int* l, const int* r) {
+        return *l < *r;
+    }
+};
+    
+int main() {
+    vector<int*> myValues(4);
+    
+    int i = 0;
+    for (auto it = myValues.rbegin(); it != myValues.rend(); ++it) {
+          *it = new int(++i);
+    }
+    
+    do {
+        for (vector<int*>::iterator it = myValues.begin(); it != myValues.end(); ++it) {
+            cout << ' ' << **it;
+        }
+        cout << endl;
+    } while (next_permutation(myValues.begin(), myValues.end(), std::less<int*>()));
+    
+    myValues.clear();
+}
+
