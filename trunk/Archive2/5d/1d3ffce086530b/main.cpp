1329886924f51477bc5e4bbbbe05eff09926782f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-23 20:17:07 +0000

    Update archive 5d

---
+++ trunk/Archive2/5d/1d3ffce086530b/main.cpp
@@ -0,0 +1,22 @@
+template <typename T>
+class TheUseOfDlctOptionalPlacementNewIsNotAvailableInThisCompilerVersion;
+
+template <typename T>
+void fn1(T t) {
+#if __GNUC__ && __GNUC__ < 4
+#error "NOT SUPPORTED ON GCC < 4.0"
+#endif
+}
+
+template <typename T>
+void fn2(T t) {
+#if __GNUC__ && __GNUC__ == 4 && __GNUC_MINOR__ >= 9
+    TheUseOfDlctOptionalPlacementNewIsNotAvailableInThisCompilerVersion<T> error;
+#endif
+}
+
+int main()
+{
+    fn1(1);
+    fn2(2);
+}
