8d770c770e37c14f5921b9e903172939acf5f384
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-24 23:11:53 +0000

    Update archive f4

---
+++ trunk/Archive2/f4/eeb56620c35ab7/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+    double operando1;
+    double operando2;
+    char operador;
+    cout << "digite a operação" <<endl;
+    cin >> operando1 >> operador >> operando2;
+    if( operador=='+'){
+        cout << operando1+ operando2;
+    }else if(operador== '*'){
+        cout << operando1 * operando2;
+    }else if(operador== '-'){
+        cout << operando1 - operando2;
+    }else if(operador== '/'){
+        cout << operando1 / operando2;
+    }
+
