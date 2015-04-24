edd85c280e17fac6d1be87ab08943b552c610c50
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-24 23:12:47 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/81590e8923ea24/main.cpp
@@ -0,0 +1,23 @@
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <iostream>
+
+namespace bip = boost::interprocess;
+
+struct MyWorker : boost::noncopyable {
+    MyWorker() : _shm(init_shm()) { }  
+  private:
+    static bip::managed_mapped_file init_shm() {
+        try {
+            return bip::managed_mapped_file(bip::open_or_create, "089f8a0f-956a-441d-9b9e-e0696c43185f", 10ul<<20);
+        } catch(...) {
+            std::cout << "Look ma! I'm handling my exceptions\n" << "\n";
+            // try smaller size for example
+            return bip::managed_mapped_file(bip::open_or_create, "089f8a0f-956a-441d-9b9e-e0696c43185f", 10ul<<10);
+        }
+    }
+    bip::managed_mapped_file _shm;
+};
+
+int main() {
+    MyWorker instance;
+}
