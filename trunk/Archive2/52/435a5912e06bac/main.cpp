dc01d39c97d1dd9b1cc8d8b25198752611b14029
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-24 23:36:36 +0000

    Update archive 52

---
+++ trunk/Archive2/52/435a5912e06bac/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename T>
+class A
+{
+private:
+    T mValue;
+public:
+    template<typename U=T>
+    auto operator +=(const U &rhs)
+        -> decltype(mValue += rhs)
+    {
+        mValue += rhs;
+        return mValue;
+    }
+};
+
+
+int main()
+{
+    A<int> a;
+    a += 8; //+= will forward to the += for the int
+
+    struct Test {  /*no operators defined*/ };
+    A<Test> b;
+}
