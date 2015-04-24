4c279a3363c201748536b045b8eda7d2f215d6e6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-24 23:41:58 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/50d3401737487b/main.cpp
@@ -0,0 +1,26 @@
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <iostream>
+
+namespace bip = boost::interprocess;
+
+struct MyWorker : boost::noncopyable {
+    MyWorker() 
+    try : _shm(new bip::managed_shared_memory(bip::open_or_create, "089f8a0f-956a-441d-9b9e-e0696c43185f", 10ul<<20)) 
+    {
+        // empty constructor body
+    } catch (...) 
+    { 
+        std::cout << "Look ma! I'm handling my exceptions\n" << "\n";
+        _shm = nullptr;
+    }
+
+    ~MyWorker() {
+        delete _shm;
+    }
+  private:
+    bip::managed_shared_memory* _shm;
+};
+
+int main() {
+    MyWorker instance;
+}
