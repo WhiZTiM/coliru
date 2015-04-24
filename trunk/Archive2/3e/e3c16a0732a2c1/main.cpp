2f62009cc8541a8211f5f7fb736fb3afaa5542e9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-24 11:30:30 +0000

    Update archive 3e

---
+++ trunk/Archive2/3e/e3c16a0732a2c1/main.cpp
@@ -0,0 +1,52 @@
+#include <boost/range/algorithm.hpp>
+#include <boost/date_time.hpp>
+
+#include <iostream>
+
+namespace pt = boost::posix_time;
+
+class MyType {
+    using Clock = pt::second_clock;
+    pt::ptime _ts;
+
+  public:
+    MyType(int seconds) : _ts(Clock::local_time() + pt::seconds(seconds)) {}
+
+    pt::ptime GetTime() const { return _ts; }
+
+    friend std::ostream& operator<<(std::ostream& os, MyType const&v) {
+        return os << "[" << v._ts << "]";
+    }
+};
+
+namespace detail {
+    template <typename F> struct GreaterBy {
+        GreaterBy(F&& f) : _f(std::move(f)) {}
+
+        template <typename A, typename B>
+            bool operator()(A const& a, B const& b) const {
+                return std::greater<decltype(_f(a))>()(_f(a), _f(b));
+            }
+      private:
+        F _f;
+    };
+}
+
+template <typename F>
+detail::GreaterBy<F> order_by_desc(F&& f) {
+    return std::move(f);
+}
+
+int main() {
+    std::vector<MyType> values { 1, -3, 77, 130, 12, -99 };
+
+    std::cout << "Before sort: ";
+    for (auto& v : values)
+        std::cout << v << " ";
+
+    std::sort(values.begin(), values.end(), order_by_desc(std::mem_fn(&MyType::GetTime)));
+
+    std::cout << "\nAfter sort: ";
+    for (auto& v : values)
+        std::cout << v << " ";
+}
