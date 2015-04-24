82c945495edde518fe6895e2dfb98c699e916f3d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-24 22:54:16 +0000

    Update archive 6a

---
+++ trunk/Archive2/6a/09d229735cfc72/main.cpp
@@ -0,0 +1,33 @@
+#include <string>
+
+std::string computeMethodName(const std::string& function, const std::string& prettyFunction);
+
+#define __COMPACT_PRETTY_FUNCTION__ computeMethodName(__FUNCTION__,__PRETTY_FUNCTION__).c_str() //c_str() is optional
+
+
+std::string computeMethodName(const std::string& function, const std::string& prettyFunction) {
+    size_t locFunName = prettyFunction.find(function); //If the input is a constructor, it gets the beginning of the class name, not of the method. That's why later on we have to search for the first parenthesys
+    size_t begin = prettyFunction.rfind(" ",locFunName) + 1;
+    size_t end = prettyFunction.find("(",locFunName + function.length()); //Adding function.length() make this faster and also allows to handle operator parenthesys!
+    if (prettyFunction[end + 1] == ')')
+        return (prettyFunction.substr(begin,end - begin) + "()");
+    else
+        return (prettyFunction.substr(begin,end - begin) + "(...)");
+}
+
+struct B {};
+typedef B(*f)(B);
+
+#include <iostream>
+struct A {
+    static f B(f) {
+        std::cout << __FUNCTION__ << '\n';
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+        std::cout << __COMPACT_PRETTY_FUNCTION__;
+        return '\0';
+    }
+};
+
+int main() {
+    A::B(0);
+}
