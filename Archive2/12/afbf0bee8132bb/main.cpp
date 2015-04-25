d4e5a45c3d0a0a51b5864c4725b027eadc4f48a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-25 19:50:31 +0000

    Update archive 12

---
+++ Archive2/12/afbf0bee8132bb/main.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+int main() {
+    int array_length = 0;
+    std::string city_name;
+    
+    for(int x = 0; x < array_length; x++)
+    { if(city_name[x] == 'a' || 'e')
+      city_name[x] = ' ';}   
+}
