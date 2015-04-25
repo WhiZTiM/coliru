172afe20bd2f80de714ea918f01a37b6e1ead80b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-25 19:53:18 +0000

    Update archive 70

---
+++ trunk/Archive2/70/e9db306bff0b53/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+std::set< std::vector<int> > unique_n_combinations( std::vector<int> seq, std::size_t n = 3 )
+{
+    std::set< std::vector<int> > n_tuples ;
+    
+    if( seq.size() >= n ) do
+    {
+        std::vector<int> temp( seq.begin(), seq.begin() + n );
+        std::sort( temp.begin(), temp.end() ) ;
+        n_tuples.insert( std::move(temp) ) ;
+    }
+    while( std::next_permutation( seq.begin(), seq.end() ) ) ;
+
+    return n_tuples ;
+}
+
+void print_unique_n_combinations( std::vector<int> seq, std::size_t n = 3 )
+{
+    std::sort( seq.begin(), seq.end() ) ;
+    seq.erase( std::unique( seq.begin(), seq.end() ), seq.end() ) ;
+    
+    for( const auto& n_tuple : unique_n_combinations( seq, n ) )
+    {
+        for( int v : n_tuple ) std::cout << v << ' ' ;
+        std::cout << '\n' ;
+    }
+    std::cout << "-----------------------------\n" ;
+}
+
+int main()
+{
+    print_unique_n_combinations( { 0, 1, 2, 3 }, 3 ) ;
+    print_unique_n_combinations( { 0, 1, 2, 3, 4, 5, 6 }, 4 ) ;
+}
