e72c24846c9f9c37c6a9512307118a92fd4122a4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-25 16:18:15 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/21677fb55303c8/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <atomic>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+using namespace std;
+
+void call_from_thread(int thread_id)
+{
+    cout << "Thread [" << thread_id << "] launched." << endl;
+}
+
+int main()
+{
+    unsigned int nr_threads  = {6};
+    
+    auto threads = std::vector<std::thread>();
+
+    for(unsigned int i =0; i<nr_threads; ++i)
+    {
+        threads.push_back(std::thread(call_from_thread, i));
+    }
+
+    for (auto& thread : threads)
+    {
+        thread.join();
+    }
+    
+    return 0;
+}
+
