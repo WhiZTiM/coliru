0cb5a9e0a4cfd74dd498674366a2a95ac3551ae7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-26 19:26:53 +0000

    Update archive 75

---
+++ Archive2/75/cf3d9de0fc5ee6/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++14 -O2 -Wall -Wpedantic -pthread main.cpp && ./a.out
---
+++ Archive2/75/cf3d9de0fc5ee6/main.cpp
@@ -0,0 +1,17 @@
+bool f(bool inpoot)
+{
+    if (inpoot == true)
+    {
+        return true;
+    }
+    
+    if (inpoot == false)
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    
+}
