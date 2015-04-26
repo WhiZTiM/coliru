3f10bd22cda06cf2c06a1e21613986c1723101a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-26 16:01:03 +0000

    Update archive 6d

---
+++ Archive2/6d/137211d4648d77/main.cpp
@@ -0,0 +1,15 @@
+#include <cstddef>
+
+struct A {
+    static constexpr std::size_t f() { return 4; }
+};
+
+template<std::size_t N>
+struct B : A {
+    alignas(f()) char a[N];
+};
+
+int main(int argc, char** argv) {
+    B<4> b;
+    return 0;
+}
