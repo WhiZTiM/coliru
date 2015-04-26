9f5489e76f50a9ebd68f13f818688c20c9ab55af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-26 13:32:05 +0000

    Update archive 82

---
+++ trunk/Archive2/82/21fed931791075/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "plusOuMoins.h"
+
+void plusMoins()
+{
+    printf("Coucouuuuu");
+}
+
+void a2()
+{
+    a1();
+}
+
+void a1()
+{
+    plusMoins();
+}
