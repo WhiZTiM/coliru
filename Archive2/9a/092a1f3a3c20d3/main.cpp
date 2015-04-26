95a27a2e28d8f94edabd0dffa077eb90adfcf5cd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-26 20:28:32 +0000

    Update archive 9a

---
+++ Archive2/9a/092a1f3a3c20d3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/spirit/include/karma.hpp>
+int main()
+{
+   int n,m;
+   std::cin >> n >> m;
+
+   std::cout.sync_with_stdio(false);
+   int j=0;
+   while (j <n) {
+       int i = 0 ;
+       while (i<m) {
+               using boost::spirit::karma::int_;
+               using boost::spirit::karma::generate;
+               generate(std::ostreambuf_iterator<char>(std::cout),
+                        int_ << ',' << int_ << '\n', i, j);
+               i++;
+        }
+       j++;
+   }
+}
