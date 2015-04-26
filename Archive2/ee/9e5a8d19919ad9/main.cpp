ebd4688435c058725de49fc960589dc60508488c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-26 20:01:09 +0000

    Update archive ee

---
+++ Archive2/ee/9e5a8d19919ad9/main.cpp
@@ -0,0 +1,19 @@
+bool f(bool inpoot)
+{
+    if (inpoot == true)
+    {
+        return true;
+    }
+    
+    if (inpoot == false)
+    {
+        return false;
+    }
+    
+    __builtin_unreachable();
+}
+
+int main()
+{
+    
+}
