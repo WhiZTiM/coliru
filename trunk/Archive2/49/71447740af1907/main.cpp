8eb73bc6f583f3f82c6ccafddd46e328ec1b8e54
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-26 16:29:37 +0000

    Update archive 49

---
+++ trunk/Archive2/49/71447740af1907/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+int x, y;
+printf("escreva um numero: \n");
+scanf("%i",&x);
+if (x>= 0)
+printf("positivo");
+else
+    printf("negativo");
+return 0;
+
+
+
+
+
+
+
+
+}
