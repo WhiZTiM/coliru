7c173b4166828792e84692423bc693f621bcac75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-27 22:36:56 +0000

    Update archive d3

---
+++ trunk/Archive2/d3/3a76eb5ac56f50/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <cstdlib>
+
+using namespace std;
+
+
+void openFiles(std::ofstream& myfile, std::string& result, std::string& name, std::string& ending)
+{
+    std::string str;
+    for(int i=1;i != 101;++i) {
+        if(i%20==0) {
+            ostringstream temp;            // use a string stream to convert int...
+            temp << i;
+            str = temp.str();              // ...to str
+            result = name + str + ending;  // concatenating strings.
+            myfile.open(result);
+            myfile << "The value is:" << i;
+            myfile.close();
+        }
+    }
+}
+
+int main ()
+{
+    std::string name = "John"; //int age = 21;
+    std::string result;
+    std::string ending = ".txt";
+    std::ofstream myfile;
+    openFiles(myfile, result, name, ending);
+}
