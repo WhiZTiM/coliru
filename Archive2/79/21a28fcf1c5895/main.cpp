b0bbe298d08bc3cb04c7ebee730bb7df413a091d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-27 21:17:29 +0000

    Update archive 79

---
+++ Archive2/79/21a28fcf1c5895/main.cpp
@@ -0,0 +1,22 @@
+//Notas da prova(Tarefa da OBI2009, Modalidade Programação Júnior, Fase 1)
+#include <iostream>
+using namespace std;
+
+int main(){
+int nota;
+cin >> nota;
+
+if(A nota está entre 86 a 100?){
+    Se sim é conceito A, então a saida deve ser A
+}else if(A nota está entre 61 a 85?){
+    Se sim é conceito B, então a saida deve ser B
+}else if(A nota está entre 36 a 60?){
+    Se sim é conceito C, então a saida deve ser C
+}else if(A nota está entre 1 a 35?){
+    Se sim é conceito D, então a saida deve ser D
+}else if(A nota é 0?){
+    Se sim é conceito E, então a saida deve ser E
+}
+
+return 0;
+}
