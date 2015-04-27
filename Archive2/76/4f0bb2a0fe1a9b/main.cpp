79a629e2d2d91e8805bb12c7652ddd0208ceeb0c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-27 20:53:06 +0000

    Update archive 76

---
+++ Archive2/76/4f0bb2a0fe1a9b/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdint>
+#include <type_traits>
+
+enum class FooEnum : uint8_t{
+    Foo1 = 0, 
+    Foo2 = 1
+};
+
+constexpr uint32_t operator|= (uint32_t& lhs, FooEnum rhs) {
+    return lhs == 1 ? 0 : lhs |= 1u << static_cast<uint8_t>(rhs);
+}
+
+int main() {
+    uint32_t bar{0};
+    bar|=FooEnum::Foo1;
+}
