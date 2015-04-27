02151a073a4fd10d98c4aa41f312e0521fa89def
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-27 21:11:16 +0000

    Update archive b0

---
+++ trunk/Archive2/b0/299ed8803537f8/cmd.sh
@@ -0,0 +1,13 @@
+clang++ -std=c++11 -Os -Wall -pthread -pedantic main.cpp && ./a.out <<DOT
+digraph G {
+  rankdir=LR
+  I [label="", style=invis, width=0]
+  I -> 0
+  0 [label="0", peripheries=2]
+  0 -> 0 [label="a"]
+  0 -> 1 [label="!a"]
+  1 [label="1"]
+  1 -> 0 [label="a"]
+  1 -> 1 [label="!a"]
+}
+DOT
