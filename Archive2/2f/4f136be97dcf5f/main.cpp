3c99452257d2dfdedce129a04577e1bbbc50b387
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-27 20:37:57 +0000

    Update archive 2f

---
+++ Archive2/2f/4f136be97dcf5f/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bitset>
+
+template <typename T>
+T twos_complement(T bs) {
+    return T((~bs).to_ulong() + 1);
+}
+
+template <std::size_t N>
+struct bits {
+    bits(unsigned long b) : b{b} { }
+    
+    bits 
+    operator ~ () const {
+        return {~b};
+    }
+    
+    unsigned long
+    to_ulong() const {
+        return b;
+    }
+    
+    operator std::bitset<N> () {
+        return {b};
+    }
+    
+    unsigned long b;
+};
+
+int
+main() {
+    std::cout << (std::bitset<4>) twos_complement(bits<4>{13}) << '\n';
+}
