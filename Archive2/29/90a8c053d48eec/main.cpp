b04fd9231ec27ace6a2cd6cdadd36f4fa348ec66
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-28 11:07:37 +0000

    Update archive 29

---
+++ Archive2/29/90a8c053d48eec/main.cpp
@@ -0,0 +1,26 @@
+// vector::data
+#include <iostream>
+#include <vector>
+
+int main ()
+{
+  std::vector<int> myvector (5);
+
+  int* p = myvector.data();
+
+  *p=10; ++p;
+  *p=20; ++p;
+  *p=30; ++p;
+  *p=40; ++p;
+  *p=50; ++p;
+  *p=60; ++p;
+  
+  //p[2] = 100;
+
+  std::cout << "myvector contains:";
+  for (unsigned i=0; i<myvector.size(); ++i)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+
+  return 0;
+}
