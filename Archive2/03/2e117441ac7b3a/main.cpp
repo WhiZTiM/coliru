7a9167c97022a31cf48cd2cce3d445e686ad6e52
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-28 06:14:24 +0000

    Update archive 03

---
+++ Archive2/03/2e117441ac7b3a/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+ 
+enum color
+{
+  red = 0,
+  green,
+  blue
+};
+
+#define COLOR_A green
+//#define COLOR_B green //undefined
+
+int main()
+{
+ 
+#ifdef COLOR_A
+    std::cout << "1: COLOR_A=" << COLOR_A << ", OK\n";
+#else
+    std::cout << "1: no\n";
+#endif
+
+#if (COLOR_B == blue)
+    std::cout << "2: COLOR_B is blue, DOH!\n";
+#else
+    std::cout << "2: no, OK\n";
+#endif
+
+#if (COLOR_A == COLOR_B)
+    std::cout << "3: COLOR_A = COLOR_B, DOH!\n";
+#else
+    std::cout << "3: no, OK\n";
+#endif
+
+#if (defined(COLOR_A) && COLOR_A == blue)
+    std::cout << "4: COLOR_A is blue, DOH!\n";
+#else
+    std::cout << "4: no, OK\n";
+#endif
+
+
+
+}
