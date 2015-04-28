03ac2e898414281cb2d150b311cb635293b59204
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-28 12:07:21 +0000

    Update archive 62

---
+++ trunk/Archive2/62/b6e586c1ff6884/main.cpp
@@ -0,0 +1,8 @@
+// { dg-do compile { target c++11 } }
+    template<typename T> void f(T);
+    
+    template <class T> class A {
+      friend constexpr void f<>(int);  // { dg-error "'constexpr' is not allowed" }
+    };
+    
+    int main() {}
