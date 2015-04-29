7435dcfa44fab8d4f71e0a0c319feb63f51c5aa8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-29 23:10:18 +0000

    Update archive db

---
+++ Archive2/db/0def671e195927/main.cpp
@@ -0,0 +1,44 @@
+#include <atomic>
+#include <thread>
+#include <iostream>
+#include <chrono>
+
+
+
+struct blah {
+    std::atomic_flag quitflag_;
+    
+    blah() { quitflag_.test_and_set(); }
+    
+    void request_quit() { std::cout << "quitting.."; quitflag_.clear(); }
+    
+    void run() {
+     
+        while (quitflag_.test_and_set()) {
+            std::cout << "still here..";
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+        }
+    }
+};
+
+
+int main() {
+    
+    blah b;
+    
+    std::thread t{&blah::run,&b};
+    
+    // Wait for external termination request
+    while( true )
+    {
+        if( external_request_to_quit() )
+        {
+            b.request_quit();
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(6));
+    }
+    
+    
+    t.join();    
+}
