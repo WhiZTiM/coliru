9a613c8f451c725e94b1f6d55b12e5aa10b672ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-29 08:05:17 +0000

    Update archive c7

---
+++ Archive2/c7/511aac3c28dad5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+
+#define foreach(TYPE, ELEMENT, COLLECTION_TYPE, COLLECTION)\
+for(COLLECTION_TYPE::iterator ELEMENT##MACRO_TEMP_IT = COLLECTION.begin(); ELEMENT##MACRO_TEMP_IT != COLLECTION.end(); ++ELEMENT##MACRO_TEMP_IT)\
+for(bool ELEMENT##MACRO_B = true; ELEMENT##MACRO_B;)\
+for(TYPE ELEMENT = *(ELEMENT##MACRO_TEMP_IT); ELEMENT##MACRO_B; ELEMENT##MACRO_B = false)
+
+int main()
+{
+    std::list<int> indexes{1, 2, 3};
+    
+    foreach(int, i, std::list<int>, indexes)
+    {
+        std::cout << i << std::endl;
+    }
+    
+    foreach(int, i, std::list<int>, indexes)
+        std::cout << i << std::endl;
+}
