3f7906a63ff0e3dad2db9dc4a0f93fa5aecb5463
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-29 06:15:12 +0000

    Update archive d0

---
+++ Archive2/d0/a4810877146542/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+ 
+int main()
+{  
+    map< int, int > map1;
+ 
+    map1.insert( map< int, int >::value_type(1, 35));
+    std::cout << "Found " << search->second << '\n';
+   
+}
