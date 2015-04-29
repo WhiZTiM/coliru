43bac3bd3da53ddb95b53b071d1fc115bf610e8b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-29 05:46:36 +0000

    Update archive 75

---
+++ Archive2/75/5db3af886e227d/main.cpp
@@ -0,0 +1,22 @@
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+struct MySweetPOD {
+    int check_this;
+    int sweet_pod_out;
+    double its_awesome;
+};
+
+int main()
+{
+    // create temp MySweetPod with args, copy all members of temp MySweetPod to allocated MySweetPod
+    shared_ptr<MySweetPOD> pod0 = make_shared<MySweetPod>(MySweetPod{ 1,2,3.0 });
+
+    // allocate MySweetPod with args
+    shared_ptr<MySweetPod> pod1(new MySweetPod{1,2,3.0});
+
+    return 0;
+}
