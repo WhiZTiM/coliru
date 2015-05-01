729295b4880c99162c1fb574f6160f2ca80ff26f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-01 13:09:43 +0000

    Update archive ae

---
+++ trunk/Archive2/ae/8d8d2367e6e408/main.cpp
@@ -0,0 +1,30 @@
+#include <functional>
+#include <iostream>
+#include <memory>
+
+template <typename T>
+using Callback = std::function<void(const T)>;
+
+template <typename T>
+void Call(const T yyy, Callback<T>&& callback) {
+    callback(yyy);
+}
+
+template <typename T>
+class MyCallback {
+public:
+    explicit MyCallback(const T counter_init) : counter_{counter_init} {}
+    void operator ()(const T xxx) {
+        std::cout << counter_ << '\t' << xxx << std::endl;
+        ++counter_;
+    }
+private:
+    T counter_;
+};
+
+int main() {
+    const auto callback = std::make_unique<MyCallback<int>>(0);
+    Call(111, *callback);
+    Call(222, *callback);
+    return 0;
+}
