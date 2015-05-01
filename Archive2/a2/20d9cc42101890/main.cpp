1473b0cb5aefb255599fd3b9e10c63dd29a80c98
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-01 17:36:33 +0000

    Update archive a2

---
+++ Archive2/a2/20d9cc42101890/main.cpp
@@ -0,0 +1,38 @@
+#include <cstddef>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include <utility>
+#include <iostream>
+
+typedef size_t Index;
+typedef unsigned long Natural;
+typedef std::vector<Index> PolyIndex;
+typedef const PolyIndex& crPolyIndex;
+template<Index N> struct PortIdxType{};
+
+bool operator> (int, PolyIndex);
+
+template<typename D>
+struct base
+{
+    template<Index N, typename T = D>
+    static auto foo() -> decltype(std::declval<T*>() -> getOutput<N>(PolyIndex({})));
+};
+
+struct derived
+{
+    static int getOutput;
+};
+
+
+template<typename T>
+void print_type()
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+
+int main()
+{
+    print_type< decltype(base<derived>::foo<0>()) >();
+}
