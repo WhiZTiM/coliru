f31a8b0d595f1ae32f540a13ad364737c59eccd4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-01 10:32:56 +0000

    Update archive 88

---
+++ trunk/Archive2/88/6660f50140ed34/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/algorithms/distance.hpp>
+#include <boost/geometry/strategies/distance.hpp>
+
+
+int main() {
+    using namespace boost::geometry;
+    using P = model::d2::point_xy<float>;
+    using V = traits::coordinate_type<P>::type;
+
+    std::cout << "Coordinate type:  " << typeid(V).name() << "\n";
+
+    P a, b;
+    std::cout << "Calculation type: " << typeid(default_distance_result<P, P>::calculation_type).name() << "\n";
+    std::cout << "Result type:      " << typeid(default_distance_result<P, P>::type).name()             << "\n";
+
+    using S = strategy::distance::pythagoras<V>;
+    std::cout << "Calculation type: " << typeid(distance_result<P, P, S>::calculation_type).name() << "\n";
+    std::cout << "Result type:      " << typeid(distance_result<P, P, S>::type).name()             << "\n";
+    
+    static_assert(boost::is_same<distance_result<P, P, S>::type, float>::value, "oops");
+}
