950cb974e0d6f5fe3a3e817f53b43aab9da305d2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-01 14:10:46 +0000

    Update archive c3

---
+++ Archive2/c3/67e5fa74a61420/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <boost/assign/list_of.hpp>
+using boost::assign::map_list_of;
+
+const std::map<int, std::map<int, char> > test = map_list_of
+    (100, map_list_of
+        (1, 'a')
+        (2, 'b')
+    )
+    (101, map_list_of
+        (1, 'c')
+        (2, 'd')
+    )
+;
+
+int main()
+{
+    std::cout << test.find(101)->second.find(2)->second << "\n";
+}
