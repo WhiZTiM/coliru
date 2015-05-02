f35d50285a3c138ad4119d27bf56e2adfaba2cbd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-02 08:09:29 +0000

    Update archive 3d

---
+++ trunk/Archive2/3d/1430b69af039dd/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+ 
+template<typename T>
+class Foo; // forward declare to make function declaration possible
+ 
+template<typename T> // declaration
+std::ostream& operator<<(std::ostream&, const Foo<T>&);
+ 
+template<typename T>
+class Foo {
+ public:
+    Foo(const T& val) : data(val) {}
+ private:
+    T data;
+ 
+    // refers to a full specialization for this particular T 
+    friend std::ostream& operator<< <> (std::ostream&, const Foo&);
+    // note: this relies on template argument deduction in declarations
+    // can also specify the template argument with operator<< <T>"
+};
+ 
+// definition
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const Foo<T>& obj)
+{
+    return os << obj.data;
+}
+ 
+int main()
+{
+    Foo<double> obj(1.23);
+    std::cout << obj << '\n';
+}
