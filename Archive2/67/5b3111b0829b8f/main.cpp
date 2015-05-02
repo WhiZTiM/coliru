0a10b7f60ad5f3c818e01d19fbffb578cd31cdee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-02 20:24:56 +0000

    Update archive 67

---
+++ Archive2/67/5b3111b0829b8f/main.cpp
@@ -0,0 +1,26 @@
+class E {
+    int x;
+    
+    class B { };
+    
+public:
+    class I {
+        B b; // error: E::B is private
+        int y;
+        void f(E* p, int i)
+        {
+            p->x = i; // error: E::x is private
+            if ( &b - y ) {} 
+        }
+    };
+    
+    /*int g(I* p)
+    {
+        return p->y; // error: I::y is private
+    }*/
+};
+
+int main()
+{
+    
+}
