505a483d1efdf2e6beefe6dae932f0cd0691b42c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-03 22:14:32 +0000

    Update archive 2b

---
+++ trunk/Archive2/2b/5c80198f86aaaf/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <functional>
+ 
+int main()
+{
+    std::array<int, 10> s{5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
+ 
+    std::replace(s.begin(), s.end(), 8, 88);
+ 
+    for (int a : s) {
+        std::cout << a << " ";
+    }
+    std::cout << '\n';
+ 
+    std::replace_if(s.begin(), s.end(), 
+                    std::bind(std::less<int>(), std::placeholders::_1, 5), 55);
+    for (int a : s) {
+        std::cout << a << " ";
+    }
+    std::cout << '\n';
+}
