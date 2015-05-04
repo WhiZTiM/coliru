88c43f83907e95a547383c100c681b6dae0f8e35
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-04 16:44:44 +0000

    Update archive e2

---
+++ Archive2/e2/ef3bcfa2c42602/cmd.sh
@@ -0,0 +1,21 @@
+cat>3test.txt<<TEST; g++ -std=c++11 -Os -Wall -pedantic main.cpp  && ./a.out
+t # 0
+v 0 35
+v 1 47
+v 2 15
+v 3 14
+v 4 86
+e 0 1 10
+e 1 2 77
+e 1 3 17
+e 4 2 43
+t # 1
+v 0 35
+v 1 47
+v 2 15
+v 3 14
+v 4 86
+e 0 1 10
+e 1 2 7
+e 1 3 17
+e 4 2 4
