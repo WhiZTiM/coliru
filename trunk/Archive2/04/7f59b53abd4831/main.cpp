b0bb412f474fb66ddcde9287d99cd35f1754bbc3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-04 11:44:00 +0000

    Update archive 04

---
+++ trunk/Archive2/04/7f59b53abd4831/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+template<typename T>
+class ISO{
+public:
+    ISO(){}
+    ISO(T iso) : _iso{iso} {}
+    virtual ~ISO() {}
+    
+    virtual void setIso(T iso) = 0;
+
+protected:
+    T _iso;
+};
+
+class Sx510ISO : public ISO<std::string>{
+public:
+    using ISO::ISO;
+    ~Sx510ISO(){}
+    void setIso(std::string iso) override {setIso(iso);}
+    
+    void printIso(){ std::cout << "iso is " << _iso << "\n";}
+};
+
+class Camera{
+public:
+    void setIso(Sx510ISO iso) {_iso = iso;}
+    
+private:
+    Sx510ISO _iso;
+};
+
+int main()
+{
+    Sx510ISO iso{"100"};
+    iso.printIso();
+}
