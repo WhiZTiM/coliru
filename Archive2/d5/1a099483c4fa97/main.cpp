c2f15b2ff9833647c1e0b0e785065cc9619bdaa7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-04 16:49:56 +0000

    Update archive d5

---
+++ Archive2/d5/1a099483c4fa97/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+	std::vector<int> vec_second = {6, 7};
+
+	auto it = vec.begin() + 2;
+    //it = vec.erase(it, it + vec_second.size();
+
+	if((it = vec.erase(it, it + vec_second.size())) == vec.end())
+	{
+		vec.reserve(vec.size() + vec_second.size());
+		vec.insert(vec.end(), vec_second.begin(), vec_second.end());
+	}
+	else
+		vec.insert(it, vec_second.begin(), vec_second.end());
+
+	for(auto const& x : vec)
+		cout << x << " ";
+
+	cin.get();
+}
