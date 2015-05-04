611b998c971446c3d54c8eb94f24a5082b58e2e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-04 22:38:01 +0000

    Update archive 9f

---
+++ trunk/Archive2/9f/ec9918f98b28bc/main.cpp
@@ -0,0 +1,29 @@
+#include <array>
+#include <iostream>
+
+//std::array is copyable, so you can return it by value!
+std::array<int,2> retarray() { return {1,2}; }
+
+//int[2] retcarrray() { return ???; }
+
+//you can also return and accept references to std::arrays...
+std::array<int,2>& modarray(std::array<int,2>& arr) { arr[0]+=1; return arr; }
+
+//...without hurting yourself
+int (&modcarray(int (& arr)[2]))[2] { arr[0]+=1; return arr; }
+
+int main()
+{
+    std::array<int,2> arr{1,2};
+    int carr[2] = {1,2};
+    
+    arr = retarray();
+    
+    modarray(arr);
+    modcarray(carr);
+    
+    //std array knows its size!
+    arr.size();
+}
+
+//std::array is awesome!
