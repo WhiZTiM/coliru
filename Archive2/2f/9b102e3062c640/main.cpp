b692e25b226fddd91397dd681e50c3f48da4b199
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-04 04:27:58 +0000

    Update archive 2f

---
+++ Archive2/2f/9b102e3062c640/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+    int input = 0;
+    float input2 = 0;
+    //Since this is a float, this can also be written as 0.0f
+    
+    std::cin >> input >> input2;
+    
+    std::cout << "Input 1: " << input << std::endl;
+    std::cout << "Input 2: " << input2 << std::endl;
+    return 0;
+}
