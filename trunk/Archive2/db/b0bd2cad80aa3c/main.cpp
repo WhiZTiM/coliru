dc2d3a627ed8bbcd2a480ce095fcb2373e8ec991
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-05 00:16:47 +0000

    Update archive db

---
+++ trunk/Archive2/db/b0bd2cad80aa3c/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+    int preco;
+    int consumo;
+    cin >> consumo;
+    if (consumo<11){
+        preco=7;
+        }else if(consumo<31){
+            preco=7+(consumo-10);
+        }else if(consumo<101){
+            preco=7+20+(consumo-30)*2;
+        }else{
+            preco=7+20+140+(consumo-100)*5;
+    }
+    cout << preco << endl;
+    return 0;
+    }
