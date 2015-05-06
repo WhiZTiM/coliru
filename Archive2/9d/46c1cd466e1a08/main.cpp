b9e6556dc58035afca280c6214c1bd9b8651027b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-06 18:03:46 +0000

    Update archive 9d

---
+++ Archive2/9d/46c1cd466e1a08/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <functional>
+#include <future>
+#include <string>
+
+
+int main() {
+    auto lambda = [] (std::string&& message) {
+        std::cout << message << std::endl;
+    };
+    auto bound = std::bind(lambda, std::string{"hello world"});
+    auto future = std::async(bound);
+    future.get();
+}
