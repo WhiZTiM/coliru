891b83537b80f3e703683a96e36f22d0f74b2cc5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-06 22:52:22 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/a4b1fa86a872ee/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/multi_array.hpp>
+#include <iostream>
+
+int main() {
+    using namespace boost;
+    using MA = multi_array<double, 2>;
+
+    MA ma(extents[12][34]);
+    auto& ma_shape = reinterpret_cast<boost::array<size_t, MA::dimensionality> const&>(*ma.shape());
+
+    // demo
+    std::cout << "[" << ma_shape[0] << "][" << ma_shape[1] << "]\n";
+
+    // resize
+    MA other;
+    assert(!std::equal(std::begin(ma_shape), std::end(ma_shape), other.shape()));
+
+    other.resize(ma_shape);
+    assert(std::equal(std::begin(ma_shape), std::end(ma_shape), other.shape()));
+
+    // reshape
+    other.resize(extents[1][12*34]);
+    assert(!std::equal(std::begin(ma_shape), std::end(ma_shape), other.shape()));
+
+    other.reshape(ma_shape);
+    assert(std::equal(std::begin(ma_shape), std::end(ma_shape), other.shape()));
+}
