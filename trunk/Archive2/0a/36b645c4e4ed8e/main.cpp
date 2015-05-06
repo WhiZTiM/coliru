9add305c5d53495ae74c742669539e6187ab48c7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-06 20:44:20 +0000

    Update archive 0a

---
+++ trunk/Archive2/0a/36b645c4e4ed8e/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <functional>
+#include <future>
+#include <string>
+
+
+template <typename T>
+std::future<void> print(T&& t) {
+    return std::async([] (T&& message) {
+        std::cout << message << std::endl;
+    }, std::forward<T>(t));
+}
+
+int main() {
+    auto future = print(std::string{"hello world"});
+    future.get();
+}
