b3a7b8958bba96d4e262e4419b0b354f145f00eb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-07 00:10:24 +0000

    Update archive 4b

---
+++ Archive2/4b/f8336b6146ca53/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct A
+{
+    A operator+(A const& a) { cout << 1 << endl; return A(); }
+    A& operator++() { cout << 2 << endl; return *this; }
+    A operator++(int) { cout << 3 << endl; return *this; }
+    bool operator!() { cout << 4 << endl; return true; }
+    
+};
+
+A operator+(A const& a, A const& b) { cout << 5 << endl; return A(); }
+A& operator++(A& a) { cout << 6 << endl; return a; }
+A operator++(A const& a, int) { cout << 7 << endl; return A(); }
+bool operator!(A const& a) { cout << 8 << endl; return false; }
+
+int main()
+{
+    A a, b;
+    
+    a + b;
+    ++a;
+    a++;
+    !a;
+    
+    return 0;
+}
+
