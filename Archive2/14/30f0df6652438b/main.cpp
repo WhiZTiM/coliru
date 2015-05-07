e87ab7af40782f31032a2590cbaefc4258b93cfc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-07 23:18:59 +0000

    Update archive 14

---
+++ Archive2/14/30f0df6652438b/main.cpp
@@ -0,0 +1,33 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/reverse_graph.hpp>
+#include "/Archive2/45/a4410ef1bd3024/main.cpp" // alias <libs/graph/src/read_graphviz_new.cpp>
+
+struct VertexInfo {
+    std::string label;
+};
+
+struct EdgeInfo {
+};
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, VertexInfo, EdgeInfo> Graph;
+
+Graph read() {
+    Graph g;
+
+    using namespace boost;
+    dynamic_properties dp;
+    dp.property("node_id", get(&VertexInfo::label, g));
+    read_graphviz("digraph { a -> b; a -> c; b -> d; b -> e; e -> f; e -> c; }", g, dp);
+
+    return g;
+}
+
+int main() {
+    using namespace boost;
+
+    auto g = read();
+    auto r = make_reverse_graph(g);
+
+    write_graphviz(std::cout, r, make_label_writer(get(&VertexInfo::label, r)));
+}
