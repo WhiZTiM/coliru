2b7f8f787221e6de0c0aa7879b2c93edcf141f01
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-07 14:56:01 +0000

    Update archive ac

---
+++ Archive2/ac/56cf9a881082a3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include
+
+struct FlagNth
+{
+    FlagNth( size_t n ) : current( 0 ), n_( n ) {}
+    size_t current_;
+    const size_t n_;
+};
+ 
+int main()
+{
+    std::cout << "" << '\n';
+}
