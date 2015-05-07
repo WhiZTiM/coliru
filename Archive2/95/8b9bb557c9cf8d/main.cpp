5b2094439856030bb037f964533ae2d4300a23d4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-07 20:53:15 +0000

    Update archive 95

---
+++ Archive2/95/8b9bb557c9cf8d/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Scan
+{ };
+
+class Dust
+{
+    Dust() = default;
+    
+    void Scan();
+    
+    void Foo() { ::Scan scan; };
+};
+
+void Dust::Scan()
+{
+    
+}
+
+int main(void)
+{
+
+}
