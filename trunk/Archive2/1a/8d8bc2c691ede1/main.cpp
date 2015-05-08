b295cfc82c67b9ae5b4e51522fec07b2082e9fcc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-08 10:47:20 +0000

    Update archive 1a

---
+++ trunk/Archive2/1a/8d8bc2c691ede1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <string.h>
+
+#include <arpa/inet.h>
+
+typedef struct berval {
+    long    	bv_len;
+	char		*bv_val;
+} BerValue;
+
+
+int main()
+{
+    std::string foo = "cq?yy 0a";
+    // imp1
+   
+   const char* password = foo.c_str();
+   char* pass_copy = new char[strlen(password)+1];
+   strcpy(pass_copy, password);
+   struct berval pass_struct;
+   pass_struct.bv_val = pass_copy;
+   pass_struct.bv_len = strlen(pass_copy);
+   
+   std::cout << pass_struct.bv_len << " " << pass_struct.bv_val << std::endl;
+   
+   //imp2
+   std::vector<char> pass_copy1 { password, password + strlen(password) + 1 };
+   pass_struct.bv_val = pass_copy1.data();
+   pass_struct.bv_len = pass_copy1.size();
+   
+   std::cout << pass_struct.bv_len << " " << pass_struct.bv_val << std::endl;
+}
