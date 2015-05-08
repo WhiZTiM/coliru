746c4dc8658a0733725d25f31ac625e40e727a4a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-08 15:15:18 +0000

    Update archive b6

---
+++ Archive2/b6/79c22667d355d9/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <functional>
+
+class A
+{};
+
+int main()
+{
+  std::unordered_map<std::string, std::reference_wrapper<const A>> stringToRef;
+  A a;
+  stringToRef.insert(std::make_pair("Test", std::cref(a)));
+  return 0;
+}
