97ac1e41d3f9a44e909d6cb6d8dbc71d0465ff7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-08 00:25:57 +0000

    Update archive af

---
+++ Archive2/af/b841a382454aac/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <type_traits>
+
+template<class...>struct types{using type=types;};
+namespace details {
+  template<template<class...>class Z, class types, class=void>
+  struct test_apply:std::false_type{};
+  template<template<class...>class Z, class...Ts>
+  struct test_apply<Z,types<Ts...>,std::void_t<Z<Ts...>>>:
+    std::true_type
+  {};
+};
+
+template<template<class...>class Z, class...Ts>
+using test_apply=details::test_apply<Z,types<Ts...>>;
+
+template<class X>
+using cout_result = decltype( std::cout << std::declval<X>() );
+
+template<class X>
+using can_cout_stream = test_apply< cout_result, X >;
+
+template<class T>
+std::enable_if_t<!can_cout_stream<const T&>{}>
+f(const T& x) {
+    std::cout << "Blubb" << std::endl;
+}
+
+int main(int, char **) {
+    struct A {} a;
+    f(a);
+    return 0;
+}
