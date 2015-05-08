3381820863670d098d9df0bc103c28f3556c5adc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-08 00:28:16 +0000

    Update archive d2

---
+++ trunk/Archive2/d2/7f10c7059a7480/main.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+#include <iostream>
+
+template<typename, typename, typename=void>
+struct has_cout
+    : std::false_type {};
+
+template<typename stream_t, typename T>
+struct has_cout<stream_t, T, decltype(std::declval<stream_t&>() << std::declval<T>(),void())>
+//              ^^^^^^^^                                   ^
+    : std::true_type {};
+
+template<typename stream_t, typename T>
+auto f(stream_t &s, const T& x) 
+  -> typename std::enable_if<!has_cout<stream_t, T>::value>::type
+{
+    s << "Huhu" << std::endl;
+}
+  
+struct A {};
+
+int main()
+{
+    // f(std::cout, 1); // not OK
+    A a;
+    f(std::cout, a); // OK
+}
