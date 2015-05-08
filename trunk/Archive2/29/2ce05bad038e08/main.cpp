17df0271830d1e8bbbee46587be68deb7bc3a02b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-08 11:02:42 +0000

    Update archive 29

---
+++ trunk/Archive2/29/2ce05bad038e08/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <exception>
+#include <stdexcept>
+
+using namespace std;
+
+void f(int n) throw (int) { throw (float)n; }
+void g(int n) throw (float) { throw n; }
+
+void onExit() {
+    cout << "onExit" << endl;
+}
+
+void onTerminate() {
+	cout << "onTerminate" << endl;
+	exit(EXIT_FAILURE);
+}
+void onUnexpected() {
+	cout << "onUnexpected" << endl;
+	throw -1;
+}
+
+int main() {
+
+    atexit(onExit);
+	set_terminate(onTerminate);
+	set_unexpected(onUnexpected);
+
+	int n = 1;
+
+	try { f(n); }
+	catch (int n) { cout << "catch 1" << endl; }
+	catch (...) { cout << "catch 2" << endl; }
+
+	cout << "end main" << endl;
+	return EXIT_SUCCESS;
+}
