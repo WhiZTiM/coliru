623a1eb5bc785026322cd41bb49bc0718b7a212f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-08 13:34:37 +0000

    Update archive 4d

---
+++ Archive2/4d/d9ee314eb72675/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+struct myStruct_t
+{
+    myStruct_t ()
+    {
+        cout << "**Ctor!"<< endl;
+    }
+    ~myStruct_t ()
+    {
+        cout << "**Dtor!"<< endl;
+    }
+    void foo () const
+    {
+        cout << "Foo!" << endl;
+    }
+};
+    
+myStruct_t fn ()
+{
+    return myStruct_t ();
+}
+
+int main()
+{
+    cout << "Hello" << endl;
+    {
+        const myStruct_t& r = fn ();
+        r.foo ();
+    }
+    cout << "Goodbye" << endl;
+    return 0;
+}
