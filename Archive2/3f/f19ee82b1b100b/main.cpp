0448f2a1949334113d43922c08fcdc4d84cc4d7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-09 23:44:08 +0000

    Update archive 3f

---
+++ Archive2/3f/f19ee82b1b100b/main.cpp
@@ -0,0 +1,53 @@
+#include <utility>
+#include <type_traits>
+
+template<std::size_t...> struct seq{ using type = seq; };
+
+template<class S1, class S2> struct concat;
+
+template<std::size_t... I1, std::size_t... I2>
+struct concat<seq<I1...>, seq<I2...>>
+  : seq<I1..., I2...>{};
+
+template<template<std::size_t> class pred, std::size_t B, std::size_t N>
+struct gen_seq : concat<typename gen_seq<pred, B, N/2>::type, typename gen_seq<pred, B + N/2, N - N/2>::type>::type {};
+
+template<template<std::size_t> class pred, std::size_t B> struct gen_seq<pred, B, 0> : seq<> {};
+template<template<std::size_t> class pred, std::size_t B> struct gen_seq<pred, B, 1> : std::conditional<pred<B>::value,seq<B>,seq<>>::type {};
+
+template<std::size_t N>
+struct MyData
+{
+    std::size_t values[N];
+
+    static constexpr std::size_t size()
+    { return N; }
+};
+
+template<std::size_t... Is>
+constexpr MyData<sizeof...(Is)> MyGen(seq<Is...>)
+{
+    return {{ Is... }};
+}
+
+template<template<std::size_t> class pred, std::size_t N>
+constexpr auto MyGen() -> decltype(MyGen(typename gen_seq<pred,0,N>::type()))
+{
+    return MyGen(gen_seq<pred,0,N>());
+}
+
+template< std::size_t N > struct my_pred : std::integral_constant< bool, N % 3 == 0 > {};
+
+#include <iostream>
+template<typename T>
+void print_type() { std::cout << __PRETTY_FUNCTION__ << "\n"; }
+
+int main()
+{
+    auto data = MyGen<my_pred, 10>();
+    for(auto const e : data.values) std::cout << e << ", ";
+    static_assert( data.size() == 4, "Oops" );
+
+    auto data2 = MyGen<my_pred, 10000>();
+    static_assert( data2.size() == 3334, "Oops" );
+}
