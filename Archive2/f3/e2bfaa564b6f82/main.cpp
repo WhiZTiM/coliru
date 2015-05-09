8f637c82bcf84a52ee4167273bb5aa62f93dd1cc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-09 23:25:17 +0000

    Update archive f3

---
+++ Archive2/f3/e2bfaa564b6f82/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+
+std::mutex m;
+std::condition_variable v;
+
+void test_wait_until(int ms)
+{
+    std::unique_lock<std::mutex> lock(m);
+
+    std::cout << ms << "ms start\n";
+    auto expires = std::chrono::system_clock::now() + std::chrono::milliseconds(ms);
+    bool run = true;
+
+    do
+    {
+        // This loop will run at 100% cpu time until
+        // the timeout expires.
+
+        auto status = v.wait_until(lock, expires);
+
+        if(status == std::cv_status::timeout){
+            std::cout << ms << "ms expired\n";
+            run=false;
+        }
+
+        if(status == std::cv_status::no_timeout){
+            // If the commend below is removed the
+            // termial will be filled by the printout.
+            // until the timeout expires.
+
+            std::cout << ms << "ms did not expire\n";
+        }
+    }while(run);
+}
+
+
+int main()
+{
+    test_wait_until(  100);
+}
