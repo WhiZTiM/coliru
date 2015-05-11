b32c41c61320ad810e5b30316aeaf477636016c8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-11 20:39:05 +0000

    Update archive 81

---
+++ trunk/Archive2/81/45d7664131630b/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <type_traits>
+
+template<typename T>
+struct inst_check_wrapper
+{
+    friend constexpr int inst_flag(inst_check_wrapper<T>);
+};
+
+template<typename T>
+struct writer 
+{
+    friend constexpr int inst_flag(inst_check_wrapper<T>) 
+    {
+        return 0;
+    }
+};
+
+template <typename T, bool B = noexcept(inst_flag(inst_check_wrapper<T>()))>
+constexpr bool is_instantiated()
+{
+    return B;
+}
+
+template <typename T>
+struct MyStruct
+{
+    template <typename T1 = T, int = sizeof(writer<MyStruct<T1>>)>
+    MyStruct()
+    { }
+};
+
+int main () 
+{
+    static_assert(!is_instantiated<MyStruct<int>>(), "failure");
+    std::cout << sizeof(MyStruct<int>);
+    static_assert(is_instantiated<MyStruct<int>>(), "failure");
+}
