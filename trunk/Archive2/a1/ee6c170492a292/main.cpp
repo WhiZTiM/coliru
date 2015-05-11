79092b0be8a6dd5692eb35d221ce2ff284af20ec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-11 00:14:27 +0000

    Update archive a1

---
+++ trunk/Archive2/a1/ee6c170492a292/main.cpp
@@ -0,0 +1,36 @@
+class A
+{
+    int a = 5;
+public:
+    constexpr int f()
+	{
+		return 10/a;
+	}
+
+	constexpr int g()
+	{
+		a = 0;
+		return 0;
+	}
+
+	constexpr int h(int a, int b)
+	{
+        return a;
+	}
+
+	constexpr int i()
+	{
+		return h(f(), g()); // Floating point exception(core dumped)
+        //return h(g(), f()); // 0
+	}
+};
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+	A a;
+	// std::array<int, a.i()> arr;
+    std::cout << a.i();
+}
