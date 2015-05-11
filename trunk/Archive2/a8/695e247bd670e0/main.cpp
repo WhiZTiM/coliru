de85665d7551ed1a47086f5f9e9ba2f2d91e08b6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-11 20:06:37 +0000

    Update archive a8

---
+++ trunk/Archive2/a8/695e247bd670e0/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace mp = boost::multiprecision;
+
+mp::cpp_int fib(std::size_t n) {
+  mp::cpp_int a = 1;
+  mp::cpp_int b = 0;
+  for (std::size_t i = 0; i != n; ++i) {
+    a += b;
+    std::swap(a, b);
+  }
+  return b;
+}
+
+int main() {
+  std::cout << fib(2000) << std::endl;
+}
