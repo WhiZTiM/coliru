e76dea6bc5f8c1dde6f1daafc2e4570a9b757374
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-11 23:50:01 +0000

    Update archive 64

---
+++ Archive2/64/62aae131cb4b86/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <vector>
+
+int main()
+{
+    std::vector<boost::multiprecision::cpp_int> fib1, fib2;
+    boost::multiprecision::cpp_int last = 0, next = 1;
+    
+    int n = 100000;
+    
+    int i = 0;
+    while (i != n) {
+        fib1.push_back(last);
+        last  = next;
+        next += last;
+    	++i;
+    }
+
+	i = 0, last = 0, next = 1;
+    while (i != n) {
+        fib2.push_back(last);
+        last  = next;
+        next += last;
+        ++i;
+    }
+    
+    boost::multiprecision::cpp_int sumFib1;
+    for (auto && e : fib1) {
+        sumFib1 += e;
+    }
+
+    boost::multiprecision::cpp_int sumFib2;
+    for (auto && e : fib2) {
+        sumFib2 += e;
+    }
+
+    std::cout << sumFib2 - sumFib1 << std::endl;
+}
