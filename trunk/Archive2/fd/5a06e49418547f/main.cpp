1e3da62bac8186ec213525244f7e2189e968b27c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-11 05:55:50 +0000

    Update archive fd

---
+++ trunk/Archive2/fd/5a06e49418547f/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class decimal_t{
+public:
+    int64_t val;
+    uint8_t precision;
+    constexpr decimal_t(int64_t val, uint8_t precision): val(val), precision(precision){}
+};
+
+constexpr decimal_t operator"" _dec(const char * t , std::size_t s){
+    int64_t n=0;
+    int8_t prec=0;
+    bool negative=false;
+    for(int i=0; i<s; ++i){
+        if(t[i]>='0' && t[i]<='9')n=n*10+(t[i]-'0');
+        if(prec) ++prec;
+        if(t[i]=='.') prec=1;
+        negative=negative || t[i]=='-';
+    }
+    if(prec) --prec;
+    if(negative) n*=-1;
+    return decimal_t(n,prec);
+}
+
+int main() {
+    constexpr decimal_t x = "123"_dec;
+    std::cout << x.val << '\n';
+}
