8527c7a37d4c631d43e53f5595b48d8ba54ac3bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-12 05:10:36 +0000

    Update archive 93

---
+++ Archive2/93/a2e6dc93ff6a11/main.cpp
@@ -0,0 +1,19 @@
+struct foo
+{
+    const int
+        *const a,
+        *const b, 
+        *const c;
+
+    foo(int a, int b, int c)
+            : a(_a), b(_b), c(_c)
+            , _a{a}, _b{b}, _c{c}
+    {}
+
+private:
+    int _a[1], _b[1], _c[1];
+
+};
+
+int main()
+{}
