fc6a1928f1d7fb78801e81572eb960a22fad2271
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-12 12:07:37 +0000

    Update archive 97

---
+++ Archive2/97/1d38aaebad2954/main.cpp
@@ -0,0 +1,34 @@
+#include <type_traits>
+#include <iostream>
+
+template <class B>
+typename std::enable_if < std::is_same<B,int>::value ||std::is_same<B,double>::value ,std::string   >::type
+ add (std::string str , B b){
+ return str + std::to_string(b);    
+}
+
+template <class B>
+typename std::enable_if < std::is_same<B,int>::value ||std::is_same<B,double>::value  ,std::string   >::type
+ add (B b, std::string str){
+ return std::to_string(b)+ str ;    
+}
+
+template <class A , class B>
+typename std::enable_if < (std::is_same<B,int>::value ||std::is_same<B,double>::value)&& (std::is_same<A,int>::value ||std::is_same<A,double>::value) , decltype(A()+B())  >::type
+ add (B b, A a){
+ return a+b;  
+}
+
+std::string add (std::string a , std::string b){
+return a+b;    
+} 
+
+int main (void){
+ std::cout << add("hi ", "there")<<"\n";
+ std::cout << add ("hi there" , 3)<<"\n";
+ std::cout << add (3 , "hi there")<<"\n";
+ std::cout << add (2.2 , 1.1)<<"\n";
+ std::cout << add (3.14 , 2)<<"\n";
+ std::cout << add (4 , 2)<<"\n";
+return 0;    
+}
