05a76d08cafb95a122a232c6f5b1ee80be03be47
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-12 23:09:08 +0000

    Update archive a9

---
+++ trunk/Archive2/a9/1149a2506d504a/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+
+
+// Tuple key search string (name~type).
+typedef string pcuTupleSearchStrIdT;
+
+// Map of Tuple containers, keyed on Tuple name/type.
+typedef map<pcuTupleSearchStrIdT,
+            int> pcuTupleStrSearchT;
+
+
+
+bool findTuple(pcuTupleStrSearchT tupleList,std::string refEntityTag, int *tuple)
+{
+  pcuTupleStrSearchT::iterator tupleItr = tupleList.find(refEntityTag);
+  if (tupleItr == tupleList.end())
+    {
+      // Found nothing    
+        std::cout << "Tuple not found for ref-entity-tag " << refEntityTag << " " << std::endl;
+        return false;
+    }
+    else
+    {
+       // Found nothing    
+        *tuple = tupleItr->second;
+        std::cout << "Tuple  found for ref-entity-tag " << refEntityTag << "(" << tuple  << ")" <<  std::endl;
+        
+        return true;
+    }
+}
+
+int main()
+{
+  
+    
+  pcuTupleStrSearchT tupleList;
+  int tupleValue = 0;
+
+  tupleList["connection/20#element/15"]=10;
+  tupleList["connection/20#element/15#bvc/127"]=30;
+  tupleList["connection/20#element/15#cgi-cell/131858433"]=50;
+  tupleList["connection/20#element/15#cell/131858433#tdma/3"]=70;
+ 
+
+
+  if (true == findTuple(tupleList,"connection/20#element/15#bvc/127", &tupleValue))
+  {
+      
+      std::cout << "returned tuple is " << tupleValue << std::endl;
+  }
+  
+  
+
+}
