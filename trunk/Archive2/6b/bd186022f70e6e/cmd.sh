beb78ca9a493c1a4e36fc393b72b32534c33b34d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-12 22:46:51 +0000

    Update archive 6b

---
+++ trunk/Archive2/6b/bd186022f70e6e/cmd.sh
@@ -0,0 +1,14 @@
+cat>sehe2.txt<<TEST; clang++ -std=c++11 -O2 -Wall -pedantic main.cpp  && ./a.out
+t # 0
+v 0 35
+v 1 47
+v 2 15
+e 0 1 10
+e 0 2 77
+t # 1
+v 0 35
+v 1 15
+v 2 47
+e 0 2 10
+e 0 1 77
+TEST
