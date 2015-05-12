d3f238bb3e983b0f7709f38d0a3973ceda77d822
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-12 15:13:57 +0000

    Update archive 47

---
+++ trunk/Archive2/47/743db96c0f3a02/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename A, typename... Bs>
+struct Hello;
+
+template <typename A>
+struct Hello<A> {
+  template <typename A2>
+  friend std::ostream & operator<<(std::ostream & s, const Hello<A2> & h) {
+    return s << "specialized\n";
+  }
+};
+
+template <typename A, typename... Bs>
+struct Hello {
+  template <typename A2, typename... B2s>
+  friend std::ostream & operator<<(std::ostream & s, const Hello<A2,B2s...> & h) {
+    return s << "generic\n";
+  }
+};
+
+
+int main()
+{
+  std::cout << Hello<int>() << Hello<float>() << Hello<int,int>() << Hello<int,float>();
+}
