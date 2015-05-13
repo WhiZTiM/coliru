0486adc3c13b68e5f863c351f66a6aacd861a858
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 21:43:17 +0000

    Update archive a8

---
+++ trunk/Archive2/a8/0e51936c403f6d/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bitset>
+#include <cstdint>
+
+using u64 = std::uint64_t;
+
+// See http://graphics.stanford.edu/~seander/bithacks.html#NextBitPermutation
+u64 next(u64 v) {
+    u64 t = v | (v - 1);
+    return (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1));
+}
+
+// Or without __builtin_ctz: (Slightly slower.)
+//u64 next(u64 v) {
+//    u64 t = (v | (v - 1)) + 1;
+//    return t | ((((t & -t) / (v & -v)) >> 1) - 1);
+//}
+
+constexpr u64 N = 10;
+constexpr u64 M = 5;
+
+int main() {
+    u64 x = (1l << M) - 1;
+    do {
+       std::cout << std::bitset<N>(x) << std::endl;
+       x = next(x);
+    } while (!(x & 1l << N));
+}
