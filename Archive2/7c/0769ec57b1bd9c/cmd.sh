5070c3342665e1271bd1740e2b23c6d4a77616f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 13:10:15 +0000

    Update archive 7c

---
+++ Archive2/7c/0769ec57b1bd9c/cmd.sh
@@ -0,0 +1,17 @@
+cat>2.txt<<TEST; clang++ -std=c++11 -Os -Wall -pedantic main.cpp  && ./a.out
+t # 0
+v 0 4
+v 1 2
+v 2 3
+e 0 1 12
+e 0 2 14
+t # 1
+v 0 1
+v 1 2
+v 2 4
+v 3 3
+e 0 1 11
+e 1 2 12
+e 1 3 13
+e 2 3 14
+TEST
