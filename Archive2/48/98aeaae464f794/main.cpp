ca351c7a8d5df217699cc2f65bb06dbfc7561099
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 13:04:22 +0000

    Update archive 48

---
+++ Archive2/48/98aeaae464f794/main.cpp
@@ -0,0 +1,88 @@
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/export.hpp>
+#include <boost/serialization/variant.hpp>
+
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/stream.hpp>
+
+namespace Todo
+{
+    class Exec {
+      public:
+        Exec(std::string const &command = "") : _command(command){};
+        void Do() const { std::cout << "foo: " << getCommand() << std::endl; };
+
+        std::string getCommand() const { return _command; };
+
+      private:
+        friend class boost::serialization::access;
+        template <class Archive> void serialize(Archive &ar, unsigned) {
+            ar &_command;
+        }
+
+        std::string _command;
+    };
+
+    using Todo = boost::variant<Exec>;
+
+    struct Factory {
+        static std::string save(Todo const& todo) {
+            std::string out;
+            {
+                namespace io = boost::iostreams;
+                io::stream<io::back_insert_device<std::string> > os(out);
+
+                boost::archive::text_oarchive archive(os);
+                archive << todo;
+            }
+
+            return out;
+        }
+
+        static Todo load(std::string const &s) {
+            Todo todo;
+            {
+                namespace io = boost::iostreams;
+                io::stream<io::array_source> is(io::array_source{ s.data(), s.size() });
+                boost::archive::text_iarchive archive(is);
+                archive >> todo;
+            }
+            return std::move(todo);
+        }
+    };
+}
+
+namespace visitors {
+    namespace detail {
+        template <typename F> struct internal_vis : boost::static_visitor<void> {
+            internal_vis(F& f) : _f(f) {}
+            template <typename... T>
+                void operator()(T&&... a) const { return _f(std::forward<T>(a)...); }
+            private:
+                F& _f;
+        };
+    }
+
+    template <typename F, typename V>
+    void apply(F const& f, V const& v) { return boost::apply_visitor(detail::internal_vis<F const>(f), v); }
+
+    template <typename F, typename V>
+    void apply(F const& f, V& v) { return boost::apply_visitor(detail::internal_vis<F const>(f), v); }
+}
+
+int main() {
+    Todo::Factory factory;
+
+    // ROUNDTRIP save,load
+    auto todo = factory.load(
+            factory.save(
+                Todo::Exec("ls -al /home/ajonen")
+            )
+        );
+
+    std::cout << "Type: " << std::hex << std::showbase << todo.which() << std::endl;
+
+    visitors::apply([](auto const& cmd) { cmd.Do(); }, todo);
+}
