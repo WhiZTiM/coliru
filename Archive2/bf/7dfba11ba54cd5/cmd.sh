1631b1ca674b4189ee08f4b979b3bf84490d6256
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 02:42:55 +0000

    Update archive bf

---
+++ Archive2/bf/7dfba11ba54cd5/cmd.sh
@@ -0,0 +1,3 @@
+g++-4.9 -std=c++14 -pedantic -Wall -Wextra main.cpp && ./a.out
+g++-5.1 -std=c++14 -pedantic -Wall -Wextra main.cpp && ./a.out
+clang++ -std=c++14 -pedantic -Wall -Wextra main.cpp && ./a.out
