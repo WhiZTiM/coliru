2e8f9a8e24b3c697335f6f6c4b112a1a7d07a6f6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 14:33:15 +0000

    Update archive 1a

---
+++ trunk/Archive2/1a/33d5fd5eba3b2d/main.cpp
@@ -0,0 +1,67 @@
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/identity.hpp>
+
+using namespace boost::multi_index;
+
+struct struct1{};
+struct struct2{};
+struct struct3{};
+struct struct4{};
+
+typedef multi_index_container<
+ int,
+ indexed_by<
+  ordered_unique<tag<struct1>,identity<int>>,
+  ordered_unique<tag<struct2,struct3>,identity<int>>,
+  ordered_unique<tag<struct4>,identity<int>>
+ >
+> multi_t;
+
+#include <boost/mpl/at.hpp>
+#include <boost/mpl/fold.hpp>
+#include <boost/mpl/insert.hpp>
+#include <boost/mpl/map.hpp>
+#include <boost/mpl/pair.hpp>
+
+template<typename Map,typename Index>
+struct add_to_tag_to_iterator_map:
+  boost::mpl::fold<
+    typename Index::tag_list,
+    Map,
+    boost::mpl::insert<
+      boost::mpl::_1,
+      boost::mpl::pair<typename Index::iterator,boost::mpl::_2>
+    >
+  >
+{};
+  
+template<typename MultiIndexContainer>
+struct tag_to_iterator_map:
+  boost::mpl::fold<
+    typename MultiIndexContainer::index_type_list,
+    boost::mpl::map<>,
+    add_to_tag_to_iterator_map<boost::mpl::_1,boost::mpl::_2>
+  >
+{};
+
+template<typename MultiIndexContainer,typename Iterator>
+struct tag_from_iterator:
+  boost::mpl::at<
+    typename tag_to_iterator_map<MultiIndexContainer>::type,
+    Iterator
+  >
+{};
+  
+#include<iostream>
+
+int main()
+{
+  using iterator1=multi_t::nth_index<0>::type::iterator;
+  using iterator2=multi_t::nth_index<1>::type::iterator;
+  using iterator3=multi_t::nth_index<2>::type::iterator;
+  
+  std::cout<<typeid(tag_from_iterator<multi_t,iterator1>::type).name()<<"\n";
+  std::cout<<typeid(tag_from_iterator<multi_t,iterator2>::type).name()<<"\n";
+  std::cout<<typeid(tag_from_iterator<multi_t,iterator3>::type).name()<<"\n";
+}
