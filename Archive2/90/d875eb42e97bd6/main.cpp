e4cda3f92382b0c2b6a3b2ec1f85b928e4408bff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 06:16:11 +0000

    Update archive 90

---
+++ Archive2/90/d875eb42e97bd6/main.cpp
@@ -0,0 +1,21 @@
+#ifndef __BASEUINT_H__
+#define __BASEUINT_H__
+
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+#define ZPHONE_VERSION "0.1"
+#define LOGPATH "a.txt"
+const int STABLIZE_PERIOD = 3000000000 ;
+
+#define NULL 0
+typedef int BintInt ;
+
+#define DEFAULTPORT 15062
+
+
+#endif 
