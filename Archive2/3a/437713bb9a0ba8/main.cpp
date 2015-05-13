001247bd05830906776ede9c4c6ea5e860311ce7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-13 20:31:04 +0000

    Update archive 3a

---
+++ Archive2/3a/437713bb9a0ba8/main.cpp
@@ -0,0 +1,38 @@
+#ifdef CRAPPY_COMPILER
+
+template<bool C> struct assert        { typedef void* type; };
+template<>       struct assert<false> { typedef assert type; };
+
+/* define all assert "messages" here */
+struct types_must_match { 
+    template<bool C> 
+    static int assertion_failed(typename assert<C>::type);
+};
+
+struct precision_loss_in_conversion {
+    template<bool C>
+    static int assertion_failed(typename assert<C>::type);
+};
+/* end assert messages */
+
+#define STATIC_ASSERT(expr, msg) typedef int msg[(expr) ? 1 : -1];
+
+#else
+
+#define STATIC_ASSERT(expr, msg) static_assert(expr, #msg)
+
+#endif
+
+
+template<bool B>
+void test()
+{
+    STATIC_ASSERT(B, types_must_match);
+}
+
+int main()
+{
+    test<true>();
+    
+    test<false>();
+}
