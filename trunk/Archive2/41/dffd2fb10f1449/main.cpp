9ac26a176d303a07e974836916e2a83b3c1f75f3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-14 14:16:10 +0000

    Update archive 41

---
+++ trunk/Archive2/41/dffd2fb10f1449/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+struct Block
+{
+
+    Block(int blockNum, std::vector<char> const &data, std::vector<char> const &blockHash,
+          int blockNonce, int parentBlockId, bool isAdded) : blockNum(blockNum), data(data),
+                                                             blockHash(blockHash),
+                                                             blockNonce(blockNonce),
+                                                             parentBlockId(parentBlockId),
+                                                             isAdded(isAdded) { }
+
+    int blockNum;
+    std::vector<char> data;
+    std::vector<char> blockHash;
+    int blockNonce;
+    int parentBlockId;
+    bool isAdded;
+};
+
+int main()
+{  
+    std::map<int,Block> m;
+    const char * data = "abcd";
+    std::vector<char> vec(data, data + 4);
+    Block b(1,vec,vec,0,0,false);
+    Block a(b);
+    Block c(1,vec,vec,0,0,true);
+    c=a;
+    m.emplace(std::make_pair(1,b));
+    std::cout << b.data << b.blockNonce << std::endl;
+    std::cout << a.data << a.blockNonce << std::endl;
+    std::cout << c.data << c.blockNonce << std::endl;
+}
