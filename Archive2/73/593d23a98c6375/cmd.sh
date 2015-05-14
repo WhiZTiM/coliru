3b19ff32288cfcfa6fb51ef2282d936cc479757c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-14 02:19:18 +0000

    Update archive 73

---
+++ Archive2/73/593d23a98c6375/cmd.sh
@@ -0,0 +1,5 @@
+#title Semaphore vs Condition variable
+g++ -DSEMAPHORE -std=c++11 -O2 -Wall -Wextra -Werror -pedantic -pthread main.cpp -o sema
+g++ -std=c++11 -O2 -Wall -Wextra -Werror -pedantic -pthread main.cpp -o cond
+./sema
+./cond
