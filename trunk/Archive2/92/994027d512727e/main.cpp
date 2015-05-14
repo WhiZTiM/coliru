a4913c6103dd12b0a92e553cefe3f0591aed6561
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-14 18:12:48 +0000

    Update archive 92

---
+++ trunk/Archive2/92/994027d512727e/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class A
+{
+public:
+    virtual void Foo(int foos[]);
+    virtual void Foo(int foo);
+};
+
+class B : A
+{
+public:
+    void Foo(int foo) override;
+};
+
+int main()
+{
+    B* b = new B();
+    
+    int ints[] = {0, 1, 2, 3};
+    b->Foo(ints);
+}
