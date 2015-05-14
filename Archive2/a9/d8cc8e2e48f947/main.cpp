2189dc2d2eb9354c1ba054dd8aec11a70959246c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-14 14:57:19 +0000

    Update archive a9

---
+++ Archive2/a9/d8cc8e2e48f947/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	const int orig_size = 66;
+	const float skip_ratio = .6;
+	const int new_size = skip_ratio * orig_size;
+    
+    std::cout << "orig_size: " << orig_size << "\n";
+	std::cout << "new_size: " << new_size << "\n";
+	
+	const int p = new_size * 2;
+	const int n = orig_size * 2;
+	int d = p - orig_size;
+	for (int i = 0; i < orig_size; ++i) {
+        if (d >= 0) {
+            std::cout << i << ' ';
+            d -= n;
+        }
+        d += p;
+    }
+	std::cout << std::endl;
+	
+	return 0;
+}
