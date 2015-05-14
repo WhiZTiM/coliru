171a2eb35f70f71679f46fc6cd710bb2bfb5fa4c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-14 07:37:05 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/c716a77caaf656/main.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h> 
+int main () {
+    int x;
+    scanf("%i", &x);
+    switch(x){ 
+      case 61: 
+      printf("Brasilia"); 
+      break;
+      case 71: 
+      printf("Salvador");
+      break; 
+      case 11:
+      printf("Sao Paulo");
+      break; 
+      case 21: 
+      printf("Rio de Janeiro");
+      break;
+      case 32: 
+      printf("Juiz de Fora");
+      break; 
+      case 19: 
+      printf("Campinas");
+      case 27: 
+      printf("Vitoria");
+      break;
+      case 31: 
+      printf("Belo Horizonte");
+      break; 
+      default: 
+      printf("DDD nao cadastrado"); 
+      
+      
+        
+        
+        
+        
+        
+        
+        
+    }
+    return 0; 
+    }
