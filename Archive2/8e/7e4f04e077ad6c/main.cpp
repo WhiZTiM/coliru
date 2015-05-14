86856f070fb01a60fbe22cf8f1864eee475f6a62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-14 09:29:19 +0000

    Update archive 8e

---
+++ Archive2/8e/7e4f04e077ad6c/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#define t(p) if(!(p))o<<
+#define r(x) ,b=x;
+#define g(d,w) t(i%d) #w"zz" r(1)
+int main(){auto& o=std::cout;int i=0 r(0);while(++i<101){b=0;g(3,Fi)g(5,Bu)t(b)i;o<<"\n";}}
