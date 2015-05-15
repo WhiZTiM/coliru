3915a5220418fecbd5bcf6f0a62f1583d38874be
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-15 10:48:01 +0000

    Update archive 56

---
+++ Archive2/56/b622c922860373/cmd.sh
@@ -0,0 +1,11 @@
+cat>input.txt<<XML; g++-4.9 -std=c++11 -Os -Wall -pedantic main.cpp -lboost_system -lboost_filesystem && ./a.out ; find
+<folder>
+    <name>project</name>
+    <folder>
+        <name>src</name>
+        <file><name>main.cpp</name></file>
+    </folder>
+    <file><name>Makefile</name></file>
+    <file><name>README.md</name></file>
+</folder>
+XML
