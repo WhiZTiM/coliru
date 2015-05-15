feac6470d6e5aae0d576af815cf29025a062322f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-15 10:48:01 +0000

    Update archive 56

---
+++ trunk/Archive2/56/b622c922860373/main.cpp
@@ -0,0 +1,38 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/filesystem.hpp>
+
+#include <iostream>
+
+using namespace boost::property_tree;
+namespace fs = boost::filesystem;
+
+namespace project_definition {
+    void apply(ptree const& def, fs::path const& rootFolder) {
+        for (auto node : def) {
+            if ("folder" == node.first) {
+                fs::path where = rootFolder / node.second.get<std::string>("name");
+                fs::create_directories(where);
+
+                apply(node.second, where);
+            }
+            if ("file" == node.first) {
+                std::ofstream((rootFolder / node.second.get<std::string>("name")).native(), std::ios::trunc);
+            }
+        }
+    }
+}
+
+int main()
+{
+    ptree projdef;
+    read_xml("input.txt", projdef);
+
+    try {
+        project_definition::apply(projdef, "./rootfolder/");
+    } catch(std::exception const& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+}
+
