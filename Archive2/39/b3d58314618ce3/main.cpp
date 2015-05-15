e8b82c23eb39646a20aeef8a92cca38e6d6b0d53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-15 21:29:32 +0000

    Update archive 39

---
+++ Archive2/39/b3d58314618ce3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+include <iostream>
+using namespace std;
+int main()
+    {
+     int num;
+     int base = 0;
+     cin >> num;
+     while (num != 0){
+
+     if(num > base){
+       base = num;
+
+     }
+     cin >> num;
+
+
+     }
+
+     cout << base;
+
+
+
