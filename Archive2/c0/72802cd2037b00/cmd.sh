7b8942433dd62664e85c6442adacce41c091351a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-16 20:17:55 +0000

    Update archive c0

---
+++ Archive2/c0/72802cd2037b00/cmd.sh
@@ -0,0 +1,3 @@
+cat>macAddr.txt<<"JSON"; g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+[{"mac":"60:21:C0:20:6A:8C","power":-73,"sensor_id":254,"timestamp":"2015-05-16 19:32:19"}] trailing
+JSON
