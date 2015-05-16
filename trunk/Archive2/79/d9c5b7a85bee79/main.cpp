ddcd95564236cb79c50a1f087bbd05b6a9819ba1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-16 14:51:53 +0000

    Update archive 79

---
+++ trunk/Archive2/79/d9c5b7a85bee79/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int x,y;
+    x=50
+    
+    if((x>45) && (x<=60))     //se x for maior que 45 e menor que 60
+    printf("o numero %d esta entre 45 e 60\n",x); //será impresso na tela esta frase.
+}
