3dea26745a923790547602d9db385dce092c7f23
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-17 08:36:53 +0000

    Update archive 8d

---
+++ trunk/Archive2/8d/4283e68de561cf/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <functional>
+#include <deque>
+
+struct observable
+{
+    virtual ~observable() = default ;
+
+    using observer = std::function< void( const observable& ) > ;
+
+    template < typename CALLABLE > void attach( CALLABLE&& fn )
+    { observers.emplace_back( std::forward<CALLABLE>(fn) ) ; }
+
+    virtual void raise_event() { for( const observer& obs : observers ) obs(*this) ; }
+
+    private: std::deque<observer> observers ;
+};
+
+void non_member_event_handler( const observable& source )
+{ std::cout << "non_member_event_handler(sender:" << std::addressof(source) <<  ")\n" ; }
+
+int main()
+{
+    observable subject ;
+
+    struct an_observer
+    {
+        explicit an_observer( observable& obs )
+        { obs.attach( std::bind( &an_observer::on_event, this, std::placeholders::_1 ) ) ; }
+        // on_event is private; a member of the class can still access it.
+
+        private:
+            void on_event( const observable& source ) const
+            { std::cout << "an_observer::on_event(sender:" << std::addressof(source) <<  ")\n" ; }
+    };
+
+    an_observer observer(subject) ; // handle event in a private member function
+    
+    // handlwe event in a closure
+    subject.attach( []( const observable& source )
+                    { std::cout << "closure(sender:" << std::addressof(source) <<  ")\n" ; } ) ;
+
+    subject.raise_event() ;
+    std::cout << "------------\n" ;
+    
+    // handle event in a function at namespace scope
+    subject.attach(non_member_event_handler) ;
+
+    struct function_object 
+    {
+        void operator() ( const observable& source ) const
+        { std::cout << "function_object::operator()(sender:" << std::addressof(source) <<  ")\n" ; }
+    };
+    
+    // handle event using a function object
+    subject.attach( function_object{} ) ;
+
+    subject.raise_event() ;
+}
