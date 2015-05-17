9d88b8a9e09b33953dbeeb651422ca132b78a2c7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-17 03:10:11 +0000

    Update archive f4

---
+++ trunk/Archive2/f4/671d5b4ea23f8d/main.cpp
@@ -0,0 +1,12 @@
+#include <random>
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    random_device device; // Dispositivo do sistema para gerar seeds.
+    mt19937 rng(device()); // Gerador Mersenne Twister inicializado com uma seed.
+    uniform_int_distribution<int> random(1, 10); // Gerar números de randômicos de 1 à 10 via Mersenne Twister.
+    cout << random(rng) << endl; // Gera um número.
+    cout << random(rng) << endl; // Gera outro número.
+}
