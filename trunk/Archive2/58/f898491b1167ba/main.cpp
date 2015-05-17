f8a3ff74d7c4fbc118267ec90cd01e2e8b6fb339
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-17 05:03:23 +0000

    Update archive 58

---
+++ trunk/Archive2/58/f898491b1167ba/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <memory>
+#include <array>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+struct block
+{
+    virtual ~block() = default ;
+    // ...
+    const int block_number = ++slno ;
+    static int slno ;
+};
+
+int block::slno = 0 ;
+
+struct tetrimino
+{
+    // transfer of ownership is required for std::unique_ptr<block>
+    std::array< std::unique_ptr<block>, 4 > blocks ;
+
+    tetrimino() { new_blocks() ; }
+
+    // give it four new blocks
+    void new_blocks()
+    {
+        // note: std::make_unique<> requires C++14
+        // http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
+        // with C++11, ptr = std::unique_ptr<block>( new block( /* ... */ ) ) ;
+        for( auto& ptr : blocks ) ptr = std::make_unique<block>( /* ... */ ) ;
+    }
+
+    void cycle_blocks( std::vector< std::unique_ptr<block> >& pile )
+    {
+        // transfer ownership of the blocks to pile (move the smart pointers into the pile)
+        // see: http://en.cppreference.com/w/cpp/algorithm/move
+        // http://en.cppreference.com/w/cpp/iterator/back_inserter
+        std::move( std::begin(blocks), std::end(blocks), std::back_inserter(pile) ) ;
+
+        print( std::cout << "after move, " ) << '\n' ;
+
+        new_blocks() ; // give it four new blocks
+        print( std::cout << "after new_blocks(), " ) << '\n' ;
+    }
+
+    std::ostream& print( std::ostream& stm ) const
+    {
+        stm << "tetrimino holds blocks: " ;
+        for( const auto& ptr : blocks )
+            if(ptr) stm << ptr->block_number << ' ' ;
+            else stm << "nil " ;
+        return stm ;
+    }
+};
+
+std::ostream& operator<< ( std::ostream& stm, const tetrimino& tet ) { return tet.print(stm) ; }
+
+int main()
+{
+    tetrimino tet ;
+    std::cout << tet << '\n' ;
+    std::vector< std::unique_ptr<block> > pile ;
+
+    for( int i = 0 ; i < 3 ; ++i )
+    {
+        tet.cycle_blocks(pile) ;
+
+        std::cout << "pile now holds blocks: " ;
+        for( const auto& ptr : pile ) std::cout << ptr->block_number << ' ' ;
+        std::cout << "\n---------------------------\n" ;
+    }
+}
