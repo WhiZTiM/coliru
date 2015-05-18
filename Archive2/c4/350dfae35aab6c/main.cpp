282df2fefe9cc1a783748abf195e7c913ff4445e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-18 20:03:27 +0000

    Update archive c4

---
+++ Archive2/c4/350dfae35aab6c/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+    float v, x;
+    
+    printf ("Entre com o preço de custo:");
+    scanf ("%f", &v);
+    
+    if (v<'20'){
+        x=v*1,45;
+        printf ("valor de venda é: %f", x);
+    } else
+    x=v*1,3;
+    printf ("valor de venda é: %f", x);
+    
+    return 0
+}
