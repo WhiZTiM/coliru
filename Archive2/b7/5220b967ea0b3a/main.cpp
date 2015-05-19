17bea6fc1ee08dba6523532f5b1f3ad86c6948bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 00:27:58 +0000

    Update archive b7

---
+++ Archive2/b7/5220b967ea0b3a/main.cpp
@@ -0,0 +1,17 @@
+    #define override
+    
+    struct A
+    {
+        virtual void foo();
+        void bar();
+    };
+     
+    struct B : A
+    {
+        void foo() const override; // Error: B::foo does not override A::foo
+                                   // (signature mismatch)
+        void foo() override; // OK: B::foo overrides A::foo
+        void bar() override; // Error: A::bar is not virtual
+    };
+    
+    int main(){}
