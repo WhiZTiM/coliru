15418f83daa3d17e05fc657a6842b5161c05dd1f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 15:15:34 +0000

    Update archive 04

---
+++ trunk/Archive2/04/6607987ef5e10e/main.cpp
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    float  num1 = 123.0;
+    double num2  = 123.0;
+    if(num1 == num2)
+        printf("num1 and num2 are equal\n");
+    else
+        printf("num1 and num2 are not equal\n");
+
+    system("PAUSE");
+    return 0;
+}
