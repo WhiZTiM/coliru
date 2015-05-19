350dc35b8100f558ee1a3e01855c5971dd2c6c66
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 15:24:25 +0000

    Update archive b1

---
+++ Archive2/b1/66639848a981b4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <boost/date_time/gregorian/gregorian.hpp>
+
+// Third party libraries zijn het niet altijd eens, stel dat deze uit verschillende libraries komen
+// ... of uit dezelfde, wat in legacy code vaak genoeg gebeurt: twee classes die min of meer hetzelfde doen
+struct Human { int age_in_years; };
+struct Person { int gregorian_birthyear; };
+
+auto get_current_year(){
+    return boost::gregorian::day_clock::local_day().year();
+}
+
+auto get_age_in_years(Human x){ return x.age_in_years; }
+auto get_age_in_years(Person x){ return get_current_year() - x.gregorian_birthyear; }
+
+int main(){
+    // Stel dat deze objecten uit een database komen oid.
+    Human nick{24};
+    Person anne{1991};
+    
+    std::cout <<
+        "Nick is " << get_age_in_years(nick) << " jaar oud\n" <<
+        "Anne is ongeveer " << get_age_in_years(anne) << " jaar oud" << 
+        std::endl;
+}
