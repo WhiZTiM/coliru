d58c21e78d1120ea4adce859e02d9ffb0691d15d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 12:51:22 +0000

    Update archive 3d

---
+++ Archive2/3d/974b23cacd052f/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <experimental/algorithm>
+#include <experimental/functional>
+ 
+int main()
+{
+    std::string in = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
+                     " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+    std::string needle = "pisci";
+    auto it = std::experimental::search(in.begin(), in.end(),
+                   std::experimental::make_boyer_moore_searcher(
+                       needle.begin(), needle.end()));
+    if(it != in.end())
+        std::cout << "The string " << needle << " found at offset "
+                  << it - in.begin() << '\n';
+    else
+        std::cout << "The string " << needle << " not found\n";
+}
