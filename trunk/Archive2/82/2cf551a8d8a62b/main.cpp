948fbaa1cd006a61f183f62ace4fa98a8d98f1eb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 00:34:30 +0000

    Update archive 82

---
+++ trunk/Archive2/82/2cf551a8d8a62b/main.cpp
@@ -0,0 +1,21 @@
+#include <boost/range/irange.hpp>
+#include <iostream>
+
+template<class Integer>
+decltype(auto) iota(Integer last)
+{
+    return boost::irange(0, last);    
+}
+
+template<class Integer, class StepSize>
+decltype(auto) iota(Integer last, StepSize step_size)
+{
+    return boost::irange(0, last, step_size);    
+}
+
+int main(  )
+{
+    std::cout << "Hello World" << std::endl;
+    for (auto x : iota(5))
+        std::cout << x;
+}
