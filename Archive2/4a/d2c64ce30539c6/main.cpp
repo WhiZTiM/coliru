2b03a8ef855a84643f7ed7f44767d09b5f43eab6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 21:27:53 +0000

    Update archive 4a

---
+++ Archive2/4a/d2c64ce30539c6/main.cpp
@@ -0,0 +1,30 @@
+int foo(int i)
+{
+    int res = 0;
+    if (i % 2) {
+        if (i % 3) {
+            goto labelA;
+        }
+        // The lack of a goto here is a fallthrough bug case which should not happen.
+    } else if (i % 3) {
+        goto labelB;
+    } else {
+        goto labelC;
+    }
+
+    goto labelNoReturn; // -- this point should not be reached. --
+
+labelA:
+    res = 1;
+    goto final;
+labelB:
+    res = 2;
+    goto final;
+labelC:
+    res = 3;
+    goto final;
+final:
+    return res;
+labelNoReturn:
+    {} 
+}
