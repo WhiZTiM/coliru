5ed7db06342c2f6ee47f940a894813aef6ef5118
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-19 23:21:16 +0000

    Update archive ed

---
+++ trunk/Archive2/ed/b0ec678ba9f948/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+template <typename Container, typename Function>
+void for_each(Container c, Function f){
+    for(auto e : c){
+		f(e);
+	}
+}
+
+int main(){
+    auto const cijfers = std::vector<int>{13, 33, 37};
+    auto const print = [](auto e){ std::cout << e << std::endl; };
+    for_each(cijfers, print);
+}
+
