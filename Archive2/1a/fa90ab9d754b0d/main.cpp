286d79d4f5dc9ab8e0a236b15c9acbb9262776a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-20 15:10:18 +0000

    Update archive 1a

---
+++ Archive2/1a/fa90ab9d754b0d/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+struct X { X(std::string s) : s_(s) { } std::string s_; };
+
+template <typename T>
+void f(T& x, const char* p)
+{
+    x.~T();
+    new (&x) T(p);
+}
+
+int main()
+{
+    X x("hello");
+	f(x, "goodbye");
+	std::cout << x.s_ << '\n';
+}
