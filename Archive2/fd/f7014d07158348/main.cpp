e270d087767c7eeabf88a7a03e4fc8b660bfa870
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-20 20:43:03 +0000

    Update archive fd

---
+++ Archive2/fd/f7014d07158348/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+
+
+template<int I, typename F>
+void wow(F& f)
+{
+    int n = I >> 1;
+    switch(I%2)
+    {
+        case 0: do{
+            cout << "lali" << endl;
+            for(auto i : f){
+                i();
+            }
+        case 1:
+            cout << "pop" << endl;
+            for(auto i : f){
+                i();
+            }
+        }while(--n);
+    };
+}
+
+
+int main()
+{
+    // a pure-function love lambda
+    auto make_it_true = [](
+        auto total,
+        auto bin,
+        auto line,
+        auto apart=0){
+        return [total, bin, line, apart] () mutable{
+            int space;
+            if (bin){
+                space = total - (line<<1) - apart;
+                while(space-->0){
+                    cout << " ";
+                }
+                int half_line = line;
+                line = line<<1;
+                while(line-->half_line){
+                    cout << "_";
+                }
+                while(apart-->0){
+                    cout << " ";
+                }
+                while(line-->=0){
+                    cout << "_";
+                }
+                cout << endl;
+            }
+            else{
+                space = total - line;
+                while(space-->0){
+                    cout << " ";
+                }
+                while(line-->0){
+                    cout << "_";
+                }
+                cout << endl;
+            }
+        };
+    };
+
+    using MAKE_IT_TRUE = function<void(void)>;
+    MAKE_IT_TRUE mit[6] = {
+        make_it_true(11,1,2,5),
+        make_it_true(13,0,13,0),
+        make_it_true(12,0,11,0),
+        make_it_true(11,0,9,0),
+        make_it_true(8,0,3,0),
+        make_it_true(7,0,1,0)};
+
+    wow<520>(mit);
+}
