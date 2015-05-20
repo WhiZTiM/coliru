8031208c0a861d50e1823c6cdcafcb698ba41341
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-20 21:26:32 +0000

    Update archive 38

---
+++ trunk/Archive2/38/19e3f26ed42f2a/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+
+template<int I, typename F>
+void wow(F f)
+{
+    int n = I >> 1;
+    switch(I%2)
+    {
+        case 0: do{
+            cout << "lali" << endl;
+            f(11,1,2,5);
+            f(13,0,13,0);
+            f(12,0,11,0);
+            f(11,0,9,0);
+            f(8,0,3,0);
+            f(7,0,1,0);
+        case 1:
+            cout << "pop" << endl;
+            f(11,1,2,5);
+            f(13,0,13,0);
+            f(12,0,11,0);
+            f(11,0,9,0);
+            f(8,0,3,0);
+            f(7,0,1,0);
+        }while(--n);
+    };
+}
+
+
+int main()
+{
+    // a pure-function love lambda
+    auto make_it_true = [](
+        auto total,
+        auto bin,
+        auto line,
+        auto apart=0){
+        int space;
+        if (bin){
+            space = total - (line<<1) - apart;
+            while(space-->0){
+                cout << " ";
+            }
+            int half_line = line;
+            line = line<<1;
+            while(line-->half_line){
+                cout << "_";
+            }
+            while(apart-->0){
+                cout << " ";
+            }
+            while(line-->=0){
+                cout << "_";
+            }
+            cout << endl;
+        }
+        else{
+            space = total - line;
+            while(space-->0){
+                cout << " ";
+            }
+            while(line-->0){
+                cout << "_";
+            }
+            cout << endl;
+        }
+    };
+    wow<520>(make_it_true);
+}
