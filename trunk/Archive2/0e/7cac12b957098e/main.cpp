4f4d9be369c27438847e89e6836b0d34dd2f7bb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-21 14:46:40 +0000

    Update archive 0e

---
+++ trunk/Archive2/0e/7cac12b957098e/main.cpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+ 
+int principal ( ) 
+{ 
+    const std :: size_t N =  100 ; 
+    int * a = nueva int [ N ] ;
+ 
+    para  ( std :: size_t n =  0 ; n < N ;  ++ n ) 
+        un [ n ]  = n ;
+ 
+    eliminar [ ] una ; 
+}
