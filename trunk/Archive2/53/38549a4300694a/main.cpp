42d59fe766868057ed57e86eac804819e37e9b3a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-21 16:58:45 +0000

    Update archive 53

---
+++ trunk/Archive2/53/38549a4300694a/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+template<typename ... Ts>
+struct System : public System<Ts> ...
+{
+    template<typename T>
+    T& get()
+    {
+        static_assert(std::is_base_of<System<T>, System<Ts...>>::value, "the requested type does not exist in this system");
+        return System<T>::value;
+    }
+};
+
+template<typename T>
+struct System<T>
+{
+    T value{};
+};
+
+int main()
+{
+    System<int, std::string, char> test;
+ 
+    test.get<std::string>() = "Hello world ! ";
+    std::cout << test.get<std::string>() << std::endl << test.get<int>() << std::endl;
+ 
+    return 0;
+}
