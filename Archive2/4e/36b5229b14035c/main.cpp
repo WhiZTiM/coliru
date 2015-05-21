91066601dfb5b7c91c4eb957368f67433adba6d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-21 08:53:34 +0000

    Update archive 4e

---
+++ Archive2/4e/36b5229b14035c/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <tuple>
+#include <iostream>
+
+int main()
+
+{
+
+    using namespace std::literals::string_literals;
+    auto s = "hello, world!\n"s;
+    auto ‫iterators‬ = std::make_pair(s.rbegin(), s.rend());
+
+    for(; ‫iterators‬.first != ‫iterators‬.second; ++‫iterators‬.first) std::cout << *‫iterators‬.first;
+
+}
