3bd2f26cf31dee577da6de86c7f64a1a143f9522
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-21 09:14:07 +0000

    Update archive 70

---
+++ trunk/Archive2/70/688b13f7455cf4/main.cpp
@@ -0,0 +1,31 @@
+//#define BOOST_SPIRIT_DEBUG
+#include <boost/spirit/include/qi.hpp>
+namespace qi  = boost::spirit::qi;
+
+int main() {
+    typedef std::string::const_iterator It;
+    std::string const line = "<005F> <0061> [<00660066> <00660069> <00660066006C>]";
+
+    It f = line.begin(), l = line.end();
+
+    qi::int_parser<uintmax_t, 16> hex_int;
+    qi::rule<It, uintmax_t()> braced_hex = '<' >> hex_int >> '>';
+    BOOST_SPIRIT_DEBUG_NODE(braced_hex);
+
+    std::vector<uintmax_t> list;
+    bool result = qi::phrase_parse(f, l, *braced_hex >> '[' >> *braced_hex >> ']', qi::space, list);
+
+    if (result) {
+        std::cout << "Parse success: " << list.size() << "\n";
+
+        for (auto& v : list)
+            std::cout << v << " ";
+    }
+    else {
+        std::cout << "Parse failed\n";
+    }
+
+    if (f!=l) {
+        std::cout << "Remaining unparsed: '" << std::string(f,l) << "'\n";
+    }
+}
