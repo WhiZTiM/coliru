d96f54a681f6c953b9f7423ebc62533d9240810e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 15:13:45 +0000

    Update archive a5

---
+++ trunk/Archive2/a5/db5b3c54efb52b/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void do_something(int) {}
+
+void fun (const std::vector<int> &vec) {
+    for (std::size_t i = 0; i < vec.size() - 1; ++i)
+        do_something(vec[i]);
+}
+
+int main()
+{
+    std::vector<int> foo;
+}
