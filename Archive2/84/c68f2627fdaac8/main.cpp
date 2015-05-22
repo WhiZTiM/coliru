e58269271cc2a02a4befbef581c436bf069e5d70
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 17:39:24 +0000

    Update archive 84

---
+++ Archive2/84/c68f2627fdaac8/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+    
+    std::vector<int> arr={15,5,6,55,44};
+    std::sort(arr);
+    
+    for (auto& v : arr)
+        printf("%d\n", v);
+    
+}
