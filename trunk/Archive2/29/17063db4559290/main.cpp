f3d18f9532307ba91766249ce9f8d4f32d3daf00
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 10:51:50 +0000

    Update archive 29

---
+++ trunk/Archive2/29/17063db4559290/main.cpp
@@ -0,0 +1,27 @@
+
+class GebruikerPunt {
+    // Het punt dat de gebruiker kiest
+    int x;
+    int y;
+    int radius;
+    List<PuntWaarDokterWoont> dokters;
+}
+
+class PuntWaarDokterWoont {
+    int x;
+    int y;
+}
+
+int main()
+{
+    GoogleMap << PuntWaarDokterWoont(1, 2);
+    GoogleMap << PuntWaarDokterWoont(4, 6);
+    GoogleMap << PuntWaarDokterWoont(2, 5);
+    
+    GebruikerPunt userChoice(10, 20, 9);
+    for (Alle punten in google maps in de square (1, 11, 19, 29) (x1, y1, x2, y2))
+    {
+        als hij in de cirkel ligt
+           userChoice.dokters << PuntWaarDokterWoont;
+    }
+}
