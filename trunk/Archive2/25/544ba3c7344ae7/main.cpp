052add0e07297072921bf7e073dcfecd7d2e59ac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 18:00:32 +0000

    Update archive 25

---
+++ trunk/Archive2/25/544ba3c7344ae7/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void f()
+{
+    double z = -1337.0;
+    std::cout << z << "\n";
+}
+
+void g()
+{
+    double a = a+a;
+    std::cout << a << "\n";
+}
+
+int main()
+{
+    f();
+    g();
+}
