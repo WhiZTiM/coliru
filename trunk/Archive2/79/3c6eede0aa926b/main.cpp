9d54900519c31a6f0f85fe975d2ad0768299dba8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 20:53:04 +0000

    Update archive 79

---
+++ trunk/Archive2/79/3c6eede0aa926b/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <tuple>
+#include <functional>
+
+template<typename T>
+struct tuple_with_placeholder : T
+{
+    using T::T;
+
+    template<typename PH>
+    auto operator[](PH) const
+    {
+        return std::get<std::is_placeholder<PH>::value-1>(*this);
+    }
+};
+
+#define LR(EXPR) (auto&&... ps) { \
+    tuple_with_placeholder<decltype(std::forward_as_tuple(ps...))> p(ps...); \
+    using namespace std::placeholders; \
+    return EXPR; \
+}
+
+int main()
+{
+    std::cout << []LR(p[_1])("test");
+    std::cout << []LR(p[_1]+p[_2])(2, 3); 
+    std::cout << []LR(p[_1]+p[_2])(std::string("hello "), std::string(" world!"));
+}
