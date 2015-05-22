24d4466f8ac002453993d63c728d10ebb902ba75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 14:27:15 +0000

    Update archive 94

---
+++ Archive2/94/fe817394bb4c5a/main.cpp
@@ -0,0 +1,20 @@
+// g++ -o ex1 ex1.cc && ./ex1
+ 
+#include <iostream>
+ 
+using namespace std;
+ 
+int myArray[] = {16, 2, 77, 40, 12071};
+ 
+int n, result=0;
+ 
+//=========================================
+int main ()
+{
+ for ( n=0 ; n<5 ; ++n )
+ {
+  result += myArray[n];
+ }
+ cout << "Sum: " << result << endl ;
+ return 0;
+}
