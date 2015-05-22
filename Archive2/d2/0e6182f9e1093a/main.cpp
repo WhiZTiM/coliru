0674436f663c6cb4470bbe42440c094e1864ca16
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 18:39:07 +0000

    Update archive d2

---
+++ Archive2/d2/0e6182f9e1093a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#pragma aaa
+using namespace std;
+
+int main()
+{
+  int A[] = {1, 2, 3, 4, 5, 6};
+  for (int current : A)
+    cout << current << endl;
+  cout << "done" << endl;
+  return 0;
+}
