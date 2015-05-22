b6dc8ddc3c709d79c4f37393ba51adca21f787ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 17:23:14 +0000

    Update archive 6e

---
+++ trunk/Archive2/6e/968ca90e6192db/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+namespace N
+{
+    template <typename T>
+    class Foo {
+        static const T bar;
+    };
+}
+
+namespace O
+{
+    typedef N::Foo<int> Baz;
+
+    template<>
+    const int Baz::bar = 1;
+}
+
+int main() {
+    
+}
