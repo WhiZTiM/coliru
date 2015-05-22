6b7fe964de3575c155f6e926eb08c9598120c653
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 08:35:11 +0000

    Update archive 92

---
+++ Archive2/92/ed9b2424f06a48/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+#include <memory>
+#include <vector>
+
+template<typename T>
+class record{ 
+public:
+    record(T c_value){
+        value = c_value;   
+    }
+  T value;
+};
+
+
+template<typename T>
+class registry{
+public:  
+  registry(T c_data){
+    data = std::make_shared<record<T> >(c_data);   
+  }
+  
+  std::shared_ptr<record<T> > data;
+  std::vector<std::shared_ptr<registry> > records;
+};
+
+
+
+int main()
+{
+   auto h1 = std::make_shared<registry<int> >(30);
+   auto h2 = std::make_shared<registry<int> >(40);
+   h1->records.push_back(h2);
+   
+   
+   std::cout << h1->data->value << std::endl;
+   std::cout << h1->records[0]->data->value << std::endl;
+   
+    
+}
