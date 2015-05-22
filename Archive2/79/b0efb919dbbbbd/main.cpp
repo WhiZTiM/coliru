5e50c73b07ec3f400b2ab02eb4d6e6814dcad08c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-22 09:14:51 +0000

    Update archive 79

---
+++ Archive2/79/b0efb919dbbbbd/main.cpp
@@ -0,0 +1,11 @@
+#include <type_traits>
+
+template<typename T, typename = typename std::enable_if<std::is_same<T, int>::value>::type>
+void do_the_thing() {}
+
+template<typename T, typename std::enable_if<std::is_same<T, int>::value, int>::type = 0>
+void also_do_the_thing() {}
+
+int main() {
+    also_do_the_thing<int>();
+}
