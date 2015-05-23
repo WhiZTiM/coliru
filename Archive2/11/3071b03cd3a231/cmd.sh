fd9d1846710e35d7a53ca3d9e0e0cb5961aaab92
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-23 15:17:53 +0000

    Update archive 11

---
+++ Archive2/11/3071b03cd3a231/cmd.sh
@@ -0,0 +1,2 @@
+echo clag++ && clang++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out;
+echo g++ && g++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out;
---
+++ Archive2/11/3071b03cd3a231/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+template<int L, typename T>
+class Vector {
+
+  friend
+  Vector operator+(const Vector&, const Vector&) {
+    return Vector();
+  }
+
+private:
+  T data[L];
+};
+
+int main() {
+  Vector<42, double> v42d;
+  Vector<24, int> v24i;
+  (void) v42d;
+  (void) v24i;
+  
+  printf("ok!\n");
+}
