c838027046f34f65cd596b34ab388a95811a1f04
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-23 23:15:20 +0000

    Update archive f0

---
+++ Archive2/f0/39d32d3492814c/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>     // std::cout
+#include <vector>       // std::vector
+#include <string>       // std::string
+#include <chrono>
+
+class Generic
+{
+public:
+    template<typename T> Generic& operator<<(const T& v)
+    {
+        handleData(v);
+    	return *this;
+    }
+protected:
+	template<typename T> void handleData(const T& v)
+	{
+		std::cout << "Generic Handle Data: " << v << std::endl;
+	}
+};
+
+class Dummy
+{
+public:
+    Dummy(int v) : _v(v)
+	{
+		std::cout << "InitDummy with " << v << std::endl;
+	}
+	int getVal() const
+	{
+		return _v;
+	}
+private:
+	int _v;
+};
+
+class Specific : public Generic
+{
+public:
+    template<typename T> Specific& operator<<(const T& v)
+    {
+        handleData(v);
+    	return *this;
+    }
+
+protected:
+    void handleData(const Dummy& v)
+    {
+        Generic::handleData(v.getVal());
+    }
+    
+    using Generic::handleData;
+};
+
+int main () {
+    Specific g;
+    
+    g << 5 << "hello" << Dummy(666);
+    return 0;
+}
