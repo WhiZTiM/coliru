4ba617130fa77f0e23e9b8df5a0318d4512f4b7c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-23 00:13:54 +0000

    Update archive 07

---
+++ Archive2/07/b0f3e1545b7ec1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Int {
+public:
+    int v;
+    Int(int a) { v = a; }
+    Int &operator[](int x) {
+        v+=x;
+        return *this;
+    }
+};
+ostream &operator<< (ostream &o, Int &a) {
+    return o << a.v;
+}
+
+int main() {
+    Int i = 2;
+    cout << i[0];
+    cout << i[2];
+    return 0;
+}
