e967753c2a8e5158062ddead57a3c1d30882b1dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-24 23:40:11 +0000

    Update archive 3d

---
+++ Archive2/3d/437a26b141e339/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main ()
+{
+    vector<int> vec{13,45,6,73,21,3};
+
+for (unsigned i=0; i<vec.size();i++){
+    	cout<<vec[i]<<" ";
+}
+}
