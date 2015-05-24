f02a6000a402b9e230bdf99e84a824842cff3209
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-24 14:36:46 +0000

    Update archive 79

---
+++ Archive2/79/6b09e9bb7abff5/main.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+#include <cstdio> 
+
+struct X {
+    X()  { puts(__PRETTY_FUNCTION__); }
+    ~X() { puts(__PRETTY_FUNCTION__); }
+};
+
+int main() {
+    X obj;
+
+    // watch this:
+    obj.~X();
+    new(&obj) X;
+}
