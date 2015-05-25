5ff636a7b63df224ebb6ab4b569a0ee485587ad2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-25 23:55:47 +0000

    Update archive 36

---
+++ trunk/Archive2/36/efe3418d16cdc2/main.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <istream>
+#include <sstream>
+
+template <typename Graph>
+bool read_coloring_problem(std::istream& dimacs, Graph& g) {
+    size_t vertices = 0, edges = 0;
+
+    std::string line;
+    while (getline(dimacs, line))
+    {
+        std::istringstream iss(line);
+        if (!vertices||!edges)
+        {
+            char ch; std::string format;
+            iss >> ch >> format >> vertices >> edges;
+        } else
+        {
+            char ch;
+            size_t from, to;
+
+            if (iss>>ch) switch(ch) {
+                case 'c': break;
+                case 'e': 
+                    if (edges-- && (iss >> from >> to) && (add_edge(from-1, to-1, g).second))
+                        break;
+                default: 
+                    return false;
+            }
+        }
+    }
+
+    return !(edges || !dimacs.eof());
+}
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <fstream>
+#include <iostream>
+
+using namespace boost::graph;
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+
+int main() {
+    Graph g2;
+    std::ifstream input("myciel4.col");
+    if (read_coloring_problem(input, g2))
+    {
+        std::cout << "Nodes: " << num_vertices(g2) << "\n";
+        std::cout << "Edges: " << num_edges(g2)    << "\n";
+        boost::write_graphviz(std::cout, g2);
+    } else {
+        std::cout << "Parse error\n";
+    }
+}
