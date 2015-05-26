4b29e3b721065e3ea860bab00af153803203844e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 21:40:35 +0000

    Update archive 28

---
+++ trunk/Archive2/28/0d54d631212577/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    
+    int i =4;
+    std::cout << " i "  << i << std::endl;
+    std::cout << " ~i "  << ~i << std::endl;
+    std::cout << " -i "  << -i << std::endl;
+    std::cout << " ~(-i) "  << ~-i << std::endl;
+    std::cout << " ~-i "  << ~(-i) << std::endl;
+    
+}
