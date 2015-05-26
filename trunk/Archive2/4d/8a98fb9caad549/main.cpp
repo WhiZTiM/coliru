891e5aedd875fb380f2c9868282731730d181b4d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 20:14:58 +0000

    Update archive 4d

---
+++ trunk/Archive2/4d/8a98fb9caad549/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int i,tot;
+char stringa[100][100];
+
+cout<<"inserisci tot: ";
+cin>>tot;
+for(i=0;i<tot;i++) {
+                     cout<<"inserisci la frase: ";
+                     gets(stringa[i]);
+} 
+  
+   
+}
