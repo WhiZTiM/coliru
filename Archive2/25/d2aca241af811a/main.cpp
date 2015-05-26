aa9d02c82ee92859d8c54f7fcbbc2832156147b7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 09:05:04 +0000

    Update archive 25

---
+++ Archive2/25/d2aca241af811a/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+template<typename ...Args>
+void printer(Args&&... args) {
+    (std::cout << ... << args) << '\n';
+}
+
+
+template<typename Func, typename T>
+auto Min(Func f, T l, T r)
+{
+    return f(l, r);
+}
+
+template<typename Func, typename T, typename... Args>
+auto Min(Func f, T l, T r, Args... args)
+{
+    return Min(f, f(l, r), args...);
+}
+
+template<typename Func, typename... Args>
+auto Mul_Fold(Func, Args... args)
+{
+    return ( ... * args);
+}
+
+
+template<typename... Args>
+bool Check(Args&& ... args)
+{
+    return (... && args);
+}
+
+
+
+int main()
+{
+    std::cout << Min([](auto l, auto r) { return l < r ? l : r;}, 1, 2, 0) << "\n";
+    std::cout << Mul_Fold([](auto l, auto r) { return l * r; }, 1, 2, 3) << "\n";
+    
+    auto test1 = [] { return true; };
+    auto test2 = [] { return false; };
+    auto test3 = [] { return true; };
+    
+    
+    
+    std::cout << std::boolalpha << Check(test1, test2) << "\n";
+    std::cout << std::boolalpha << Check(test1, test3) << "\n";
+}
