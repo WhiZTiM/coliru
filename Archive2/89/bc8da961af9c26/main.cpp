52e42c429111cfcd1ce3decb2556331c93d7792e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 09:46:47 +0000

    Update archive 89

---
+++ Archive2/89/bc8da961af9c26/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE* fp = fopen("from.txt", "w"); // create file "from.txt"
+    if(!fp) { perror("from.txt"); return 1; }
+    fputc('a', fp); // write to "from.txt"
+    fclose(fp);
+ 
+    int rc = rename("from.txt", "../to.txt");
+    if(rc) { perror("rename"); return 1; }
+ 
+    fp = fopen("../to.txt", "r");
+    if(!fp) { perror("to.txt"); return 1; }
+    printf("%c\n", fgetc(fp)); // read from "to.txt"
+    fclose(fp);
+}
