465854afd39af32b44cdeb802e725878ce1c5526
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 09:44:15 +0000

    Update archive 67

---
+++ Archive2/67/93ea7d423207c2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <regex>
+ 
+int main()
+{
+    std::string lines[] = {"Roses are #ff0000",
+                           "violets are #0000ff",
+                           "all of my base are belong to you"};
+ 
+    std::regex color_regex("#([a-f0-9]{2})"
+                            "([a-f0-9]{2})"
+                            "([a-f0-9]{2})");
+ 
+    for (const auto &line : lines) {
+        std::cout << line << ": " 
+                  << std::regex_search(line, color_regex) << '\n';
+    }   
+ 
+    std::smatch color_match;
+    for (const auto &line : lines) {
+        std::regex_search(line, color_match, color_regex);
+        std::cout << "matches for '" << line << "'\n";
+        for (size_t i = 0; i < color_match.size(); ++i)
+            std::cout << i << ": " << color_match[i] << '\n';
+    }   
+}
