e1de2ef5acf2f7b076566cf889947d3686e3a060
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 21:54:38 +0000

    Update archive ae

---
+++ Archive2/ae/4586c718e67d07/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+void print(const char* str)
+{
+  std::cout << "length   = " << std::char_traits<char>::length(str) << '\n';
+}
+
+template <size_t SIZE>
+void print(const char(&str)[SIZE])
+{
+  std::cout << "length[] = " << SIZE << '\n';
+}
+
+int main()
+{
+  print("foo");
+  
+  char a[] = {'f', 'o', 'o'};
+  print(a);
+}
