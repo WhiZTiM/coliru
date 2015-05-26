759907767238dfff6b4f8a55ca97b26825c17ff2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-26 14:58:02 +0000

    Update archive ae

---
+++ trunk/Archive2/ae/bff3189a99eaff/main.cpp
@@ -0,0 +1,17 @@
+#include <utility>     // std::move
+
+struct Myclass 
+{
+   Myclass &  truc() { return           *this;  }
+   Myclass && toto() { return std::move(*this); }
+};
+
+void foo(Myclass &) {}
+
+Myclass bar() { return Myclass(); }
+
+int main()              
+{
+  foo( bar().truc() );   // OK ??
+//foo( bar().toto() );   // error
+}
