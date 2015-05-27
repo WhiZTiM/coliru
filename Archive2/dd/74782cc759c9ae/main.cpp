58f3a0f2f7eb6d652dc6b763efc8f24901a35a11
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-27 20:57:30 +0000

    Update archive dd

---
+++ Archive2/dd/74782cc759c9ae/main.cpp
@@ -0,0 +1,40 @@
+#include <boost/range/adaptor/transformed.hpp>
+#include <boost/range/algorithm.hpp>
+
+#include <iostream>
+#include <functional>
+#include <memory>
+
+struct A {
+    A() = default;
+    A(const A&) = delete;
+    A& operator=(const A&) = delete;
+};
+
+struct Base {
+    virtual int foo(const A&, int b) = 0;
+    virtual ~Base() {}
+};
+
+struct B : Base {
+    B() = default;
+    B(const B&) = delete;
+    B& operator=(const B&) = delete;
+
+    int foo(const A&, int b) override {
+        return -b;
+    }
+};
+
+int main() {
+    A a;
+    std::shared_ptr<Base> b = std::make_shared<B>();
+    std::vector<int> values{1, 2, 3, 2};
+    
+    using std::placeholders::_1;
+    int min = *boost::min_element(values | boost::adaptors::transformed(
+            std::bind(&B::foo, b.get(), std::ref(a), _1)));
+    
+    std::cout << min << std::endl;
+}
+
