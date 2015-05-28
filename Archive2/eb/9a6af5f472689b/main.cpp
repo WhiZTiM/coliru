ffd73e10c12c41b1e2405207bc9d2187b8d06698
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-28 17:19:22 +0000

    Update archive eb

---
+++ Archive2/eb/9a6af5f472689b/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+
+template<typename ValueType>
+struct container {
+    
+    explicit container(std::size_t n) {
+        vec.reserve(n);
+    }
+    
+    container& operator=(ValueType a) {
+        vec.push_back(a);
+        return (*this);
+    }
+
+    std::vector<ValueType> vec;
+    
+};
+
+template<typename ValueType>
+container<ValueType>& operator,(container<ValueType>& m, ValueType a) {
+    m.vec.push_back(a);
+    return m;
+}
+
+int main() {
+    container<int> M(5);
+    M = 1, 2, 3, 4, 5;
+    for (auto i : M.vec) std::cout << i << ' ';
+}
