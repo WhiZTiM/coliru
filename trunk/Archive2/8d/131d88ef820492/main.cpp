f878a51387e4aa5b2972bbbcf67e6a3f358fe286
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-28 18:26:04 +0000

    Update archive 8d

---
+++ trunk/Archive2/8d/131d88ef820492/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+ 
+int main()
+{
+    std::cout << M_PI * 2 / 8 << std::endl;
+    std::cout << 2 / 8 * M_PI << std::endl;
+    std::cout << 2. / 8. * M_PI << std::endl;
+}
