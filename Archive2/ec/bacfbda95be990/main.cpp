01f1aebceefc0261bb1a35ccda3ffcf53099c5ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-29 14:17:24 +0000

    Update archive ec

---
+++ Archive2/ec/bacfbda95be990/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+
+/*
+ * This simple definition is the only one that is available
+ * to external units.
+ * The file is named "blabla_exported.h"
+ */
+
+template <typename T>
+struct Blob {
+    template <typename X>
+    int blib(X x);
+};
+
+/*
+ * The actual implementation of the function template is in another file
+ * This file is not available to external units
+ */
+ 
+ template <typename T>
+ template <typename X>
+ int Blob<T>::blib(X x) {
+     /* The actual template uses a third party lib
+      * whose headers are not available to the "external units" ...
+      */
+     return 42;
+ }
+
+/* My need:
+ * Get the function template specializations I need
+ * to be compiled
+ */
+
+template class Blob<int>; // Explicit instantiation of class template
+
+template int Blob<int>::blib(bool); // Explicit instantitation of member function
+
+int main() {
+    Blob<int> t;
+    std::cout << t.blib(2.0) << std::endl;
+    return 0;
+}
