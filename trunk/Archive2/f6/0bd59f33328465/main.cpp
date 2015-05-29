66aab3c52b7286cd844a675b772d583e6c091cc8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-29 20:23:41 +0000

    Update archive f6

---
+++ trunk/Archive2/f6/0bd59f33328465/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstddef>
+
+#define __offsetof(type, member) ((std::size_t) &(((type*)0)->member))
+
+struct S {
+    char c;
+    double d;
+};
+
+int main()
+{
+    s
+}
