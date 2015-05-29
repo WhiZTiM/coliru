40a1cf2e92cddd94f0be9327402ad67c3bf34869
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-29 15:39:18 +0000

    Update archive 71

---
+++ trunk/Archive2/71/0d897e555b0d49/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct C
+{
+    operator int()
+    {
+        std::cout << "C::operator int()" << std::endl;
+        return 0;
+    }
+    operator void()
+    {
+        std::cout << "C::operator void()" << std::endl;
+    }
+};
+
+int main()
+{
+    C c;
+    (int)c;
+    (void)c;
+    //
+    // this one does print
+    c.operator void();
+}
