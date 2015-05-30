f626e82b0769131270a16af718a20d99457fe427
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-30 15:36:21 +0000

    Update archive ea

---
+++ Archive2/ea/112a1b42f3b424/main.cpp
@@ -0,0 +1,33 @@
+#include <type_traits>
+
+template<class T>
+struct some_trait : std::false_type {};
+
+template<class T>
+struct some_other_trait : std::false_type {};
+
+// never called in an evaluated context
+template<class T, class = std::enable_if_t<some_other_trait<T>::value, int>>
+some_trait<T> has_some_trait(T);
+
+namespace detail
+{
+
+template<class T, class C = decltype(has_some_trait(T{}))>
+using check_trait_t = std::enable_if_t<C::value, int>;
+
+} // detail::
+
+template<class T, detail::check_trait_t<T> = 0>
+void foo(T){}
+
+#include <iostream>
+
+struct X{};
+
+std::true_type has_some_trait(X);
+
+int main()
+{
+    foo(X{});
+}
