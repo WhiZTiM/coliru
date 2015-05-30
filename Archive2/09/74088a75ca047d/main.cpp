acb0a75db7fc2d71d7e402b215e1a4ef6d25e4f8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-30 11:21:03 +0000

    Update archive 09

---
+++ Archive2/09/74088a75ca047d/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<unsigned> v1{ 1, 2, 3 }; 
+    std::vector<unsigned> v2{ 1, 2, 3 }; 
+    std::vector<unsigned> v3{ 3, 4, 5 };
+    
+    std::cout << (v1 == v2) << std::endl;
+    std::cout << (v1 < v3) << std::endl;
+    std::cout << (v3 < v2) << std::endl;
+    
+    
+    std::vector<std::vector<unsigned>> vv { v1, v2, v3 }; 
+    
+    auto it = std::find(vv.begin(), vv.end(), v3); 
+    if (it != vv.end())
+    {
+        std::cout << "I found v3, it has the following elements\n"; 
+        for (auto &&a : *it)
+        {
+            std::cout << a << std::endl;
+        }
+    }
+    
+}
