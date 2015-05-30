b622b47c4d99ddc853d0276ce7504226fdae685f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-30 12:35:36 +0000

    Update archive a8

---
+++ Archive2/a8/8056650c51580b/main.cpp
@@ -0,0 +1,27 @@
+        #include <vector>
+   		#include <sstream>
+		#include <iostream>
+		#include <boost/spirit/include/qi.hpp>
+		#include <boost/spirit/include/qi_match.hpp>
+		#include <boost/fusion/include/io.hpp>
+
+		using type = boost::fusion::vector<char, int, double >;
+
+		int main() {
+			std::istringstream istr{
+				"***: @a_-091 , *** 1"
+			};
+
+			std::vector<type> data;
+
+			namespace qi = boost::spirit::qi;
+			istr >> std::noskipws >> qi::match(*(
+				*qi::omit[qi::char_ - '@'] >> '@' >> qi::char_ >> '_' >> qi::int_ >> (*qi::omit[qi::char_ - qi::digit] > qi::double_)  //ADDED A COUPLE OF PARENTHESES
+				), data);
+
+			for (size_t i = 0; i != data.size(); ++i) {
+				std::cerr << data[i] << "\n";
+			}
+
+			return 0;
+		}
