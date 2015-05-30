5b19c6f92e416c7cbf5124c644c9c62d6e930be3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-30 13:57:36 +0000

    Update archive 70

---
+++ Archive2/70/5d921eaf86a031/main.cpp
@@ -0,0 +1,28 @@
+        #include <vector>
+   		#include <sstream>
+		#include <iostream>
+		#include <boost/spirit/include/qi.hpp>
+		#include <boost/spirit/include/qi_match.hpp>
+		#include <boost/fusion/include/io.hpp>
+
+		using type = boost::fusion::vector<boost::fusion::vector<char, int>, double >; //CHANGED THE TYPE TO FIT THE ATTRIBUTE OF THE RULE
+
+		int main() {
+			std::istringstream istr{
+				"***: @a_-091 , *** 1"
+			};
+
+			std::vector<type> data;
+
+			namespace qi = boost::spirit::qi;
+			istr >> std::noskipws >> qi::match(*(
+				*qi::omit[qi::char_ - '@'] >> '@' >> qi::char_ >> '_' >> qi::int_ >> *qi::omit[qi::char_ - qi::digit] > qi::double_  
+				), data);
+
+			for (size_t i = 0; i != data.size(); ++i) {
+				std::cerr << data[i] << "\n";
+			}
+
+
+			return 0;
+		}
