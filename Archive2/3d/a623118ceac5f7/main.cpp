97e7a52ebb96666899337baa326e121314c82171
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-30 23:10:11 +0000

    Update archive 3d

---
+++ Archive2/3d/a623118ceac5f7/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <functional>
+#include <iterator>
+
+using Work = std::vector<std::function<void()>>;
+void begin(Work& work) {
+    for(auto& f : work)
+        f();
+}// begins the work
+
+void test(Work work) {
+    begin(work); // dammit why don't it start the work
+}
+
+#include <iostream>
+
+int main() {
+    test({ []{std::cout << "Running\n";}, });
+    test({ []{std::cout << "Bye\n";}, });
+}
