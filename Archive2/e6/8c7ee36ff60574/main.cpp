562f2b386e0daa2929e38c581b6e91111f8b4e02
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-31 12:33:42 +0000

    Update archive e6

---
+++ Archive2/e6/8c7ee36ff60574/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <type_traits>
+
+using namespace std;
+
+class MyClass
+{};
+
+template <typename Object>
+class List
+{
+    
+private:
+
+template <bool B> class dispatcher {};
+using ObjectPtr   = dispatcher<true>;
+using ObjectValue = dispatcher<false>;
+
+void M_insert(const Object &p, ObjectPtr) { /* Object is a pointer */ std::cout << "inserting pointer\n";  }
+void M_insert(const Object &p, ObjectValue) { /* Object is not a pointer */ std::cout << "inserting non-pointer\n"; }
+
+public:
+
+    void insert(const Object & x)
+    {
+        M_insert(x, dispatcher<std::is_pointer<Object>::value> {});
+    }
+
+};
+
+int main()
+{
+     List<MyClass*> l1;
+     List<MyClass> l2;
+     
+     l1.insert( new MyClass{});
+     l2.insert( MyClass{} );
+     
+    
+}
