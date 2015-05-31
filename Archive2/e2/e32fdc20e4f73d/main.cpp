edda840309aab3f068c5ea4f5295808ad995d7e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-31 00:15:56 +0000

    Update archive e2

---
+++ Archive2/e2/e32fdc20e4f73d/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <memory>
+
+int main()
+{
+        struct ABCDEF {
+            int a;
+            double b;
+            int* c;
+            std::vector<int> e;
+            std::shared_ptr<float> f;
+        };
+
+        ABCDEF x{
+            0,{},nullptr,{ 1,2,3,4 },{}
+        };
+
+        ABCDEF y;
+        y.a = 0;
+        y.c = nullptr;
+        y.e = {1,2,3,4};
+        
+        ABCDEF z = { .a = 0, .c = nullptr, .e = { 1,2,3,4 } };
+        
+    return 0;
+}
