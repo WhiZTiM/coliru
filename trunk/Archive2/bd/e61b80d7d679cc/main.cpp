f2054edd09b0a49e0d6194aa2ea242c7468e2f9a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-31 18:40:13 +0000

    Update archive bd

---
+++ trunk/Archive2/bd/e61b80d7d679cc/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <time.h>
+
+template<class T>
+class Tuple
+{
+public:
+    T data;
+	Tuple()
+	{
+		data = rand() % 10 + 1;
+	}
+	~Tuple()
+	{
+
+	}
+	bool operator<(const Tuple &t) const
+	{
+		return data < t.data;
+	}
+};
+
+
+int main()
+{
+	Tuple<int> t[2];
+	srand(time(NULL));
+	for (int i = 0; i < 2; i++)
+		t[i] = Tuple<int>();
+
+	if (t[0] < t[1])
+		std::cout << "it smaller" << std::endl;
+
+	for (int i = 0; i < 2; i++)
+		std::cout << i << ": " << t[i].data << std::endl;
+}
