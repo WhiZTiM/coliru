d404ab6ad2fdbd827fb15823eb7209353178a876
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-31 23:54:39 +0000

    Update archive 8c

---
+++ trunk/Archive2/8c/828379a6f26e67/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct A {
+    void id() { cout << "A ";}
+};
+
+struct B {
+    void id() { cout << "B ";}
+};
+
+struct D {
+    void id() { cout << "D ";}
+};
+
+struct E {
+    void id() { cout << "E ";}
+};
+
+
+template<class A, class... As>
+struct C : public A, public As... {
+    void id(){
+        A::id();
+        using expander = int[];
+        (void) expander { 0, ((void) As::id(), 0)... };
+    }
+};
+
+int main(){
+    C<A, B, D, E> c;
+    c.id();
+    //expected: result A B 
+}
