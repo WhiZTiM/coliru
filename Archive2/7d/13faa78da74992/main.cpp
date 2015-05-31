dcbcd041d157df7b0360cebe43d2a42ddcfcc724
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-05-31 18:08:42 +0000

    Update archive 7d

---
+++ Archive2/7d/13faa78da74992/main.cpp
@@ -0,0 +1,30 @@
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <fstream>
+
+using namespace boost::archive;
+
+void save()
+{
+    std::ofstream file{"archive.txt"};
+    text_oarchive oa{file};
+    int i = 1;
+    oa << i;
+}
+
+void load()
+{
+    std::ifstream file{"archive.txt"};
+    text_iarchive ia{file};
+    int i = 0;
+    ia >> i;
+    std::cout << i << '\n';
+}
+
+int main()
+{
+    save();
+    load();
+
+    return 0;
+}
