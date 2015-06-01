fd4fa726d7ab270dad42ddcbf35392ad258e3d8a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-01 12:10:44 +0000

    Update archive e4

---
+++ Archive2/e4/654cef0fed8a22/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <boost/date_time/date.hpp>
+
+using namespace boost;
+using namespace std;
+
+boost::thread thread_obj;
+boost::thread thread_obj1;
+
+void func(void)
+{
+    char x;
+    cout << "enter y to interrupt" << endl;
+    cin >> x;
+    if (x == 'y') {
+        cout << "x = 'y'" << endl;
+        thread_obj.interrupt();
+        cout << "thread interrupt" << endl;
+    }
+}
+
+void real_main() {
+    boost::system_time const timeout = boost::get_system_time() + boost::posix_time::seconds(3);
+    try {
+        boost::this_thread::sleep(timeout);
+    }
+    catch (boost::thread_interrupted &) {
+        cout << "thread interrupted" << endl;
+    }
+}
+
+int main()
+{
+    thread_obj1 = boost::thread(&func);
+    thread_obj = boost::thread(&real_main);
+    thread_obj.join();
+}
