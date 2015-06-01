2e936f7fe1f6b60c665534fbcc0f7d5b34b9607d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-01 15:43:49 +0000

    Update archive 0d

---
+++ Archive2/0d/a6bb2805f89988/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    char triangolo; char rettangolo; char c;
+    printf("Inserire il nome della figura da trattare: ");
+    scanf("%c", &c);
+      
+}
