794cff344aef7dbce0c4964008d49946c9e0b306
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-01 13:54:23 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/b78f571d9aba50/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <std::size_t N>
+struct X
+{
+    friend auto foo(X<N>);
+};
+
+struct empty {};
+
+template <std::size_t N, typename T = empty>
+struct Y
+{
+    friend auto foo(X<N>) { return T{}; }
+};
+
+int main()
+{
+    Y<10, int> y;
+    foo(X<10>{});
+    return 0;
+}
