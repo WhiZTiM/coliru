ff1465286b8af4016324f6e7ee6451f9be2c5b97
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-01 09:48:23 +0000

    Update archive b4

---
+++ Archive2/b4/e8db974ed6d9e1/main.cpp
@@ -0,0 +1,10 @@
+#include <ios>
+#include <iostream>
+#include <istream>
+ 
+int main()
+{
+   int n = 3;
+   std::cout << "3 + 1 = " << -??-n  << std::endl;
+   std::cout << "(3 - 1) * (3 + 1) = " << ??--n * -??-n << std::endl;
+}
