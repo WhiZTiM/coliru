24e728e839ca4482cb8d262c0346b6a271a7e09e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 21:39:22 +0000

    Update archive 5c

---
+++ trunk/Archive2/5c/a1b63b15e1b2dc/main.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int i=0; 
+int d;
+
+void arrayadd(int A[5],int B[5], int R[5]);
+
+int main(void){
+    
+	int A[5];
+	int B[5];
+	int R[5];
+	
+	i=0;
+	d=0;
+	for (i=0;i<5;i++){
+		
+		printf("Enter the A[%d] value: ",i);
+		scanf("%d",&d);
+		A[i]=d;
+	}
+	i=0;
+	d=0;
+	for (i=0;i<5;i++){
+		printf("Enter the B[%d] value: ",i);
+		scanf("%d",&d);
+		B[i]=d;
+		
+	}
+	arrayadd(A,B,R);
+	printf("\n%d , %d, %d, %d, %d \n", R[0], R[1], R[2], R[3], R[4]);
+	
+	return 0;
+}
+
+void arrayadd(int A[5],int B[5], int R[5]){
+	i=0;
+	for(i=0;i<5;i++){
+		R[i]=A[i]+B[i];
+	}
+	
+}
