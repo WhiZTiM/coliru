69df00ac17cb9140f462a69e40eaa52b46063ea1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 12:45:45 +0000

    Update archive a6

---
+++ Archive2/a6/e11b72a78d52fc/main.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+#include <complex>
+template<class> struct IsComplex : std::false_type {};
+template<class T> struct IsComplex<std::complex<T>> : std::true_type {};
+
+/**
+ * Evaluates to a true type if the given complex type is an Array of Structs, false otherwise
+ * Defaults to false for Real values
+ */
+template< typename T, bool T_isComplex = IsComplex<T>::value >
+struct IsAoS;
+
+/**
+ * Undefined for (unknown) complex types
+ */
+template< typename T >
+struct IsAoS< T, false >: std::false_type{};
+
+template< typename T >
+struct IsAoS< std::complex<T> >: std::true_type{};
+
+
+int main() {
+    static_assert(!IsAoS<int>::value , "");
+    static_assert(IsAoS<std::complex<double>>::value , "");
+    return 0;
+}
