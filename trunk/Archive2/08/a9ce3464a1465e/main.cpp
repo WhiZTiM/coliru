3d1160d21d57c64284b37c8bdf79febfa9eb76f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 08:17:57 +0000

    Update archive 08

---
+++ trunk/Archive2/08/a9ce3464a1465e/main.cpp
@@ -0,0 +1,48 @@
+#include <boost/range/algorithm.hpp>
+#include <boost/range/numeric.hpp>
+#include <boost/timer/timer.hpp>
+#include <iterator>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+struct Complex
+{
+    double re = 0., im = 0.;
+};
+
+Complex operator+(Complex x, Complex y)
+{
+    return {x.re + y.re, x.im + y.im};
+}
+
+Complex operator*(Complex x, Complex y)
+{
+    return {x.re*y.re - x.im*y.im, x.re*y.im + x.im*y.re};
+}
+
+Complex random_complex()
+{
+    return {rand() / 1000. - 1000., rand() / 1000. - 1000.};
+}
+
+int main()
+{
+    constexpr auto N = 1u << 24;
+    vector<Complex> v, u;
+    v.reserve(N);
+    generate_n(back_inserter(v), N, random_complex);
+    u = v;
+    boost::random_shuffle(v);
+    boost::random_shuffle(u);
+
+    {
+        boost::timer::auto_cpu_timer t;
+        boost::transform(v, u, v.begin(), [](auto x, auto y) { return x*y; });
+    }
+
+    volatile auto anti_opti = boost::accumulate(u, Complex{});
+    (void)anti_opti;
+}
+
