1ba0236c365dc32dd7df5836a1cc9ffe122a0328
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 19:24:00 +0000

    Update archive dd

---
+++ Archive2/dd/7dea2ed4a35420/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <cstdio>
+
+using namespace std;
+
+map<unsigned long long int, unsigned long long int> a;
+
+unsigned long long int dp(unsigned long long int n)
+{   
+   if (a.find(n) == a.end())
+      a[n] = dp(n/2) + dp(n/3) + dp(n/4);
+
+   return a[n];
+}
+
+int main()
+{
+    for (unsigned long long int i = 1; i <= 24; i++) {
+        a[i] = i;
+        if (i == 12 || i == 24)
+           a[i] = i + 1;
+    }
+    
+    std::stringstream ss;
+    ss << "12 1 24 10000 1000000000";
+    
+    unsigned long long int n = 0;
+    cin >> n;
+
+    while (!feof(stdin)) {
+        printf("%llu\n", dp(n));
+        cin >> n;
+    }
+}
