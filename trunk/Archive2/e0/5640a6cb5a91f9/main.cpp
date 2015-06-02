52b430d605e6e2459908f50492accb72348d93c3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 15:22:25 +0000

    Update archive e0

---
+++ trunk/Archive2/e0/5640a6cb5a91f9/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << R"(<ORDER>
+<ITEM>
+<SN>132487A-J</SN>
+<NAME>crank casing</NAME>
+<Person age="12" passed="Yes">Alive</Person>
+<QTY>1</QTY>
+</ITEM>
+</ORDER>)";
+}
