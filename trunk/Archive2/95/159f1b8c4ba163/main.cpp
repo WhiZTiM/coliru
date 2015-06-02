2474ddf184a3ac4a8c7aeccae8bcb91c8de00df0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 23:07:52 +0000

    Update archive 95

---
+++ trunk/Archive2/95/159f1b8c4ba163/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string unsorted = "37980965";
+    
+    for(int i = 1; i < unsorted.size(); ++i)
+        for(int j = 0; j < i; ++j) {
+            auto a = unsorted[i];
+            auto b = unsorted[j];
+            (((a) >= (b)) || (((a) ^= (b)), ((b) ^= (a)), ((a) ^= (b))));
+            unsorted[i] = a ;
+            unsorted[j] = b ;
+        }
+        
+    std::cout << unsorted ;
+}
