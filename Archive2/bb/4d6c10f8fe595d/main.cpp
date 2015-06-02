6ab875f54dc8573bc958cf63b26e952f0431dfc4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-02 00:39:49 +0000

    Update archive bb

---
+++ Archive2/bb/4d6c10f8fe595d/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bitset>
+
+int main() {
+    int a = -58, b = a>>3, c = -315;
+
+    std::cout << "a = " << std::bitset<8>(a)  << std::endl;
+    std::cout << "b = " << std::bitset<8>(b)  << std::endl;
+    std::cout << "c = " << std::bitset<16>(c) << std::endl;
+}
