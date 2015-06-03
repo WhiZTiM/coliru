83abe18a13f497be73af29217e21ea9f7419b18f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-03 19:14:04 +0000

    Update archive 51

---
+++ trunk/Archive2/51/7770277221bdb8/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <vector>
+
+template<class T>
+class Point
+{
+};
+
+template<class T>
+class FeatureGeneric
+{
+};
+
+template<class T>
+class FeaturePoint : public FeatureGeneric<T>
+{
+};
+
+int main()
+{
+    FeaturePoint<Point,Point> fp;
+}
