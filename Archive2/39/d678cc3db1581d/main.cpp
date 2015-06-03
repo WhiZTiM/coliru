2de8eacef0167d692a8a3dd10cbe8aada0d2b204
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-03 16:27:32 +0000

    Update archive 39

---
+++ Archive2/39/d678cc3db1581d/main.cpp
@@ -0,0 +1,21 @@
+template<typename Arg>
+struct foo {
+    using type = typename foo<Arg*>::type;
+};
+
+template<typename Arg>
+int bar()
+{
+    return typename foo<Arg>::type {};
+}
+
+int main()
+{
+    return bar<int>();
+}
+
+// program is wrong: at this point of instantiation foo<T>::type will not terminate
+template int bar<int>();
+
+template<typename T>
+struct foo<T*> { using type = int; };
