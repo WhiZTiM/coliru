a7c2122fd97c21942b1ffd589f148b3fe218155d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-03 23:37:45 +0000

    Update archive 6c

---
+++ Archive2/6c/60e99ce2b09702/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+int main() {
+  std::stringstream ss("1 0 1 0");
+  std::vector<bool> vec;
+  std::copy(std::istream_iterator<bool>(ss), {}, std::back_inserter(vec));
+  for(int i(0), sz(vec.size()); i < sz; ++i) std::cout << vec[i] << " ";
+}
