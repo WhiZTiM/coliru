6126310ee3f5d383a46d06be59eddd67b10c85cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-03 01:58:24 +0000

    Update archive 9f

---
+++ trunk/Archive2/9f/5ee53ba1c08c33/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class FOO
+{
+public:
+  void operator() (string word)         // first: functor
+  {
+    cout << word << endl;
+  }
+
+  void m_function(string word)          // second: member-function
+  {
+    cout << word << endl;
+  }
+} FUNCTOR;
+
+
+void function(string word)              // third: non-member function
+{
+  cout << word << endl;
+}
+
+template<class T>
+void eval(T fun)
+{
+  fun("Using an external function");
+}
+
+int main()
+{
+    FUNCTOR("Normal call");
+    eval(FUNCTOR);
+
+    function("Normal call");
+    eval(function);
+
+    FUNCTOR.m_function("Normal call");
+    eval(std::bind(&FOO::m_function, FUNCTOR, std::placeholders::_1));
+    eval([](char const *c) { FUNCTOR.m_function(c); });
+}
