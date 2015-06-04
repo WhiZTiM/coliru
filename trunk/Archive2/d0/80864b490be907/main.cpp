a77af2b195db3a58cd73191b33f56bfc4cac51e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-04 12:44:47 +0000

    Update archive d0

---
+++ trunk/Archive2/d0/80864b490be907/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <functional>
+#include <map>
+
+namespace streaming {
+
+    template <typename T>
+    struct tag : std::reference_wrapper<T> {
+        using std::reference_wrapper<T>::reference_wrapper;
+    };
+
+    using tag_ostream = tag<std::ostream>;
+
+    template <typename T1, typename T2>
+    static inline tag_ostream operator<<(tag_ostream os, std::pair<T1, T2> const& p) {
+        os.get() << "std::pair{" << p.first << ", " << p.second  << "}";
+        return os;
+    }
+
+    template <typename Other>
+    static inline tag_ostream operator<<(tag_ostream os, Other const& o) {
+        os.get() << o;
+        return os;
+    }
+}
+
+int main() {
+    std::map<int, std::string> standback { { 42, "I'm gonna try" }, { 1729, "science" } };
+
+    streaming::tag_ostream cout = std::cout;
+
+    for (auto& entry : standback)
+        cout << entry << "\n";
+}
