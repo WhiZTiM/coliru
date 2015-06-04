1d97b9e432753973aa99a18fbbd394beb23489dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-04 12:15:21 +0000

    Update archive 89

---
+++ Archive2/89/08c6e61815b1e4/main.cpp
@@ -0,0 +1,42 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/visitors.hpp>
+#include <iterator>
+
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS>;
+using VertexPair = std::pair<Graph::vertex_descriptor, Graph::vertex_descriptor>;
+
+struct Visitor : boost::default_dfs_visitor {
+    using V = Graph::vertex_descriptor;
+
+    void discover_vertex(V v, const Graph& /*g*/) {
+        if (!root) {
+            root = v;
+        }
+        if (!done) std::cerr << v << "\n";
+    }
+
+    void finish_vertex(V v, const Graph& /*g*/) {
+        done |= (root == v);
+    }
+
+    // the TerminatorFunc
+    bool operator()(V, Graph const&) const { return done; }
+  private:
+    bool done = false;
+    boost::optional<V> root;
+};
+
+int main() {
+    // [N0, N1[N2, N3, N4[N5], N6] :
+    // VertexPair const data[] { {1, 2}, {1, 3}, {1, 4}, {4, 5}, };
+    VertexPair const data[] { {0, 1}, {0, 2}, {0, 3}, {2, 4}, };
+    Graph g(std::begin(data), std::end(data), 7);
+
+    Visitor vis_termfuc; // combines visitor and terminator functions
+    std::vector<boost::default_color_type> colormap(num_vertices(g));
+    boost::depth_first_visit(g, 2, vis_termfuc, colormap.data(), vis_termfuc);
+
+    //boost::write_graphviz(std::cout, g);
+}
