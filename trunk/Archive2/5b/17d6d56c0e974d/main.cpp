23f3c3113112bb53f6e271a7bdb7ae75a4c354c4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-04 21:44:32 +0000

    Update archive 5b

---
+++ trunk/Archive2/5b/17d6d56c0e974d/main.cpp
@@ -0,0 +1,32 @@
+#include <tuple>
+#include <iostream>  
+#include <ostream>
+using namespace std;
+
+template<class Type>
+struct haveChildren
+{   
+    const std::tuple<int, Type> myChildren;
+
+    template<typename... Args>
+    haveChildren(Args&&... args) : myChildren(5, Type{ std::forward<Args>(args)... }) {}
+
+    friend ostream& operator<< (ostream& streamReceiver, const haveChildren<Type>& streamSender)
+    {
+        int myInt;  int myChar;
+        std::tie(myInt, myChar) =   std::get<1>(streamSender.myChildren);
+
+        return streamReceiver << "My int is " << myInt << " my char is " << ((char)myChar);
+    }
+};
+
+struct haveChildrenCharAndInt : public haveChildren<std::tuple<char, int>>
+{
+    haveChildrenCharAndInt() : haveChildren(10,'x') {}
+};
+
+int main()
+{
+    cout << haveChildrenCharAndInt();
+    return 0;
+}
