13ac30f75a31041a55457cad71f776142e78136f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-04 11:32:42 +0000

    Update archive 9c

---
+++ trunk/Archive2/9c/4e6e5749535290/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct RapidDog {
+    
+};
+
+template<class  T>  void  enemyAdjust(T  objparm)
+{
+  //objparm.moveposition();
+  //objparm.fireweapon();
+} 
+
+template void enemyAdjust(RapidDog objparm);
+
+int main()
+{
+    RapidDog obj;
+    enemyAdjust(obj);
+}
+
