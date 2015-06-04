10befecce4723cbb2ac764bdbbe2211e0bb9f936
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-04 15:52:01 +0000

    Update archive 58

---
+++ trunk/Archive2/58/aa756c05aa671a/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <complex>
+ 
+int main()
+{
+    std::complex<double> z(1, 1);
+    std::cout << z << " cartesian is rho = " << std::abs(z)
+              << " theta = " << std::arg(z) << " polar\n";
+}
