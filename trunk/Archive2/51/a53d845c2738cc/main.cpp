2de376b7c1358edb80194f0fc07d1984557775f2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-05 15:24:19 +0000

    Update archive 51

---
+++ trunk/Archive2/51/a53d845c2738cc/main.cpp
@@ -0,0 +1,15 @@
+template<typename T> class get_false { public:
+    static constexpr bool val = false; 
+};
+
+template<typename T>
+class MyClass_2 {
+  static_assert(get_false<T>::val, "Failure");
+};
+
+template<typename T, typename U = MyClass_2<T>>
+class MyClass { U u; };
+
+int main() {
+    MyClass<bool> obj; // I suppose this isn't instantiated
+}
