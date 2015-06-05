669a765312bded46f1c093ba7b810d577e9e0abe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-05 15:06:41 +0000

    Update archive c6

---
+++ Archive2/c6/e061aabfa9dcf6/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+struct client { client( /* ... */ ) { /* ... */ }  /* ... */ };
+
+int main()
+{
+    std::vector< std::unique_ptr<client> > clients ;
+    for( int i = 0 ; i < 10 ; ++i ) clients.push_back( std::make_unique<client>( /* ... */ ) ) ;
+
+    for( int i = 0 ; i < 10 ; i += 2 ) clients[i] = nullptr ;
+    for( const auto& ptr : clients )
+        if(ptr) std::cout << ptr.get() << ' ' ; else std::cout << "<nullptr> " ;
+    
+    // erase-remove
+    clients.erase( std::remove( std::begin(clients), std::end(clients), nullptr ), std::end(clients) ) ;
+    
+    std::cout << "\n\nafter erase-remove: " ;
+    for( const auto& ptr : clients )
+        if(ptr) std::cout << ptr.get() << ' ' ; else std::cout << "nullptr " ;
+    std::cout << '\n' ;
+}
