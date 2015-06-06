9a83a6ce96ade0d4994317d457d88b67e32f8e8b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-06 14:47:31 +0000

    Update archive c7

---
+++ Archive2/c7/caee930c5bf18d/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+void float cal_average( float num1, float num2 , float num3 , float num4, float *ptr_suma, float *ptr_avg ); // function prototype for average 
+
+ 
+int main(void) {
+  
+ float a,b,c,d;
+ float suma, average; 
+
+ printf("Enter a 4 float number");
+ if (scanf(" %f %f %f %f",  &a,&b,&c,&d) !=4) 
+   {
+    Printf("There was an error, no 4 float number\n");
+    return 1;
+   }
+   
+  cal_average(a,b,c,d,&suma,&average);
+  
+  Printf("La suma de ,%2.f + %2.f + %2.f + %2.f es %2.f "
+       "  Y el average es %3.f", a,b,c,d,suma,average);
+  
+  
+}
+
+// el cuerpo de la funcion prototypo
+
+void float cal_average( float num1, float num2 , float num3 , float num4, float *ptr_suma, float *ptr_avg);
+{
+    
+   *ptr_suma = (num1 + num2 + num3 + num4);
+   *ptr_avg = *ptr_suma / 4.0;
+}
+
