11a7b400b1b646ca00252833bf6afb51e50ef609
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-06 17:41:57 +0000

    Update archive 35

---
+++ Archive2/35/a12848a1b7916f/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() 
+{
+    std::vector <double> x;
+    double f = 23.4305;
+    std::string f_str = std::to_string(f);
+    std::cout << f_str << '\n';
+}
