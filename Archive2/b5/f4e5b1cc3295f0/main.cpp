55e88eca3e51f090c02d863984c36061005f9d4d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-06 06:30:35 +0000

    Update archive b5

---
+++ Archive2/b5/f4e5b1cc3295f0/main.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+
+template <typename T, typename Comparer = std::greater<T>>
+class LinkedList
+{
+  //...
+  void insert(T& value)
+  {
+      Comparer comp;
+      //...
+      if ( comp(value, current_item) ) // tests if value > current_item
+  }
+  //...
+};
