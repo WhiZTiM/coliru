d47f8377810eea62c1d62f8ea0ba2d06f46942f7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-07 20:05:11 +0000

    Update archive f0

---
+++ trunk/Archive2/f0/54833cf62b605c/main.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+#include <vector>
+
+struct Foo
+{
+   int tag = 0;
+   std::function<void ()> code;
+};
+
+int main()
+{
+   std::vector<Foo> v;
+   v.push_back(Foo{ 0, [](){} });
+}
