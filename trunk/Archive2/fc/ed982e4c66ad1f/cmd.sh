986247b061046bb7a908f49f89fa4f0a15c963e8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-08 15:03:05 +0000

    Update archive fc

---
+++ trunk/Archive2/fc/ed982e4c66ad1f/cmd.sh
@@ -0,0 +1,1 @@
+g++  --std=c++14 -O0 -Wall -pthread main.cpp -pedantic -fno-elide-constructors  && ./a.out
---
+++ trunk/Archive2/fc/ed982e4c66ad1f/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <type_traits>
+
+template<typename T> struct check_{
+    
+   typedef char yes;
+   typedef char no[2];
+   
+   template<typename V>
+   static void help(V v);
+      
+   template<typename U>
+   static auto test(U *u) -> decltype(help(*u+1), yes());
+   
+   template<typename U>
+   static no& test(...);
+   
+   static const bool value = sizeof(test<T>(0)) == sizeof(yes);
+ };
+
+template <typename T>
+typename std::enable_if<check_<T>::value, T>::type 
+f(T t)
+{
+    std::cout << "satisfies" << std::endl;
+    return t;
+} 
+template <typename T>
+typename std::enable_if<!check_<T>::value, T>::type 
+f(T t )
+{
+    std::cout << "doesn't satisfy" << std::endl;
+    return t;
+}    
+
+struct A{
+     A(){};
+};
+
+int main()
+{
+    
+    A a;
+    f(a);
+    f(1);
+    
+}
