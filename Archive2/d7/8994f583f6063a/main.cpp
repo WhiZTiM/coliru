49d39decb5650d7fb21e03c945eda9bba34c95ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-08 21:50:24 +0000

    Update archive d7

---
+++ Archive2/d7/8994f583f6063a/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T, int I> class VecN {
+        //template<typename T, size_t J> friend class VecN;//Let's be friends with ourselves!
+    public:    
+    T vec[I];
+    
+    VecN(){//constructor
+        for (int i = 0; i<I; ++i)
+            vec[i] = T(0);
+    }
+    VecN(const T& x){//constructor
+        for (int i = 0; i<I; ++i)
+            vec[i] = T(x);
+    }
+    template<typename F> auto map(F f) -> VecN<decltype(f(vec[0])), I> {
+        VecN<decltype(f(0)), I> dst;
+        for (int i = 0; i < I; ++i)
+            dst[i] = f(vec[i]);
+        return dst;
+    }
+    T& operator[](size_t i) {//Used if VecN is an rvalue
+        return vec[i];
+    }
+};
+
+int main()
+{
+    VecN<VecN<double, 3>, 3> x(0);
+    x.map([](const VecN<double, 3> x){return VecN<double, 3>(1);});
+}
