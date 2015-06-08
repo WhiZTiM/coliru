20c5cf8f72f0f8b9551613d6fa9424b3228af6b0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-08 14:48:46 +0000

    Update archive cf

---
+++ trunk/Archive2/cf/3554a9fa3bb8de/main.cpp
@@ -0,0 +1,16 @@
+g(obj x) {
+    if (something(x)) {
+        return x; //base case
+    }
+    x.val += 1 //x[0] is of 
+    g(x.child) //x.child is of type "obj" 
+}
+
+g(obj x) {
+    while (true) {
+        if (something(x)) {
+            return x; // base case
+        }
+        x.val += 1
+    }
+}
