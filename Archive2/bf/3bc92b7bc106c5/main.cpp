8b9b9668412902eb48ceb8a000c098289d19c8cc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-08 15:15:17 +0000

    Update archive bf

---
+++ Archive2/bf/3bc92b7bc106c5/main.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <boost/bimap/bimap.hpp>
+#include <boost/bimap/set_of.hpp>
+#include <boost/bimap/unordered_set_of.hpp>
+#include <boost/bimap/tags/tagged.hpp>
+#include <iostream>
+#include <typeinfo>
+
+typedef double Address;
+
+namespace bm = boost::bimaps;
+
+template <class T>
+class Test {
+    private:
+        struct key_idx;
+        struct value_idx;
+        typedef bm::bimap<
+            bm::unordered_set_of<bm::tagged<T,       struct key_idx> >,
+            bm::set_of          <bm::tagged<Address, struct value_idx> >
+                > BidirectionalMap;
+
+        typedef typename BidirectionalMap::value_type Record;
+
+        BidirectionalMap _theBimap;
+
+    public:
+        void aFunction(const Address &anAddress, T const& parameters)
+        {
+            std::cout << __PRETTY_FUNCTION__ << "\n";
+            std::cout << typeid(key_idx*).name() << "\n";
+            std::cout << typeid(value_idx*).name() << "\n";
+            auto newRecord = _theBimap.insert(Record("TheGivenAdddress", anAddress));
+            auto byNumber  = _theBimap.template by<value_idx>().find(anAddress);
+            auto byAddress = _theBimap.template by<key_idx>().find(parameters);
+
+            (void) newRecord, (void) byAddress, (void) byNumber;
+        }
+};
+
+int main() {
+    Test<std::string> t;
+    t.aFunction(3.14, "hello");
+}
