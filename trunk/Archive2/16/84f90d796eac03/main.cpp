e503ef9c0e251324388432e39946e97dd6698028
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-08 13:48:03 +0000

    Update archive 16

---
+++ trunk/Archive2/16/84f90d796eac03/main.cpp
@@ -0,0 +1,17 @@
+g(obj x) {
+    if (something(x)) {
+        return x; //base case
+    }
+    x.val += 1 //x[0] is of 
+    g(x.child) //x.child is of type "obj" 
+}
+
+g(obj x) {
+    while (true) {
+        if (something(x)) {
+            return x; // base case
+        }
+        x.val += 1
+        x = g_helper(x.child);
+    }
+}
