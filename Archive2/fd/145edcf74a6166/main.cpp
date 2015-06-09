f4c7cc1752107b75e5b179a120e807e907e0a5d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-09 08:06:17 +0000

    Update archive fd

---
+++ Archive2/fd/145edcf74a6166/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+class Stack
+{
+    private:
+    T* items;int top=0, size=0;
+    public:
+    void Push(T items)
+    {
+        
+    }
+    T Pop()
+    {
+        return 0;
+    }
+};
+
+int main()
+{
+    Statck<int> s1;
+    Statck<string> s1;
+    
+    return 0;
+}
+
+
