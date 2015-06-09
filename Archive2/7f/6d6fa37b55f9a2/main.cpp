1859c838298d9f373da7c26dc6b5c3375d7db01e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-09 00:10:45 +0000

    Update archive 7f

---
+++ Archive2/7f/6d6fa37b55f9a2/main.cpp
@@ -0,0 +1,72 @@
+#define BOOST_NO_ASSERT
+
+#include <boost/coroutine/all.hpp>
+#include <exception>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace boost;
+
+template<unsigned N>
+struct coroutine_stack
+{
+    static constexpr auto size = N;
+    char buf[N];
+
+    auto allocator()
+    {
+        struct
+        {
+            void *stack_top;
+
+            void allocate(coroutines::stack_context &sctx, size_t s)
+            {
+                if(s > N) throw bad_alloc{};
+                sctx.size = N;
+                sctx.sp = stack_top;
+            }
+            void deallocate(coroutines::stack_context &) const {}
+        } res = {buf + N};
+        return res;
+    }
+};
+
+unsigned long counter = 0;
+
+int main() try
+{
+    struct Fiber
+    {
+        coroutine_stack<256> stack;
+        coroutines::symmetric_coroutine<void>::call_type coro;
+
+        Fiber()
+            :   coro{[](auto &yield)
+                {
+                    while(true)
+                    {
+                        ++counter;
+                        yield();
+                    }
+                }, coroutines::attributes(stack.size), stack.allocator()}
+        {}
+    };
+    
+    vector<Fiber> fibers(3.5e6);
+    cout << fibers.size() << " coroutines;"
+         << " allocated for stacks+coroutines: "
+         << sizeof(fibers.front()) * fibers.size() / (1u << 20)
+         << "MiB" << endl;
+
+    while(true)
+    {
+        for(auto &fiber : fibers)
+            fiber.coro();
+        cout << counter << endl;
+    }
+}
+catch(const std::exception &e)
+{
+    cout << e.what() << endl;
+}
