5ce17b93e4ddb6b4b0ea1fb686a840bc0417877d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-09 09:59:16 +0000

    Update archive aa

---
+++ Archive2/aa/73039c2d56901e/main.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iostream>
+int main()
+{
+    std::cout << std::fixed
+              << "2.4/0.4 = " << 2.4/0.4 << '\n'
+              << "floor(2.4/0.4) = " << std::floor(2.4/0.4) << '\n'
+              << "floor(6.0) = " << std::floor(6.0) << '\n';
+}
---
+++ Archive2/aa/7708b45285b139/main.cpp
@@ -0,0 +1,40 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <iostream>
+#include <sstream>
+
+using boost::property_tree::ptree;
+
+int main()
+{
+    ptree pt;
+    pt.add("TokenNo",       "Deb123456");
+    pt.add("CPUID",         "Deb123456");
+    pt.add("CommandID",     "05");
+    pt.add("CommandStatus", "null");
+    pt.add("IsEncrypted",   "0");
+
+    ptree cs;
+    cs.add("Add",           "97");
+    cs.add("EstbCD",        "99999999");
+    cs.add("EID",           "XY");
+    cs.add("CID",           "0154400000");
+    cs.add("DATE",          "14042015");
+    cs.add("TIME",          "1835");
+    cs.add("IOMODE",        "I");
+    cs.add("REASONCODE",    "55");
+    cs.add("LAT",           "87");
+
+#if 1 // if you are a sane person
+    pt.put_child("CommandString", cs);
+
+    write_json(std::cout, pt);
+#else
+    // or, if you really wanted braindead non-JSON:
+    std::ostringstream oss;
+    write_json(oss, cs);
+
+    pt.put("CommandString", oss.str());
+    write_json(std::cout, pt);
+#endif
+}
