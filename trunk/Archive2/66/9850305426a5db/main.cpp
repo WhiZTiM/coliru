cb2e922d98aee850f06999e6896c54f388a34871
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-09 15:36:07 +0000

    Update archive 66

---
+++ trunk/Archive2/66/9850305426a5db/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#define HAI do{std::cout << __PRETTY_FUNCTION__ << '\n'; }while(0)
+
+namespace EMPTY_detail {
+    template <class T>
+    void EMPTY(T &t, char(*)[sizeof &T::EMPTY]) {
+        t.EMPTY();
+    }
+    
+    template <class T>
+    void EMPTY(T &t, ...) {
+        (void)t;
+        HAI;
+    }
+}
+
+template <class T>
+void EMPTY(T &t) {
+    EMPTY_detail::EMPTY(t, NULL);
+}
+
+struct HasEmpty {
+    void EMPTY() { HAI; }
+};
+
+struct HasNoEmpty {
+    
+};
+
+int main() {
+    HasEmpty he;
+    HasNoEmpty hne;
+    int i;
+    EMPTY(he);
+    EMPTY(hne);
+    EMPTY(i);
+}
