c1935b09d25587cb145984cd2e9de3bbd16630ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-09 14:43:25 +0000

    Update archive aa

---
+++ trunk/Archive2/aa/d43b1ffcf7d7c3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T, int I> class VecN {
+    public:    
+    T vec[I];
+    
+    VecN(){//constructor
+        for (int i = 0; i<I; ++i)
+            vec[i] = T(0);
+    }
+    VecN(const T& x){//constructor
+        for (int i = 0; i<I; ++i)
+            vec[i] = T(x);
+    }
+    template<typename F> auto map(F f) -> VecN<decltype(f(*vec)), I> {
+        VecN<decltype(f(*vec)), I> dst;
+        for (int i = 0; i < I; ++i)
+            dst[i] = f(vec[i]);
+        return dst;
+    }
+    T& operator[](size_t i) {//Used if VecN is an rvalue
+        return vec[i];
+    }
+};
+
+int main()
+{
+    VecN<VecN<double, 3>, 3> x(0);
+    x.map([](const VecN<double, 3> x){return VecN<double, 2>(1);});
+}
