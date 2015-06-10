85425582864f8d4d5d021fa7eb9ddcd16bfd132b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-10 18:37:30 +0000

    Update archive dc

---
+++ Archive2/dc/97e1eb86fb05ed/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+class A
+{
+public:
+   class B{ public: ~B(){ std::cout << "B dtor" << '\n'; } };
+   A() { p = new B(); }
+   operator B*() { return p; }
+private:
+   B* p;
+};
+
+int main()
+{
+   A* a = new A();
+   delete *a;
+}
