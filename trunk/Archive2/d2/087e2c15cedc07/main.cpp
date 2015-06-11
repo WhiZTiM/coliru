3af07f64ac5876c3f7b9d53b72ffed4dc37b095a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-11 03:38:16 +0000

    Update archive d2

---
+++ trunk/Archive2/d2/087e2c15cedc07/main.cpp
@@ -0,0 +1,108 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+#if 1
+    #define log(x) std::cout << x << std::endl;
+#else
+    #define log(x)
+#endif
+
+template<typename type>
+std::vector<type> operator+(const std::vector<type> l, const std::vector<type> r){
+    std::vector<type> ans;
+    std::transform(l.begin(), l.end(), r.begin(), std::back_inserter(ans), std::plus<type>());
+    return ans;
+};
+template<typename type>
+std::vector<type> operator*(const std::vector<type> l, const std::vector<type> r){
+    std::vector<type> ans;
+    std::transform(l.begin(), l.end(), r.begin(), std::back_inserter(ans), std::multiplies<type>());
+    return ans;
+};
+template<typename type>
+std::vector<type> operator-(const std::vector<type> l, const std::vector<type> r){
+    std::vector<type> ans;
+    std::transform(l.begin(), l.end(), r.begin(), std::back_inserter(ans), std::minus<type>());
+    return ans;
+};
+template<typename type>
+std::vector<type> operator/(const std::vector<type> l, const std::vector<type> r){
+    std::vector<type> ans;
+    std::transform(l.begin(), l.end(), r.begin(), std::back_inserter(ans), std::divides<type>());
+    return ans;
+};
+
+template<class type> struct point{
+
+    point(std::initializer_list<type> coords) : coordinates(coords) {}
+
+    //template<typename a = type, typename... b> point(a coordinate, b... coordinates){
+    //    this->coordinates = {coordinate, coordinates...};
+    //};
+
+	point(point const &) = default;
+
+	std::vector<type> coordinates;
+
+    template<typename a = type> point(std::vector<a> const &coordinates){
+        this->coordinates = coordinates;
+    };
+    friend point<type> operator+(const point<type>& l, const point<type>& r){
+        point<type> ans(l.coordinates + r.coordinates);
+        return ans;
+    };
+    friend point<type> operator*(const point<type>& l, const point<type>& r){
+        point<type> ans(l.coordinates * r.coordinates);
+        return ans;
+    };
+    friend point<type> operator-(const point<type>& l, const point<type>& r){
+        point<type> ans(l.coordinates - r.coordinates);
+        return ans;
+    };
+    friend point<type> operator/(const point<type>& l, const point<type>& r){
+        point<type> ans(l.coordinates / r.coordinates);
+        return ans;
+    };
+
+	template <class T>
+	point &operator=(point<T> const &other) {
+		coordinates.resize(other.coordinates.size());
+		std::copy(other.coordinates.begin(), other.coordinates.end(), coordinates);
+		return *this;
+	}
+
+	template <class T>
+	point(point<T> const &other) : coordinates(other.coordinates.size()) {
+		std::copy(other.coordinates.begin(), other.coordinates.end(), coordinates.begin());
+	}
+
+    friend std::ostream& operator<<(std::ostream& stream, const point& p){
+        switch(p.coordinates.size()){
+        case 1:
+            std::cout << "(" << p.coordinates[0] << ")";
+            break;
+        default:    
+            std::cout << "("; 
+            for(int i = 0; i < p.coordinates.size(); ++i){
+                if(i == (p.coordinates.size() - 1))
+                    std::cout << p.coordinates[i];
+                else
+                    std::cout << p.coordinates[i] << ", ";
+            }
+            std::cout << ")";
+            break;
+        }
+		return stream;
+    }
+};
+
+int main(){
+	point<int> a({ 2,5,5,4,3 });
+	point<int> b({ 3,5,3,5,7 });
+    point<double> c(a/b);
+    log(c);
+    return 0;
+}
