949f5dd6526ce244d903bf48a946357ab6d7cc22
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-11 05:16:16 +0000

    Update archive 76

---
+++ Archive2/76/d2ee49b7368353/main.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <functional>
+#include <random>
+#include <string>
+#include <vector>
+
+template<typename T, size_t size>
+std::ostream& operator<<(std::ostream& os, const std::array<T, size>& arr) {
+    for (auto& el : arr) {
+        os << el << ' ';
+    }
+    return os;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    for (auto& el : vec) {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main() {
+    using IntType = int16_t;
+    
+    std::array<IntType, 20> E;
+    
+    std::random_device rd;
+    std::uniform_int_distribution<IntType> dist{
+        std::numeric_limits<IntType>::min(),
+        std::numeric_limits<IntType>::max()};
+    
+    std::generate(std::begin(E), std::end(E), std::bind(dist, std::ref(rd)));
+    
+    std::cout << "Array E: " << E << std::endl;
+    
+    
+    std::vector<IntType> F;
+    auto is_positive = [](auto a) { return a > 0; };
+    auto is_even     = [](auto a) { return !(a & 1); };
+    
+    std::copy_if(std::max_element(E.cbegin(), E.cend()), E.cend(),
+            std::copy_if(E.cbegin(), std::min_element(E.cbegin(), E.cend()),
+                    std::back_inserter(F), is_positive), is_even);
+        
+    std::cout << "Array F: " << F << std::endl;
+}
