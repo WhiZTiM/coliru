5abb1c07aa7429599ceba6937201a3bc15b6750b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-11 15:14:46 +0000

    Update archive 8e

---
+++ trunk/Archive2/8e/25487d59b98d59/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "boost/multi_array.hpp"
+
+using namespace std;
+using namespace boost;
+
+typedef multi_array<int, 3> array3i;
+
+class Test {
+private:
+    array3i test_array_;
+
+    void init(int const x, int const y, int const z)
+    {
+        //test_array_.resize(extents[x][y][z]);
+        test_array_ = array3i(extents[x][y][x]); 
+        cout << "Size should be: " << test_array_.size() << endl;
+        for (int j = 0; j < x; j++) {
+            for (int jj = 0; jj < y; jj++) {
+                for (int jjj = 0; jjj < z; jjj++) {
+                    test_array_[j][jj][jjj] = j + jj + jjj;
+                }
+            }
+        }
+    }
+
+public:
+    array3i test_array() {return test_array_;};
+
+    Test(int x, int y, int z) {
+        init(x, y, z);
+    };
+};
+
+int main(int argc, const char * argv[]) {
+
+    Test test(2,3,5);
+
+    cout << "Size from getter: " << test.test_array().size() << endl;
+
+    return 0;
+}
