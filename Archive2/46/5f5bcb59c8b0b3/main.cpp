4caf4e6a65fd157ffcc9b6ab2d20824c634a4a2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-11 00:21:56 +0000

    Update archive 46

---
+++ Archive2/46/5f5bcb59c8b0b3/main.cpp
@@ -0,0 +1,51 @@
+#include <boost/asio/coroutine.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace boost;
+
+#include <boost/asio/yield.hpp>
+
+#define await(wanted, future) \
+yield future([self = *this](auto x) mutable \
+{ \
+    auto copy = self; \
+    copy.wanted = x; \
+    copy(); \
+}) \
+/**/
+
+auto foo = [](auto continuation) // .then
+{
+    continuation(1);
+    continuation(2);
+    continuation(3);
+};
+
+auto bar = [](auto continuation) // .then
+{
+    continuation('a');
+    continuation('b');
+    continuation('c');
+};
+
+struct coroutine : asio::coroutine
+{
+    int x;
+    char y;
+
+    void operator()()
+    {
+        reenter(*this)
+        {
+            await(x, foo); // x = await foo;
+            await(y, bar); // y = await bar;
+            cout << x << " " << y << endl;
+        }
+    };
+};
+
+int main()
+{
+    coroutine{}();
+}
