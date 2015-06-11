ecc167ed04cb07c95914ec6800408b1b20f06d6c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-11 18:12:25 +0000

    Update archive 06

---
+++ Archive2/06/e64e9c31afe2f8/main.cpp
@@ -0,0 +1,13 @@
+#define var auto // 😏
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    var x = 4;
+    var y = 2;
+    var s = "The answer: ";
+    cout << s << x << y << endl;
+    return 0;
+}
