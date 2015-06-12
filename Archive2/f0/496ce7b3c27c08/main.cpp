9a00b47e58f384292db2b4f534edc402b1c0100b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 06:26:59 +0000

    Update archive f0

---
+++ Archive2/f0/496ce7b3c27c08/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+const std::string one( "one" );
+const std::string two = "two" ;
+const std::string three { "three" };
+const std::string four = { "four" };
+
+template < typename T > void print( const T& v ) { std::cout << v << ' ' ; }
+
+template < typename FIRST, typename... REST > void print( const FIRST& first, const REST&... rest )
+{
+    print(first) ;
+    print(rest...) ;
+}
+
+int main()
+{
+    // copy-elision is possible
+    for( const auto& str : { one, two, three, four } ) std::cout << str << ' ' ;
+    std::cout << '\n' ;
+
+    // explicitly avoid making copies of the strings (wrap the references)
+    for( const std::string& str : { std::cref(one), std::cref(two), std::cref(three), std::cref(four) } )
+        std::cout << str << ' ' ;
+    std::cout << '\n' ;
+
+    // loop at compile time (variadic template)
+    print( one, two, three, four, 12345, 67.89, '\n' ) ;
+}
