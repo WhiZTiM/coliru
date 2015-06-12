085e4dfd8baa6989bec5c7b8a3976c51424130b4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 14:57:03 +0000

    Update archive 7f

---
+++ Archive2/7f/18a3f2589544a6/main.cpp
@@ -0,0 +1,70 @@
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/make_shared.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+class AsyncHttpConnection : public boost::enable_shared_from_this<AsyncHttpConnection> {
+  public:
+    AsyncHttpConnection(boost::asio::io_service& io_service, std::string host, std::string path)
+        : resolver_(io_service), socket_(io_service), host_(host), path_(path)
+    {
+        // cannot use shared_from_this in constructor!
+    }
+
+    void start() {
+        tcp::resolver::query query(host_, "http");
+
+        resolver_.async_resolve(query, 
+                boost::bind(&AsyncHttpConnection::handle_resolve, 
+                    shared_from_this(), boost::asio::placeholders::error,
+                    boost::asio::placeholders::iterator));
+    }
+
+    ~AsyncHttpConnection() {
+        std::cout << "Destructor for AsyncHttpConnection\n";
+    }
+
+  private:
+    tcp::resolver resolver_;
+    tcp::socket socket_;
+    std::string host_;
+    std::string path_;
+    boost::asio::streambuf request_;
+    boost::asio::streambuf response_;
+
+    void handle_resolve(const boost::system::error_code& err, tcp::resolver::iterator /*endpoint_iterator*/)
+    {
+        if (!err) {
+            // code here
+        } else {
+            std::cout << err.message() << std::endl; // GOT "Operation Canceled" here
+        }
+    }
+};
+
+class AsyncHttpClient {
+  public:
+    AsyncHttpClient(boost::asio::io_service& io_service, std::string host) : host_(host)
+    {
+        io_service_ = &io_service; // store address of io_service
+    }
+
+    void async_call(std::string path) {
+        boost::make_shared<AsyncHttpConnection>(*io_service_, host_, path);
+    }
+
+  private:
+    std::string host_;
+    boost::asio::io_service* io_service_; // pointer, because io_service is uncopyable;
+};
+
+int main()
+{
+    boost::asio::io_service io_service;
+    AsyncHttpClient client(io_service, "www.boost.org");
+    client.async_call("/doc/libs/1_51_0/doc/html/boost_asio/example/http/client/async_client.cpp");
+    io_service.run();
+}
