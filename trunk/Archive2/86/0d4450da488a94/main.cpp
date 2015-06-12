906b2d8f20a91ecd6acb9221394ad1ae471c3198
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 11:57:06 +0000

    Update archive 86

---
+++ trunk/Archive2/86/0d4450da488a94/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/lexical_cast.hpp>
+
+namespace qi = boost::spirit::qi;
+
+int main()
+{
+    using It = std::string::const_iterator;
+    std::string const test("3");
+
+    It beg = test.begin(), end = test.end();
+
+    int result;
+    if (qi::phrase_parse(beg, end, qi::int_, qi::space, result))
+        std::cout << "Result is " << boost::lexical_cast<std::string>(result) << std::endl;
+}
