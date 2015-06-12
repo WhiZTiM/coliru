96a3f014fb9b230d4acd3cc1594c5f6f3959816d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 02:28:34 +0000

    Update archive 52

---
+++ trunk/Archive2/52/451127e54c8591/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct A
+{
+  A(int, int) {}
+
+  A(int p1 = 0, int p2 = 0, int p3 = 0) {}
+
+  // There are a lot more of constructors for A class 
+};
+
+struct B : public A
+{
+ // using A::A; // I want to inherit many base constructors in order to save work
+
+  // But I need this particular constructor and it should override the
+  // equivalent base constructor
+  B(int p1 = 0, int p2 = 0, int p3 = 0):A(p1,p2,p3) {std::cout<<"created B";}
+};
+
+int main(int argc, char *argv[])
+{
+  B b(1); // gives "error: call to constructor of 'B' is ambiguous", what
+      // of course is true, but I would want to inherit the other
+      // base constructors
+}
+
