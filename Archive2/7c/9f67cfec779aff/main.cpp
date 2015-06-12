80ca8baaed736581cc0f2261f3829d63a4bfb1fc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 09:32:38 +0000

    Update archive 7c

---
+++ Archive2/7c/9f67cfec779aff/main.cpp
@@ -0,0 +1,22 @@
+#include <range/v3/all.hpp>
+#include <vector>
+#include <tuple>
+#include <iostream>
+
+struct Example
+{
+    void sortByMarks(){
+        ranges::sort(ranges::view::zip(ids, marks), [](const auto& l, const auto& r) { return std::get<1>(l) < std::get<1>(r); });
+    }
+    
+    std::vector<int> ids = {1,2,3,4,5};
+    std::vector<int> marks = {47,44,88,52,46};
+};
+
+
+int main() {
+    Example e;
+    e.sortByMarks();
+    ranges::copy(e.ids, ranges::ostream_iterator<>(std::cout, "\t")); std::cout << '\n';
+    ranges::copy(e.marks, ranges::ostream_iterator<>(std::cout, "\t")); std::cout << '\n';
+}
