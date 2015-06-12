8571edf934000295dcfd3289457f6fa214292d27
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 15:53:01 +0000

    Update archive 9d

---
+++ Archive2/9d/065471c9836617/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+#include <type_traits>
+using namespace std;
+
+int main()
+{
+    int const  x = 50;
+    int const& y = x;
+    cout << std::is_const<std::remove_reference<int const>::type>::value << endl; // 1
+    cout << std::is_const<std::remove_reference<int const&>::type>::value << endl; // 0
+    
+    return 0;
+}
