913b9f2b3cf37f19c21543c7a29a8cd8cbafa729
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 19:27:45 +0000

    Update archive 87

---
+++ trunk/Archive2/87/3fca55f77cc52e/main.cpp
@@ -0,0 +1,19 @@
+#include <type_traits>
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct is_const_ref
+{
+     static constexpr int value = std::is_same<T, const T&>::value;
+};
+
+int main() 
+{
+int const  x = 50;
+int const& y = x;
+
+cout << is_const_ref<decltype(x)>::value << endl;
+cout << is_const_ref<decltype(y)>::value << endl;
+
+}
