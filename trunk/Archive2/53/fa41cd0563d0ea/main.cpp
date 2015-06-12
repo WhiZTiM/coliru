7c28370ccfb81f3cbf3f8a7095c52c68c51a7fac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 10:19:58 +0000

    Update archive 53

---
+++ trunk/Archive2/53/fa41cd0563d0ea/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template<typename... Ts>
+struct A
+{
+    template<typename T>
+    T do_something()
+    {
+        return T();
+    }
+};
+
+template<typename... Ts>
+struct B
+{
+    static void do_something()
+    {
+        A<int, int> a;
+        a.do_something<int>();
+    }
+};
+
+int main()
+{
+    B<int, int, int>::do_something();   
+}
