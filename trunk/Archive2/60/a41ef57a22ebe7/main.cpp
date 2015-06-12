410c39322c42908dd398064629110d0185167ca8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-12 21:22:31 +0000

    Update archive 60

---
+++ trunk/Archive2/60/a41ef57a22ebe7/main.cpp
@@ -0,0 +1,12 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+
+int main()
+{
+    std::chrono::steady_clock time_ = std::chrono::steady_clock::now();
+}
