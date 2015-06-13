2bb042a03822511bc6924e3e1ed54e0334c09ac8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-13 16:46:48 +0000

    Update archive 74

---
+++ trunk/Archive2/74/1ef53abc1bae3f/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+template<int M, class Pred>
+constexpr auto fun(Pred pred)
+{
+    return pred(1) <= M;
+}
+
+template<int M>
+struct C
+{
+        template<int N>
+        static constexpr auto pred(int x) noexcept
+        {
+            // simulate a constexpr lambda (not allowed in C++14)
+            struct lambda
+            {
+                    int n_, x_;
+                    
+                    constexpr auto operator()(int y) const noexcept
+                    {
+                            return n_ * x_ + y;
+                    }
+            };
+
+            return fun<M>(lambda{N, x});
+        }
+};
+
+int main()
+{
+    constexpr auto res = C<7>::template pred<2>(3);
+    std::cout << res; // prints 1, since 2 * 3 + 1 <= 7;
+}
