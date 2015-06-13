9ba3f70f42d8e82a1be416d8d69b767498117df8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-13 11:09:17 +0000

    Update archive b5

---
+++ trunk/Archive2/b5/57037696ac7170/main.cpp
@@ -0,0 +1,12 @@
+/*Solidras
+13/06/2015*/
+
+#include <iostream>
+ 
+int main() {
+  std::cout << "╔════╦════╦════╗" << std::endl;
+  std::cout << "║ 12 ║ 34 ║ 56 ║" << std::endl;
+  std::cout << "╠════╬════╬════╣" << std::endl;
+  std::cout << "║ ab ║ cd ║ ef ║" << std::endl;
+  std::cout << "╚════╩════╩════╝" << std::endl;
+}
