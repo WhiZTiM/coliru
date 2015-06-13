a61905c08fbf573c6e55f6ee83564b7d2d061b5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-13 11:56:38 +0000

    Update archive 08

---
+++ trunk/Archive2/08/8ab00efec19b73/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+using namespace std;
+
+
+template<typename F, typename... T>
+void for_each1(F fun, T&&... args)
+{
+    (fun (forward<T>(args)), ...);
+}
+
+template<typename F, typename... T>
+void for_each2(F fun, T&&... args)
+{
+    (..., fun (forward<T>(args)));
+}
+
+int main()
+{
+     for_each1([](auto i) { cout << i << " "; }, 4, 5, 6);
+     for_each2([](auto i) { cout << i << " "; }, 4, 5, 6);
+     error();
+}
