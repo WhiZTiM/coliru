e2d3c83c6af29ef23672cc88f0655912775dfe28
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-14 23:25:27 +0000

    Update archive b3

---
+++ Archive2/b3/e4ec97b5215b7b/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int sum = 0;
+    for (int i = 50; i <>
+int factorial(int x) {
+    return !x ? 1 : x * factorial(x – 1);
+}
+int main()
+{
+    int n; while( scanf("%d", &n) == 1)
+    printf("%d\n", factorial (n));
+} 
