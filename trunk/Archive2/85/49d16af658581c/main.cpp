18dbcbfdf5289e320093f103149606c98d9b268b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-15 15:48:20 +0000

    Update archive 85

---
+++ trunk/Archive2/85/49d16af658581c/main.cpp
@@ -0,0 +1,5 @@
+struct T1 {};
+int main()
+{
+    const T1 nd;    //  error: const class type with implicit ctor
+}
---
+++ trunk/Archive2/85/da72a5625ee7ac/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <functional>
+
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+
+
+
+int main()
+{
+  
+    
+    return 0;
+}
+
+
+
+
