849ed7b1047d73081f328875a5098a218e75e6e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 15:45:21 +0000

    Update archive 04

---
+++ trunk/Archive2/04/0321fa4edf87bd/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    
+    char highLeftCorner = 201;
+    char mysterious = 65;
+    std::cout << highLeftCorner << std::endl; // le caractère qui pose problème.
+    std::cout << mysterious << std::endl; // et celui qui n'en pose pas.
+    
+    return 0;
+
+}
