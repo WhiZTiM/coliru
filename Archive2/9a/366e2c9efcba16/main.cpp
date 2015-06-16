17d3b91e5c881e1b9753f88b53cde277f2b437eb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 14:39:04 +0000

    Update archive 9a

---
+++ Archive2/9a/366e2c9efcba16/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+void f(char const *const p) {
+ std::cout << "hello" << std::endl;   
+}
+int f(char const *p) {
+ std::cout << "world" << std::endl;   
+ return 0;
+}
+
+int main()
+{
+    
+}
