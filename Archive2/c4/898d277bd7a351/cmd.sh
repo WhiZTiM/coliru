a4c4d62d7a84b15c0fbdd8aae4aea2953bfacbe8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 22:45:39 +0000

    Update archive c4

---
+++ Archive2/c4/898d277bd7a351/cmd.sh
@@ -0,0 +1,2 @@
+printf "WITHOUT STRICT ALIASING:\n"  ; g++ -std=c++14 -O2 main.cpp -fno-strict-aliasing && ./a.out
+printf "\n\nWITH STRICT ALIASING:\n" ; g++ -std=c++14 -O2 main.cpp                      && ./a.out
