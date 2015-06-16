e4ba5c6c97917dc3b5a1a655fab28012ae84cf7a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 00:44:40 +0000

    Update archive 6a

---
+++ Archive2/6a/66b8f2cefce6fc/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+
+struct InputIter {
+    typedef input_iterator_tag iterator_category;
+};
+
+struct OutputIter {
+    typedef output_iterator_tag iterator_category;
+};
+
+template<class T>
+class Vector {
+public:
+    explicit Vector(size_t, T=T());
+
+    template <class InputIt,
+              typename = std::enable_if_t<std::is_same<input_iterator_tag, typename InputIt::iterator_category>{}>>
+    Vector(InputIt, InputIt)
+    {
+        std::cout << "input iterator overload called" << std::endl;
+    }
+};
+
+template<class T>
+Vector<T>::Vector(size_t, T)
+{
+    std::cout << "size_t overload called" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    InputIter input;
+    OutputIter output;
+
+    Vector<int> a(0U, 0);
+    Vector<int> b(input, input);
+    // Should fail to compile:
+    Vector<int> c(output, output);
+}
