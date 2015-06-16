36ceabd0e72049b0bc0eeb175871026918bf5e24
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 18:10:14 +0000

    Update archive c0

---
+++ Archive2/c0/ef3d0b82583213/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+
+int main(int argc, char ** argv)
+{
+    int i;
+    char *str = "12a3";
+    std::stringstream ss;
+    ss << str;
+    ss >> i;
+    std::cout << !ss << std::endl;
+}
