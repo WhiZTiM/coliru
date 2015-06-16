6a921590a11c99214313b4c8344d9de3ddf3dccc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 17:52:24 +0000

    Update archive b4

---
+++ trunk/Archive2/b4/57b0d56244975a/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+struct Struct {
+    Struct() { std::cout << "empty" << '\n'; }
+    Struct(const Struct& s) { std::cout << "copy" << '\n'; }
+    Struct(Struct&& s) noexcept { std::cout << "move" << '\n'; }
+    ~Struct() { std::cout << "destructor" << '\n'; }
+};
+
+std::vector<Struct> vector;
+
+void func() {
+    std::cout << "init structs:" << '\n';
+    Struct s1, s2, s3;
+    std::cout << '\n';
+    
+    //vector.reserve(2);
+    
+    std::cout << "push_back copy:" << '\n';
+    vector.push_back(s1);
+    std::cout << '\n';
+    
+    std::cout << "push_back move:" << '\n';
+    vector.push_back(std::move(s2));
+    std::cout << '\n';
+    
+    std::cout << "push_back move:" << '\n';
+    vector.push_back(std::move(s3));
+    std::cout << '\n';
+    
+    std::cout << "push_back move:" << '\n';
+    vector.push_back(std::move(s3));
+    std::cout << '\n';
+    
+    std::cout << "push_back move:" << '\n';
+    vector.push_back(std::move(s3));
+    std::cout << '\n';
+}
+
+int main()
+{
+    func();
+}
