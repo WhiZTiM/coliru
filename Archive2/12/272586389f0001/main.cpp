e0136a768103e8a3bd4a8278cdc6a940cf09c6c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-16 14:42:14 +0000

    Update archive 12

---
+++ Archive2/12/272586389f0001/main.cpp
@@ -0,0 +1,14 @@
+// Modifier le code suivant pour afficher les messages demandés
+
+#include <iostream>
+
+int main() {
+    
+    char highLeftCorner = 201;
+    char mysterious = 65;
+    std::cout << highLeftCorner << std::endl;
+    std::cout << mysterious << std::endl;
+    
+    return 0;
+
+}
