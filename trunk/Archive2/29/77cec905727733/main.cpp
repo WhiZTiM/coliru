c3daa29aad87332e351bf635fdfa29a811323b0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-17 22:21:49 +0000

    Update archive 29

---
+++ trunk/Archive2/29/77cec905727733/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <tuple>
+#include <array>#include <iostream>
+#include <array>
+#include <tuple>
+#include <chrono> 
+
+typedef std::chrono::high_resolution_clock clk;
+typedef std::chrono::milliseconds ms;
+
+int main() {
+    clk c;
+    
+    auto t1 = c.now();
+    
+    std::array<
+      std::array<
+        std::array<std::tuple<int,int>,
+        25>,
+      25>,
+    25> mat3;
+    
+    for (size_t i = 0; i < 25; ++i) {
+      for (size_t j = 0; j < 25; ++j) {
+        for (size_t k = 0; k < 25; ++k) {
+          mat3[i][j][k] = std::make_tuple(i*25*25+j*25+k, 10*(i*25*25+j*25+k));
+        }
+      }
+    }
+    
+    for (size_t i = 0; i < 25; ++i) {
+      for (size_t j = 0; j < 25; ++j) {
+        for (size_t k = 0; k < 25; ++k) {
+          std::cout << '(' << std::get<0>(mat3[i][j][k]) << ','
+          << std::get<1>(mat3[i][j][k]) << ')' << '\n';
+        }
+      }
+    }
+      
+    auto t2 = c.now();
+    
+    ms time_span = std::chrono::duration_cast<ms>(t2 - t1);
+    std::cout << "In milliseconds: " << time_span.count() << std::endl;
+    return 0;
+}
