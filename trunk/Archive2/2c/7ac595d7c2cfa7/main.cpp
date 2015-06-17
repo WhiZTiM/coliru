54f0ec86b4cd3eadb070f880693d1e771d9b4649
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-17 03:04:44 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/7ac595d7c2cfa7/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+int main() {
+    int count; // how many numbers are there
+    std::cin >> count;
+    std::string buffer;
+    
+    //two vectors to store output
+    std::vector<long int> input;
+    std::vector<long int> output;
+    
+    //take the inputs
+    for(int i = 0; i < count; ++i) {
+        std::cin >> buffer;
+        input.push_back(std::stoi(buffer));
+        buffer.clear();
+    }
+    
+    //check that we have the inputs
+    for(auto it : input) {
+        std::cout << it << std::endl;
+    }
+    
+    //lambda to test for palindromes
+    auto is_palindrome = [](long int n) {
+        auto str = std::to_string(n);
+        std::stack<char> stack;
+        
+        //Load each character into the stack
+        for(auto it : str) {
+           stack.push(it);
+        }
+        
+        //Use the property of a stack to take out in a reverse order
+        for(size_t i = 0; !stack.empty(); stack.pop()) {
+            if (stack.top() != str[i])
+                return false;
+            else    
+                ++i;
+        }
+        
+        return true;
+    };   
+    
+    //test for the palindromes; iterate
+    for(auto it : input) {
+        int n;
+        for (n = it+1; ; ++n) {
+            if(is_palindrome(n))
+                break;
+            else
+                continue;
+        }
+        output.push_back(n);
+    }
+    
+    //output the outputs
+    for(auto it : output) {
+        std::cout << "next palindrome: " << it << '\n';   
+    }
+    return 0;
+}
