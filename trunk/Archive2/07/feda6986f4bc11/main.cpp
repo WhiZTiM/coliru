da367579a1585147d52f4095d2f870ba74699e48
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-17 13:01:56 +0000

    Update archive 07

---
+++ trunk/Archive2/07/feda6986f4bc11/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace ajlb {
+    
+template<typename T>
+struct metafunc {
+    enum { Result = 1 };
+};
+
+}
+
+namespace plop {
+    struct s {};
+    
+    template<>
+    struct metafunc<s> {
+        enum { Result = 2 };
+    };
+}
+
+int main() {
+    std::cout << ajlb::metafunc<plop::s>::Result << std::endl;
+}
