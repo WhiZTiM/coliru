d66514205359dbe2fed578e9447b70b559121556
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-17 01:03:59 +0000

    Update archive 19

---
+++ trunk/Archive2/19/0fd726c23bedd9/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+bool expect(std::istream& is, char match) {
+    char input;
+    return (is >> input && input == match);
+}
+
+int main() {
+    double degrees = 0;
+    double minutes = 0;
+    double seconds = 0;
+    
+    if (expect(std::cin, '(') && (std::cin >> degrees)
+        && expect(std::cin, ',') && (std::cin >> minutes) 
+        && expect(std::cin, ',') && (std::cin >> seconds) 
+        && expect(std::cin, ')')) {
+        std::cout << "Angle: " << degrees << "d " << minutes << "\' " << seconds << "\" " << "\n";        
+    } else {
+        std::cout << "ERROR!\n";   
+        return 1;
+    }
+    return 0;
+
+}
