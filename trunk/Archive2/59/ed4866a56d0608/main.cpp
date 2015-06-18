87fbfdf8cbb05f45f5cd89b914c879cfb5820db8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-18 05:24:59 +0000

    Update archive 59

---
+++ trunk/Archive2/59/ed4866a56d0608/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cerrno>
+#include <cstring>
+#include <cfenv>
+#pragma STDC FENV_ACCESS ON
+int main()
+{
+    std::cout << "log(1) = " << std::log(1) << '\n'
+              << "base-5 logarithm of 125 = " << std::log(125)/std::log(5) << '\n';
+    // special values
+    std::cout << "log(1) = " << std::log(1) << '\n'
+              << "log(+Inf) = " << std::log(INFINITY) << '\n';
+    // error handling 
+    errno=0; std::feclearexcept(FE_ALL_EXCEPT);
+    std::cout << "log(0) = " << std::log(0) << '\n';
+    if(errno == ERANGE)
+        std::cout << "    errno == ERANGE: " << std::strerror(errno) << '\n';
+    if(std::fetestexcept(FE_DIVBYZERO))
+        std::cout << "    FE_DIVBYZERO raised\n";
+}
