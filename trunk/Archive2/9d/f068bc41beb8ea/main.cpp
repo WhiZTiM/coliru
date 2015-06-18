889b7a321647057ec53945a814077eb383cc8269
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-18 07:01:24 +0000

    Update archive 9d

---
+++ trunk/Archive2/9d/f068bc41beb8ea/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <functional>
+#include <typeinfo>
+#include <vector>
+
+int add(int a, int b) {return a + b;}
+
+
+
+int main() {
+    
+    std::vector<std::function<int(int)>> funcs;
+    
+    for (int i = 0; i < 5; ++i) {
+        auto f = [i](int j) {return(i + j);};
+        funcs.push_back(f);
+    }
+    
+   
+    int i = 0;
+    for (int k = 0; k > -10; k = k - 2) {
+        std::cout << funcs[i](k) << std::endl;
+        ++i;
+    }
+    
+    return 0;   
+}
