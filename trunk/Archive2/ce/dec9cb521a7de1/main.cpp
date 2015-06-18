05a27efb4ff6c81d0c4a473d965f21ef28b4db29
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-18 02:03:24 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/dec9cb521a7de1/main.cpp
@@ -0,0 +1,39 @@
+// g++ -O3 question.cpp -o question.exe -I. --std=c++11 -lprotobuf-lite -lpthread -lz -losmpbf
+#include <iostream>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/labeled_graph.hpp>
+#include <boost/graph/iteration_macros.hpp>
+
+struct MyVertex {
+    long long label;
+
+    MyVertex(long long label = 0) : label(label) {}
+};
+
+
+typedef boost::adjacency_list<boost::listS,          // Store out-edges of each vertex in a std::list
+                              boost::listS,          // Store vertex set in a std::list
+                              boost::bidirectionalS, // The file dependency graph is directed
+                              MyVertex,              // vertex properties
+                              boost::no_property     // edge properties
+                              > AdjGraph;
+
+int main() {
+    AdjGraph g;
+
+    std::map<size_t, AdjGraph::vertex_descriptor> vertex_map;
+    
+    for (int i = 1; i<6; ++i)
+        vertex_map.emplace(10*i, add_vertex(10*i, g));
+
+    boost::add_edge(vertex_map[10], vertex_map[30], g);
+    boost::add_edge(vertex_map[10], vertex_map[50], g);
+    boost::add_edge(vertex_map[30], vertex_map[40], g);
+    boost::add_edge(vertex_map[50], vertex_map[20], g);
+
+    BGL_FORALL_EDGES(e, g, AdjGraph) {
+        auto source_n = boost::source(e, g);
+        std::cerr << g[source_n].label << "\n";
+    }
+}
