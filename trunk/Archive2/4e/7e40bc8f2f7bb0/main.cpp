bb95113de3d2e986022d298dca05cdccf1de0c82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-18 16:25:59 +0000

    Update archive 4e

---
+++ trunk/Archive2/4e/7e40bc8f2f7bb0/main.cpp
@@ -0,0 +1,18 @@
+#include <boost/range/adaptors.hpp>
+#include <iostream>
+
+int main()
+{
+    using namespace boost::adaptors;
+
+    std::vector<int> input = {10, 20, 30, 40, 50};
+    for (auto element : input | indexed(0)) {
+        element.value() = 100 / (1+element.index());
+    }
+
+    for (int i : input) {
+        std::cout << i << ' ';
+    }
+
+    return 0;
+}
