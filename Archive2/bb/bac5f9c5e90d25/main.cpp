c0abb25b5901bdb5eb397998ecb9b5ddbb58b580
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-19 03:41:42 +0000

    Update archive bb

---
+++ Archive2/bb/bac5f9c5e90d25/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+enum counterSet{
+    FIRST,
+    SECOND
+};
+
+namespace detail{
+    template <counterSet Set_>
+    class count{
+    public:
+        template <typename Incremented_>
+        static size_t increment(){
+            static size_t c = counter++;
+            return c;
+        }
+    private:
+        static size_t counter;
+    };
+    template <counterSet Set_>
+    size_t count<Set_>::counter = 0;
+}
+
+template <counterSet Set_, typename Member_>
+class counter{
+public:
+    template <typename Incremented_>
+    size_t incrementCounter(){return detail::count<Set_>::template increment<Incremented_>();}
+};
+
+template <counterSet Set_>
+class Foo : public counter<Set_, Foo<Set_>> {
+};
+
+int main()
+{
+    Foo<FIRST>
+        foo1;
+    Foo<SECOND>
+        foo2;
+        
+    foo1.incrementCounter<double>();
+    std::cout 
+        << foo1.incrementCounter<char>() << '\n'
+        << foo1.incrementCounter<float>() << '\n'
+        << foo1.incrementCounter<char>() << '\n'
+        << foo2.incrementCounter<int>() << '\n';
+
+}
