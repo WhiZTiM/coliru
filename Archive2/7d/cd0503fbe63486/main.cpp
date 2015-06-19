816a6dd404e157811ae71481de003984cbcc7087
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-19 14:22:29 +0000

    Update archive 7d

---
+++ Archive2/7d/cd0503fbe63486/main.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+
+enum TStatus {  ok,sold,defect  };
+
+class check {
+public:
+    //enum TStatus {  ok,sold,defect  };
+
+public:
+
+    check();
+    ~check();
+    TStatus getStatus();
+
+};
+
+
+
+
+    check::check() { }
+
+    TStatus check::getStatus()
+    {
+    //......
+      return TStatus::ok;
+    }
+
+    check::~check() { }
+
+
+int main() {}
+    
