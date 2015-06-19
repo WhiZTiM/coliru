f6ca82fbaa228ceac923e165cd902fc91af18db8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-19 12:19:28 +0000

    Update archive 14

---
+++ trunk/Archive2/14/dade63e766fb5a/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    virtual void f(){
+		cout << 'a' << endl;
+	}
+
+	virtual void h(){
+		cout << 'A' << endl;
+	}
+};
+
+class S1 : virtual public A
+{
+public:
+	virtual void f(){
+		cout << "s1" << endl;
+	}
+};
+
+class S2: virtual public A
+{
+public:
+	void h(){
+		cout << "s2" << endl;
+	}
+};
+
+class GS: public S1, public S2
+{
+};
+
+int main(){
+	/*GS gs;
+	gs.f();*/
+
+	S1 *q = new GS;
+	q->h();
+	return 1;
+}
