210551711e8fe40f60bbac2a339aa6c555697501
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-19 18:38:44 +0000

    Update archive bd

---
+++ trunk/Archive2/bd/2ec179f09bc35b/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct true_type
+{
+    static constexpr bool value = true;
+};
+
+struct false_type
+{
+	static constexpr bool value = false;
+};
+
+template <typename T> struct is_integral : false_type {};
+template <> struct is_integral<int> : true_type {};
+template <> struct is_integral<short> : true_type {};
+template <> struct is_integral<long> : true_type {};
+
+template <typename T>
+struct A
+{
+    static_assert(is_integral<T>::value, "Typ T musi byc typem calkowitoliczbowym!");
+};
+
+int main()
+{
+    A<int> a; // OK
+    A<double> b; // Błąd!
+}
