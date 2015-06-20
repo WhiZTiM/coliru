23243c06bf50f02bbcb4e2d6feb93ae671385dc6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-20 17:47:28 +0000

    Update archive da

---
+++ trunk/Archive2/da/3781973dc38e23/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <funkcjonalna>
+ 
+int main ( ) 
+{ 
+    std:: function < int ( ) > f = nullptr ; 
+    try  { 
+        f ( ) ; 
+    }  catch ( const std :: bad_function_call & e )  { 
+        std:: cout  << e. what ( )  <<  ' \n ' ; 
+    } 
+}
