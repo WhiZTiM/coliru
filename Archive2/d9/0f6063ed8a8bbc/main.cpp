8f8d84b55e69fdfbeac1b79c1570339e271216d9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-20 12:13:43 +0000

    Update archive d9

---
+++ Archive2/d9/0f6063ed8a8bbc/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <type_traits>
+
+template<typename Head, typename... Tail>
+struct Is_Admitted {
+	constexpr static bool value = Is_Admitted<Head>::value && Is_Admitted<Tail...>::value;
+};
+
+template<>
+template<typename T>
+struct Is_Admitted<T> : public std::false_type{};
+
+template<> struct Is_Admitted<int> : public std::true_type{};
+template<> struct Is_Admitted<double> : public std::true_type{};
+
+int main()
+{
+    std::cout << Is_Admitted<int, double>::value << '\n';
+    std::cout << Is_Admitted<int, char>::value << '\n';
+}
