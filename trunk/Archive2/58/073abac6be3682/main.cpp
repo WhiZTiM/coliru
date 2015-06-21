6def45278d607a35c19f1c5a5ac2c292a1deb5d0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-21 15:41:19 +0000

    Update archive 58

---
+++ trunk/Archive2/58/073abac6be3682/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <range/v3/all.hpp>
+
+using namespace ranges;
+
+int main() {
+    std::vector<std::string> positions = {"1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c"};
+    std::vector<std::string> normals = {"1d", "1e", "1f", "2d", "2e", "2f", "3d", "3e", "3f"};
+    std::vector<std::string> interpolated = view::zip_with(view::join, positions | view::chunk(3), normals | view::chunk(3));
+    for(auto s : interpolated) {
+        std::cout << s << " ";
+    }
+    //Expected output is
+    // 1a 1b 1c 1d 1e 1f 2a 2b 2c 2d 2e ... and so forth
+    return 0;
+}
