0cdbd43ab21ba483e92aa0339d355011ef4a50c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-21 12:12:55 +0000

    Update archive be

---
+++ trunk/Archive2/be/f8a7e6fd427de9/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+int main(){
+
+    char num[10];
+    fstream file;
+    file.open("text.txt", ios::trunc|ios::in|ios::out| ios::binary);
+    if (!file)
+    {
+        cerr << "File could not be opened" << endl;
+        exit(1);
+    } // End if
+
+    for (int i = 0; i <= 5; i++){
+        cout << "Enter an integer " << endl;
+        cin >> num[i];        //Input a number
+    }
+    file.write((char*)num, sizeof(num));  //Function write to write data to file
+
+    memset(num, 0, sizeof(num));
+    file.seekg(0);
+    file.read((char*)num, sizeof(num));  //Function to read data from the file
+
+    for (int i = 0; i <= 5; i++){
+        cout << endl << num[i] << ", ";
+    }
+    file.close();
+  //  system("pause");
+}
