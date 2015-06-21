287060d8fb07b2af61eb3e3417e4a8efddabbdf3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-21 21:08:08 +0000

    Update archive b1

---
+++ Archive2/b1/9d306618a1c7d4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <boost/regex.hpp>
+using namespace std;
+using namespace boost;
+int main() {
+    regex rule("(?<test>\\d+)");
+    string str = "11.22.33.44";
+    boost::smatch result;
+    string::const_iterator start = str.begin();
+    string::const_iterator end = str.end();
+    while (regex_search(start, end, result, rule)) {
+        std::cout << result["test"].str() << std::endl; // here
+        start = result["test"].second;                  // it seems it work
+    }
+}
