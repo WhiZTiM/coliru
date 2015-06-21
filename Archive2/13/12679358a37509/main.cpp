340225b7d9bd362b1db80302cde10152996da016
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-21 02:46:24 +0000

    Update archive 13

---
+++ Archive2/13/12679358a37509/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+int main() {
+int i,n;
+printf("Digite o número de vezes que você AMA a Vitória Machado de Jesus: ");
+scanf("%d", &n);
+for(i=0;i<n;i++){
+    printf("\nEu te amo desde o %dº dia até o último dia da minha vida!", i);
+    
+}
+    
+}
