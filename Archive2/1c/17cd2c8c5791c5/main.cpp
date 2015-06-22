e509f585498b92fdac980109c8dccae28a6b9557
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-22 03:54:48 +0000

    Update archive 1c

---
+++ Archive2/1c/17cd2c8c5791c5/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+ 
+bool mycomp(int i, int j)
+{
+    return (((i < 0) && (j > 0)) || ((i > 0) && (j < 0)));
+}
+ 
+int main()
+{
+
+std::cout << std::boolalpha;
+
+// Not antisymetric
+std::cout << mycomp(2, -3) << '\n';
+std::cout << mycomp(-3, 2) << '\n';
+
+// Not transitive
+std::cout << mycomp(-2, -2) << '\n';
+std::cout << mycomp(-3, 2) << '\n';
+std::cout << mycomp(2, 2) << '\n';
+
+}
