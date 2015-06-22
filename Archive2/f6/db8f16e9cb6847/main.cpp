b86fdec4823eb2a52f8c5ad443ab5ef3d6b21303
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-22 13:06:10 +0000

    Update archive f6

---
+++ Archive2/f6/db8f16e9cb6847/main.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <limits>
+#include <functional>
+#include <random>
+#include <string>
+
+template<typename T, size_t size>
+std::ostream& operator<<(std::ostream& os, const std::array<T, size>& arr) {
+    for (auto& el : arr) {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main() {
+    using IntType = int16_t;
+    
+    std::array<IntType, 20> a;
+    
+    std::random_device rd;
+    std::uniform_int_distribution<IntType> dist{1, 50};
+    
+    std::generate(std::begin(a), std::end(a), std::bind(dist, std::ref(rd)));
+    
+    std::cout << a << std::endl;
+}
