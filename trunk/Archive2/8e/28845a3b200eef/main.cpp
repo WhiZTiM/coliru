9ca37cfe0aec5c32b258331ad202ea9408cdd586
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-22 15:21:15 +0000

    Update archive 8e

---
+++ trunk/Archive2/8e/28845a3b200eef/main.cpp
@@ -0,0 +1,15 @@
+#include <range/v3/all.hpp>
+#include <iostream>
+#include <vector>
+
+int main() 
+{
+    std::vector<float> rng = {1.2,-1.2,2.3,-2.3};
+
+    auto even_indices = 
+        ranges::view::iota(0ul, rng.size()) | 
+        ranges::view::filter([](auto i) { return !(i & 1); })
+    ;
+    auto min_ind = *ranges::min_element(even_indices, [&rng](auto L, auto R) { return rng[L] < rng[R]; });
+    std::cout << rng[min_ind];
+}
