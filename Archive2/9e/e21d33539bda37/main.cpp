38be9b57efdbd8e260339f2f37885d3daba45f4f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-22 21:54:20 +0000

    Update archive 9e

---
+++ Archive2/9e/e21d33539bda37/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+//Base data structure to encapsulate only data.
+struct data {
+    data(int i) : i(i) {}
+    
+    int i;   
+};
+
+//Wrapper around our data structure, with interfaces to access
+//the values and the data; implement your own constructor to
+//gate the value
+class PrimeInt {
+public:
+    PrimeInt(const int i)
+    : d(i) {}
+
+    PrimeInt(const data& other)
+    : d(other) {}
+    
+    PrimeInt(data&& other)
+    : d(std::move(other)) {}
+    
+    PrimeInt& operator=(const PrimeInt&) = default;
+    PrimeInt& operator=(PrimeInt&&) = default;
+    
+    int get() {return d.i;};
+    
+    operator data() {return d;};
+
+private:
+    data d;
+};
+
+//Wrapper around our data structure, with interfaces to access
+//the values and the data; implement your own constructor to
+//gate the value
+class OddInt {
+public:
+    OddInt(const int i)
+    : d(i) {}
+
+    OddInt(const data& other)
+    : d(other) {}
+    
+    OddInt(data&& other)
+    : d(std::move(other)) {}
+    
+    OddInt& operator=(const OddInt&) = default;
+    OddInt& operator=(OddInt&&) = default;
+    
+    int get() {return d.i;};
+    
+    operator data() {return d;};
+
+private:
+    data d;
+};
+
+//Notice that we can even use std::move with a value if we wish.
+//This is much easier than casting, as we can pass around resources
+//from an object. Even better (or worse), you could use pointers (but this is
+//another story).
+int main() {
+    PrimeInt pi(10);
+    
+    std::cout << pi.get() << std::endl;
+    
+    OddInt oi(pi);
+    
+    std::cout << oi.get() << std::endl;
+    
+    return 0;
+}
