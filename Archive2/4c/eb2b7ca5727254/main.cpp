6e2f763a29e929d2b0df6b376d07f2e6deb4f6c7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-23 00:15:10 +0000

    Update archive 4c

---
+++ Archive2/4c/eb2b7ca5727254/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+template<typename T, std::size_t N>
+std::vector<T> arr2vec(T (&a)[N])
+{
+    std::vector<T> temp{};
+    return std::move(temp);
+}
+    
+
+int main(void)
+{
+    std::string arr[3] = {"a", "b", "c"};
+    auto x = arr2vec(arr);
+}
