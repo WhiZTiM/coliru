12326a227d957d1da86c9b7a16c9badfe349c9b4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-23 11:37:17 +0000

    Update archive e3

---
+++ Archive2/e3/25eed8b4ea61b5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+struct Empty {};
+struct Bit {unsigned bit:1; };
+int main()
+{
+    Empty e;
+    Bit b;
+    std::string s1;
+    std::string s2("askjfaksjdflkajsdlkfjalskdjflkjasldjflkasjdlfkjaskldjflskjdlf");
+    std::cout << "size of empty string: "     << sizeof s1        << '\n'
+              << "size of non-empty string: "     << sizeof s2        << '\n'
+              << ">>>>>>>>>>len: "               << s1.size() << " vs. " << s2.size() <<"\n"
+              << "size of pointer : "        << sizeof &e       << '\n'
+              << "size of array of 10 int: " << sizeof(int[10]) << '\n';
+}
