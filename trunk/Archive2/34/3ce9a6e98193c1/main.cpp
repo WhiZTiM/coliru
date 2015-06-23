8aa736ed06e82e77cbd2415c958dd889bc8d8f93
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-23 12:07:38 +0000

    Update archive 34

---
+++ trunk/Archive2/34/3ce9a6e98193c1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <regex>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string test = R"(<html><div><script>var link = "http://example.com/?key=dynamic_key";</script></div></html>)";
+
+	regex re(R"(http://example\.com/.*")");
+	smatch match;
+
+	if (regex_search(test, match, re)) {
+		cout << "OK" << endl;
+        for (auto i : match) {
+            cout << i << endl;
+        }
+	}
+	return 0;
+}
