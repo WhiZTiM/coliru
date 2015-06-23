bfdaa668c3b1a3ed759682a52a72eb9de1c96a3b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-23 18:52:14 +0000

    Update archive 6c

---
+++ Archive2/6c/51d55e2f503df1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define LOUD() { std::cout << __PRETTY_FUNCTION__ << "\n"; }
+
+void f(char) LOUD()
+
+namespace A {
+    struct B {
+        friend void f(char);   // ::f(char) is a friend
+        friend void f(int);    // A::f(int) is a friend
+        
+        void bf();
+    };
+    void B::bf()
+    {
+        f(1);  // calls A::f(int);
+        f('x');  // also calls A::f(int) because ::f is hidden
+    }
+    void f(int) LOUD()
+}
+
+int main()
+{
+    A::B{}.bf();
+}
