f39ff7fa2d1e89dcf6552fadcefa3da4466f119c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-23 05:38:24 +0000

    Update archive 6d

---
+++ trunk/Archive2/6d/b667cc2e3eb218/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int cutoff(int n){
+    int sum, count, nextOdd;
+    for (sum=0, count=1, nextOdd = 0; nextOdd <=n; nextOdd = 1 + 2*(count++ - 1), sum += nextOdd);
+    return sum;
+}
+
+int main()
+{
+// printf("Enter a positive odd integer: \n");
+ int c = 6;
+// scanf("%d",&c);
+ printf("The sum of odd numbers from 1 to %d is %d.\n", c, cutoff(c));
+}
