fbde13a04b5e69110214ad98ee1277b7c8fe2447
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-24 18:50:28 +0000

    Update archive 1e

---
+++ Archive2/1e/15e41848f83728/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+
+template<typename T>
+using v_itt = std::vector<T>::iterator;
+
+
+int main(int argc, char ** argv)
+{
+}
