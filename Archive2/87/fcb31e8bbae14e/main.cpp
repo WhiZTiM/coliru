f22468e34de4c838f748fa1a657488f386dc07dc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-24 16:57:27 +0000

    Update archive 87

---
+++ Archive2/87/fcb31e8bbae14e/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <type_traits>
+
+template < typename T, typename MEMBER >
+typename std::enable_if< std::is_member_object_pointer< MEMBER T::* >::value && std::is_const<MEMBER>::value >::type
+test( MEMBER T::* )
+{
+    std::cout << "pointer to const member object\n" ;
+}
+
+template < typename T, typename MEMBER >
+typename std::enable_if< std::is_member_object_pointer< MEMBER T::* >::value && !std::is_const<MEMBER>::value >::type
+test( MEMBER T::* )
+{
+    std::cout << "pointer to (non-const) member object\n" ;
+}
+
+template < typename T, typename MEMBER >
+typename std::enable_if< std::is_member_function_pointer<MEMBER T::*>::value >::type
+test( MEMBER T::* )
+{
+    std::cout << "pointer to member function\n" ;
+}
+
+int main()
+{
+    struct example
+    {
+        int non_const_member_object = 0 ;
+        const int const_member_object = 2 ;
+        void member_function(int) {}
+    };
+    
+    test( &example::non_const_member_object ) ;
+    test( &example::const_member_object ) ;
+    test( &example::member_function ) ;
+}
