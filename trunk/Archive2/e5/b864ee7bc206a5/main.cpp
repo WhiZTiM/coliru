80fc11e0ca03f2cb526ef6b8f4c087d8095e05d2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-24 03:18:31 +0000

    Update archive e5

---
+++ trunk/Archive2/e5/b864ee7bc206a5/main.cpp
@@ -0,0 +1,49 @@
+#include<vector>
+#include<string>
+#include<iostream>
+
+
+using namespace std;
+
+ struct myStruct 
+{
+        uint32_t content1;
+        uint32_t content2;
+        uint32_t content3;
+        uint32_t content4;
+};
+    
+class myClass
+    {
+        public:
+            void Update();
+            void Retrieve(std::vector<myStruct> &list);
+            
+        private:
+            std::vector<myStruct> m_list;
+    };
+    void myClass::Update()
+    {
+        myStruct temp;
+
+       temp.content1=2;
+       temp.content2=3;
+        m_list.push_back(temp);
+    }
+
+ 
+    void myClass::Retrieve( std::vector<myStruct> &list)
+    {
+        for(uint32_t i=0; i<m_list.size(); i++)
+            std::cout << m_list[i].content1 << std::endl;
+                list = m_list;
+    }
+    
+int main(){
+ myClass me;
+ me.Update();
+ 
+ std::vector<myStruct> list;
+ me.Retrieve(list);
+    
+}
