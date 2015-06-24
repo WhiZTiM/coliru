d16383c8dd0eea9cc6851bcd722d1e3544a4ca18
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-24 18:29:22 +0000

    Update archive 59

---
+++ trunk/Archive2/59/47c7c35194321f/main.cpp
@@ -0,0 +1,26 @@
+#include <boost/fusion/container.hpp>
+#include <boost/fusion/algorithm.hpp>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    using namespace boost;
+    
+    double x = 0.5;
+    int y = 11;
+    char z = 'a';
+    
+    auto closures = fusion::make_vector
+    (
+        [&]{ return x; },
+        [&]{ return y; },
+        [&]{ return z; }
+    );
+    
+    fusion::for_each(closures, [](auto a)
+    {
+        cout << a() << endl;
+    });
+}
+
