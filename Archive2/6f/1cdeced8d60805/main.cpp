d7dda276bfd6be9d664e05472d557ae493b57a17
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-24 09:55:37 +0000

    Update archive 6f

---
+++ Archive2/6f/1cdeced8d60805/main.cpp
@@ -0,0 +1,39 @@
+#include <type_traits>
+#include <iostream>
+
+template <typename T, typename = decltype(&T::mm)>
+static std::true_type has_mm_impl(int);
+
+template <typename T> static std::false_type has_mm_impl(...);
+
+template <typename T> struct has_mm : decltype(has_mm_impl<T>(0)) {};
+
+// Une version avec enable_if
+template <typename T, typename = void> struct has_mm_enable_if_version : std::false_type { };
+
+template <typename T> struct has_mm_enable_if_version<T, typename std::enable_if<bool(sizeof(&T::mm))>::type> : std::true_type { };
+
+struct Foo { int mm; };
+struct Foo2 { int mm2; };
+
+int main()
+{
+    static_assert(has_mm<Foo>::value, "Does not have mm");
+    static_assert(!has_mm<Foo2>::value, "Have mm");    
+    static_assert(has_mm_enable_if_version<Foo>::value, "Does not have mm");
+    static_assert(!has_mm_enable_if_version<Foo2>::value, "Have mm");    
+
+// boost hana
+//    auto has_mm = is_valid([](auto&& x) -> decltype((void)x.mm) { });
+//    static_assert(has_mm(Foo()), "Does not have mm");
+//
+
+    std::cout << "hi w0rld" << std::endl;
+
+    return 0;
+}
+
+
+
+
+
