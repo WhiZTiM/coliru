ed5ac5507a61efab391c466e6865cc56211ab118
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-24 08:45:08 +0000

    Update archive 37

---
+++ Archive2/37/c48a1a8d840e93/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template <class OwnerType, typename FieldType>
+void Test(FieldType OwnerType::*)
+{
+    cout << "Field" << endl;
+}
+
+template <class OwnerType, typename FieldType>
+void Test(const FieldType OwnerType::*)
+{
+	cout << "Const Field" << endl;
+}
+
+template <class OwnerType, typename ArgType>
+void Test(void (OwnerType::*)(ArgType))
+{
+	cout << "Method" << endl;
+}
+
+struct Example
+{
+	int field;
+	const int constField;
+	void method(int) {}
+};
+
+int main()
+{
+    Test(&Example::field);
+	Test(&Example::constField);
+    Test(&Example::method);
+}
