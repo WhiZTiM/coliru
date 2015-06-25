9ded738b661321078d925c2c91c3ab2daf593d58
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-25 21:43:19 +0000

    Update archive ff

---
+++ Archive2/ff/2a9b64b6e3d3f4/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <boost/spirit/home/support/container.hpp>
+
+#define DEFINE_HAS_SIGNATURE(traitsName, funcName, signature)               \
+    template <typename U>                                                   \
+    class traitsName                                                        \
+    {                                                                       \
+    private:                                                                \
+        template<typename T, T> struct helper;                              \
+        template<typename T>                                                \
+        static char check(helper<signature, &funcName>*);                   \
+        template<typename T> static int check(...);                         \
+    public:                                                                 \
+        static                                                              \
+        const bool value = sizeof(check<U>(0)) == sizeof(char);             \
+    }
+
+DEFINE_HAS_SIGNATURE(has_resize, T::resize, void (T::*)(std::size_t));
+
+class xstream { }; // For this example, the class doesn't need to do anything.
+
+template <typename T>
+typename boost::enable_if_c<
+    !boost::spirit::traits::is_container<T>::value,
+    xstream &>::type
+    operator>>(xstream &ibs, T &b)
+{
+    std::cout << "non-container type" << std::endl;
+    return ibs;
+}
+
+template <typename C>
+typename boost::enable_if_c<
+    boost::spirit::traits::is_container<C>::value && has_resize<C>::value,
+    xstream &
+>::type
+operator>>(xstream &ibs, C &c)
+{
+    std::cout << "variable-length container type" << std::endl;
+    ibs >> *c.begin();
+    return ibs;
+}
+
+template <typename C>
+typename boost::enable_if_c<
+    boost::spirit::traits::is_container<C>::value && !has_resize<C>::value,
+    xstream &
+>::type
+operator>>(xstream &ibs, C &c)
+{
+    std::cout << "fixed-length container type" << std::endl;
+    ibs >> *c.begin();
+    return ibs;
+}
+
+int main()
+{
+    int i;
+    std::vector<int> vi;
+    std::set<int> si;
+    xstream xs;
+    xs >> i >> vi >> si;
+}
