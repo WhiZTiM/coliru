8f9b7708b909dacc7597134321408e9679b76a0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-25 14:23:56 +0000

    Update archive 2b

---
+++ trunk/Archive2/2b/c0dc89c3fb717f/main.cpp
@@ -0,0 +1,47 @@
+/* sizeof(Empty) 1                                                                                
+ sizeof(Derived1) 1 
+ sizeof(Derived2) 8 
+ sizeof(Derived3) 1 
+ sizeof(Derived4) 16 
+ sizeof(Dummy) 1
+*/
+
+#include <iostream>
+using namespace std;
+
+class Empty
+{};
+
+class Derived1 : public Empty
+{};
+
+class Derived2 : virtual public Empty
+{};
+
+class Derived3 : public Empty
+{    
+char c;
+};
+
+class Derived4 : virtual public Empty
+{
+char c;
+};
+
+class Dummy
+{
+ char c;
+};
+
+int main()
+{
+
+    cout << "sizeof(Empty) " << sizeof(Empty) << endl;
+    cout << "sizeof(Derived1) " << sizeof(Derived1) << endl;
+    cout << "sizeof(Derived2) " << sizeof(Derived2) << endl;
+    cout << "sizeof(Derived3) " << sizeof(Derived3) << endl;
+    cout << "sizeof(Derived4) " << sizeof(Derived4) << endl;    
+    cout << "sizeof(Dummy) " << sizeof(Dummy) << endl;
+    return 0;
+
+}
