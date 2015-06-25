8e63da483178ff3f398afac54bd93568a4dbe9e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-25 12:45:49 +0000

    Update archive b6

---
+++ trunk/Archive2/b6/234953a1cf2952/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int value;
+    node *next;
+} node;
+
+
+
+node * newBeg(node * head, int value){
+    node *temp1, *temp2;
+    char foundFlag = 0;
+    temp1 = head;
+    while(temp1->next && foundFlag == 0){
+        if(temp1->next->value == value){
+            printf("found\n");
+            return temp1->next;
+        }
+    }
+    return head;
+}
+
+int main()
+{
+    node *l = malloc(sizeof *l);
+    l->value = 2;
+    l->next = malloc(sizeof *l);
+    l->next->value = 3;
+    l->next->next = NULL;
+    newBeg(l,3);
+    free(l->next);
+    free(l);
+    newBeg(NULL,3);
+    return 0;
+}
