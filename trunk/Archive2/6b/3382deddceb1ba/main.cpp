05da88bf8a918e35517fb9967b713c12bb422564
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-25 23:28:15 +0000

    Update archive 6b

---
+++ trunk/Archive2/6b/3382deddceb1ba/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/detail/default_ops.hpp>
+#include <boost/multiprecision/number.hpp>
+#include <iostream>
+
+namespace {
+    namespace bmp = boost::multiprecision;
+    
+    template <int N> bmp::number<bmp::cpp_dec_float<N> > const my_const_pi 
+        = bmp::default_ops::get_constant_pi<bmp::cpp_dec_float<N> >();
+}
+
+int main() {
+    std::cout << std::setprecision(50)   << my_const_pi<50>   << "\n";
+    std::cout << std::setprecision(5000) << my_const_pi<5000> << "\n";
+}
+
