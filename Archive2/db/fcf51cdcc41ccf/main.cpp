484cb8ec795d51009a317d1c0a42c188e67a32f8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-26 00:55:15 +0000

    Update archive db

---
+++ Archive2/db/fcf51cdcc41ccf/main.cpp
@@ -0,0 +1,41 @@
+#include<type_traits>
+#include<iostream>
+
+    template<int N> void runSimulation() { std::cout<<"runSimulation " << N<<std::endl; }
+        
+    //overload for maximum index
+    constexpr size_t max_index = 100;
+
+    namespace detail
+    {
+        void callSimulation_impl(size_t i, std::integral_constant<size_t, max_index>) {}
+        
+        template<size_t N>
+        void callSimulation_impl(size_t i, std::integral_constant<size_t, N>)
+        {
+            if(i==N)
+            {
+                runSimulation<N>();
+            }
+            else
+            {
+                callSimulation_impl(i, std::integral_constant<size_t, N+1>());
+            }
+        }
+    }
+    
+    void callSimulation(size_t i)
+    {
+        detail::callSimulation_impl(i, std::integral_constant<size_t, 0>());
+    }
+
+
+    int main(int argc, char *argv[])
+    {
+        callSimulation(10);   // calls runSimulation<10>();
+
+        //or also:
+
+        callSimulation(rand()%max_index);
+    }
+    
