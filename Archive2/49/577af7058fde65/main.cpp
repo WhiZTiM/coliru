a90a49a4a4dba738f50ecd1d4e3b970853cf1ff3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-26 08:17:50 +0000

    Update archive 49

---
+++ Archive2/49/577af7058fde65/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+ 
+int main()
+{
+    std::cout << std::fixed;
+    std::complex<double> z1(-2, 0);
+    std::cout << "acos" << z1 << " = " << std::acos(z1) << '\n';
+ 
+    std::complex<double> z2(-2, -0.0);
+    std::cout << "acos" << z2 << " (the other side of the cut) = "
+              << std::acos(z2) << '\n';
+ 
+    // for any z, acos(z) = pi - acos(-z)
+    const double pi = std::acos(-1);
+    std::complex<double> z3 = pi - std::acos(z2);
+    std::cout << "cos(pi - acos" << z2 << ") = " << std::cos(z3) << '\n';
+}
