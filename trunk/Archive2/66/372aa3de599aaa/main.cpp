8b3c2186747fe4acc4bfc70da8203319c281dba1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-26 15:15:20 +0000

    Update archive 66

---
+++ trunk/Archive2/66/372aa3de599aaa/main.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <iostream>
+
+float nearest_x_dot_5(float x)
+{
+    return round(x - 0.5) + 0.5;
+}
+
+int main() {
+    float v[] = { -3.64 , -3.12, -2.90, -0.45, 0.01, 7.65, 8.45, 9.63 };
+    for (auto i : v) {
+        std::cout << nearest_x_dot_5(i) << std::endl;
+    }
+}
