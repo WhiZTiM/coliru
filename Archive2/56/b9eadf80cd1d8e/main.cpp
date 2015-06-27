c378410f1e626a0dab7512f353f314ab094bdb23
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-27 21:45:47 +0000

    Update archive 56

---
+++ Archive2/56/b9eadf80cd1d8e/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+
+auto main() -> int
+{
+    std::multimap<std::string, std::set<std::string>> authors = {{"Captain", {"Nothing", "Everything"}},
+                                                                {"ChajusSaib", {"Foo", "Blah", "Cat"}}};
+
+    for (auto const &auth : authors) {
+        std::cout << "Author: " << auth.first << std::endl;
+        std::cout << "Books:" << std::endl;
+        for (auto const &book: auth.second)
+            std::cout << "\t" << book << std::endl;
+        std::cout << std::endl;
+    }        
+
+    return 0;
+}
