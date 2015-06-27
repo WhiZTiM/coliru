56daa4a507bdc608d3e90db60c20a22667f625ed
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-27 05:10:11 +0000

    Update archive c8

---
+++ trunk/Archive2/c8/7a488177288672/main.cpp
@@ -0,0 +1,31 @@
+int main()
+{
+        int i = 7 ;
+
+        switch(i)
+        {
+            case 3 :
+                // ...
+
+            case 1 :
+            {
+                int j = 25 ;
+                ++j ;
+                // ...
+            }
+                break ;
+
+            case 7 : 
+                     
+                ;
+                // ...
+        }
+        
+        int k = 6 ;
+        if( i < 10 ) goto label ; 
+
+        // int k = 6 ;
+        ++k ;
+
+    label:  ; 
+}
