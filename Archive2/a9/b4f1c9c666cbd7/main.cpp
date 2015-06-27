d30eb723a9cc3f7a75c6998bf208720ca67d4b0c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-27 02:43:16 +0000

    Update archive a9

---
+++ Archive2/a9/b4f1c9c666cbd7/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+struct Outer {
+    struct TheFriend{ void f() {std::cout << Inner::i << '\n'; } };
+    class Inner {
+       friend struct TheFriend;
+       static const int i = -1;
+    };
+};
+struct TheFriend{ void f() { std::cout << Outer::Inner::i << '\n'; } };
+
+
+int main()
+{
+    TheFriend global;
+    Outer::TheFriend outer;
+    global.f();
+    outer.f();
+}
