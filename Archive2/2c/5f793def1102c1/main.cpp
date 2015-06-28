dd1b1952c7943a4e125cd985e01c85c4967e3045
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-28 21:27:44 +0000

    Update archive 2c

---
+++ Archive2/2c/5f793def1102c1/main.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include <cstdio>
+
+void empty_destructor(void* /*pointer*/) {
+  /* Empty. */
+}
+
+typedef std::unique_ptr<void, decltype(&empty_destructor)> handle_with_destructor;
+typedef std::unique_ptr<void> handle;
+
+template<class handle_type>
+class snoopy {
+  handle_type handle;
+};
+
+int main()
+{
+  /* Find out whether the pointer to the destructor is stored. */
+  std::printf("Without destructor: %li" "\n" "With destructor %li",
+              sizeof(snoopy<handle>), sizeof(snoopy<handle_with_destructor>)
+             );
+}
