2e18e12db5fda4d686a139e600987a90c34a4298
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-28 14:21:25 +0000

    Update archive f8

---
+++ trunk/Archive2/f8/86071a0cdab8e1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define and &&
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    
+    if(true and true)
+    {
+        //nothing useful
+    }
+    
+    return 0;
+}
