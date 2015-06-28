570faa3b4a9a718d9f76a518ca5d0da5a92fb82a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-28 03:38:01 +0000

    Update archive d4

---
+++ Archive2/d4/00c9a56d732446/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+char* find(char name[], char allNames[][10], int length)
+{
+    int i=0;
+    for (i = 0; i < length; i++) {
+        if (strcmp(allNames[i],name)==0) {
+            printf("%i",i);
+            return name;
+        }
+    }
+    return NULL;
+}
+
+int main(){
+  char allNames[][10] = {"cat","dog","frog","log","bog"};
+  char name[] = "log";
+  int length=5;
+  printf("%s",find(name,allNames,length));
+
+}
