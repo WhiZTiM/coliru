e01e4a8894ea244cec97ab2d70195215f8568a12
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-29 23:26:53 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/0a2eebfe8d80c7/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct Item {
+    int groupID;
+    int sequence;
+    std::string command;
+
+    bool operator<(Item const& other) const {
+        return sequence < other.sequence;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, Item const& i) {
+        return os << "Item { group:" << i.groupID << ", sequence:" << i.sequence << ", command:'" << i.command << "' }";
+    }
+};
+
+#include <algorithm>
+int main() {
+    Item arr[] = {
+        { 1, 0, "DefaultState" },
+        { 2, 2, "Restart"      },
+        { 3, 1, "Beep"         },
+    };
+
+    std::sort(std::begin(arr), std::end(arr));
+
+    for (auto& item : arr)
+        std::cout << item << '\n';
+}
