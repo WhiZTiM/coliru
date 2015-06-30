d32cfe6ce6955b4fa948bbdf089a6ba33a5d514a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-30 23:49:22 +0000

    Update archive 43

---
+++ Archive2/43/8eb23e7a690a2e/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/icl/interval_set.hpp>
+#include <iostream>
+
+using Set      = boost::icl::interval_set<int>;
+using Interval = Set::interval_type;
+
+int main() {
+
+    Set record;
+
+    // pool Group A - returns 1, 4
+    // pool Group B - returns 2, 3
+    // pool Group A - returns 6
+    // pool Group B - returns 5
+
+    int stepcount = 0;
+
+    for (int sample : {
+            1, 4,
+            2, 3,
+            6,
+            5 })
+    {
+        record.insert(sample);
+        std::cout << "state at #" << ++stepcount << ": " << record << "\n";
+    }
+}
