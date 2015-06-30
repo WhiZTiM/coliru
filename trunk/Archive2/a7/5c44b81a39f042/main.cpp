fb114378d4d7f110fc86f9a6c39388301b627c87
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-30 06:14:39 +0000

    Update archive a7

---
+++ trunk/Archive2/a7/5c44b81a39f042/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct A
+{
+    virtual void foo() = 0;
+    A(){ init(); }
+    void init(){ foo(); }
+};
+
+struct B : A
+{
+    virtual void foo(){ }
+};
+
+B a;
+
+int main(){
+    return 0;
+}
+
