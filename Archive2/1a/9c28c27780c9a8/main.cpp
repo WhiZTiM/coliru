b6475f114667f7319299fae3695b1581d67352c8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-06-30 15:31:44 +0000

    Update archive 1a

---
+++ Archive2/1a/9c28c27780c9a8/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include <time.h>
+ 
+int ana ( void ) 
+{ 
+    time_t çağın =  0 ; 
+    printf ( "% ld saniye dönem başladığından beri \ n " , ( uzun ) dönem ) ; 
+    printf ( "% s" , asctime ( gmtime ( ve dönem ) ) ) ; 
+}
