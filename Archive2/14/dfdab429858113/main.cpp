241858e4fffd08a39cf0ce924bdcd95d36c0b2f3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 15:04:20 +0000

    Update archive 14

---
+++ Archive2/14/dfdab429858113/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include <string>
+
+void func(std::string str)
+{
+}
+
+void my_func()
+{
+    func(false);
+}
+
+int main(){
+    my_func();
+    return 0;
+}
