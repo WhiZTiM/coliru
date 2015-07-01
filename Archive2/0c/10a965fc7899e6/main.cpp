9f23da6940123ca3fa6a0c87caced59a7a66a7a4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 21:16:07 +0000

    Update archive 0c

---
+++ Archive2/0c/10a965fc7899e6/main.cpp
@@ -0,0 +1,25 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <fstream>
+#include <iostream>
+
+using boost::property_tree::ptree;
+
+int main() {
+
+    std::fstream ifs("input.txt");
+    ptree pt;
+
+    read_ini(ifs, pt);
+
+    // let's get just the section2
+    
+    if (boost::optional<ptree&> oops = pt.get_child_optional("oops")) {
+        std::cout << "There is a section `oops`\n";
+    } else {
+        std::cout << "There is NO section `oops`\n";
+    }
+
+    ptree& sub1 = pt.get_child("section2"); // we want the CCs!
+    write_ini(std::cout, sub1);
+}
