59af0df5b43164314d1366a5d1c751a25f867413
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 07:18:52 +0000

    Update archive a8

---
+++ trunk/Archive2/a8/66bae96f8a474c/cmd.sh
@@ -0,0 +1,3 @@
+#title mutex.lock() vs while (!mutex.try_lock()) and latency
+echo "Locking: " ; g++ -std=c++14 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+echo "Spinning: " ; g++ -DSPINLOCK -std=c++14 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
