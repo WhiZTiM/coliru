4bca021bdc9e8ffe134b458e288e54522167323a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 03:03:57 +0000

    Update archive 22

---
+++ Archive2/22/7587a2269b51be/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+ 
+template <typename T, typename Copy>
+struct value_ptr : public std::unique_ptr<T>
+{
+    template <typename... Args>
+	value_ptr(Args&&... args) : std::unique_ptr<T>(std::forward<Args>(args)...) {}
+    
+	value_ptr(const value_ptr& rhs) : std::unique_ptr<T>(Copy()(rhs)) {}
+	value_ptr(value_ptr& rhs) : value_ptr(static_cast<const value_ptr&>(rhs)) {}
+	value_ptr(value_ptr&&) = default;
+	value_ptr& operator=(value_ptr&&) = default;
+	value_ptr& operator=(const value_ptr& rhs) = default;
+};
+ 
+template <typename T>
+struct ptr_copy
+{
+	std::unique_ptr<T> operator()(const std::unique_ptr<T>& other)
+	{
+		return std::make_unique<T>(*other);
+	}
+};
+ 
+template <typename T>
+using copying_ptr = value_ptr<T, ptr_copy<T>>;
+ 
+int main() 
+{
+	copying_ptr<int> p1 = new int(5);
+	copying_ptr<int> p2 = p1;
+	std::cout << p1.get() << " " << p2.get();
+	return 0;
+}
