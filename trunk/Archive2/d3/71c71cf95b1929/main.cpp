327610e25e2c6e3252e194b378c1b7e4cb6e0ede
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 16:33:48 +0000

    Update archive d3

---
+++ trunk/Archive2/d3/71c71cf95b1929/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct B {
+    virtual void f() = 0;
+    virtual void g() = 0;
+};
+
+template <class T>
+struct M1 : B { };
+
+template <class T>
+struct M2 : M1<T> { 
+    virtual void g() override
+    {
+        this->f();
+    }
+};
+
+struct D : M2<int> {
+    virtual void f() override { std::cout << "D::f\n"; }
+};
+
+int main() {
+    D d;  
+    d.g();
+
+	return 0;
+}
