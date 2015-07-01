51e3364d896dc4649cb7e611a36a3377516f2113
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 18:38:58 +0000

    Update archive 33

---
+++ Archive2/33/cbef1a55b1bd1e/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct Test
+{
+    int a;
+    int b;
+    int c;
+};
+
+bool less(Test lhs, Test rhs)
+{
+    return (lhs.a < rhs.a) ||
+           ((!(rhs.a < lhs.a) && (lhs.b < rhs.b)) ||
+            (!(rhs.b < lhs.b) && (lhs.c < rhs.c)));
+}
+
+int main()
+{
+    Test t1 = {8, 10, 12};
+    Test t2 = {8, 9, 8};
+    
+    std::cout  << "less(t1 ,t2)" << less(t1, t2) << std::endl;
+    std::cout  << "less(t2 ,t1)" << less(t1, t1) << std::endl;
+}
