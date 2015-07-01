528ba2968fa49b887a12925b16481e16129c5227
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 19:01:06 +0000

    Update archive ed

---
+++ Archive2/ed/129914f37e856f/main.cpp
@@ -0,0 +1,12 @@
+struct Outer {
+
+    void f() {}
+    class Inner {
+       friend class C;
+       friend void f();
+       static int const i = 0;
+    };
+    class C {};
+};
+class C { static int const k = Outer::Inner::i; };
+void f() { int i = Outer::Inner::i; }
