4219a803a6c54e2e8df2fdcd16af73002bffbda7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 16:44:23 +0000

    Update archive 87

---
+++ trunk/Archive2/87/b5b3f7707ae8cf/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+// Until now:
+
+struct CurrentSafeInt
+{
+    CurrentSafeInt(int i) : i(i) {}
+    int i = 0;
+    operator int() { return i; }
+};
+
+typedef CurrentSafeInt CurrentResourceAmount;
+typedef CurrentSafeInt CurrentMonsterId;
+
+
+// Probably it can be done in other ways, but I think this one is very nice:
+
+template <typename T>
+struct SafeInt
+{
+    SafeInt(int i) : i(i) {}
+    int i = 0;
+    operator int() { return i; }
+};
+
+struct ResourceAmount_tag {};
+typedef SafeInt<ResourceAmount_tag> ResourceAmount;
+
+struct MonsterId_tag {};
+typedef SafeInt<MonsterId_tag> MonsterId;
+
+
+int main()
+{
+    CurrentResourceAmount cra = {1};
+    CurrentMonsterId cmi = cra; // I don't want this to be allowed.
+    std::cout << cmi << std::endl;
+    
+    ResourceAmount ra = {2};
+    //MonsterId mi = ra;
+    MonsterId mi = (int)ra;
+    std::cout << mi << std::endl;
+}
