75f55bac3ff33e394790b105d1c494f0e5e58e55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 23:57:02 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/395078691c8d67/cmd.sh
@@ -0,0 +1,14 @@
+cat > input.txt <<HERE; clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp && ./a.out
+[section1]
+
+huh=value1
+slam=value2
+cram=value3
+
+[section2]
+
+rabbits=creditcard1
+die=creditcard2
+eagerly=creditcard3
+
+HERE
