f767b9758b8f0892e8cb12969740d65e6c064527
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-01 23:19:25 +0000

    Update archive 25

---
+++ trunk/Archive2/25/2a7e8e5519a170/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+
+struct vao_maker_attribute {
+    std::string name;
+    
+    template<class T>
+    vao_maker_attribute(std::string name, T) {
+    }
+};
+
+struct vao {
+    vao(std::initializer_list<vao_maker_attribute> attr) {
+    }
+};
+
+int main() {
+    auto my_vao = vao{{"position", float{}}, {"normal", float{}}};
+}
