b11b0e23343e52ecbc9229bb911e255a72536be6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-02 13:41:59 +0000

    Update archive b4

---
+++ Archive2/b4/920cf47ab60f12/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+using namespace std;
+
+// the Root hierarchy
+
+class Root {
+public:
+  virtual ~Root() {}
+};
+
+class A : public Root {};
+
+class B : public Root {};
+
+
+// the Wrapper hierarchy
+
+template<typename T>
+class WrapperRoot {
+public:
+  virtual ~WrapperRoot() {}
+  virtual T name() = 0;
+};
+
+template<typename T>
+class WrapperA : public WrapperRoot<T> {
+public:
+  virtual T name() { return T("WrapperA\n"); }
+};
+
+template<typename T>
+class WrapperB : public WrapperRoot<T> {
+public:
+  virtual T name() { return T("WrapperB\n"); }
+};
+
+// util
+template<typename T, typename... Args>
+auto make_unique(Args... args) -> unique_ptr<T> {
+  return unique_ptr<T>(new T(forward<Args>(args)...));
+}
+
+// the "wrap" function I want to implement
+template<typename T>
+auto wrap(Root& root) -> unique_ptr<WrapperRoot<T>>
+{
+    if (dynamic_cast<A*>(&root)) {
+        return make_unique<WrapperA<T>>();
+    } 
+    else if (dynamic_cast<B*>(&root)) {
+        return make_unique<WrapperB<T>>();
+    } 
+
+    throw std::runtime_error("No wrapper for that type");
+}
+
+
+
+int main() {
+  unique_ptr<Root> a = make_unique<A>();
+  unique_ptr<Root> b = make_unique<B>();
+  cout << wrap<string>(*a)->name()
+       << wrap<string>(*b)->name();
+}
