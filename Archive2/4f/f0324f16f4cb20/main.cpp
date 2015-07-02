6c1b0242433b45d7bdc8b38fdf26906825bb20fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-02 19:14:32 +0000

    Update archive 4f

---
+++ Archive2/4f/f0324f16f4cb20/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+//baseExecutor.h
+class baseExecutor
+{
+    public:
+    virtual bool execA() const = 0;
+
+    private:
+    virtual void execB() =0;
+    virtual void execC()=0;
+};
+
+//Executor.h
+class Executor : public baseExecutor
+{
+    public:
+    virtual bool execA() const;
+    virtual void execB(){std::cout << "B" << std::endl;}
+    virtual void execC();
+};
+
+//Executor.cpp
+bool Executor::execA() const
+{
+    return true;
+}
+void Executor::execC()
+{
+    std::cout << "C" << std::endl;
+}
+
+int main()
+{
+    Executor e;
+    return 0;
+}
