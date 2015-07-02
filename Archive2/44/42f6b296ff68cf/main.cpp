32993944544a26f1861e2b638dbbb0140e65d431
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-02 03:38:42 +0000

    Update archive 44

---
+++ Archive2/44/42f6b296ff68cf/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <type_traits>
+
+#include <boost/range/irange.hpp>
+#include <boost/range/numeric.hpp>
+#include <boost/range/size.hpp>
+
+int main()
+{
+    int a[] = {10, 20, 30, 40};
+    int b[] = {1, 2, 3, 4, 5};
+
+    std::cout << 
+        boost::accumulate(boost::irange<unsigned>(0, boost::size(a)),
+                          0,
+                          [&](int acc, int index) {
+                            return acc + a[index] * 2 - b[index + 1];
+                          })
+        << '\n';
+}
