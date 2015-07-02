ab51fda9096e20d75e85ca466e29f995c15b2725
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-02 19:47:28 +0000

    Update archive 85

---
+++ Archive2/85/6795215d7a95d9/main.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+
+
+template<int N>
+bool branchless(const std::array<int, N>& lhs, const std::array<int, N>& rhs)
+{
+    bool result = true;
+    for (auto i = 0; i != N; ++i)
+    {
+        result &= (lhs[i] == rhs[i]);
+    }
+    return result;
+}
+f
+template<int N>
+bool branchful(const std::array<int, N>& lhs, const std::array<int, N>& rhs)
+{
+    for (auto i = 0; i != N; ++i)
+    {
+        if (lhs[i] != rhs[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+template<int N>
+struct RandomNumberlist : std::array<int, N>
+{
+    RandomNumberlist()
+    {
+        for (auto& i : *this)
+        {
+            i = rand() % 4 != 0; // 1
+        }
+    }
+};
+ 
+ 
+
+    
+enum
+{
+    num_iterations = 10 * 1000
+};
+
+
+volatile long no_optimize;
+
+template<int num_comparisons, typename F>
+long run(const char* fname, F f)
+{
+    using namespace std::chrono;
+    using Clock = high_resolution_clock;
+    
+    
+    auto number_of_matches = 0ul;
+
+    auto& a = *new std::array<RandomNumberlist<num_comparisons>, num_iterations>();
+    auto& b = *new std::array<RandomNumberlist<num_comparisons>, num_iterations>();
+
+    auto start_time = Clock::now();
+    for (auto& el : a)
+    {
+        number_of_matches += f(el, b[&el - &a[0]]);
+    }
+    auto elapsed_time = duration_cast<microseconds>(Clock::now() - start_time).count();
+    no_optimize += number_of_matches;
+
+    (void)fname;
+    return elapsed_time;
+    delete &a;
+    delete &b;
+}
+
+
+int main()
+{
+    #define RUN(n, f, g) \
+    { std::cout << n << " " << std::flush; double a = run<n>(#f " " #n, &f<n>); double b = run<n>(#g " " #n, &g<n>); std::cout << a << ' ' << b << " " << int(0.5 + 100.0 * a / b) << "% \n"; }
+
+    std::cout << "num_comparisons branchful branchless branchful/branchless\n";
+    RUN(2, branchful, branchless);
+    RUN(4, branchful, branchless);
+    RUN(5, branchful, branchless);
+    RUN(10, branchful, branchless);
+}
