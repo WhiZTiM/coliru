b3353473812734ef81753827079ac25984d92d7a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-03 23:37:25 +0000

    Update archive 80

---
+++ trunk/Archive2/80/5a54707c5003e5/main.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+template <class T>
+
+class MatrixViaVector{
+
+public:
+    MatrixViaVector();
+    MatrixViaVector(int m,int n);
+    template <class H>
+    friend ostream& operator <<(ostream& outs, const MatrixViaVector<H> &obj);
+private:
+    int m,n;
+    vector<vector<T>> matrix;
+};
+
+
+
+ template <class T>
+    MatrixViaVector<T>::MatrixViaVector(){
+
+        //creates a 3 by 3 matrix with elements equal to 0
+
+
+        for (int i=0;i<3;i++){
+            vector<int> row;
+            for (int j=0;j<3;j++)
+                row.push_back(0);
+            matrix.push_back(row);
+        }
+    }
+
+template <class T>
+MatrixViaVector<T>::MatrixViaVector(int x,int y)//creates a m by n matrix????
+{
+    //creates a matrix with dimensions m and n with elements equal to 0
+    m=x;
+    n=y;
+    for (int i=0;i<m;i++){
+        vector<int> row;
+        for (int j=0;j<n;j++)
+            row.push_back(1);
+        matrix.push_back(row);
+    }
+}
+
+    template <class T>
+    ostream& operator <<(ostream& outs, const MatrixViaVector<T> & obj)
+    {
+        //obj shud have the vector and therefore I should be able to use its size
+
+       for (std::vector<int>::size_type i = 0; i < obj.m; i++){
+        for (std::vector<int>::size_type j = 0; j < obj.n; j++)
+            outs << " "<< obj.matrix[i][j];
+        outs<<endl;
+    }
+        outs<<endl;
+        return outs;
+    }
+
+    int main()
+    {
+
+    //MatrixViaVector <int> A;
+    MatrixViaVector <int> Az(3,2);//created an object with dimensions 3by2????
+   // cout<<A<<endl;
+    cout<<Az<<endl;//this prints out a 3 by 3 matrix which i dont get????????
+    }
+
