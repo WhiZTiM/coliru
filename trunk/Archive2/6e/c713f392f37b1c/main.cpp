6a09e9cfb6039152d5e635c81b554c23878c6db4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-03 03:17:14 +0000

    Update archive 6e

---
+++ trunk/Archive2/6e/c713f392f37b1c/main.cpp
@@ -0,0 +1,45 @@
+#include <type_traits>
+
+/** This is a base class that implements the watching behvaior */
+class Watchable
+{
+    // Some stuff...
+};
+
+/** This is a pointer that let's you observe the lifetime of types extending "Watchable" */
+template <typename T>
+struct WatchPtr final
+{
+	// Some more stuff...
+
+	// Make sure that "T" extends "Watchable"
+	WatchPtr( T* p = nullptr ) : value(p)
+	{ static_assert(std::is_base_of<Watchable, T>::value, "..."); }
+
+private:
+
+	T* value;
+};
+
+/** For various reasons, we have chosen to forward-declare this type */
+class SomeTypeThatMustBeForwardDeclared;
+
+/** This type wants to have a WatchPtr to "SomeType..." */
+class SomeOtherType
+{
+	WatchPtr<SomeTypeThatMustBeForwardDeclared> value;
+	// Here you get some crazy template errors
+	// "But why? "SomeType..." is defined right below this!
+	// You don't even really need the definition at this moment anyway!
+	// Because fuck you, that's why.
+};
+
+/** Now we define it */
+class SomeTypeThatMustBeForwardDeclared : public Watchable
+{
+	// Blah blah blah
+};
+
+int main() {
+    SomeOtherType test ;
+}
