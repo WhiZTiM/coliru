786ae69d9def54c96f0de157e61b261ce5c24cfb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-03 18:12:19 +0000

    Update archive 0f

---
+++ trunk/Archive2/0f/8a77b62b8b6057/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    float value = 1.05f;
+    int v = 100*value;
+    std::cout << "100*1.05f=" << v << std::endl;
+    
+    return 0;
+}
