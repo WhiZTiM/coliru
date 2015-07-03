04c7e4b5a019cfacfc6f7a18cdbe9b9874b2a0d3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-03 17:34:26 +0000

    Update archive b8

---
+++ trunk/Archive2/b8/f7371504b32d1f/main.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <iostream>
+#include <utility>
+
+inline void print_some(const char *) { std::cout << std::endl; }
+
+template<typename T, typename... Ts>
+inline void print_some(const char *sep, T first, Ts... rest) {
+    std::cout << sep << first;
+    print_some(sizeof...(rest) ? ", " : "", rest...);
+}
+
+template<typename T, T... Is>
+void print_seq(std::integer_sequence<T, Is...>) {
+    print_some("", Is...);
+}
+
+int main() {
+    print_seq(std::make_index_sequence<42>{});
+    constexpr auto N = 1'000'000; // OK for clang++  
+    assert(std::make_index_sequence<N>{}.size() == N);
+}
