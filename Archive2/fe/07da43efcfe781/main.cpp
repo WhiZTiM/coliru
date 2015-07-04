dc2ee79dca35cb6d9f6e12ba6ad233c6630360b7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-04 03:11:04 +0000

    Update archive fe

---
+++ Archive2/fe/07da43efcfe781/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define mprint(str, e) (printf("%s = ", str), _Generic((e), \
+    int: printf("%d", (e)),    \
+    char: printf("%c", (e)),   \
+    float: printf("%f", (e)),  \
+    long: printf("%ld", (e)),  \
+    char *: printf("%s", (e))))
+
+#define print(...) mprint(#__VA_ARGS__, (__VA_ARGS__))
+
+int main(void)
+{
+    print(isalnum('a'));
+    return 0;   
+}
