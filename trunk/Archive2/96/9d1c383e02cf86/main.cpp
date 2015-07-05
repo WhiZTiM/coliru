dc31b83793a4332c0c232c31fa4bc76cb230c08f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-05 20:40:23 +0000

    Update archive 96

---
+++ trunk/Archive2/96/9d1c383e02cf86/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+//template<typename T>
+//std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+//{
+//    for (auto& el : vec)
+//    {
+//        os << el << ' ';
+//    }
+//    return os;
+//}
+
+
+void printVector(const std::vector<std::string> & vec)
+{
+    std::vector<std::string>::const_iterator it;
+    for( it = vec.cbegin(); it != vec.cend(); ++it)
+    {
+        std::cout<<*it<<std::endl;
+    }
+    std::cout<<std::endl;
+}
+
+int main()
+{
+
+    std::vector<std::string> myVector;
+    myVector.push_back("apple");
+    myVector.push_back("pinApple");
+    myVector.push_back("arbuz");
+    myVector.push_back("cactus");
+    myVector.push_back("avakado");
+    printVector(myVector);
+    std::vector<std::string> tmpVec;
+    tmpVec = myVector;  
+    std::vector<std::string>::iterator start = find_if(tmpVec.begin(), tmpVec.end(), [] 
+        (const std::string & tmp)
+        {
+            return tmp[0]=='a' ? true : false;
+        }   
+        );
+    std::vector<std::string>::iterator end = find_if(start, tmpVec.end(), [] 
+        (const std::string & tmp)
+        {
+            return tmp[0]!='a' ? true : false;
+        }   
+        );   
+
+    tmpVec.erase(end, tmpVec.end());
+    std::cout<<"Edited vector:"<<std::endl;
+    printVector(tmpVec);
+    return 0;
+    
+}
+    
