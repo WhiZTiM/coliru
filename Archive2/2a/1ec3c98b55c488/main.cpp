8c615e599e9788d4a4ffb696fdd65c46d9b6542e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-05 20:48:09 +0000

    Update archive 2a

---
+++ Archive2/2a/1ec3c98b55c488/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <utility>
+
+struct bar {
+    bar(int a) {
+        std::cout << a;
+    }
+};
+
+std::pair<bar, bar> create() {
+    return std::make_pair(1, 2);
+}
+
+class foo {
+    private:
+        bar a;
+        bar b;
+        foo(std::pair<bar, bar> temp, bool disambiguator) : a(temp.first), b(temp.second) {
+        }
+        
+    public:
+        foo() : foo(create(), true) {
+        }
+    
+};
+
+int main() {
+    foo f;
+}
