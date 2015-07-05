480e0cfe946d3c720fa877070b0537c864ce4891
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-05 09:36:22 +0000

    Update archive 78

---
+++ trunk/Archive2/78/0715968a77e1ec/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+ 
+int main(void)
+{
+    clock_t t = clock();
+    double d;
+    for (int n=0; n<10000; ++n)
+       for (int m=0; m<10000; ++m)
+           d += d*n*m; // reads and writes to a non-volatile 
+    printf("Modified a non-volatile variable 100m times. "
+           "Time used: %.2f seconds\n",
+           (double)(clock() - t)/CLOCKS_PER_SEC);
+ 
+    t = clock();
+    volatile double vd;
+    for (int n=0; n<10000; ++n)
+       for (int m=0; m<10000; ++m)
+           vd += vd*n*m; // reads and writes to a volatile 
+    printf("Modified a volatile variable 100m times. "
+           "Time used: %.2f seconds\n",
+           (double)(clock() - t)/CLOCKS_PER_SEC);
+}
