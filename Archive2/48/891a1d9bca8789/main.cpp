190d7ece1d13fd9fc802c0468ede2555dc28c96e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-05 18:39:22 +0000

    Update archive 48

---
+++ Archive2/48/891a1d9bca8789/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <iomanip>
+
+void test_it( std::vector<int>& seq, const char* tag )
+{
+    const auto start = std::clock() ;
+    std::sort( seq.begin(), seq.end() ) ;
+    const auto end = std::clock() ;
+    std::cout << std::setw(30) << tag << ": " << std::setw(4)
+              << (end-start) * 1000.0 / CLOCKS_PER_SEC << " millisecs\n" ;
+}
+
+int main()
+{
+    std::vector<int> seq( 8'000'000 ) ;
+    test_it( seq, "all values compare equal" ) ;  
+
+    std::iota( seq.rbegin(), seq.rend(), 0 ) ;
+    test_it( seq, "pre-sorted in reverse order" ) ;  
+    
+    test_it( seq, "already sorted" ) ;  
+
+    std::shuffle( seq.begin(), seq.end(), std::mt19937{} ) ; 
+    test_it( seq, "shuffled (repeatable)" ) ;  
+    
+    std::mt19937 twister{ std::random_device{}() };
+    twister.discard( twister.state_size ) ; 
+    std::shuffle( seq.begin(), seq.end(), std::move(twister) ) ; 
+    test_it( seq, "shuffled (random)" ) ;  
+}
