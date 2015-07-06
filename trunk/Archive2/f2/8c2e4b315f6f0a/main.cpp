3968e1959e01cfae86755d2b570c4567a8241e9b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-06 18:40:40 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/8c2e4b315f6f0a/main.cpp
@@ -0,0 +1,22 @@
+#include<regex>
+#include<iostream>
+
+using namespace std;
+int main() {
+    int count = 0,t;
+    cin >> t;
+    string s="I love #hacker";
+    bool ans;
+    while(t--){
+        cin >> s;
+        smatch sm;
+       // regex rgx("(.*)hacker(.*)",regex_constants::icase); //change your regex for regex_match
+        regex rgx("hacker",regex_constants::icase);
+        ans = regex_search(s,sm,rgx);
+        if(ans){
+            count += 1;
+        }         
+    }
+    cout << ans << endl;
+    return 0;
+}
