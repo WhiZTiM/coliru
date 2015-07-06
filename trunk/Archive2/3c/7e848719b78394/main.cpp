c58893341a0c347d5d2333fcccfe5537de658837
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-06 20:18:15 +0000

    Update archive 3c

---
+++ trunk/Archive2/3c/7e848719b78394/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <type_traits>
+
+auto gL = 
+    [](auto&& func, auto&&... params)
+        -> std::enable_if_t<not std::is_same<void,
+                                             decltype(std::forward<decltype(func)>(func)(std::forward<decltype(params)>(params)...))
+                                            >{},
+                            decltype(std::forward<decltype(func)>(func)(std::forward<decltype(params)>(params)...))
+                           >
+    {
+        return std::forward<decltype(func)>(func)(
+            std::forward<decltype(params)>(params)...); 
+    };
+    
+int main() {
+    gL([](const char * s){std::cout << s << '\n'; return 5;}, "foo");
+//	gL([](const char * s){std::cout << s << '\n'; return;}  , "bar");   // this one gets SFINAE'd out
+}
