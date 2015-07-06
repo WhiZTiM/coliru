0d7a364117b8e1c7a9e742da44b11bcc997db4a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-06 06:52:51 +0000

    Update archive 21

---
+++ Archive2/21/6ea83c08605c45/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <random>
+#include <ctime>
+#include <iomanip>
+
+std::string random_string() // generate a random string
+{
+    static char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                             ",./<>?;':\"[]\\{}|!@#$%^&*()_+-=`~" ;
+    static constexpr std::size_t ALPHABET_SIZE = sizeof(alphabet) - 1 ;
+    static std::mt19937 rng( std::random_device{}() ) ;
+    static std::uniform_int_distribution<int> distribution( 10, ALPHABET_SIZE ) ;
+
+    const auto sz = distribution(rng) ;
+    std::shuffle( alphabet, alphabet+ALPHABET_SIZE, rng ) ;
+    return { alphabet, alphabet+sz } ;
+}
+
+// insert a string maintaining sorted order
+template < typename A, template<typename,typename> class CNTR >
+void insert( CNTR< std::string, A >& sorted_seq, const std::string& v ) // invariant: sorted
+{ sorted_seq.insert( std::upper_bound( std::begin(sorted_seq), std::end(sorted_seq), v ), v ) ; }
+
+// erase the number at the middle of the sequence, maintaining sorted order
+template < typename A, template<typename,typename> class CNTR >
+void erase( CNTR< std::string, A >& seq ) // invariant: !empty()
+{ seq.erase( std::next( std::begin(seq), seq.size()/2 ) ) ; }
+
+template < typename A, template<typename,typename> class CNTR >
+auto test_it( CNTR< std::string, A >& seq, const std::vector<std::string>& values )
+{
+    const auto start = std::clock() ;
+    for( const auto& v : values ) insert( seq, v ) ;
+    while( seq.size() > values.size()/2 ) erase(seq) ;
+    return ( std::clock() - start ) * 1000.0 / CLOCKS_PER_SEC ;
+}
+
+int main()
+{
+    constexpr std::size_t N = 16'000 ;
+    std::vector<std::string> values(N) ;
+    std::generate( std::begin(values), std::end(values), random_string ) ;
+
+    std::cout << "vector: " ;
+    std::vector<std::string> vec ;
+    std::cout << std::setw(5) << test_it( vec, values ) << " millisecs\n" ;
+
+    std::cout << "  list: " ;
+    std::list<std::string> lst ;
+    std::cout << std::setw(5) << test_it( lst, values ) << " millisecs\n" ;
+}
