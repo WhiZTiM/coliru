2a3590f6eb46d00e2fe36e57fc24b605abb6dc27
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-06 18:29:19 +0000

    Update archive ae

---
+++ Archive2/ae/b0df4034d770c3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <locale>
+#include <iomanip>
+ 
+int main()
+{
+    std::tm t = {};
+    std::istringstream ss("2011-Februar-18 23:12:34");
+    ss.imbue(std::locale("de_DE.utf-8"));
+    ss >> std::get_time(&t, "%Y-%b-%d %H:%M:%S");
+    if (ss.fail()) {
+        std::cout << "Parse failed\n";
+    } else {
+        std::cout << std::put_time(&t, "%c") << '\n';
+    }
+}
