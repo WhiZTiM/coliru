9a90e126713b18662e724a787bf8ac1ffd30b2bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-07 18:54:40 +0000

    Update archive 85

---
+++ Archive2/85/cfc32f7b792a9c/main.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <functional>
+
+    struct noop {
+      struct anything {
+        template<class T>
+        operator T(){ return {}; }
+        template<class T>
+        operator const T&()const{ static const T t{}; return t; }
+      };
+      template<class...Args>
+      anything operator()(Args&&...)const{return {};}
+    };
+
+class foo {} ;
+
+int main()
+{
+    std::function<void(int)> f1 = noop{};
+    f1(3);
+    std::function<int(std::string, char, double)> f2 = noop{};
+    int x = noop::anything{};
+    (void)x;
+    std::function<foo const&(int)> f3 = noop{};
+}
