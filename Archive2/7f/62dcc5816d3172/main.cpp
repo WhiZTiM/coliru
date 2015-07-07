a84cfbcf605f6ac456bd8c9e7ba00bac082201d5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-07 11:30:06 +0000

    Update archive 7f

---
+++ Archive2/7f/62dcc5816d3172/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+ 
+using namespace std;
+
+int main()
+{
+    std::vector<int> data = { 1, 1, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6 };
+ 
+    auto lower = std::lower_bound(data.begin(), data.end(), 4);
+//    auto upper = std::upper_bound(data.begin(), data.end(), 4);
+ 
+    cout << lower;
+}
