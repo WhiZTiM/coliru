8aa4675dafabe09119bbaa4a3f43deb3bb8b3786
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-07 23:53:15 +0000

    Update archive 96

---
+++ trunk/Archive2/96/5bfbcd9dc9dc46/main.cpp
@@ -0,0 +1,65 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <sstream>
+#include <iostream>
+
+using boost::property_tree::ptree;
+
+enum class YesNo { No, Yes };
+
+static inline std::ostream& operator<<(std::ostream& os, YesNo v) {
+    switch(v) {
+        case YesNo::Yes: return os << "Yes";
+        case YesNo::No:  return os << "No";
+    }
+    return os << "??";
+}
+
+struct YesNoTranslator {
+    typedef std::string  internal_type;
+    typedef YesNo        external_type;
+ 
+    boost::optional<external_type> get_value(internal_type const& v) {
+        if (v == "Yes") return YesNo::Yes;
+        if (v == "No")  return YesNo::No;
+
+        return boost::none;
+    }
+
+    boost::optional<internal_type> put_value(external_type const& v) {
+        switch(v) {
+            case YesNo::Yes: return std::string("Yes");
+            case YesNo::No:  return std::string("No");
+            default: throw std::domain_error("YesNo");
+        }
+    }
+};
+
+static YesNoTranslator trans;
+
+int main() {
+
+    std::istringstream iss(R"(
+            <?xml version="1.0"?>
+            <demo>
+                <positive>Yes</positive>
+                <negative>No</negative>
+                <invalid>Bogus</invalid>
+            </demo>
+        )");
+
+    ptree pt;
+    read_xml(iss, pt);
+
+
+    for (auto&& field : { "demo.positive", "demo.negative", "demo.invalid" })
+    {
+        try {
+            std::cout << "With 'No' default: '" << field << "':\t" << pt.get(field, YesNo::No, trans) << "\n";
+            std::cout << "Without default:   '" << field << "':\t" << pt.get<YesNo>(field, trans)     << "\n";
+        } catch(std::exception const& e) {
+            std::cout << "Error parsing '"      << field << "':\t" << e.what()                        << "\n";
+        }
+    }
+
+}
