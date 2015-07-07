34032a87f7874e87225bd68311d67d03d779450b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-07 13:21:29 +0000

    Update archive fa

---
+++ trunk/Archive2/fa/6fb2fbff6a1ba3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+int main()
+{
+   int count=0;
+   int total marks=0;
+   float average;
+   int x;
+   cout<<"Enter the student score:/n";
+   cin>>x;
+   total marks=total marks+x;
+   count=count++;
+   average=total marks/count;
+   cout<<"The student total marks:"<<"total marks"<<"/n";
+   cout<<"The average is:"<<"average"<<;
+   return 0;
+}
