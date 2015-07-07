261f82b161248f914b519a78c5d8d8435909657c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-07 12:34:36 +0000

    Update archive de

---
+++ Archive2/de/fba3d99dade4a4/main.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <chrono>
+#include <sstream>
+#include <iostream>
+#include <memory>
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+
+    /*
+    auto t1 = system_clock::now();
+    // algoritm call
+    auto t2 = system_clock::now();
+
+    cout << "Time: " << duration<double>(t2-t1).count() << endl;
+    */
+
+    /*vector<int> srcVec(10);
+
+    iota( srcVec.begin(), srcVec.end(), 0 );
+
+    random_device rd;
+    mt19937 g( rd() );
+    shuffle( srcVec.begin(), srcVec.end(), g );
+
+    cout << "srcVec: [";
+    copy( srcVec.cbegin(), srcVec.cend(), ostream_iterator<int>( cout, ", " ) );
+    cout << "]" << endl;*/
+
+    return 0;
+}
