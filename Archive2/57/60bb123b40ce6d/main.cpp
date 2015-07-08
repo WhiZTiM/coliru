19a8aab944e7b3a085534e00cd24415b79683ad5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-08 16:24:43 +0000

    Update archive 57

---
+++ Archive2/57/60bb123b40ce6d/main.cpp
@@ -0,0 +1,37 @@
+//#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+//#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+//#pragma GCC diagnostic ignored "-Wunused-variable"
+#include <boost/spirit/include/karma.hpp>
+#include <boost/spirit/include/qi.hpp>
+//#pragma GCC diagnostic pop // pops
+
+#include <iostream>
+
+int main() {
+    typedef  unsigned long long ull;
+
+    std::string const curline = "1;2;;3,4;5";
+    std::cout << "parsing: '" << curline << "'\n";
+
+    namespace qi = boost::spirit::qi;
+
+#if 0 // THIS IS UNDEFINED BEHAVIOUR:
+    auto ids     = -qi::ulong_long % ','; // '-' allows for empty vecs.
+    auto grammar = ids % ';';
+#else // THIS IS CORRECT:
+    auto ids     = qi::copy(-qi::ulong_long % ','); // '-' allows for empty vecs.
+    auto grammar = qi::copy(ids % ';');
+#endif
+
+    std::vector<std::vector<ull> > r;
+    qi::parse(curline.begin(), curline.end(), grammar, r);
+
+    std::cout << "got:      ";
+    for (auto v: r){
+        for (auto i : v)
+            std::cout << i << ",";
+        std::cout << ";";
+    }
+    std::cout <<"\n";
+}
