3c9dda6eb9ed719c3a6554d66af9c8fb4575cbc5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-08 23:15:34 +0000

    Update archive 59

---
+++ Archive2/59/be9e666d171d3c/main.cpp
@@ -0,0 +1,51 @@
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
+
+template <typename It, typename Skipper = qi::space_type>
+struct my_grammar : qi::grammar<It, std::string(), Skipper> {
+
+    my_grammar() : my_grammar::base_type(start) {
+        using namespace qi;
+
+        start     = q_literal('"') | q_literal('\'');
+
+        q_literal = lit(_r1) >> *('\\' >> char_ | (char_ - lit(_r1))) >> lit(_r1);
+
+        BOOST_SPIRIT_DEBUG_NODES( (start)(q_literal) )
+    }
+
+  private:
+    qi::rule<It, std::string(), Skipper> start;
+
+    // drop skipper to make these rules implicitly 'lexeme'
+    // see: http://stackoverflow.com/questions/17072987/boost-spirit-skipper-issues/17073965#17073965
+    qi::rule<It, std::string(char)> q_literal;
+};
+
+int main() {
+    using It = std::string::const_iterator;
+    my_grammar<It> g;
+
+    for (std::string const& input : {
+            "\"hello world\"",
+            "\"hello \\\"world\\\"\"",
+            "'bye world'",
+            "'bye \"\\'world\\'\"'",
+            "bogus" })
+    {
+        std::cout << "\n------- Parsing: " << input << '\n';
+        It f = input.begin(), l = input.end();
+
+        std::string result;
+        bool ok = qi::phrase_parse(f, l, g, qi::space, result);
+
+        if (ok)
+            std::cout << "Parse success: " << result << "\n";
+        else
+            std::cout << "Parse failed\n";
+
+        if (f!=l)
+            std::cout << "Remaining unparsed input '"  << std::string(f,l) << "'\n";
+    }
+}
