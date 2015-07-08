f666d8cde33101b21111fae875596ca4998391b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-08 22:05:13 +0000

    Update archive e2

---
+++ Archive2/e2/88a584b486359d/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <cmath>
+
+class exponential{
+    double
+        base_,
+        power_;
+public:
+    exponential(double base, double power) : base_{base}, power_{power}{}
+    
+    double &base(){return base_;}
+    const double &base() const {return base_;}
+    double &power(){return power_;}
+    const double &power() const {return power_;}
+    double toDouble(){return std::pow(base_, power_);}
+    
+    exponential operator*(const exponential &exp){
+        if(base_ == exp.base_){
+            return {base_, power_ + exp.power_};
+        }
+        else if(power_ == exp.power_){
+            return {base_ * exp.base_, power_};
+        }
+        return {std::pow(base_, power_) * std::pow(exp.base_, exp.power_), 1};
+    }
+    exponential operator/(const exponential &exp){
+        if(base_ == exp.base_){
+            return {base_, power_ - exp.power_};
+        }
+        else if(power_ == exp.power_){
+            return {base_ / exp.base_, power_};
+        }
+        return {std::pow(base_, power_) / std::pow(exp.base_, exp.power_), 1};
+    }
+};
+
+int main(){
+    std::cout << (exponential{0.5, 10000}/exponential{0.5, 10000}).toDouble();
+}
