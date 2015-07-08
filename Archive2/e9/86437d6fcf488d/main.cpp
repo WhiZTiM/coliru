a457386ce7b4f567bc1ba091745367505d6dcb08
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-08 23:26:26 +0000

    Update archive e9

---
+++ Archive2/e9/86437d6fcf488d/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+long cantor(long x, long y)
+{
+    return (x + y) * (x + y + 1) / 2 + y; 
+}
+
+
+int main()
+{
+    for (int i = 0; i != 20; ++i)
+    {
+        for (int j = 0; j != 20; ++j)
+        {
+            std::cout << i << "x" << j << " => " << cantor(i, j) << '\n';
+        }
+    }
+}
+
