f1ddbc573c621339d239c948357fc29d9b5535d6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-08 13:30:12 +0000

    Update archive d5

---
+++ trunk/Archive2/d5/ff3a538eeaf5b2/main.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+
+int main()
+{
+    std::stringstream input;
+    input
+        << "Server: nginx\r\n"
+        << "Date: Wed, 08 Jul 2015 12:12:25 GMT\r\n"
+        << "Content-Type: application/json\r\n"
+        << "Content-Length: 153\r\n"
+        << "Connection: close\r\n"
+        << "Access-Control-Allow-Origin: *\r\n"
+        << "Access-Control-Allow-Credentials:      true\r\n"
+        << "An-Empty-Header:\r\n"
+    ;
+    
+    std::string str;
+    while (std::getline(input, str)) {
+        auto index = str.find(':');
+        std::pair<std::string,std::string> keyVal;
+        if (index != std::string::npos) {
+            keyVal = std::make_pair(
+                str.substr(0,index),
+                str.substr(index+1)
+            );
+            while (!keyVal.second.empty() && keyVal.second.front() == ' ') {
+                keyVal.second.erase(0,1);
+            } 
+        }
+        
+        std::cout << keyVal.first << '|' << keyVal.second;
+    }
+}
