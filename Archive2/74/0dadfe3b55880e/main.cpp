7b9ba3d0c73750e097549ce44aff8c557217389e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-08 16:51:30 +0000

    Update archive 74

---
+++ Archive2/74/0dadfe3b55880e/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct A
+{
+    int a;
+    int b;
+    A(int _a, int _b) : a(_a), b(_b) {};
+};
+
+struct B
+{
+    int a;
+    int b;
+};
+
+template <typename T> void fn(T* in)
+{
+    printf("a = %i, b = %i\n", in->a, in->b);
+}
+
+int main()
+{
+    fn(&A{ 1, 2 });      //OK, no extra magick
+    fn(&B {3, 4});      //error: expression must be an lvalue or function designator 
+    fn(&(B)B{ 3, 4 });  //OK with explicit cast to B (but why?)
+}
