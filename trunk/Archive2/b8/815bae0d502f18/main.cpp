a4fccd60f24d5ce55ba172efa31421b845c9745d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-09 21:40:03 +0000

    Update archive b8

---
+++ trunk/Archive2/b8/815bae0d502f18/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <omp.h>
+
+int main()
+{
+    std::vector<double> v(4);
+    std::generate(v.begin(),  v.end(), []() { static double n{0.0}; return n ++;} );
+    
+    int counter = 0;
+    int numOfThreadsUsed = 0;
+    
+    #pragma omp parallel shared(counter) num_threads(1)
+    {
+        for (auto& el :  v) 
+        {
+            printf("outer loop, thread no. %d, \n", omp_get_thread_num());
+            counter++;
+            
+            double t = el - 1.0;
+            #pragma omp parallel for num_threads(omp_get_num_procs())
+            for (size_t i = 0; i < v.size(); i ++)
+            {
+                printf("inner loop, thread no. %d, \n", omp_get_thread_num());
+                el += v[i];
+                el-= t;
+            }
+        }
+        
+        numOfThreadsUsed = omp_get_num_threads();
+    }
+    
+    
+    printf("outer loop has been executed %d times (once for each thread)\n", numOfThreadsUsed);
+    printf("this can be seen by checking the counter. if only executed once it should be the same size as the vector (%lu). but it really is %d\n", v.size(), counter);
+        
+}
+
