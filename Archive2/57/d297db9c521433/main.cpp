707978caddac8af1f11b219a0a2ce88002fdc30e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-09 19:04:01 +0000

    Update archive 57

---
+++ Archive2/57/d297db9c521433/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+//std::mutex mx;
+
+void print(char ch, int count)
+{
+    //std::lock_guard<std::mutex> lock(mx);
+    for (int i = 0; i < count; ++i)
+    {
+        std::cout << ch;
+    }
+    std::cout << std::endl;
+}   
+        
+int main() {
+     std::thread t1(print, 'x', 10);
+     std::thread t2(print, '*',20);
+     
+     t1.join();
+     t2.join();
+     return 0;
+}
