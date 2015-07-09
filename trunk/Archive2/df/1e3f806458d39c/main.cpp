68a7cce6760d3714287d4cf1b42e46146cf19dac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-09 16:43:41 +0000

    Update archive df

---
+++ trunk/Archive2/df/1e3f806458d39c/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// enum that takes 16 bits
+
+ 
+int main(int argc, char **argv)
+{
+    int i=1;
+    
+    while(i>0)
+    {
+ 
+    std::cout << "col = " <<i<<argv[0]<<std::endl;
+    i++;
+}
+
+    }
