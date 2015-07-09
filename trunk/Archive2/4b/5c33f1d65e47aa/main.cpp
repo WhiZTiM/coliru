11bbb83b01d9e7678804689c82b92b17c3767f5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-09 07:20:47 +0000

    Update archive 4b

---
+++ trunk/Archive2/4b/5c33f1d65e47aa/main.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {   
+    int *a, *b;
+    //a = (int *)malloc(sizeof(int));
+    //*a = 10;
+    *a = 10;
+    b = a;
+    
+    printf("%d\n", *b);
+    printf("%d\n", *a);
+}
