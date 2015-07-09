5027b5d6103adea206530ff2d1323564aed41797
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-09 12:34:47 +0000

    Update archive b2

---
+++ trunk/Archive2/b2/4b42992028eeee/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+
+int main() {
+    double ratio = 0.0 - 0.0;
+    double ratio2 = -0.0 + 0.0;
+    double ratio3 = 0.0 * -1.0;
+    std::cout << ratio << ',' << ratio2 << ',' << ratio3 << std::endl;
+   return 0;
+}
