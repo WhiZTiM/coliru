00a7962e9dbc202983017c63d6187a85eb83538b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-09 12:13:17 +0000

    Update archive dc

---
+++ Archive2/dc/ef283377c9720f/main.cpp
@@ -0,0 +1,67 @@
+#include <boost/spirit/home/lex.hpp>
+#include <boost/spirit/home/lex/lexer_lexertl.hpp>
+#include <boost/spirit/home/support/iterators/multi_pass.hpp>
+#include <boost/spirit/home/support/iterators/detail/combine_policies.hpp>
+#include <boost/spirit/home/support/iterators/detail/no_check_policy.hpp>
+#include <boost/spirit/home/support/iterators/detail/ref_counted_policy.hpp>
+#include <boost/spirit/home/support/iterators/detail/buffering_input_iterator_policy.hpp>
+#include <boost/spirit/home/support/iterators/detail/split_std_deque_policy.hpp>
+#include <string>
+#include <iostream>
+
+namespace lex = boost::spirit::lex;
+
+typedef lex::lexertl::token< std::string::const_iterator > token;
+typedef lex::lexertl::lexer< token > lexer;
+
+
+enum token_id
+{
+    byte_order_mark = boost::spirit::lex::min_token_id,
+    bad_token
+};
+
+class working_lexer : public lex::lexer< lexer >
+{
+public:
+    working_lexer()
+    {
+        self.add
+            ( R"(\xef\xbb\xbf)", byte_order_mark )
+            ;
+    }
+};
+
+class broken_lexer : public lex::lexer< lexer >
+{
+public:
+    broken_lexer()
+    {
+        self.add_pattern
+            ( "HEX", "[0-9a-zA-Z]" )
+            ;
+        self.add
+            ( R"(\xef\xbb\xbf)", byte_order_mark )
+            // why does the above no longer match once I have one of these?
+            //( R"(\/\/[^\n]*|\/\*([^\*]|\*[^\/])*\*\/)", bad_token ) // C-style comments
+            ( R"(\"([^\n\"\\]|\\[\"nrt\\])*\")", bad_token ) // ""-strings with \n, \r, \t and \\ escapes
+            ;
+    }
+};
+
+int main( int, char** )
+{
+    std::string code( "\xef\xbb\xbf" );
+    const auto end = code.cend();
+    std::cout << "Working lexer:" << std::endl;
+    auto it = code.cbegin();
+    bool success = lex::tokenize( it, end, working_lexer{} );
+    std::cout << "Success: " << std::boolalpha << success << std::endl;
+    std::cout << "Consumed everything: " << std::boolalpha << ( it == end ) << std::endl;
+    std::cout << "Broken lexer:" << std::endl;
+    it = code.cbegin();
+    success = lex::tokenize( it, end, broken_lexer{} );
+    std::cout << "Success: " << std::boolalpha << success << std::endl;
+    std::cout << "Consumed everything: " << std::boolalpha << ( it == end ) << std::endl;
+    return 0;
+}
