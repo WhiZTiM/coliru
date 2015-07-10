3a10e1ac70f18c3ccae39ed3120156b83c74f969
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-10 04:02:50 +0000

    Update archive 89

---
+++ Archive2/89/72531326ce3a06/cmd.sh
@@ -0,0 +1,7 @@
+clang++ -std=c++14 -O2 -I/usr/include/python2.7 -Wall -pedantic main.cpp -lpython2.7 -shared -fPIC -o mymodule.so
+python2.7 << EOF
+import mymodule
+import sys
+assert(42 == mymodule.run_mymodule("a", "bc", "def", sys))
+EOF
+echo $?
