b3b770116a6a12f1370300b46cc04858fbff7277
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-10 21:23:00 +0000

    Update archive 8e

---
+++ trunk/Archive2/8e/93392c9dbdbe54/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <mutex>
+ 
+int main()
+{
+    std::shared_ptr<Base> p = std::make_shared<Derived>();
+ 
+    std::cout << "Created a shared Derived (as a pointer to Base)\n"
+              << "  p.get() = " << p.get()
+              << ", p.use_count() = " << p.use_count() << '\n';
+    std::thread t1(thr, p), t2(thr, p), t3(thr, p);
+    p.reset(); // release ownership from main
+    std::cout << "Shared ownership between 3 threads and released\n"
+              << "ownership from main:\n"
+              << "  p.get() = " << p.get()
+              << ", p.use_count() = " << p.use_count() << '\n';
+    t1.join(); t2.join(); t3.join();
+    std::cout << "All threads completed, the last one deleted Derived\n";
+}
