dcd57d0629e1757be803f857fcc3cacc1e7c794d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-10 16:34:29 +0000

    Update archive 01

---
+++ Archive2/01/e75a659b6c75de/cmd.sh
@@ -0,0 +1,5 @@
+cat <<HERE>input.txt; clang++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp -lboost_system -lboost_filesystem -lboost_iostreams && ./a.out
+>gi|31563518|ref|NP_852610.1| microtubule-associated proteins 1A/1B light chain 3A isoform b [Homo sapiens]
+MKMRFFSSPCGKAAVDPADRCKEVQQIRDQHPSKIPVIIERYKGEKQLPVLDKTKFLVPDHVNMSELVKI
+IRRRLQLNPTQAFFLLVNQHSMVSVSTPIADIYEQEKDEDGFLYMVYASQETFGFIRENE
+HERE
