0f6d37f9a9d0a50b1a1c81dd3b582da1971660cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-11 11:48:33 +0000

    Update archive cf

---
+++ trunk/Archive2/cf/7160ed84ecc3b2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <iterator>
+#include <functional>
+#include <boost/iterator/transform_iterator.hpp>
+
+int main()
+{
+    std::map<int, int> floor_plan;
+    floor_plan[0] = 1;
+    floor_plan[1] = 2;
+            
+    auto second = std::mem_fn(&std::map<int, int>::value_type::second);        
+    const size_t distance = std::accumulate(boost::make_transform_iterator(std::begin(floor_plan), second)
+                                          , boost::make_transform_iterator(std::end(floor_plan), second)
+                                          , 0);
+    
+    std::cout << "Total: " << distance;
+}
