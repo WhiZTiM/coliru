6afbc77aa23a3eacb9b5cf2246439e7612484e85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-11 09:02:58 +0000

    Update archive d7

---
+++ Archive2/d7/1da761e4a060fd/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+std::map< std::string, int > read_map( std::istream& stm = std::cin )
+{
+    std::map< std::string, int > map ;
+    std::string key ;
+    int value ;
+
+    // key does not contain white space
+    while( stm >> key >> value ) map.emplace( key, value ) ;
+
+    /*
+    // key may contain white space
+    while( std::getline( stm, key ) && // read a complete line as key (unformatted input)
+           stm >> value && // read an int as value (formatted input)
+           stm.ignore( 1000000, '\n' ) ) // extract and discard the trailing new line
+    {
+        map.emplace( key, value ) ;
+    }
+    */
+
+    return map ;
+}
+
+int main()
+{
+    for( auto p : read_map() ) std::cout << p.first << ' ' << p.second << '\n' ;
+}
