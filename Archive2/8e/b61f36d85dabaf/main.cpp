2fa9e0d911394a346063f793c23b90465e8b0c68
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-11 00:15:56 +0000

    Update archive 8e

---
+++ Archive2/8e/b61f36d85dabaf/main.cpp
@@ -0,0 +1,15 @@
+namespace A
+{
+    template <class T> struct some_class
+    { /* some definition */ };
+}
+namespace B
+{
+    template <class T> using some_class = A::some_class<T>;
+}
+namespace B
+{
+    template <class T>
+    decltype(auto) some_func(const some_class<T>& obj)
+    { /* function definition */ }
+}
