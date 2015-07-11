565e625620b5bfaab9b487b53de281b4addbb124
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-11 20:07:30 +0000

    Update archive d4

---
+++ Archive2/d4/8d6a8db4699180/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+struct A{
+    A(){ std::cout << "def\n"; }
+	A(const A& a){ std::cout << "copy\n"; }
+	A(A&& a){ std::cout << "move\n"; }
+};
+
+A f(A& a){
+	return std::move(a);
+}
+
+int main(){
+	
+	A a;
+	A b = std::move(f(a));
+
+}
