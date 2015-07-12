4cb15b53cb6356c2b1d7ecb629efe527618134cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-12 15:09:08 +0000

    Update archive 8f

---
+++ Archive2/8f/f826b8ee337eb4/main.cpp
@@ -0,0 +1,28 @@
+#include <type_traits>
+#include <iostream>
+
+struct Base {
+    Base() : i(42) { }
+    int i;
+};
+
+struct Derived : Base {
+    Derived() : j(44) { }
+    int j;
+};
+
+template<int& i> int f() { return i;}
+
+int main(int argc, char** argv) {
+    void* p = ::operator new(sizeof(Derived));
+    
+    Base* b = static_cast<Base*>(p);
+    Derived* d = static_cast<Derived*>(p);
+    
+    new (b) Base();
+    std::cout << b->i << "\n"; // At this stage we only care about i being valid.
+    new (d) Derived();
+    std::cout << d->i << " " << d->j << "\n"; // Now i and j should be valid.
+    
+    return 0;
+}
