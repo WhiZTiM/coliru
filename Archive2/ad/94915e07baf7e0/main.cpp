045b6bfeb1694744a4ac16faa092d5c4f8aa8b53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-12 12:18:37 +0000

    Update archive ad

---
+++ Archive2/ad/94915e07baf7e0/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <type_traits>
+#include <cstring>
+
+struct A {
+    double a, b, c, d;
+};
+
+int main(int argc, char** argv) {
+    std::aligned_storage_t<sizeof(A), alignof(A)> storage[2];
+    char* p = reinterpret_cast<char*>(storage);
+    
+    A* a = reinterpret_cast<A*>(p);
+    A* b = reinterpret_cast<A*>(p + alignof(A));
+    
+    new (a) A{1.0, 2.0, 3.0, 4.0};
+    std::cout << a->a << " " << a->b << " " << a->c << " " << a->d << "\n";
+    new (b) A(std::move(*a));
+    std::cout << b->a << " " << b->b << " " << b->c << " " << b->d << "\n";
+    
+    new (a) A{1.0, 2.0, 3.0, 4.0};
+    std::cout << a->a << " " << a->b << " " << a->c << " " << a->d << "\n";
+    std::memmove(b, a, sizeof(A));
+    std::cout << b->a << " " << b->b << " " << b->c << " " << b->d << "\n";
+    return 0;
+}
