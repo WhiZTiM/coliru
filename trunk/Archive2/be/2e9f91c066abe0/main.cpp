a3d075027e3b3015bb3ecbffda803708783b43df
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-12 15:16:58 +0000

    Update archive be

---
+++ trunk/Archive2/be/2e9f91c066abe0/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+struct point { int x; int y; };
+
+point operator+(point const& lhs, point const& rhs) { return {lhs.x + rhs.x, lhs.y + rhs.y}; }
+point operator/(point const& lhs, int rhs) { return {lhs.x / rhs, lhs.y / rhs}; }
+std::ostream& operator<<(std::ostream& os, point const& p) { std::cout << '(' << p.x  << ", " << p.y << ')'; return os; }
+
+template<typename It>
+void sort_vertices(It begin, It end, typename It::value_type const& center) {
+    using point_type = typename It::value_type;
+
+    auto top_down = [](point_type const& a, point_type const& b) { return a.y > b.y; };
+    std::sort(begin, end, top_down);
+    auto middle = std::find_if(begin, end, [&center](point_type const& a) { return a.y <= center.y; });
+    
+    auto left_right = [](point_type const& a, point_type const& b) { return a.x < b.x; };
+    auto right_left = [](point_type const& a, point_type const& b) { return a.x > b.x; };
+    std::stable_sort(begin, middle, left_right);
+    std::stable_sort(middle, end, right_left);
+}
+
+template<typename It>
+typename It::value_type polygon_center(It begin, It end) {
+    using point_type = typename It::value_type;
+    auto sum = std::accumulate(begin, end, point_type());
+    return sum / std::distance(begin, end);
+}
+
+template<typename It>
+void sort_vertices(It begin, It end) {
+    sort_vertices(begin, end, polygon_center(begin, end));
+}
+
+template<typename It>
+void sort_polygons(It begin, It end, std::size_t m) {
+    for (It cur_begin = begin; cur_begin != end; std::advance(cur_begin, m))
+        sort_vertices(cur_begin, std::next(cur_begin, m));
+}
+
+int main() {
+    std::vector<point> points {{9, 8}, {3, 6}, {2, 3}, {6, 8}, {6, 1}, {10, 4}};
+    sort_vertices(begin(points), end(points));
+
+    std::cout << "Vertices: ";
+    for (auto p : points)
+        std::cout << p << ' ';
+    std::cout << "\nCenter: " << polygon_center(begin(points), end(points));
+}
