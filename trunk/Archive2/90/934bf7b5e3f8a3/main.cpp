65b496bacc7fcbabcc27e2e53be029dea9df9e96
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-12 11:52:20 +0000

    Update archive 90

---
+++ trunk/Archive2/90/934bf7b5e3f8a3/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T>
+class cls
+{
+    T var;
+};
+
+template <> class cls<char>;
+//template <> class cls<char>{};
+
+//cls<char> c;
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+}
