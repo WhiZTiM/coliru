23bf0cf732548e090914983a54e81aaf48d9f855
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 11:44:26 +0000

    Update archive 6f

---
+++ trunk/Archive2/6f/b65d570a81841f/main.cpp
@@ -0,0 +1,14 @@
+//Title of this code
+
+#include <iostream>
+using namespace std;
+
+void secondmain()
+{
+    cout << "Breaking all the rules!\n";
+}   
+int main()
+{
+    cout << "Let the rhythem hit em!\n";
+    secondmain();
+}
