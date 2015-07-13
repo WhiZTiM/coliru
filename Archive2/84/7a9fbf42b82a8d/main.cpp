a47ee07694615e53f4f8eb9965fbedeb8c58c13b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 16:54:35 +0000

    Update archive 84

---
+++ Archive2/84/7a9fbf42b82a8d/main.cpp
@@ -0,0 +1,86 @@
+#include <string>
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Pointed{
+public:
+    Pointed(){std::cout << "Construction\n";}
+    ~Pointed(){std::cout << "Destruction\n";}
+};
+
+class A1 {
+
+private:
+  Pointed * p_str;
+
+public:
+  A1() : p_str(new Pointed())
+  {
+      throw "A constructor";
+  }
+
+  virtual ~A1() {
+    cout << "Deletion in ~A1()\n";
+    delete p_str;
+  }
+};
+
+class B : public A1 {
+public:
+  B() {
+      throw "B constructor";
+  }
+
+  virtual ~B() override {
+  }
+
+  virtual void Test() {
+    cout << "B::Test()" << endl;
+  }
+};
+
+class A2 {
+
+private:
+  std::unique_ptr<Pointed> p_str;
+
+public:
+  A2() : p_str(make_unique<Pointed>()){}
+  virtual ~A2() {}
+
+};
+
+
+class C : public A2 {
+public:
+  C() {
+      throw "C constructor";
+  }
+
+  virtual ~C() override {
+  }
+
+  virtual void Test() {
+    cout << "C::Test()" << endl;
+  }
+};
+
+int main(int, char**)
+{
+ cout << "B::A1 (raw pointers)\n";
+
+try{
+  B b;
+    }
+    catch(...){std::cout << "Exception thrown for b\n";}
+    
+cout << "\nC::A2 (unique pointers)\n";
+    
+    try{
+  C c;
+    }
+    catch(...){std::cout << "Exception thrown for c\n";}
+
+    return 0;
+}
