a822443c35409b7e3879024612617e4e0ffd18a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 17:01:29 +0000

    Update archive 0d

---
+++ Archive2/0d/418c55ebf81f18/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+// templated
+
+template<typename Type>
+struct T {
+    T() = default;
+    T(Type const&) {}
+};
+
+template<typename Type1, typename Type2>
+T<Type1> operator*(T<Type1> lhs, T<Type2> const&) { return lhs; }
+
+// non templated
+
+struct R {
+    R() = default;
+    R(int) {}
+};
+
+R operator*(R lhs, R const&) { return lhs; }
+
+int main() {
+    auto x = R() * 1;
+    auto y = T<int>() * 1;
+}
