925d6cef8b3908cd41e8c1b3fd088f42567b7a6e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 15:58:18 +0000

    Update archive d9

---
+++ Archive2/d9/dfd0e3bd32b6c7/main.cpp
@@ -0,0 +1,50 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <functional>
+#include <vector>
+
+// #define ADL
+
+template<class T>
+struct toy {
+    T t = {};
+    toy& operator*=(T const& o){
+        t*=o;
+        return *this;
+    }
+#ifdef ADL
+    friend toy operator*(toy t, T s){
+        t*=s;
+        return t;
+    }
+    friend toy operator*(T s, toy t){
+        t*=s;
+        return t;
+    }
+#endif
+};
+#ifndef ADL
+template<class T>
+    toy<T> operator*(toy<T> t, T s){
+        t*=s;
+        return t;
+    }
+template<class T>
+    toy<T> operator*(T s, toy<T> t){
+        t*=s;
+        return t;
+    }
+#endif
+
+struct bob {
+    int state;
+    operator int&(){return state;}
+};
+
+int main() {
+    bob b;
+    toy<int> x;
+    b * x;
+}
