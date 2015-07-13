4e495b49933b1e0e4ac58b940fab2d709b30c9b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 22:16:34 +0000

    Update archive 55

---
+++ trunk/Archive2/55/c132c49d5b9a49/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <type_traits>
+ 
+template <typename...>
+struct vargs {};
+ 
+template <typename T>
+struct outer
+{
+    template <typename>
+	struct inner;
+ 
+	template <typename... Args>
+	struct inner<vargs<Args...>>
+	{
+		using result = std::true_type;
+	};
+ 
+	template <template <typename...> class C, typename... Args>
+	struct inner<C<Args...>>
+	{
+		using result = std::false_type;
+	};
+};
+ 
+struct eval_tag {};
+ 
+template <typename T>
+using eval_t = typename outer<eval_tag>::inner<T>::result;
+ 
+int main() {
+	eval_t<vargs<int, char, bool>>::foo();
+	return 0;
+}
