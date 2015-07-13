e0e5f86436200ab46a4617cc7cec949a7d0956f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 15:35:37 +0000

    Update archive 5c

---
+++ Archive2/5c/66c2500afba69e/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+template<typename Container>
+void foo(Container && v)
+{
+    v[100] = 1;
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+int main()
+{
+    foo(std::vector<int>(1000));
+    
+    std::vector<int> buffer(1000);
+    foo(buffer);
+}
