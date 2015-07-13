e360977be71ce3d51c0e37a45d1ed7851c948056
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-13 21:58:44 +0000

    Update archive 7c

---
+++ Archive2/7c/126681c1985060/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+ 
+int main()
+{
+    int j = 2;
+    do { // compound statement is the loop body
+        j += 2;
+        std::cout << j << " ";
+    } while (j < 9);
+    std::cout << '\n';
+    // common situation where do-while loop is used
+    std::string s = "aba";
+    std::sort(s.begin(), s.end());
+    do std::cout << s << '\n'; // expression statement is the loop body
+    while(std::next_permutation(s.begin(), s.end()));
+}
