9ca567809e6a9340247e6ce018d0a19d33833521
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 12:41:48 +0000

    Update archive dc

---
+++ Archive2/dc/5c4d37b0279d5d/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+void randtest(unsigned int seed, int* output, int count)
+{
+    srand (seed);
+    
+    for(int i = 0; i < count; i++)
+    {
+        output[i] = rand() % 100 + 1;
+        cout << output[i] << " ";
+    }
+    
+    cout << "\n";
+
+}
+
+int main()
+{
+    unsigned int seed;
+    const int count = 5;
+    int num[count];
+
+    seed = 1;    
+    cout << "seed = 1 , first:\n";
+    randtest(seed, num, count);
+    
+    cout << "seed = 1 , second:\n";
+    randtest(seed, num, count);
+
+    seed = 2;
+    cout << "\nseed = 2 , first:\n";
+    randtest(seed, num, count);
+    
+    cout << "seed = 2 , second:\n";
+    randtest(seed, num, count);
+
+}
