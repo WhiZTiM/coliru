f8e6c610608889992a6e30dc75b0f9548abd9588
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 00:28:18 +0000

    Update archive 35

---
+++ trunk/Archive2/35/5fc65c6a36586e/main.cpp
@@ -0,0 +1,19 @@
+template <typename T>
+struct Bar;
+
+template <typename T>
+struct Foo
+{
+    Bar<T> b;
+};
+
+template <typename T>
+struct Bar
+{
+    Bar(int = 0) {}
+};
+
+int main()
+{
+    Foo<char> f;
+}
