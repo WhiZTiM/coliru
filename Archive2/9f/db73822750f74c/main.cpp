4e3ae5c678fe36b8602927141af8a7ab552d809a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 20:54:26 +0000

    Update archive 9f

---
+++ Archive2/9f/db73822750f74c/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+void randtest(const int count)
+{
+  for(int i = 0; i < count; i++)
+  {
+    cout << rand() % 100 + 1 << " ";
+  }
+
+  cout << "\n";
+}
+
+int main()
+{
+  unsigned int seed;
+  const int count = 5;
+
+  seed = 1;    
+  srand (seed);
+  cout << "种子 = " << seed << ", 第一次: ";
+  randtest(count);
+    
+  srand (seed);
+  cout << "种子 = " << seed << ", 第二次: ";
+  randtest(count);
+
+  cout << "----------------------------------\n";
+  
+  seed = 2;
+  srand (seed);
+  cout << "种子 = " << seed << ", 第一次: ";
+  randtest(count);
+
+  srand (seed);
+  cout << "种子 = " << seed << ", 第二次: ";
+  randtest(count);
+  
+  cout << "----------------------------------\n";
+
+  seed = 1;    
+  srand (seed);
+  cout << "种子 = " << seed << ", 第一次: ";
+  randtest(count);
+    
+  cout << "种子 = " << seed << ", 第二次: ";
+  randtest(count);
+}
