8bad1b4f503b37da1297030d8bbf171848a4b97d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 03:59:41 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/5b25762d2fa701/cmd.sh
@@ -0,0 +1,4 @@
+set -x ;
+g++-5.1 -std=c++11 -O0 -Wall -pedantic -Wextra -pthread main.cpp && nm ./a.out | grep Foo
+# clang++ -std=c++14 -O0 -Wall -pedantic -Wextra -pthread main.cpp -stdlib=libc++ -lsupc++ && ./a.out
+# clang++ -std=c++14 -O3 -Wall -pedantic -Wextra -pthread main.cpp && ./a.out
