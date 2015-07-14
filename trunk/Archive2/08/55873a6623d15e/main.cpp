ae1aa980acb39edc253092fd37cc96890fca760c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 07:44:45 +0000

    Update archive 08

---
+++ trunk/Archive2/08/55873a6623d15e/main.cpp
@@ -0,0 +1,26 @@
+namespace ForADL {
+
+  struct X {
+    int i;   
+  };
+  
+  X& operator*= (X& lhs, int rhs) { lhs.i *= rhs; return lhs; }
+
+  struct Z {
+    X x;
+    operator X&() {return x;}
+  };
+
+}
+  
+struct Y : ForADL::X 
+{};
+
+   
+int main() { 
+  Y y;
+  y*=5;
+  
+  ForADL::Z z;
+  z *= 42;
+}
