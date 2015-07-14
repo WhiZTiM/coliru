e0cb153e36d315be2c971c607cbb3e6a75277edf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 19:59:44 +0000

    Update archive ea

---
+++ trunk/Archive2/ea/3be72135f174fe/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+void randtest(int* output, int count)
+{
+  for(int i = 0; i < count; i++)
+  {
+    output[i] = rand() % 100 + 1;
+    cout << output[i] << " ";
+  }
+
+  cout << "\n";
+}
+
+int main()
+{
+  unsigned int seed;
+  const int count = 5;
+  int num[count];
+
+  seed = 1;    
+  srand (seed);
+  cout << "种子 = " << seed << ", 第一次: ";
+  randtest(num, count);
+    
+  srand (seed);
+  cout << "种子 = " << seed << ", 第二次: ";
+  randtest(num, count);
+
+  cout << "----------------------------------\n";
+  
+  seed = 2;
+  srand (seed);
+  cout << "种子 = " << seed << ", 第一次: ";
+  randtest(num, count);
+
+  srand (seed);
+  cout << "种子 = " << seed << ", 第二次: ";
+  randtest(num, count);
+  
+  cout << "----------------------------------\n";
+
+  seed = 1;    
+  srand (seed);
+  cout << "种子 = " << seed << ", 第一次: ";
+  randtest(num, count);
+    
+  cout << "种子 = " << seed << ", 第二次: ";
+  randtest(num, count);
+
+}
