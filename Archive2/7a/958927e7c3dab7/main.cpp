6045bd6193db8f79d3fa825c6e2926470a740093
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 00:38:03 +0000

    Update archive 7a

---
+++ Archive2/7a/958927e7c3dab7/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+ 
+int main() {  
+    std::unordered_multimap<int,char> example = {{1,'a'},{2,'b'},{2,'c'}};
+    auto search = example.find(2);
+    if(search == example.end()) {
+        std::cout << "Not found\n";
+        return 0;
+    }
+    
+    while (search != example.end()) {
+        std::cout << "Found " << search->first << " " << search->second << '\n';
+        search++;
+    }
+}
