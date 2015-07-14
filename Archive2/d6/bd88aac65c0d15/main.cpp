12066b3caa95ea11409f7dabb0ce214c539b35b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-14 13:28:03 +0000

    Update archive d6

---
+++ Archive2/d6/bd88aac65c0d15/main.cpp
@@ -0,0 +1,69 @@
+#include <boost/range/any_range.hpp>
+#include <boost/range/adaptor/transformed.hpp>
+#include <vector>
+#include <memory>
+#include <cstdio>
+
+class DummyElement
+{
+public:
+    float f_;
+};
+
+using ElementRange = boost::any_range < DummyElement*, boost::bidirectional_traversal_tag >;
+
+using DummyElementUPtr = std::unique_ptr < DummyElement > ;
+using Int = int;
+
+class BoostAnyTest
+{
+public:
+    BoostAnyTest()
+    { 
+        for (int i = 0; i < 10; ++i)
+        {
+            auto element = DummyElementUPtr(new DummyElement());
+            printf("BoostAnyTest::ctor() 0x%p\n", element.get());
+
+            c_.emplace_back(std::tuple<Int, DummyElementUPtr>(i, std::move(element)));
+        }
+    }
+
+public:
+    ElementRange GetAll();
+
+//private:
+    using _ContainerType = std::vector < std::tuple<Int, std::unique_ptr<DummyElement>> > ;
+    _ContainerType    c_;
+};
+
+
+ElementRange
+BoostAnyTest::GetAll()
+{
+    auto transform = [ ] (const _ContainerType::value_type& v) -> DummyElement*
+    {
+        return std::get<1>(v).get();
+    };
+
+    return c_ | boost::adaptors::transformed(transform);
+}
+
+
+int main()
+{
+    BoostAnyTest    any;
+
+    //auto range = any.GetAll();
+    auto transform = [ ] (const BoostAnyTest::_ContainerType::value_type& v) -> DummyElement*
+    {
+        return std::get<1>(v).get();
+    };
+
+    auto range = any.c_ | boost::adaptors::transformed(transform);
+
+    std::for_each(std::begin(range), std::end(range), [ ] (DummyElement* element)
+    { 
+        printf("TestBoostAnyRange() 0x%p\n", element);
+    });
+}
