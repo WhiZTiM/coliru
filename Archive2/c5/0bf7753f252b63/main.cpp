646dec3d2d90c32a1b22d70969e04259758d3bc5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-15 17:45:38 +0000

    Update archive c5

---
+++ Archive2/c5/0bf7753f252b63/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+class A
+{
+    public:
+        A(int x) : m_someDataMember(x) {}
+        ~A() {}
+        int someMethod(){ return m_someDataMember; }
+
+    private:
+        int m_someDataMember;
+};
+
+class B
+{
+    public:
+    B(){}
+    ~B(){}
+    void anotherMethod(const int& someInt){ /*...*/}
+};
+
+int main()
+{
+    A a(5);
+    B b;
+    b.anotherMethod(a.someMethod());
+}
