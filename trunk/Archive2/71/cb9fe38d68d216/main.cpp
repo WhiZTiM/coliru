c6adfb51c5b8b2f9ee7945cd18b703627a8b370a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-15 11:25:42 +0000

    Update archive 71

---
+++ trunk/Archive2/71/cb9fe38d68d216/main.cpp
@@ -0,0 +1,49 @@
+      	
+			     								                        |
+												                        | RGBA
+												                        |
+                        	           	                           ColorConvert
+                            	       			                        |
+                                	   			                        | IYUV
+            		 ___________________________________________________|____________________________________________________
+            		|                                                   |                                                    |
+       	      ChannelExtract                                      ChannelExtract                                       ChannelExtract
+                	|                                                   |                                                    |
+                	| Y                                                 | U                                                  | V
+ _______ ___________|___________                       _________________|__________                         _________________|__________
+|       |                       |                     |     |                      |                       |     |                      |
+|   ROIExtract     ...      ROIExtract                | ROIExtract     ...     ROIExtract                  | ROIExtract     ...     ROIExtract
+|       |	                    |                     |     |                      |                       |     |                      |
+|       | Y_1                   | Y_n                 |     | U_1                  | U_n                   |     | V_1                  | V_n
+|       |                       |                     |     |                      |                       |     |                      |
+|     Remap        ...        Remap                   |   Remap        ...       Remap                     |   Remap        ...       Remap
+|     	|                       |                     |     |                      |                       |     |                      |
+|  _____| Y_1_r            _____| Y_n_r               |     | U_1_r                | U_1_r                 |     | V_1_r                | V_n_r
+| |     |                 |     |                     |_____|______________________|                       |_____|______________________|
+| | ROIExtract     ...    | ROIExtract                                  |                                                    |
+| |     |                 |     |                                  ROICombine                                           ROICombine
+| |     | Y_1_r_R         |     | Y_n_r_R                               |                                                    |
+| |		|                 |     |                                       | U_r                                                | V_r
+| |EqualizeHist    ...    |EqualizeHist                                 |                                                    |
+| |     |                 |     |                                       |                                                    |
+| |     | Y_1_r_R_eq      |     | Y_n_r_R_eq                            |                                                    |
+| |_____|                 |_____|                                       |                                                    |
+|       |                       |                                       |                                                    |
+|  ROICombine      ...     ROICombine                                   |                                                    |
+|       |                       |                                       |                                                    |
+|       | Y_1_r_eq              | Y_n_r_eq                              |                                                    |      
+|_______|_______________________|                                       |                                                    |
+                    |                                                   |                                                    |
+               ROICombine                                               |                                                    |
+                    |                                                   |                                                    |
+                    | Y_r_eq                                            |                                                    |
+                    |___________________________________________________|____________________________________________________|
+                                                                        |
+                                                                  ChannelCombine
+                                                                        |
+                                                                        | IYUV_r_eq
+                                                                        |
+                                                                   ColorConvert
+                                                                        |
+                                                                        | RGBA_r_eq
+                                                                        |
