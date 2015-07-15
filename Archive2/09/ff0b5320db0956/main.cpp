5cf9f03abfea66f2dfe0f5ad140c8e6843f45e2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-15 17:21:01 +0000

    Update archive 09

---
+++ Archive2/09/ff0b5320db0956/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  //Let's just use a variable for the radius.
+  float c_radius = 1.2;
+  std::cout << c_radius << std::endl;
+
+  c_radius = 2.4;
+
+  //Let's print out the area.
+  float c_area = c_radius * c_radius * 3.1415;
+  std::cout << c_radius << std::endl;
+
+  return 0;
+}
