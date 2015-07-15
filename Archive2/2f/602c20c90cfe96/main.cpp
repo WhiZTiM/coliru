4c460b25aab5a473ba32d43c5bdf5b32ea0dcbd5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-15 16:56:57 +0000

    Update archive 2f

---
+++ Archive2/2f/602c20c90cfe96/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "┌──────────────────┐" << std::endl;
+    std::cout << "| ┌────┬────┬────┐ |" << std::endl;
+    std::cout << "| │  6 |  6 │  6 │ |" << std::endl;
+    std::cout << "| ├────┼────┼────┤ |" << std::endl;
+    std::cout << "| │ ha │ me │ kh │ |" << std::endl;
+    std::cout << "| └────┴────┴────┘ |" << std::endl;
+    std::cout << "└──────────────────┘" << std::endl;
+    return 0;
+}
