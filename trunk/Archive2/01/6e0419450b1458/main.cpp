5983d68a7fd89c8525c79cbaafadacb63f7ee4c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-15 17:59:16 +0000

    Update archive 01

---
+++ trunk/Archive2/01/6e0419450b1458/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cxxabi.h>
+#include <typeinfo>
+
+template <typename T>
+const char * type_name()
+{
+    return abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, nullptr);
+}
+
+template <typename... Args>
+struct vargs {};
+
+namespace details   
+{
+    template <typename K>
+    struct outer
+    {
+        template <typename Arg>
+        struct inner
+        {
+            using result = Arg;
+        };
+    };
+}
+
+struct tag {};
+
+template <typename T>
+using test_t = typename details::outer<tag>::inner<T>::result;
+
+int main()
+{
+    using t = test_t<vargs<char, int>>;
+    std::cout << type_name<t>() << '\n';
+    return 0;
+}
