dc7656e787c8aabbd6cad97f03d57d3b13ab5967
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-16 20:12:40 +0000

    Update archive 49

---
+++ Archive2/49/ea79b0be98705c/main.cpp
@@ -0,0 +1,19 @@
+#include <map>
+#include <iostream>
+#include "boost/assign.hpp"
+
+template<typename Key, typename T>
+T convert(const Key &k, const T &d, const std::map<Key, T> &m) {
+    typename std::map<Key, T>::const_iterator it = m.find(k);
+    return it == m.end() ? d : it->second;
+}
+
+enum MUSIC { ROCK, RAP, EDM, COUNTRY };
+enum COLOR { RED, BLUE, ORANGE, WHITE };
+
+int main()
+{
+    COLOR c = convert(COUNTRY, WHITE,
+        boost::assign::map_list_of (RAP, RED) (EDM, BLUE) (ROCK, RED));
+    std::cout << c << std::endl;
+}
