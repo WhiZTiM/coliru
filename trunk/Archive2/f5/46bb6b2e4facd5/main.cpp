7b1b3ea56d2b449df9bbe827efc82e8b72c72617
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-16 01:15:31 +0000

    Update archive f5

---
+++ trunk/Archive2/f5/46bb6b2e4facd5/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+int main()
+{
+  int* p_one = new int;
+  *p_one = 25;
+  std::cout << *p_one << std::endl;
+  
+  {
+      int* p_two = new int;
+      *p_two = 100;
+      std::cout << *p_two << std::endl;
+      
+      if(p_one != nullptr) delete p_one;    // Is this correct for stoping memory leak?
+      p_one = p_two;                        // Delete p_one before asigning p_two's address.
+      
+      std::cout << *p_one << std::endl;     // p_one now has the location set aside from p_two's "new"
+  }   
+  std::cout << *p_one << std::endl;
+}
