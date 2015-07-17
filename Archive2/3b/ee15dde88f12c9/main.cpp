8a611570d7c6dac5da00b8eefe0ac978267eb8b8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-17 02:33:37 +0000

    Update archive 3b

---
+++ Archive2/3b/ee15dde88f12c9/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+#define ALPHA "Alpha"
+
+#define BETA "Beta"
+
+#define GAMMA "Gamma"
+
+struct EnumValue
+{
+    EnumValue(std::string _name): name(std::move(_name)), id(gid){++gid;}
+    std::string name;
+    int id;
+    // also provide implicit conversion operators to std::string and int
+    private:
+        static int gid;
+};
+
+int EnumValue::gid = 0;
+
+class MyEnum
+{
+    public:
+    static const EnumValue& Alpha;
+    static const EnumValue& Beta;
+    static const EnumValue& Gamma;
+    static const EnumValue& StringToEnumeration(std::string _in)
+    {
+        return enumerations.find(_in)->second;
+    }
+    
+    static const EnumValue& IDToEnumeration(int _id)
+    {
+        auto iter = std::find_if(enumerations.cbegin(), enumerations.cend(), 
+        [_id](const map_value_type& vt)
+        { 
+            return vt.second.id == _id;
+        });
+        return iter->second;
+    }
+    
+    static const size_t size()
+    {
+        return enumerations.size();
+    }
+    
+    private:
+    typedef std::unordered_map<std::string, EnumValue>  map_type ;
+    typedef map_type::value_type map_value_type ;
+    static const map_type enumerations;
+};
+
+
+const std::unordered_map<std::string, EnumValue> MyEnum::enumerations =
+{ 
+    {ALPHA, EnumValue(ALPHA)}, 
+    {BETA, EnumValue(BETA)},
+    {GAMMA, EnumValue(GAMMA)}
+};
+
+const EnumValue& MyEnum::Alpha = enumerations.find(ALPHA)->second;
+const EnumValue& MyEnum::Beta  = enumerations.find(BETA)->second;
+const EnumValue& MyEnum::Gamma  = enumerations.find(GAMMA)->second;
+
+
+int main()
+{
+    std::cout << MyEnum::Alpha.name << std::endl; // Alpha
+    std::cout << MyEnum::Beta.name << std::endl; // Beta
+    std::cout << MyEnum::Gamma.name << std::endl; // Gamma
+    
+    std::cout << MyEnum::StringToEnumeration(ALPHA).id << std::endl; //should give 0
+    std::cout << MyEnum::IDToEnumeration(0).name << std::endl; //should give "Alpha"
+}
