adb7729c58581f2dcefca4dbbf2a3e69eb67eda0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-17 13:23:32 +0000

    Update archive 6d

---
+++ Archive2/6d/6c961e7ef81d0f/main.cpp
@@ -0,0 +1,12 @@
+#include <map>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    u32string ramp = U"ÐðŁłŠšÝýÞþŽž";
+    cout << ramp << "\n";    
+    cout << ramp[0] << "\n";
+}
