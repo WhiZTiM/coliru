2dcad21482e0dd6ba5c3e332bb6be252063d3b29
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-18 07:18:51 +0000

    Update archive 25

---
+++ Archive2/25/3c88d616e8034d/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+const int _size = 4000;
+
+class factorial  {
+
+private:
+
+	int m_array[_size];
+    int m_artadryal;
+    int m_mnacord = 0; 
+    int m_qanak;
+    int h = 1;
+
+public:
+	void _factorial(int);
+	void _display();
+	
+};
+
+void factorial::_factorial(int _n) {
+    
+    while(h--) {
+       m_array[0] = 1;  
+       m_qanak = 1;    
+        m_mnacord = 0; 
+        
+        
+       for(int i = 1; i <= _n; i++) {
+            for(int j = 0; j < m_qanak; j++) {
+               m_artadryal = m_array[j] * i + m_mnacord;
+               m_array[j] = m_artadryal % 10;
+               m_mnacord = m_artadryal / 10;
+            }
+            
+             while(m_mnacord > 0) { 
+               m_array[m_qanak] = m_mnacord % 10;
+               m_mnacord = m_mnacord / 10;
+               m_qanak++;
+            }
+        }
+    }
+}
+
+void factorial::_display() {
+	for ( int i = m_qanak - 1; i >= 0; i--) {
+            cout << m_array[i];
+	}
+}
+
+int main() {
+	int _number;
+	factorial _obj;
+    cin >> _number;
+	
+	_obj._factorial(_number);
+	_obj._display();
+	
+	return 0;
+}
