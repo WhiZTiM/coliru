410e1ffc1d25fd4aea86f66d2e5fb5f076afaf02
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-19 00:37:42 +0000

    Update archive a4

---
+++ trunk/Archive2/a4/fac7535d18af74/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define System S s;s
+#define public
+#define static
+#define void int
+#define main(x) main()
+struct F{void println(char* s){std::cout << s << std::endl;}};
+struct S{F out;};
+
+public static void main(String[] args) {
+  System.out.println("Hello World!");
+}
