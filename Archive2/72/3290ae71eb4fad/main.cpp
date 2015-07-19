f45813679ed2df8c00cad50d0431e61d3fe98960
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-19 20:45:51 +0000

    Update archive 72

---
+++ Archive2/72/3290ae71eb4fad/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+ 
+using namespace std;
+ 
+int i = 0;
+std::mutex i_mutex;
+ 
+int j = 4;
+std::mutex j_mutex;
+ 
+void actOnI() {
+    i_mutex.lock();
+    cout << ++i << endl;
+    i_mutex.unlock();
+}
+ 
+ void actOnJ() {
+    j_mutex.lock();
+    cout << ++j << endl;
+    j_mutex.unlock();
+}
+ 
+int main()  {
+    std::thread iThread1(actOnI);
+    std::thread iThread2(actOnI);
+    std::thread jThread1(actOnJ);
+    std::thread jThread2(actOnJ);
+    iThread1.join();
+    iThread2.join();
+    jThread1.join();
+    jThread2.join();
+}
