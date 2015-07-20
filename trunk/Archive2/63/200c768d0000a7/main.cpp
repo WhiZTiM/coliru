84e195a2ab23a11ef02a156ea235ac7b36635fc6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 12:49:44 +0000

    Update archive 63

---
+++ trunk/Archive2/63/200c768d0000a7/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <array>
+#include <utility>
+#include <type_traits>
+#include <cstddef>
+#include <boost/lexical_cast.hpp>
+
+template <typename T>
+struct converter;
+
+template <std::size_t... Is>
+struct converter<std::index_sequence<Is...>>
+{
+    std::tuple<decltype(void(Is), std::string{})...> t;
+    
+    converter(const std::array<std::string, sizeof...(Is)>& arr) : t(arr[Is]...) {}
+    
+    template <typename... Ts>
+    operator std::tuple<Ts...>()
+    {
+        return std::tuple<Ts...>(boost::lexical_cast<typename std::decay<Ts>::type>(std::get<Is>(t))...);
+    }
+};
+
+template <std::size_t N, typename... Ts>
+converter<std::make_index_sequence<N>> Convert(const std::array<std::string, N>& arr)
+{
+    return {arr};
+}
+
+int main()
+{
+    std::array<std::string, 2> arr{ "1", "2.0" };
+    
+    std::tuple<int, double> t = Convert(arr);
+    
+    std::cout << std::get<0>(t) << " " << std::get<1>(t) << std::endl;
+}
