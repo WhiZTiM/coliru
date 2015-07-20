1314c3b9e3b500047af46d8f2b0b09dcedff1705
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 11:54:36 +0000

    Update archive d8

---
+++ trunk/Archive2/d8/ac187e285e2da3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <type_traits>
+
+struct movable
+{
+    char *a;
+    
+    movable(char *arg) noexcept : a(arg) {}
+    
+    movable(movable const &) = delete;
+    movable &operator=(movable const &) = delete;
+};
+
+int main()
+{
+    std::cout << std::is_nothrow_constructible<movable, char *>::value << std::endl;
+}
