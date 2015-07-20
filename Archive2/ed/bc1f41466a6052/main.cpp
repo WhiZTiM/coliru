16b74a03fd1aca4f71cc0c46215c29af492eb936
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 14:49:18 +0000

    Update archive ed

---
+++ Archive2/ed/bc1f41466a6052/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+struct DangerousA {
+    DangerousA() : a(5) { }
+
+    virtual ~DangerousA() { }
+
+    virtual void abc() {
+        std::cout << "ABCDEFG..." << std::endl;
+    }
+
+    virtual void printA() {
+        std::cout << "A: " << a << std::endl;
+    }
+
+    int a;
+};
+
+struct DangerousB {
+    DangerousB() : a(6) { }
+
+    virtual ~DangerousB() { }
+
+    virtual void printA() {
+        std::cout << "B: " << a << std::endl;
+    }
+
+    int a;
+};
+
+int main()
+{
+    std::cout << "Starting with a class DangerousA & DangerousB, both are unrelated" << std::endl;
+    std::cout << "Let's see what happens when we cast DangerousA to DangerousB...\n" << std::endl;
+
+    DangerousA* a = new DangerousA();
+    DangerousB* b = new DangerousB();
+
+    a->printA();
+    (reinterpret_cast<DangerousB*>(a))->printA();
+
+    std::cout << "\nDone" << std::endl;
+    std::cout << "Note how DangerousA's first function was called, not DangerousB's printA function." << std::endl;
+    
+    delete b;
+    delete a;
+}
