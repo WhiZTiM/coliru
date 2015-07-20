f4b3b6fb203814fb625f916fcd6eb348dc435100
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 00:13:10 +0000

    Update archive 63

---
+++ trunk/Archive2/63/9919d67b6493b6/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp -lboost_thread -lboost_system
+(for a in {1..5}; do (sleep 1.$RANDOM; echo -e "hello world $RANDOM\\0" | netcat 127.0.0.1 6767)& done)
+./a.out | sort | uniq -c | sort -n | tail
