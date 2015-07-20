5821a26e8e53e00dc6e9941b0fff5faddf0171b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 13:12:28 +0000

    Update archive 9c

---
+++ trunk/Archive2/9c/cf73a8b6201272/main.cpp
@@ -0,0 +1,26 @@
+#include <type_traits>
+#include <tuple>
+
+#define REPEAT_2(M, N) M(N) M(N+1)
+#define REPEAT_4(M, N)   REPEAT_2  (M, N) REPEAT_2(M, N+2)
+#define REPEAT_8(M, N)   REPEAT_4  (M, N) REPEAT_4(M, N+4)
+#define REPEAT_16(M, N)  REPEAT_8  (M, N) REPEAT_8(M, N+8)
+#define REPEAT_32(M, N)  REPEAT_16 (M, N) REPEAT_16(M, N+16)
+#define REPEAT_64(M, N)  REPEAT_32 (M, N) REPEAT_32(M, N+32)
+#define REPEAT_128(M, N) REPEAT_64 (M, N) REPEAT_64(M, N+64)
+
+template <typename... T>
+class A {
+    #define FRIEND(N) friend std::tuple_element_t<std::min((std::size_t)N+1, sizeof...(T)), std::tuple<struct foo, T...>>;
+    REPEAT_128(FRIEND, 0)
+
+    static constexpr int i = 3;
+};
+
+struct X; struct Y; struct Z;
+using ASpec = A<X, Y, Z>;
+struct X {int i = ASpec::i;};
+struct Y {int i = ASpec::i;};
+struct Z {int i = ASpec::i;};
+
+int main() {}
