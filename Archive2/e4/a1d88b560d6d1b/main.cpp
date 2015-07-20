28d98705517ba3c3ba1bbbc6582ed21ae9cf5c72
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 12:35:24 +0000

    Update archive e4

---
+++ Archive2/e4/a1d88b560d6d1b/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <locale>
+#include <cstring>
+
+using namespace std;
+
+template <typename T>
+class Bar : public T {
+private:
+	void Init(const T* money){
+		const auto vTablePtrSize = sizeof(void*);
+
+		memcpy(reinterpret_cast<char*>(this) + vTablePtrSize, reinterpret_cast<const char*>(money) + vTablePtrSize, sizeof(T) - vTablePtrSize);
+	}
+protected:
+	typename T::char_type do_decimal_point() const {
+		return typename T::char_type(',');
+	}
+
+	typename T::char_type do_thousands_sep() const {
+		return typename T::char_type('.');
+	}
+public:
+	Bar(){
+		Init(&use_facet<T>(cout.getloc()));
+	}
+
+	Bar(const T* money){
+		Init(money);
+	}
+};
+
+int main()
+{
+	cout.imbue(locale("en_US.UTF8"));
+
+    long double value;
+	istringstream BarCurrency("1.234.567,89");
+
+	BarCurrency.imbue(locale(cout.getloc(), new Bar<moneypunct<char>>()));
+
+	BarCurrency >> get_money(value);
+
+	cout << value;
+
+	return 0;
+}
