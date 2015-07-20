1425da271294b96dec787396222313bf4e4fe963
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-20 19:44:03 +0000

    Update archive 15

---
+++ Archive2/15/5e9f7fd07b8ccc/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <set>
+#include <utility>
+
+using namespace std;
+
+template<typename T, typename U>
+inline ostream& operator<<(ostream& os, const pair<T,U> &p){
+    os<<"("<<p.first<<","<<p.second<<")";
+    return os;
+}
+
+
+template<typename T>
+inline ostream& operator<<(ostream& os, set<T> &s){
+    os<<"{";
+    for(auto it = s.begin() ; it != s.end() ; it++){
+        if(it != s.begin())
+            os<<",";
+        os<<*it;
+    }
+    os<<"}";
+    return os;
+}
+
+int main()
+{
+    set<pair<int,int>> s {{1,2}};
+    cout<<s<<endl;
+}
