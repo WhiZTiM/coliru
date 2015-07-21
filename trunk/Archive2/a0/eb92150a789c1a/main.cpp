1d70b36dfba8b0a53972764651965235a15e3a17
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-21 00:36:17 +0000

    Update archive a0

---
+++ trunk/Archive2/a0/eb92150a789c1a/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/graph/adjacency_list.hpp>
+
+typedef boost::adjacency_list<
+    boost::multisetS,
+    boost::vecS, 
+    boost::directedS,
+    boost::no_property,
+    boost::no_property,
+    boost::no_property,
+    boost::setS 
+    > Graph;
+
+typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
+
+void test(Edge aE, const Graph &aG) {
+    Vertex sv = boost::source(aE, aG);
+    Vertex tv = boost::target(aE, aG);
+    typename boost::graph_traits<Graph>::out_edge_iterator it, eit;
+    for (boost::tie(it, eit) = boost::edge_range(sv, tv, aG);
+        it != eit;
+        ++it) {
+    }
+}
+
+int main() {
+}
