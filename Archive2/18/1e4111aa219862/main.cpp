a3bed4a3307f91883b0f75522d552de8158fa5a4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-21 16:03:00 +0000

    Update archive 18

---
+++ Archive2/18/1e4111aa219862/main.cpp
@@ -0,0 +1,23 @@
+#include <random>
+#include <limits>
+#include <cstddef>
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+T getRandom()
+{
+    uint8_t* t_as_uint8 = static_cast<uint8_t*>(malloc(sizeof(T)));
+    std::default_random_engine generator(std::random_device{}());
+    std::uniform_int_distribution<uint8_t> distribution;
+    std::generate(t_as_uint8, t_as_uint8 + sizeof(T), [&] () {return distribution(generator); });
+    return *(reinterpret_cast<T*>(t_as_uint8));
+}
+
+int main()
+{
+    
+    std::cout << getRandom<int>() << std::endl;
+
+    return 0;
+}
