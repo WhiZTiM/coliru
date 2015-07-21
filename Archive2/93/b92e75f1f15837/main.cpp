12841a60b398e44bab2a6b5cf2f90dc9634df920
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-21 19:45:03 +0000

    Update archive 93

---
+++ Archive2/93/b92e75f1f15837/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+    std::string password = "PassWord";
+    size_t upper_case_count = std::count_if( password.cbegin(), password.cend(), [](int c){return std::isupper(c);} );
+    std::cout << upper_case_count;
+}
