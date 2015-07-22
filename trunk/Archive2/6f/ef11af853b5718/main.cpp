845451d4c3acf25d22f83de505edfbc2fd13520b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 13:38:13 +0000

    Update archive 6f

---
+++ trunk/Archive2/6f/ef11af853b5718/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+#include <memory>
+
+struct Proxy
+{
+    typedef std::map<int, int> InputDataMap;    
+    int a;
+};
+
+template<class C, class D>
+void InsertDataIntoInputMap(
+    const typename C::InputDataMap::key_type& key,
+    D&& val)
+{
+    std::cout << "Not shared\n";
+}
+
+template<class C, class D>
+void InsertDataIntoInputMap(
+    const typename C::InputDataMap::key_type& key,
+    const std::shared_ptr<D> val)
+{
+    if (val)
+    {
+        std::cout << "Shared\n";
+    }
+}
+
+int main() {
+    int a;
+    
+    double x = 12.3;
+    std::shared_ptr<double> x2(new double(4.5));
+
+    InsertDataIntoInputMap<Proxy>(a, x2);
+}
