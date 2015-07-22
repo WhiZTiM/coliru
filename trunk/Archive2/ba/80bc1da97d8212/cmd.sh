519f8f1194928fc40ee51cfb6fdbc0ea4c9da1ec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 07:25:58 +0000

    Update archive ba

---
+++ trunk/Archive2/ba/80bc1da97d8212/cmd.sh
@@ -0,0 +1,2 @@
+echo ~~~~~~GCC~~~~~~ && g++ -std=c++14 -pedantic -Wall main.cpp && ./a.out
+echo ~~~~~CLANG~~~~~ && clang++ -std=c++14 -pedantic -Wall main.cpp && ./a.out
---
+++ trunk/Archive2/ba/80bc1da97d8212/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char **argv){
+   switch (argc) {
+case 0:
+    ; int foo;
+    foo = argc;
+    break;
+default:
+    break;
+   } 
+}
+
