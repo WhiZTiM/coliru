aebfda240f49859eb1837d55be69a9971a6b7b63
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 07:02:19 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/d1ed0ea0105d23/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <ctime>
+
+std::istream &operator>>(std::istream &is, struct tm &t) { 
+    char ign;
+    is >> t.tm_year >> ign >> t.tm_mon >> ign >> t.tm_mday;
+    t.tm_year -= 1900;
+    --t.tm_mon;
+    mktime(&t);
+    return is;
+}
+
+std::ostream &operator<<(std::ostream &os, struct tm const &t) { 
+    char buffer[1024];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", &t);
+    return os << buffer;
+}
+
+int main() {
+    std::istringstream in("2015-07-26");
+    struct tm im_lazy{};
+    in >> im_lazy;
+    std::cout << im_lazy;
+}
