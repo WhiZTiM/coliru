d7703278b2d3b9528ceee846109d8411990adede
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 10:54:49 +0000

    Update archive 8d

---
+++ trunk/Archive2/8d/27b45badca0a30/main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+class A
+{
+    //char name[40] = { "error with gcc" } ; // Compiler error
+    char otherName[40]  = {'g','c','c'};
+public:
+    const char *getName() { return otherName; }
+};
+
+void Foo()
+{
+    char sayMyName = {"Heizenberg"};
+}
+
+
+int main()
+{
+    A a;
+
+    std::cout << a.getName() << std::endl;
+}
