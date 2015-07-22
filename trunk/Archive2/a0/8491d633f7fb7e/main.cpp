ed4d7edb5cf911a6cd1fa22eedfe5577a2417d3d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 13:00:25 +0000

    Update archive a0

---
+++ trunk/Archive2/a0/8491d633f7fb7e/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int ft_atoi(const char *str)
+{
+    int sign;
+    int val;
+    int nbr;
+
+    sign = 1;
+    val = 0;
+    nbr = 0;
+    
+    while (*str)
+    {
+        if (!val && *str == '-')
+        {
+            sign = -1;
+        }
+        else
+        {
+            nbr = (int) (*str - '0');
+            val = (val * 10) + nbr;
+        }
+        ++str;
+    }
+    
+    return (val * sign);
+}
+
+
+int main()
+{
+    printf("%d\n", ft_atoi("2464"));
+    printf("%d\n", ft_atoi("-2464"));
+    return 0;
+}
