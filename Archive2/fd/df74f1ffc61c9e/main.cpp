85f3df5af2c6719cf308552c2d6cbbd29770d759
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 19:56:25 +0000

    Update archive fd

---
+++ Archive2/fd/df74f1ffc61c9e/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <new>
+
+struct test1
+{
+    test1() { std::cout << __PRETTY_FUNCTION__ << ' ' << this << '\n'; }
+    
+    static void *operator new(std::size_t size)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ' ' << size << '\n';
+        return nullptr;
+    }
+    static void operator delete(void *p)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ' ' << p << '\n';
+    }
+};
+
+struct test2
+{
+    test2() { std::cout << __PRETTY_FUNCTION__ << ' ' << this << '\n'; }
+    
+    static void *operator new(std::size_t size)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ' ' << size << '\n';
+        throw std::bad_alloc();
+    }
+    static void operator delete(void *p)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ' ' << p << '\n';
+    }
+};
+
+struct test3
+{
+    test3() { std::cout << __PRETTY_FUNCTION__ << ' ' << this << '\n'; }
+    
+    static void *operator new(std::size_t size) noexcept // replace noexcept with throw() in C++03
+    {
+        std::cout << __PRETTY_FUNCTION__ << ' ' << size << '\n';
+        return nullptr;
+    }
+    static void operator delete(void *p)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ' ' << p << '\n';
+    }
+};
+
+template<typename T>
+void tester()
+{
+    std::cout << __PRETTY_FUNCTION__ << ' ' << '\n';
+    try {
+        auto t = new T();
+        delete t;
+    } catch(std::exception const& e) {
+        std::cout << "Caught: " << e.what() << "\n";
+    }
+    std::cout << "Done\n\n";
+}
+
+int main()
+{
+    tester<test1>();
+    tester<test2>();
+    tester<test3>();
+}
