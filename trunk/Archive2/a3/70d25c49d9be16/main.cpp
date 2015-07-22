87e98067775f8390763138796c0ef692d078073d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 14:01:47 +0000

    Update archive a3

---
+++ trunk/Archive2/a3/70d25c49d9be16/main.cpp
@@ -0,0 +1,52 @@
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/map.hpp>
+#include <iostream>
+#include <sstream>
+
+template <typename T>
+struct StateFullCmp {
+    bool invert_;
+    StateFullCmp(bool invert) : invert_(invert) {}
+
+    bool operator()(T const&a, T const& b) const {
+        return invert_? (b<a) : (a<b);
+    }
+};
+
+namespace boost { namespace serialization {
+    template <typename Ar, typename T>
+        void serialize(Ar& ar, StateFullCmp<T>& cmp, unsigned) {
+            ar & cmp.invert_;
+        }
+} }
+
+int main() {
+    using MyMap = std::map<int, std::string, StateFullCmp<int> >;
+    std::stringstream ss;
+
+    {
+        MyMap const m {
+            { { 1, "one" }, { 2, "two" }, { 3, "three" } },
+            StateFullCmp<int> { true } // inverted order
+        };
+
+        std::cout << "====== original order\n";
+        for (auto& e : m)
+            std::cout << e.first << " -> " << e.second << "\n";
+
+        boost::archive::text_oarchive oa(ss);
+        oa << m; 
+    }
+
+    {
+        MyMap deserialized(StateFullCmp<int>{false}); // OOPS
+        boost::archive::text_iarchive ia(ss);
+        ia >> deserialized; 
+
+        std::cout << "====== deserialized order\n";
+        for (auto& e : deserialized)
+            std::cout << e.first << " -> " << e.second << "\n";
+    }
+}
+
