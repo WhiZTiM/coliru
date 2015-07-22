2e32057e1cead531b0a5b9cec35fcf5b1cc7cfa0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-22 15:35:57 +0000

    Update archive 1b

---
+++ trunk/Archive2/1b/a7df9f06caa2b0/main.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <iostream>
+ 
+class C1 {
+    public:
+        int mem1;
+        C1() {}
+};
+
+struct T1 {
+    int mem1;
+    std::string mem2;
+}; // no constructors
+struct T2 { 
+    int mem1;
+    std::string mem2;
+    T2(const T2&) {} // a constructor, but no default
+};
+struct T3 { 
+    int mem1;
+    std::string mem2;
+    T3() {} // user-provided default ctor
+};
+ 
+std::string s{}; // calls default ctor, the value is "" (empty string)
+int main()
+{
+    int n{};     // non-class value-initialization, value is 0
+    double f = double(); // non-class value-init, value is 0.0
+    int* a = new int[10](); // array of 10 zeroes
+ 
+    T1 t1{}; // no ctors: zero-initialized
+             // t1.mem1 is zero-initialized
+             // t1.mem2 is default-initialized
+//    T2 t2{}; // error: has a ctor, but no default ctor
+    T3 t3{}; // user-defined default ctor:
+             // t3.mem1 is default-initialized (the value is indeterminate)
+             // t3.mem2 is default-initialized
+ 
+    std::vector<int> v(3); // value-initializes three ints
+    C1 c;
+ 
+    std::cout << c.mem1 << std::endl;
+    std::cout << s.size() << ' ' << n << ' ' << f << ' ' << a[9] << ' ' << v[2] << '\n';
+    std::cout << t1.mem1 << ' ' << t3.mem1 << '\n';
+    delete[] a;
+}
