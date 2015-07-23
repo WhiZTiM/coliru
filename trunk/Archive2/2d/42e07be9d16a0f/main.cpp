6050484f9672511da1a03401840f5999b73be73a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-23 02:23:48 +0000

    Update archive 2d

---
+++ trunk/Archive2/2d/42e07be9d16a0f/main.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <type_traits>
+
+template<typename T, typename S>
+struct SomeRandomClass
+{
+    int myInt = 0;
+};
+
+template<typename T>
+struct MyTemplateClass
+{
+    void DoSomething(T & t) 
+    {
+       std::cout << "Not specialized" << std::endl;
+    }
+};
+
+template<typename T, typename S>
+struct MyTemplateClass< SomeRandomClass<T, S> >
+{
+    void DoSomething(SomeRandomClass<T,S> & t) 
+    {
+       std::cout << "Specialized" << std::endl;
+    }
+};
+
+
+int main()
+{
+    SomeRandomClass<int, int> a;
+    
+    MyTemplateClass<decltype(a)> b;
+    b.DoSomething(a); // prints "Specialized"
+    
+    double c = 3.14;
+    MyTemplateClass<decltype(c)> d;
+    d.DoSomething(c); // print "Not specialized"
+    
+    return 0;
+}
