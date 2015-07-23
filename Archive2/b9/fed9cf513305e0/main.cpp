75d16f39d9fe2ec46e27dbac08035119a6a40293
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-23 13:42:01 +0000

    Update archive b9

---
+++ Archive2/b9/fed9cf513305e0/main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <iomanip>
+#include <cstddef>
+#include <stdexcept>
+
+void dump_memory(void* mem, std::size_t size)
+{
+    using namespace std;
+    cout << "dump_memory" << endl;
+    unsigned char* data = (unsigned char*)mem;
+    for (std::size_t i = 0; i < size; ++i) {
+        // plan C-casts for the demo
+        cout << setfill('0') << setw(2) << (unsigned int)data[i] << ' ';
+    }
+    cout << endl;
+}
+
+void write_memory(void* mem, std::size_t size)
+{
+    unsigned char* data = (unsigned char*)mem;
+    for (std::size_t i = 0; i < size; ++i) {
+        // plan C-casts for the demo
+        data[i] = 0xBA;
+    }
+}
+
+struct DataBlock {
+    const std::size_t element_count;
+    static constexpr std::size_t element_size = 8;
+    void * data = nullptr;
+
+    explicit DataBlock(std::size_t elements) : element_count(elements)
+    {
+        // required alignment
+        static_assert(alignof(std::max_align_t) >= 8, "alignment error");
+        // further sanity check on element overflow the alignment boundary
+        static_assert(alignof(std::max_align_t) >= element_size, "element overflow");
+        
+        data = ::operator new(elements*element_size);
+    }
+
+    ~DataBlock()
+    {
+        ::operator delete(data);
+        data = nullptr;
+    }
+
+    DataBlock(DataBlock&) = delete; // no copy
+    DataBlock& operator=(DataBlock&) = delete; // no assign
+    // possibly shouldn't move either (then =delete)
+    DataBlock(DataBlock&&) = delete;
+    DataBlock& operator=(DataBlock&&) = delete;
+
+    template <class T>
+    T& construct(std::size_t index)
+    {
+        T* t = new (get_location<T>(index)) T{};
+        return *t;
+    }
+
+    template <class T>
+    void destroy(std::size_t index)
+    {
+        T* t = get_location<T>(index);
+        t->~T();
+    }
+
+    template <class T>
+    T& get(std::size_t index)
+    {
+        T* t = get_location<T>(index);
+        return *t;
+    }
+
+    template <class T>
+    T* get_location(std::size_t index)
+    {
+        // common location for type and size checks
+        static_assert(sizeof(T) <= element_size, "element overflow");
+        if (!(index < element_count))
+            throw std::out_of_range("DataBlock index out of range of constructed size");
+        // http://stackoverflow.com/a/6449951/3747990
+        // C++ WD n4527 3.9.2/5
+        void* t = reinterpret_cast<void*>(reinterpret_cast<unsigned char*>(data) + index*element_size);
+        // 5.4.9/13
+        return static_cast<T*>(t);
+        // C++ WD n4527 5.2.10/7 would allow this to be condensed
+        //T* t = reinterpret_cast<T*>(reinterpret_cast<unsigned char*>(data) + index*element_size);
+        //return t;
+    }
+};
+
+struct ABC {
+    ABC() { std::cout << "ABC()" << std::endl; }
+    ~ABC() { std::cout << "~ABC()" << std::endl; }
+    int _1 = 0;
+    int _2 = 0x98999998;
+    //int _3;
+};
+
+int main()
+{
+    using namespace std;
+    cout << hex;
+
+    constexpr int element_size = DataBlock::element_size;
+    constexpr int block_count = 5;
+
+    DataBlock block(block_count);
+
+    // for debugging...
+    write_memory(block.data, block_count*element_size);
+    dump_memory(block.data, block_count*element_size);
+
+    int& i = block.construct<int>(2);
+    i = 0x31323334;
+    cout << block.get<int>(2) << endl;
+    auto&& c = block.construct<char>(3);
+    char text[] = "56789";
+    // arrays can be awkward to work with, mileage may vary
+    // additional utility methods can be added as desired
+    copy(begin(text), end(text), &c);
+    cout << &block.get<char>(3) << endl;
+
+    auto&& abc = block.construct<ABC>(4);
+    abc._1 = 0x12345678;
+
+    //block.construct<ABC>(block_count); // throws
+
+    dump_memory(block.data, block_count*element_size);
+
+    block.destroy<ABC>(4);
+}
