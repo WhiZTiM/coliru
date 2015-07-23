743785ee67d18eb22593fcb9c60358344964819f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-23 15:58:16 +0000

    Update archive c9

---
+++ trunk/Archive2/c9/2abb4fc37e8fab/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <typeinfo>
+#include <string>
+#include <memory>
+
+class TestClass {
+    std::unique_ptr<int> sp;
+public:
+
+    void setTester_Way1(std::unique_ptr<int> te) {
+         auto deleter=std::move(sp);
+         sp=std::move(te);
+    }
+    
+    void setTester_Way2(std::unique_ptr<int> te) {
+         sp=std::move(te);
+}
+};
+
+int main()
+{
+    TestClass t;
+    auto p = std::make_unique<int>(10);
+    t.setTester_Way2(std::move(p));
+}
