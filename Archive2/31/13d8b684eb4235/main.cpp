7be85569313260cbb54228af0be0761c7ce7e843
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-24 19:44:28 +0000

    Update archive 31

---
+++ Archive2/31/13d8b684eb4235/main.cpp
@@ -0,0 +1,24 @@
+#include <type_traits>
+
+template <class T>
+struct function_traits
+{
+    typedef typename function_traits<
+        decltype(&T::operator())
+    >::return_type return_type;
+};
+
+template <class R, class ...Args>
+struct function_traits<R (*)(Args...)>
+{
+    typedef R return_type;
+};
+
+int main()
+{
+    auto i = 0;
+    auto testingLambda = [](int a) { return 1LL * a; };
+    typename function_traits<decltype(testingLambda)>::return_type test;
+    
+    static_assert(std::is_same<decltype(test), long long>{}, "");
+}
