aa3619d5ab0141afd38b5ea2d8eacf6a84083ac9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-24 23:47:58 +0000

    Update archive 82

---
+++ trunk/Archive2/82/ec0dc6d449b1d8/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+#include <boost/variant.hpp>
+
+template<class Result, class Func>
+struct forwarding_visitor : boost::static_visitor<Result>
+{
+    Func func;
+    forwarding_visitor(const Func& f):func(f){}
+    forwarding_visitor(Func&& f):func(std::move(f)){}
+    template<class Arg>
+    Result operator()(Arg && arg) const {
+        return func(std::forward<Arg>(arg));
+    }
+};
+
+template<class Result, class Func>
+forwarding_visitor<Result, std::decay_t<Func> > make_forwarding_visitor(Func && func) {
+    return {std::forward<Func>(func)};
+}
+
+struct Person
+{
+    std::string name() const { return "Jens"; }
+};
+
+struct Dir
+{
+    std::string name() const {return "foo"; }
+};
+
+int main()
+{
+   using variant = boost::variant<Person,Dir>;
+   variant p = Person{};
+   variant d = Dir{};
+   auto visitor = make_forwarding_visitor<std::string>([](const auto& t){ return t.name();});
+   
+   std::cout << boost::apply_visitor(visitor,p) << "\n";
+   std::cout << boost::apply_visitor(visitor,d) << std::endl;
+}
