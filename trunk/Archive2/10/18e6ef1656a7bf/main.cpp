5db93a101bfb21460df4b86805f31d7229576943
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-24 19:39:28 +0000

    Update archive 10

---
+++ trunk/Archive2/10/18e6ef1656a7bf/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+
+#include <boost/variant.hpp>
+
+using namespace std;
+
+struct Person
+{
+   string name() const { return "Jens"; }
+};
+
+struct Dir
+{
+    string name() const {return "foo"; }
+};
+
+template<class R, class callable>
+struct generic_member_visitor : boost::static_visitor<R>
+{
+    callable c;
+    generic_member_visitor(callable& c):c(c){}
+    template<class T>
+    std::string operator()(const T& t)const
+    {
+        return c(t);
+    }
+};
+
+int main()
+{
+   using variant = boost::variant<Person,Dir>;
+   variant p = Person{};
+   variant d = Dir{};
+   auto lambda = [](const auto& t){ return t.name();};
+   generic_member_visitor<std::string,decltype(lambda)> gmc(lambda);
+   
+   cout << boost::apply_visitor(gmc,p) << endl;
+   cout << boost::apply_visitor(gmc,d) << endl;
+}
