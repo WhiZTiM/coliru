00bf26b0712e0721574f1cf60bf3006b94b1546c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-24 10:34:06 +0000

    Update archive 5b

---
+++ trunk/Archive2/5b/938c774611398a/cmd.sh
@@ -0,0 +1,4 @@
+echo -e "\n\ng++ ####################################################\n"
+g++ -std=c++11 -Wall -Wextra -pedantic main.cpp
+echo -e "\n\nclang++ ####################################################\n"
+clang++ -std=c++11 -Wall -Wextra -pedantic main.cpp
