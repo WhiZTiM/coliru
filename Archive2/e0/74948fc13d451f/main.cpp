563bb979574f945fd922ed36e452d9a1ecbdfd8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-25 17:52:05 +0000

    Update archive e0

---
+++ Archive2/e0/74948fc13d451f/main.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include <iostream>
+
+int main() {
+    double dollars = 6.85;
+    
+    std::cout.precision(2);
+    std::cout.setf(std::ios::fixed, std::ios::floatfield);
+    
+    std::cout << "$" << dollars << '\n';
+    
+    std::cout.imbue(std::locale("en_US.utf8"));
+    std::cout << std::showbase << std::put_money(dollars*100);
+}
