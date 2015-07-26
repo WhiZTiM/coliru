36468d2e09caab0e5da34a3c6f721fbeb9f6658e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-26 23:43:46 +0000

    Update archive 90

---
+++ trunk/Archive2/90/e59ae21122d06c/main.cpp
@@ -0,0 +1,17 @@
+#include <chrono>
+#include <iostream>
+#include <limits>
+#include <string>
+
+template<typename T>
+long secs_since_epoch(T now) {
+    return std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
+}
+
+int main()
+{
+    std::cout << "Seconds since epoch (steady clock): " << secs_since_epoch(std::chrono::steady_clock::now()) << '\n';
+    std::cout << "Seconds since epoch (system clock): " << secs_since_epoch(std::chrono::system_clock::now()) << '\n';
+    std::cout << "Seconds since epoch (hi-res clock): " << secs_since_epoch(std::chrono::high_resolution_clock::now()) << '\n';
+    return 0;
+}
