05b15d6e9bbd48114f0ba65c15f4ed8a72b4de5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-27 06:09:31 +0000

    Update archive f6

---
+++ Archive2/f6/77810f45d774df/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -std=c++14 -stdlib=libc++ -Wall -Wextra -pedantic-errors -O3 main.cpp && ./a.out
+echo ============================================
+g++ -std=c++14 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
