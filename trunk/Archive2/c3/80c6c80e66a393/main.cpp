b40241eb1236af90490a14e64610cd3dd2a14326
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-27 23:23:33 +0000

    Update archive c3

---
+++ trunk/Archive2/c3/80c6c80e66a393/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class HasPtr{
+ public:
+    //constructor accepts a string
+    HasPtr(const string &s = string()) : ps(new string(s)), i(0), use(new size_t(1)) {}  
+    //copy constructor
+    HasPtr(const HasPtr &h) : ps(h.ps), i(h.i), use(h.use) { ++*use; }
+    //copy assignment operator 
+    HasPtr &operator=(const HasPtr &h)
+    {
+        ++*h.use;
+        if(--*use == 0)
+        {
+            delete ps;
+            delete use;
+        }
+        ps = h.ps;
+        i = h.i;
+        use = h.use;
+
+        return *this;
+    }
+    void modify(const HasPtr& h) {
+        *ps = *h.ps;
+    }
+    ~HasPtr()
+    {
+        if(--*use == 0)
+        {
+            delete ps;
+            delete use;
+        }
+    }
+
+//private:
+    string *ps;
+    int i;
+
+    size_t *use;
+};
+
+
+int main()
+{
+    HasPtr h("hi mom");
+    HasPtr h2 = h;
+    HasPtr h3("hi dad");
+    //h2 = h3;
+    h2.modify(h3);
+
+    cout << "h: " << *h.ps << endl;
+    cout << "h2: " << *h2.ps << endl;
+    cout << "h3: " << *h3.ps << endl;
+}
