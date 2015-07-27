2e9edc73b1b98ef8ce9435b996113d42988e5b08
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-27 21:12:20 +0000

    Update archive f4

---
+++ trunk/Archive2/f4/3fef4af0876a42/main.cpp
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+uint32_t kfactor_linear[2][20] = {
+    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+    {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+};
+
+void getkFactorLinearity(uint32_t linearity[2][20]){
+    for(uint32_t i = 0; i < 2; i++) {
+        memcpy(linearity[i], kfactor_linear, sizeof(linearity[i]));
+    }
+}
+
+int main(void)
+{
+    uint32_t thing[2][20];
+    getkFactorLinearity(thing);
+    for (uint32_t i = 0; i < 2; i++) {
+        for (uint32_t j = 0; j < 20; j++) {
+            //printf("addr: 0x%x\n", &(thing[i][j]));
+            printf("value: %d", thing[i][j]);
+        }
+        printf("\n");
+    }
+}
