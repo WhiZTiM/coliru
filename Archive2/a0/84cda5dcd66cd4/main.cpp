39b421b77094ff0f77110109499d01567f5d1088
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-27 13:27:17 +0000

    Update archive a0

---
+++ Archive2/a0/84cda5dcd66cd4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+class foo
+  {
+public:
+  int g (int x, int y) { return x + y ; }
+  } ;
+
+template<typename T>
+using memberf_pointer = T; 
+
+int main()
+  {
+  foo f ;
+  memberf_pointer<decltype(&foo::g)> mp = &foo::g;
+  std::cout << (f.*mp) (5, 8) << std::endl ;
+  }
