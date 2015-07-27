549ce5639fbff241e12139860b49d22319d78408
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-27 12:27:19 +0000

    Update archive 50

---
+++ trunk/Archive2/50/21bbe4fe086f31/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <chrono>
+
+#define d_engine_processor_cookie_days_live 365
+
+int main()
+{
+    using namespace std::chrono_literals;
+    auto expires = d_engine_processor_cookie_days_live * 11.86* 24h;
+
+    std::cout << "On Jupiter year lasts " << expires.count() << " earthly hours" << std::endl;
+}
