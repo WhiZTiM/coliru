36c170e725f39ba4988b9ca0468f7b84e8e36a1e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-27 15:30:07 +0000

    Update archive c3

---
+++ Archive2/c3/9d540169a6bad1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+class Foo
+{
+public:
+    int bar;
+    Foo() = default;
+    Foo(const Foo & f) = default;
+    ~Foo() = default;
+    Foo(Foo && f) = delete;
+};
+
+void bar(Foo && f)
+{
+    static Foo bar(std::move(f));
+}
+
+int main()
+{
+    Foo f;
+    f.bar = 10;
+    bar(std::move(f));
+    std::cout << f.bar;
+}
