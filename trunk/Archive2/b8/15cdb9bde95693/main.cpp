a077008a6426b96550c3146eae6d278da9fc03e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-28 20:16:38 +0000

    Update archive b8

---
+++ trunk/Archive2/b8/15cdb9bde95693/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <boost/range/counting_range.hpp>
+
+int main() {
+    std::vector<int> v{1,2,3};
+   
+    for(auto it: boost::counting_range(v)) {
+       std::cout << *it;
+    }
+    std::cout << '\n';
+}
