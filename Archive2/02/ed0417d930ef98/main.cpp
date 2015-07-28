9088ae684b674b7d93f6038e4e9ed5bf49eac195
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-28 13:29:35 +0000

    Update archive 02

---
+++ Archive2/02/ed0417d930ef98/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <unordered_map>
+
+// usual pattern any C library that has callbacks
+typedef void (*callback)(int id, void* userdata);
+
+std::unordered_map<int, std::pair<callback, void*>> callbacks;
+
+void register_callback(int id, callback c, void* userdata)
+{
+    callbacks.emplace(id, std::make_pair(c, userdata));
+}
+
+void call(int id)
+{
+    auto& whatever = callbacks.at(id);
+    whatever.first(id, whatever.second);
+}
+
+void my_callback(int id, void*)
+{
+    // this function warns on -Wextra
+    // parameter `userdata` is required to be here, I can't remove it entirely
+    std::cout << id << "\n";
+}
+
+void my_callback_2(int id, void*)
+{
+    // avoid warning by not naming the parameter
+    std::cout << id << "\n";
+}
+
+void my_callback_3(int id, void* userdata)
+{
+   // avoid warning by explicitly ignoring the parameter
+   static_cast<void>(userdata);
+   std::cout << id << "\n";
+}
+
+int main()
+{
+    register_callback(42, my_callback, nullptr);
+    register_callback(666, my_callback_2, nullptr);
+    register_callback(1337, my_callback_3, nullptr);
+    call(42);
+    call(666);
+    call(1337);
+}
