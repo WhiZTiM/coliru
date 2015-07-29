3eb60e1b3cc579015ca8c94e615c139e784a5ff7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-29 13:14:40 +0000

    Update archive 34

---
+++ trunk/Archive2/34/0a64e2b3b6448f/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <iomanip>
+
+int main ()
+{
+    system ("echo \"hello\" > foo");
+    std::ifstream f ("foo");
+    std::istream_iterator<char> i1 (f);
+    std::istream_iterator<char> i2 (f);
+    while (i1 != std::istream_iterator<char> ())
+    {
+        std::cout << *i1 << ' ' << *i2 
+                  << std::boolalpha 
+                  << " *i1=*i2:" << (*i1==*i2) 
+                  << "  i1==i2:" << (i1==i2) 
+                  <<  std::endl;
+        ++i1;
+    }
+}
