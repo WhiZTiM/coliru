5f6b0c205683456db1ea7f92e47145be081213f6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-29 03:13:16 +0000

    Update archive 17

---
+++ Archive2/17/b175565589e7f0/main.cpp
@@ -0,0 +1,13 @@
+#include <array>
+
+constexpr std::array<int, 1> array{{3}};
+constexpr int a = array[0];  // Works in >=C++14 but not in C++11
+
+constexpr int b = static_cast<std::array<int, 1> const&>(std::array<int, 1>{{3}})[0];
+
+#include <iostream>
+
+int main()
+{
+    std::cout << a << ' ' << b << '\n';
+}
