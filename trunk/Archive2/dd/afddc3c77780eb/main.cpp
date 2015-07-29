3423c291440ff382b80af7b7d1b0661cb41ca3f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-29 19:40:48 +0000

    Update archive dd

---
+++ trunk/Archive2/dd/afddc3c77780eb/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    std::cout << "Enter values:";
+    std::vector<int> array(std::istream_iterator<int>(std::cin), {});
+    std::cout << "\nThe values in the array are:\n";
+    std::copy(begin(array), prev(end(array)), std::ostream_iterator<int>(std::cout, " / "));
+    std::cout << *prev(end(array));
+}
