dcb544b4cb1efd20eee36a54492d52d2cd6b5ead
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-29 18:58:55 +0000

    Update archive 07

---
+++ trunk/Archive2/07/3db29e838d5814/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <memory>
+
+    namespace easy_op {
+      template<class Rhs>
+      struct op_t {
+        Rhs rhs;
+      };
+      static struct do_opt {
+        template<class Rhs>
+        op_t<Rhs> operator[](Rhs&& rhs)const{
+          return {std::forward<Rhs>(rhs)};
+        }
+      } op;
+      template<class Lhs, class Rhs>
+      auto operator*( Lhs&& lhs, op_t<Rhs>&& rhs )
+      -> decltype( (*std::declval<Lhs>())[std::declval<Rhs>()] )
+      {
+        return (*std::forward<Lhs>(lhs))[std::forward<Rhs>(rhs.rhs)];
+      }
+    }
+    using easy_op::op;
+
+    
+int main() {
+    std::unique_ptr< std::vector<int> > p = std::make_unique<std::vector<int>>();
+    *p = {0,1,2,3,4,5};
+    for(std::size_t i = 0; i < p->size(); ++i) {
+        std::cout << p*op[i];
+    }
+    std::cout << '\n';
+}
