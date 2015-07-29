d2ec0c73ac08dea9f351055d228bba710ead1162
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-29 03:26:06 +0000

    Update archive 70

---
+++ Archive2/70/f6c344641a7fb1/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -std=c++14 -stdlib=libc++ -Wall -Wextra -pedantic-errors -O3 main.cpp && ./a.out <<< '11 12  13 14  15 16  17 18  19 20  21 22  23 24  25 26  27 28  29 30'
+echo ============================================
+g++ -std=c++14 -Wall -Wextra -pedantic-errors main.cpp && ./a.out <<< '11 12  13 14  15 16  17 18  19 20  21 22  23 24  25 26  27 28  29 30'
