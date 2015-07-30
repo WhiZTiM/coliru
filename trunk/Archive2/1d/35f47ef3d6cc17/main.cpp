7674112f4000901a962e59289a3f648287ab345a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-30 19:13:05 +0000

    Update archive 1d

---
+++ trunk/Archive2/1d/35f47ef3d6cc17/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include<string.h>
+#include<stdlib.h>
+
+// Please implement swap below such that it takes two strings as input and swaps their values
+
+void swap( const char** strA, const char** strB)
+
+{
+ const char** temp = *strA;
+  *strA = *strB;
+  *strB = *temp;
+}
+
+
+// Aside from passing values into swap(), do not modify the rest of the function
+
+int main( int argc, char** argv )
+
+{
+
+  const char* stringA = "The cow jumped over the moon";
+
+  const char* stringB = "The dish ran away with the spoon";
+
+
+  printf( "%s\n", stringA );
+
+  printf( "%s\n", stringB );
+
+
+  swap(&stringA, &stringB);
+
+
+  printf( "%s\n", stringA );
+
+  printf( "%s\n", stringB );
+
+}
