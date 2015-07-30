31c1f9c8915718664a6c9dcc2c7432c181aa059c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-30 17:52:03 +0000

    Update archive 84

---
+++ trunk/Archive2/84/2410c49f24bcf6/main.cpp
@@ -0,0 +1,11 @@
+
+struct A {
+    A(float ) { } // (a)
+    A(int ) { } // (b)
+    A(double ) { } // (c)
+};
+
+int main() {
+    A{1.1f}; // selects (a), (c) without (a)
+    A{1.1}; // selects (c), ambiguity error without (c)
+}
