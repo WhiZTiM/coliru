83f2a028a5f14c9dadaf769bac178ea81842aebf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-30 05:15:40 +0000

    Update archive 54

---
+++ Archive2/54/4115d4c700e3af/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+#define FILL_CHAR '#'
+#define COUNT 8
+
+int main()
+{ 
+    
+    std::cout << "Count up\n";
+    for (size_t i = 1; i <= COUNT; ++i)
+    {
+        std::cout << std::setfill('#') << std::setw(i);
+        std::cout << FILL_CHAR << std::endl;
+    }
+    
+    std::cout << "Count down\n";
+    for (size_t i=0; i<COUNT; ++i)
+    {
+        std::cout << std::setfill('#') << std::setw(COUNT-i);
+        std::cout << FILL_CHAR << std::endl;
+    }
+    
+  return 0;
+}
