f4e07c6112d0bbdd4848ff83d28e0935321ecbf8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-30 21:33:27 +0000

    Update archive 84

---
+++ Archive2/84/75ab959dea4c43/main.cpp
@@ -0,0 +1,13 @@
+#include <memory>
+
+struct A {};
+struct B {
+    B(A *) {};
+};
+
+int main()
+{
+    A *a = 0;
+    std::shared_ptr<B> b1 = std::make_shared<B>(a);
+    std::shared_ptr<B> b2 = std::make_shared<B, A *>(a);
+}
