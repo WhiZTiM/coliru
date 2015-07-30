7960486ac54c2048d9a2cd9e01cbc10b71bfee40
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-30 15:17:13 +0000

    Update archive de

---
+++ trunk/Archive2/de/9d883bfcade443/cmd.sh
@@ -0,0 +1,3 @@
+clang++ --version && clang++ -std=c++14 -DWORK -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
+g++ --version && g++ -std=c++14 -DWORK -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out;clang++ --version && clang++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
+g++ --version && g++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out;
