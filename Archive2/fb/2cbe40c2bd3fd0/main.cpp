58ed7db1c3f36ae0717c38f1c3fb7b35c9c8a72f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-31 07:27:13 +0000

    Update archive fb

---
+++ Archive2/fb/2cbe40c2bd3fd0/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+bool is_double( const std::string& str )
+{
+    std::size_t pos ;
+    try
+    {
+        // http://en.cppreference.com/w/cpp/string/byte/strtof
+        std::stod( str, std::addressof(pos) ) ;
+        return pos == str.size() ; // entire string has been consumed
+    }
+    catch( const std::exception& ) { return false ; } // error in conversion
+}
+
+int main()
+{
+    for( std::string str : { "-123.456",  "45fg", "1.0e-23", "inf", "nan", "", "0xab.cdP+55" } )
+    std::cout << '"' << str << "\" ? " << std::boolalpha << is_double(str) << '\n' ;
+}
