7cf700fbf0e64bc1e990e0f6085f46b126a88c37
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-31 13:13:59 +0000

    Update archive b7

---
+++ Archive2/b7/35f27f5bec3392/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct X
+{
+    float x;
+};
+
+struct Y : public X
+{
+    float y;
+};
+
+void fx(X& x)
+{
+    std::cout << x.x << std::endl;
+}
+void fy(Y & y)
+{
+    fx(y);
+    std::cout << y.y << std::endl;
+}
+
+int main ()
+{
+    X x; x.x = 10;
+    Y y; y.x = 20; y.y = 30;
+    fx(x);
+    fy(y);
+    return 0;
+}
