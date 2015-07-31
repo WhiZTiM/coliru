5e2830e3bfe48fc748580595b800fa5275db9a7e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-31 16:05:14 +0000

    Update archive fb

---
+++ Archive2/fb/3e9214f1feeb54/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "      sizeof(short): " << sizeof(short) << '\n'
+              << "        sizeof(int): " << sizeof(int) << '\n'
+              << "       sizeof(long): " << sizeof(long) << '\n'
+              << "  sizeof(long long): " << sizeof(long long) << '\n' ;
+              
+    std::cout << "      sizeof(float): " << sizeof(float) << '\n'
+              << "     sizeof(double): " << sizeof(double) << '\n'
+              << "sizeof(long double): " << sizeof(long double) << '\n' ;
+}
