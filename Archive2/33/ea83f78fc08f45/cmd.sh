1dea60de557ffe364f4efd257f717b08b11ff3c8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-31 08:52:19 +0000

    Update archive 33

---
+++ Archive2/33/ea83f78fc08f45/cmd.sh
@@ -0,0 +1,3 @@
+clang++ --version && clang++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
+echo; echo
+g++ --version && g++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out;
