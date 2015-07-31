02b0c1d69954bb0cc6d02edc1d7f600e7131340c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-07-31 18:11:19 +0000

    Update archive 66

---
+++ trunk/Archive2/66/97dfae4eff2d2f/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+int main()
+{
+     cout<<"Sizeof cout object: "<<sizeof(cout)<<'\n';
+     cout<<"Sizeof cin object: "<<sizeof(cin)<<'\n';
+}
