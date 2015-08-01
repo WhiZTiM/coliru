8872bdc669c177f0c4ac6a6b78ac1fb921c99824
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-01 20:29:01 +0000

    Update archive af

---
+++ trunk/Archive2/af/ff8bdf3e79cbef/main.cpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <utility>
+#include <iostream>
+
+#include <boost/variant.hpp>
+    
+struct A {};
+struct B {};
+struct C {};
+
+using V = boost::variant< A, B, C >;
+
+struct visitor
+{
+    
+    using result_type = void;
+
+    result_type
+    operator () (V v) const
+    {
+#if 1
+        return boost::apply_visitor([this] (auto && x) -> result_type { return apply(std::forward< decltype(x) >(x)); }, v);
+    }
+#else
+        return boost::apply_visitor(*this, v);
+    }
+    
+    template< typename T >
+    result_type
+    operator () (T && x) const
+    {
+        return apply(std::forward< T >(x));
+    }
+#endif
+    
+private :
+    
+    result_type
+    apply(A) const
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+    
+    template< typename T >
+    result_type
+    apply(T &&) const
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+    
+public : // !
+    
+};
