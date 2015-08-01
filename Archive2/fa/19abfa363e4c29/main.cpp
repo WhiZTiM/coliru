da4f35042ef1c1b68073b7620c47b58eca6e2773
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-01 12:47:00 +0000

    Update archive fa

---
+++ Archive2/fa/19abfa363e4c29/main.cpp
@@ -0,0 +1,33 @@
+// u128-UDL by Columbo
+
+constexpr u128 operator "" _u128( char const* str ) {
+    assert (*str != '-');
+	if (*str == '+')
+		++str;
+
+	int base=10;
+	if (*str == '0') {
+		++str;
+		if (*str == 0)
+				return 0;
+		if (*str == 'X' || *str == 'x') {
+			base = 16;
+			++str;
+		}
+		else
+			base = 8;
+	}
+
+	char const digits[] = "0123456789ABCDEF";
+	u128 res = 0;
+	for (; char c = *str; ++str) {
+		const u128 max = -1;
+		assert(max/base >= res);
+		res *= base;
+		auto digit = detail::find(digits, c >= 'a' && c <= 'z'? c - ('a'-'A') : c) - digits;
+		assert(max-digit >= res);
+		res += digit;
+	}
+
+	return res;
+}
