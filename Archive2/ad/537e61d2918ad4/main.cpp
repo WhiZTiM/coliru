2c8e0ab5cd0a22de12f2e732c6ec9d8aec480793
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-02 13:29:47 +0000

    Update archive ad

---
+++ Archive2/ad/537e61d2918ad4/main.cpp
@@ -0,0 +1,33 @@
+#include <type_traits>
+
+struct first {
+    template <typename T, typename U>
+    using apply = T;
+};
+
+template <typename C, typename... Args>
+struct eval {
+    using type = typename C::template apply<Args...>;
+};
+
+template <typename C, typename... Args>
+struct curry {
+    struct type {
+        template <typename... OtherArgs>
+        struct impl {
+            using type = typename C::template apply<Args..., OtherArgs...>;
+        };
+        
+        template <typename... OtherArgs>
+        //using apply = typename C::template apply<Args..., OtherArgs...>;
+        //using apply = typename eval<C, Args..., OtherArgs...>::type;
+        using apply = typename impl<OtherArgs...>::type;
+    };
+};
+
+int main() {
+    static_assert(std::is_same<first::apply<int, char>, int>::value, "");
+    
+    using AlwaysInt = curry<first, int>::type;
+    static_assert(std::is_same<AlwaysInt::apply<char>, int>::value, "");
+}
