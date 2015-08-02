e8673bd2cec548035e6855f3a79371ee37c73c84
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-02 14:45:44 +0000

    Update archive 11

---
+++ Archive2/11/d67e7588ada0f1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+typedef std::map<std::string, int> Dictionary;
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+    
+    std::map<Dictionary, int> a, b;
+    // SLAVA
+    
+    std::cout << "Is a less than b " << ( a<b ) << std::endl;
+    
+    
+    
+}
