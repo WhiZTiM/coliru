20bc79cbea0a0bc2a4a968904ff138af08d99ace
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-02 15:20:33 +0000

    Update archive d4

---
+++ Archive2/d4/43f945436f66fb/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include <set>
+#include <vector>
+int h(std::vector<int>l){int W=0,y;std::set<std::pair<int,int>>S{{-1,0}};for(w:l){S.insert({w,y=prev(S.lower_bound({w,-1}))->second+w});W=y>W?y:W;}return W;}
+
+int main() {
+    std::cout << h({9, 1, 2, 3, 4, 5, 10}) << "\n";
+    return 0;
+}
