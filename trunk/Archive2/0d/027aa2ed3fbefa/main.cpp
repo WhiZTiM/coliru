740c5b111fca84784610db50e17596844a489f65
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-03 05:32:35 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/027aa2ed3fbefa/main.cpp
@@ -0,0 +1,15 @@
+typedef int f;
+namespace N {
+struct A {
+    friend void f(A &);
+    friend int f(A &);
+    operator int();
+    void g(A a) {
+        int i = f(a); // f is the typedef, not the friend
+                      // function: equivalent to int(a)
+    }
+};
+}
+
+int main()
+{}
