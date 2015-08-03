3182d51cf4eb08427dd162733149c5ed8252864e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-03 19:22:01 +0000

    Update archive 62

---
+++ trunk/Archive2/62/8bc19fe40f0358/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct SM {
+    int val;
+
+    SM(int a) : val(a) {
+        cout << "Constructor val=" << a << endl;
+    }
+    ~SM() {
+        cout << "Destructor val=" << val << endl;
+    }
+
+    SM& operator=(const SM&) = delete;
+    SM& operator=(SM&&) = delete;
+    SM(const SM&) = delete;
+    SM(SM&&) = delete;
+};
+
+int main() {
+    SM sc[8] = { { 0 }, { 1 }, { 2 }, { 3 }, { 4 }, { 5 }, { 6 }, { 7 } };
+}
