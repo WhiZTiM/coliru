a32ebce5c5a7965fd43db931f6e6fc719f84a6e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-03 21:06:54 +0000

    Update archive 9f

---
+++ Archive2/9f/8cc9d5612874a4/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <tuple>
+#include <cstddef>
+
+template <std::size_t...> struct index_sequence {};
+template <std::size_t N, std::size_t... Is> struct make_index_sequence : make_index_sequence<N-1, N-1, Is...> {};
+template <std::size_t... Is> struct make_index_sequence<0, Is...> : index_sequence<Is...> {};
+
+void dothing(bool testBool, const std::string& str, const std::string& file, int line, const std::string& defaultString)
+{
+    std::cout << testBool << " " << str << " " << file << " " << line << " " << defaultString << "\n";
+}
+
+template <typename... Args, std::size_t... Is>
+void dothings2(bool testBool, index_sequence<Is...>, Args&&... args)
+{
+    auto tuple = std::make_tuple(std::forward<Args>(args)...);
+    using expander = int[];
+    static_cast<void>(expander{ 0, (dothing(testBool, std::get<Is>(tuple)
+                               , std::get<sizeof...(Args)-3>(tuple)
+                               , std::get<sizeof...(Args)-2>(tuple)
+                               , std::get<sizeof...(Args)-1>(tuple)), 0)... }); 
+}
+
+template <typename... Args>
+void dothings(bool testBool, Args&&... args)
+{
+    dothings2(testBool, make_index_sequence<sizeof...(Args)-3>{}, std::forward<Args>(args)...); 
+}
+
+int main()
+{
+    dothings(true, "A", "B", "C", "D", "foo.cpp", 42, "bar");
+}
