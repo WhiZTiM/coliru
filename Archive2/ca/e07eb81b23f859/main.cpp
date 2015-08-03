59235e65d112ece615e182be4059ed23e8bb2665
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-03 06:39:09 +0000

    Update archive ca

---
+++ Archive2/ca/e07eb81b23f859/main.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+ 
+int main()
+{
+    std::vector<int> v{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4};
+    std::vector<int>::iterator result;
+ 
+    std::vector<int> t1{1, 2, 3};
+ 
+    result = std::find_end(v.begin(), v.end(), t1.begin(), t1.end());
+    if (result == v.end()) {
+        std::cout << "subsequence not found\n";
+    } else {
+        std::cout << "last subsequence is at: "
+                  << std::distance(v.begin(), result) << "\n";
+    }
+ 
+    std::vector<int> t2{4, 5, 6};
+    result = std::find_end(v.begin(), v.end(), t2.begin(), t2.end());
+    if (result == v.end()) {
+        std::cout << "subsequence not found\n";
+    } else {
+        std::cout << "last subsequence is at: " 
+                  << std::distance(v.begin(), result) << "\n";
+    }
+}
