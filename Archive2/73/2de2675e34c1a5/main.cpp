4890ca2f131f55f03738a682efbfa03ed2d8adb5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-04 19:05:19 +0000

    Update archive 73

---
+++ Archive2/73/2de2675e34c1a5/main.cpp
@@ -0,0 +1,28 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/topological_sort.hpp>
+#include <boost/function_output_iterator.hpp>
+
+typedef boost::adjacency_list<
+    boost::setS, // outedge list
+    boost::setS, // vertex list
+    boost::directedS, // undirected 
+    boost::no_property, // vertex prop
+    boost::no_property, // edge prop
+    boost::no_property, // graph prop
+    boost::setS // edgelist
+    > Graph;
+
+
+bool hasCycle(const Graph& aG) 
+{
+    try {
+        boost::topological_sort(
+            aG, 
+            boost::make_function_output_iterator([](int){}));
+    } catch(boost::not_a_dag const&)
+    {
+        return true;
+    }
+    return false;
+} 
+
