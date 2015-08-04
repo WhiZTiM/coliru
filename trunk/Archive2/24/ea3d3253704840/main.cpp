90ec3828a4d1067a48966dc40ff7598943b1b973
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-04 18:39:21 +0000

    Update archive 24

---
+++ trunk/Archive2/24/ea3d3253704840/main.cpp
@@ -0,0 +1,14 @@
+//We cannot predict the behavior of this program!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char * s = "My String";
+    char s2[] = {'a', 'b', 'c', '\0'};
+
+    cout << s2 << endl;
+
+    return 0;
+}
