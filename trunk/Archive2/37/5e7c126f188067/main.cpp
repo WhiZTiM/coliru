450a605e12dc1293bc0ecd1c3df0e35accf4c49f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-05 16:04:59 +0000

    Update archive 37

---
+++ trunk/Archive2/37/5e7c126f188067/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace mp = boost::multiprecision;
+
+int main()
+{
+    mp::cpp_int u_100 = 1;
+    for(unsigned i = 1; i <= 100; ++i)
+        u_100 *= i;
+
+    mp::cpp_int u_99 = 1;
+    for(unsigned i = 1; i <= 99; ++i)
+        u_99 *= i;
+
+    std::cout << std::setw(15) << " 99! = " << u_99 << '\n';
+    std::cout << std::setw(15) << "100! = " << u_100 << '\n';
+    std::cout << std::setw(15) << "100! - 99! = " << (u_100 - u_99) << '\n';
+}
