cacbd25309d90759d950b7c897f798689b8b76c6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-05 20:11:59 +0000

    Update archive 74

---
+++ trunk/Archive2/74/66539050640fda/main.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#include <iostream>
+ 
+int main() 
+{
+    char input[100] = "A bird came down the walk";
+    char *token = std::strtok(input, " ");
+    while (token != NULL) {
+        std::cout << token << '\n';
+        token = std::strtok(NULL, " ");
+    }
+    std::cout << std::atoi(token)-2 << std::endl;
+}
