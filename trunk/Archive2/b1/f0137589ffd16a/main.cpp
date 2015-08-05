ba60af99df13ab40dade2e99014bc65a9f4bfbb7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-05 13:01:49 +0000

    Update archive b1

---
+++ trunk/Archive2/b1/f0137589ffd16a/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+decltype(sizeof(T), std::true_type{}) is_complete_impl(int);
+
+template <typename T>
+std::false_type is_complete_impl(...);
+
+template <typename T>
+using is_complete = decltype(is_complete_impl<T>(0));
+
+class B{};
+class C;
+
+static_assert(is_complete<B>::value, "");
+static_assert(!is_complete<C>::value, "");
+
+class C {};
+
+//static_assert(is_complete<C>::value, ""); // already instanstiate with false value
+
+
+int main() {
+}
