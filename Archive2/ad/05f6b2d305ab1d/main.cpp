8fe0dc69bf1dcb0004584e82e6c050dcf00cd846
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-05 01:22:48 +0000

    Update archive ad

---
+++ Archive2/ad/05f6b2d305ab1d/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/member.hpp>
+
+namespace bmi = boost::multi_index;
+
+struct QTableElem {
+    template<class Archive> void serialize(Archive &ar,const unsigned int) {
+		ar & boost::serialization::make_nvp("dummy", dummy);
+	}
+	int dummy;
+};
+
+class QTable : public bmi::multi_index_container<
+	QTableElem,
+	bmi::indexed_by<
+		bmi::ordered_unique<bmi::member<QTableElem,int,&QTableElem::dummy> >
+	>
+> {};
+
+ 
+int main()
+{
+	using boost::serialization::make_nvp;
+	{
+		std::ofstream out("test.xml");
+		boost::archive::xml_oarchive oa(out);
+
+        QTable obj;
+		oa << make_nvp("graph", obj);
+	}
+
+	try {
+		std::ifstream xml("test.xml");
+		boost::archive::xml_iarchive ia(xml);
+		QTable obj;
+            ia >> make_nvp("graph", obj); 
+	} catch (boost::archive::archive_exception const& ae) {
+		std::cout << "Error: " << ae.code << ":'" << ae.what() << "'\n";
+	}
+}
