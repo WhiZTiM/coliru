f8c1a3ecb6bcbcef34f2fa9d2552b33f327569e2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-05 21:21:36 +0000

    Update archive 12

---
+++ trunk/Archive2/12/5d9a50a70eadfe/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+
+template <typename X>
+void show(X)
+{
+    std::cout << typeid(X).name() << std::endl;
+}
+
+
+struct E { 
+    const int x; 
+    constexpr E(int t) noexcept : x{t} {} 
+    constexpr E() noexcept : x{55} {}
+    constexpr E(E const& e) noexcept : x{e.x} {}
+};
+
+template <typename T>
+struct Foo
+{
+    //static constexpr struct E {} nested {}; // works in gcc and clang
+    static constexpr E nested{5}; // doesn't work in gcc
+};
+
+int main()
+{
+ //   Foo<int> x;
+    show(Foo<int>::nested);
+    int test = Foo<int>::nested.x;
+    //std::cout << typeid(Foo<int>::nested).name() << std::endl;
+    std::cout << test; 
+}
