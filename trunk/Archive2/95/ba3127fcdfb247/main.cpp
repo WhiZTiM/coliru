a77e78fe257396bd6b8bc10bf6e0c8369d800785
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-05 19:24:39 +0000

    Update archive 95

---
+++ trunk/Archive2/95/ba3127fcdfb247/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+int main()
+{
+    int x = 1, y = 2, z = 3;
+    (x = y) = z;
+    std::cout << x << " " << y << " " << std::endl;
+    return 0;
+}
