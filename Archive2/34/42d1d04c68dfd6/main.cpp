bf6f3fae480547ecc5cd55d32b1020793cc093da
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-06 00:53:07 +0000

    Update archive 34

---
+++ Archive2/34/42d1d04c68dfd6/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class A {
+private:
+    void foo() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    void foo(int) {std::cout << __PRETTY_FUNCTION__ << std::endl;}
+    void foo(int, char) {std::cout << __PRETTY_FUNCTION__ << std::endl;}
+    void foo(bool, int, double) {std::cout << __PRETTY_FUNCTION__ << std::endl;}
+    void bar(int) {std::cout << __PRETTY_FUNCTION__ << std::endl;}
+    void bar(bool, int, double) {std::cout << __PRETTY_FUNCTION__ << std::endl;}
+public:
+    template <typename... Args> 
+    auto doIt_impl(int, Args&&... args) -> decltype(bar(std::forward<Args>(args)...)){
+        bar(std::forward<Args>(args)...);
+    }
+    template <typename... Args> 
+    auto doIt_impl(long, Args&&... args) -> void {
+        foo(std::forward<Args>(args)...);
+    }
+    template <typename... Args> void doIt(Args&&... args) {
+        doIt_impl(0, std::forward<Args>(args)...);
+    }
+};
+
+int main() {
+    A a;
+    a.doIt();
+    a.doIt(1);
+    a.doIt(5,'t');
+}
