58b8c0bbca4d4addc59a5928ade7d132556ddcb9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-06 18:50:36 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/dd89be5be6e74c/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <sstream>
+
+bool convert(const std::string & text)
+{
+    if (text = "")
+        return false;
+    std::istringstream iss(text);
+    iss.imbue(std::locale::classic());
+    double result;
+    iss >> result;
+    if (iss.eof())
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    std::cout << convert("123.456") << std::endl;
+    std::cout << convert("123.456abc") << std::endl;
+    std::cout << convert("123.456e5") << std::endl;
+    std::cout << convert("e5") << std::endl;
+    return 0;
+}
