373f5d00c1a33c6f4207f37e4945aa5fad28b2dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 19:36:00 +0000

    Update archive 85

---
+++ Archive2/85/aa8f40cbb9cda3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+template<typename Func, typename... Args>
+auto curry(Func func, Args... args)
+{
+    return [func = std::forward<Func> (func), args = std::forward<Args>(args)...](auto&&... x)
+    {
+        return func(args..., std::forward<decltype(x)>(x)...);
+    };
+}
+
+struct foo
+{
+    void operator () (int x, int y, int z) const
+    {
+        std::cout << x << "," << y << "," << z << std::endl;
+    }
+};
+
+int main ()
+{
+    auto f = curry(foo (), 1);
+    f(2, 3);
+}
