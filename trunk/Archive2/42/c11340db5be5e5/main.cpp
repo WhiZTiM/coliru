d77e663b3335f5f75969d54d81b399a8e51152e6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 16:53:35 +0000

    Update archive 42

---
+++ trunk/Archive2/42/c11340db5be5e5/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int* test() {
+    int x;
+    x++;
+    std::cout << x << std::endl;
+    return &x;
+}
+
+void test3(int* y) {
+    int x;
+    std::cout << x << std::endl;
+    std::cout << std::hex << y << std::endl;
+}
+
+int main() {
+    int* y = test();
+    test();
+    test();
+    test();
+    test3(y);
+    return 0;
+}
