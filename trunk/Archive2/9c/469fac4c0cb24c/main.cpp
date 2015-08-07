1c99c2e77f6fb5167df814b40883c184ede1a5ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 16:08:58 +0000

    Update archive 9c

---
+++ trunk/Archive2/9c/469fac4c0cb24c/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using std::string;
+
+int main() {
+    string papa = "papa_____________________________________________________________________________________";
+    papa.~string();
+    return 0;
+}
