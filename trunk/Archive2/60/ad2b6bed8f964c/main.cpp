3437ad241740afe72bd99a0ee93bbc6b9107e6ef
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 14:56:46 +0000

    Update archive 60

---
+++ trunk/Archive2/60/ad2b6bed8f964c/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void test() {
+    int x;
+    int* p = &x;
+    x++;
+    std::cout << x << std::endl;
+}
+
+void test3() {
+    int* p;
+    std::cout << p << std::endl;
+    *p = 200;
+    int x;
+    std::cout << x << std::endl;
+}
+
+int main() {
+    test();
+    test();
+    test();
+    test();
+    test3();
+    test();
+    return 0;
+}
