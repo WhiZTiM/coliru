ccc0663c3fab2cc3fc4f7bc13d27d08a908d35fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 20:07:07 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/4ae5603d441f64/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+bool has_suffix( std::string str, std::string suffix )
+{
+    return str.size() > suffix.size() &&
+           str.rfind( suffix ) == ( str.size() - suffix.size() ) ;
+}
+
+std::string expand_not( std::string str )
+{
+    if( has_suffix( str, "'t" ) )
+    {
+        // special cases:
+        if( str == "won't" ) return "will not" ;
+        else if( str == "shan't" ) return "shall not" ;
+        // else if ...
+
+        // else the general case
+        if( has_suffix( str, "n't" ) ) return str.substr( 0, str.size() - 3 ) + ' ' + "not" ;
+    }
+
+    return str ;
+}
+
+int main()
+{
+    for( std::string str : { "wouldn't", "won't", "shan't", "didn't", "hello", "don't" } )
+         std::cout << str << "  =>  " << expand_not(str) << '\n' ;
+}
