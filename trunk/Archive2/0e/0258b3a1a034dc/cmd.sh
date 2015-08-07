38369fb130e770a33534016d4c15957db703df09
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 15:00:53 +0000

    Update archive 0e

---
+++ trunk/Archive2/0e/0258b3a1a034dc/cmd.sh
@@ -0,0 +1,4 @@
+echo "clang++ ##########################################################"
+clang++ -std=c++14 -O3 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
+echo "g++ ##########################################################"
+g++ -std=c++14 -O1 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out;
