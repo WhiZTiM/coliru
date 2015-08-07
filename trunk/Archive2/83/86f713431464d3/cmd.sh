bc2f0b9a95f2445681cc8845443684ade3e25eab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-07 18:26:02 +0000

    Update archive 83

---
+++ trunk/Archive2/83/86f713431464d3/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++14 -stdlib=libc++ -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
+echo -e '\n=============\n' && g++ -std=c++14 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
