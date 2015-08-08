475e8f8b0737a4fa8582ca4131cb36d7760b594e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-08 15:20:20 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/683adfb8ee3db2/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <cmath>
+#include <limits>
+
+template <typename T>
+class Pascal
+{
+private:
+    T val;
+
+public:
+    explicit Pascal(const T val_)
+    {
+        val = val_;
+    }
+
+    operator T() const
+    {
+        return val;
+    }
+};
+
+template <typename T>
+class dbSPL {
+private:
+    T val;
+
+public:
+    explicit dbSPL(const Pascal<T> p)
+    {
+        auto infProtect = std::numeric_limits<T>::min();
+        val = 20.0 * std::log10( infProtect + p / 20e-6 );
+    }
+    
+    operator T() const
+    {
+        return val;
+    }
+};
+
+template <typename T>
+dbSPL<T> make_dbspl(T t)
+{
+    return dbSPL<T>(Pascal<T>(t));
+}
+
+int main()
+{
+    auto dbspl = make_dbspl(0.5);
+    
+    (void)dbspl;
+}
