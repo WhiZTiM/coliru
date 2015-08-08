aa44223ec8072ace1d2285201c4dccc559870534
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-08 03:07:01 +0000

    Update archive 7a

---
+++ trunk/Archive2/7a/550ed363813e44/main.cpp
@@ -0,0 +1,38 @@
+#include <atomic>
+#include <algorithm>
+#include <cstddef>
+
+constexpr const int CACHE_LINE_BYTES = 64;
+
+template<typename T, size_t size>
+struct RingBuffer {
+    RingBuffer() : ahead(0), atail(0) {}
+
+    bool push(const T& value) {
+        size_t head = ahead.load(std::memory_order_relaxed);
+        size_t next_head = next(head);
+        if (next_head == atail.load(std::memory_order_acquire)) return false;
+        ring_[head] = value;
+        ahead.store(next_head, std::memory_order_release);
+        return true;
+    }
+
+    bool pop(T& value) {
+        size_t tail = atail.load(std::memory_order_relaxed);
+        if (tail == ahead.load(std::memory_order_acquire)) return false;
+        value = ring_[tail];
+        atail.store(next(tail), std::memory_order_release);
+        return true;
+    }
+
+private:
+    size_t next(size_t current) { return (current + 1) % size; }
+    std::atomic<size_t> ahead;
+    T ring_[size];
+    char false_share_pad[std::max(1, CACHE_LINE_BYTES - int(sizeof(size_t)) - int(size * sizeof(T)))];
+    std::atomic<size_t> atail;
+};
+
+RingBuffer<int, 32> buf;
+bool push(int x) { return buf.push(x); } // Just to generate assembly.
+bool pop(int& x) { return buf.pop(x); } // Just to generate assembly.
