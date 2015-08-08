395c4e92d8cac4b0b686b5b9954925a9071a176f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-08 16:26:34 +0000

    Update archive ac

---
+++ Archive2/ac/fd3fb5719022ac/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <type_traits>
+
+template <typename F, typename A,
+          typename B = typename std::decay<typename std::result_of<F&(A&)>::type>::type>
+std::vector<B> functional_map(F func, std::vector<A> list)
+{
+    std::vector<B> result(list.size());
+    for (int i = 0; i < (int)list.size(); ++i)
+    {
+        result[i] = func(list[i]);
+    }
+    return result;
+}
+
+int square (int x) {return x*x;}
+
+int main()
+{
+    std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> v2 = functional_map(square, v);
+    for(int y : v2)
+    {
+        std::cout << y << " ";
+    }
+}
