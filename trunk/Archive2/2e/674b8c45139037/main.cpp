8c8c36c0bbcd4fc7ad761ade702342e3bb82fc8f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-08 16:58:44 +0000

    Update archive 2e

---
+++ trunk/Archive2/2e/674b8c45139037/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <type_traits>
+
+template <typename F, typename A, typename B = typename std::result_of<F&(A&)>::type>
+std::vector<B> map(F func, std::vector<A> list)
+{
+    std::vector<B> result(list.size());
+    for(int i = 0; i < (int)list.size(); ++i) {
+        result[i] = func(list[i]);
+    }
+    return result;
+}
+
+int square (int x) {return x*x;}
+
+int main()
+{
+    std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> v2 = map(square, v);
+    for(int y : v2)
+    {
+        std::cout << y << " ";
+    }
+}
