63a528b6f76044762d04a018286dd7d25719f463
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-09 14:04:22 +0000

    Update archive fc

---
+++ trunk/Archive2/fc/43d09602dba8cb/cmd.sh
@@ -0,0 +1,6 @@
+ln -s /Archive2/76/565f70a40bf923/main.cpp create.cpp
+echo -e '======== create ==========' && clang++ -std=c++14 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors create.cpp -o create && time ./create 
+ls -l test.txt
+echo -e '\n======== parse ==========' && clang++ -std=c++14 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors main.cpp -oparse && time ./parse
+echo -e '======== create ==========' && clang++ -std=c++14 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors create.cpp -o create && time ./create 
+echo -e '\n======== parse ==========' && g++ -std=c++14 -O3 -Wall -Wextra -pedantic-errors main.cpp -oparse && time ./parse
