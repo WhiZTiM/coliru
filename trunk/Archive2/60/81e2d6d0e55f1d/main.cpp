f82ddcb85d3df7ffaaf986b15d25375c28643105
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-09 12:22:50 +0000

    Update archive 60

---
+++ trunk/Archive2/60/81e2d6d0e55f1d/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    char c[] = { 'a', 'b', 'c', 'd' };
+    
+    std::vector<char> v;
+    std::string s;
+    
+    std::copy(c, c+4, std::back_inserter(v));
+    std::copy(c, c+4, std::back_inserter(s));
+    std::copy(c, c+4, std::ostream_iterator<char>(std::cout));
+}
