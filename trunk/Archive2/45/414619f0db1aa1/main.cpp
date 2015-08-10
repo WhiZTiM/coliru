f9d38f181111481950a1931f7dce484bb5e6f3a9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-10 19:04:57 +0000

    Update archive 45

---
+++ trunk/Archive2/45/414619f0db1aa1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+class A {
+public:
+//    A(const A&) { std::cout << "copy A" << '\n'; }
+//    A(A&&) { std::cout << "move A" << '\n'; }
+    A(std::unique_ptr<int>&& u_ptr) : u_ptr(std::move(u_ptr)) {}
+    const int& getInt() { return *u_ptr; }
+
+private:
+    std::unique_ptr<int> u_ptr;
+};
+
+class B {
+public:
+    B(int u_ptr) : u_ptr(new int(u_ptr)) {}
+    A toA() && { return A(std::move(u_ptr)); }
+ 
+private:
+    std::unique_ptr<int> u_ptr;
+};
+
+int main() {
+    B b(-1);
+    A a(std::move(b).toA());
+    std::cout << a.getInt() << '\n';
+    return 0;
+}
