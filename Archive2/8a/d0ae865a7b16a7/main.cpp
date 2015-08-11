d7ddc6c50db34225b9b9129258a38edfebc7f930
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-11 09:59:37 +0000

    Update archive 8a

---
+++ Archive2/8a/d0ae865a7b16a7/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+
+template <class Comp>
+std::function<bool(typename Comp::first_argument_type)> predicate(typename Comp::second_argument_type rhs)
+{
+    return [rhs](typename Comp::first_argument_type lhs){
+        Comp c{};
+        return c(lhs, rhs);
+    };
+}
+
+int main() {
+    std::vector<int> v {1, 2, 3, 4, 5};
+    
+    v.erase(std::remove_if(begin(v), end(v), predicate<std::less<int>>(4)), end(v));
+    
+    std::copy(begin(v), end(v), std::ostream_iterator<int>(std::cout, " "));
+}
