067e466291de1cc3f515ccbeedc210ff1b56b7e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-11 03:49:06 +0000

    Update archive d9

---
+++ Archive2/d9/42cb843278e935/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int a[3][4] = { {0,1,2,3}, {4,5,6,7}, {8,9,10,11} };
+
+    using row = int[4] ; // type of row is 'array of 4 int'
+    const row* begin = a ;
+    const row* end = a+3 ;
+
+    for( const row* p = begin ; p < end ; ++p )
+    {
+        // type of 'p' is 'pointer to array of 4 int'
+        // type of '*p' is 'array of 4 int'
+        const int* row_begin = *p ;
+        const int* row_end = row_begin + 4 ;
+
+        for( const int* q = row_begin ; q < row_end ; ++q ) std::cout << std::setw(3) << *q ;
+        std::cout << "\n\n" ;
+    }
+}
