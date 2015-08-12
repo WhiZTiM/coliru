fa32ecb7935a2cefefb7a9a155182a43b1a8f789
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-12 11:16:35 +0000

    Update archive ed

---
+++ trunk/Archive2/ed/7f9d8cf3af0ac7/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string s = "Hello, World\t!\nHow's it\t going ?";
+    
+    std::cout << "Before :\n" << s << '\n';
+    
+    using std::begin; using std::end;
+    s.erase(std::remove_if(begin(s), end(s), [](auto ch) { return std::isspace(ch); }), end(s));
+    
+    std::cout << "After :\n" << s << '\n';
+    
+    return 0;
+}
