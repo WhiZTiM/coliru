2ba801a9a34f900a866f1d60e844ee0e01790a19
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-12 15:03:40 +0000

    Update archive 5a

---
+++ trunk/Archive2/5a/debe421331e92b/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        std::cerr << "No argument provided.\n";
+        return 1;   
+    }
+    std::stringstream ss(argv[1]);
+    bool b;
+    
+    if(!(ss >> std::boolalpha >> b)) {
+        std::cerr << "Incorrect argument provided.\n";
+        return 1;   
+    }
+    
+    if(b)
+        std::cout << "b is true\n";
+    else
+        std::cout << "b is false\n";
+}
