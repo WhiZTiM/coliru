d1142ea57f0b3e1ddf45ed0a3704935c90099be9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-12 12:56:22 +0000

    Update archive ec

---
+++ Archive2/ec/a761ae9bbaf585/main.cpp
@@ -0,0 +1,18 @@
+struct link {
+    link* next;
+};
+link* first;
+void h(link*& p) { // p is a reference to pointer
+    p->next = first;
+    first = p;
+    p = 0;
+}
+void k() {
+    link* q = new link;
+    h(q);
+}
+
+int x = 10;
+int* p = &x;
+int*& rx = p;
+rx = 0;
