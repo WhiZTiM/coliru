573bba29caaa5589f2d6f7eb1f1b53c21a154120
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-12 11:46:20 +0000

    Update archive 6f

---
+++ trunk/Archive2/6f/fac5c61e8bd965/main.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    const int *x;
+    int const * y;
+    int * const z;
+    int const * const p;
+    x = 0;
+    *x = 0;
+    y = 0;
+    *y = 0;
+    z = 0;
+    *z = 0;
+    p = 0;
+    *p = 0;
+    return 0;
+}
