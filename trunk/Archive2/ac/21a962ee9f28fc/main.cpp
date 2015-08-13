f88ddb74ac99bfdeb5e9af49be1ea64a3a51def7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-13 18:27:38 +0000

    Update archive ac

---
+++ trunk/Archive2/ac/21a962ee9f28fc/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <regex>
+
+
+int main (int argc, char *argv[]) {
+  std::regex pattern("[A-Z]+", std::regex_constants::icase);
+  std::regex pattern2("excelsior", std::regex_constants::icase);
+  std::string text = "EXCELSIOR";
+
+  if (std::regex_match(text, pattern)) std::cout << "works" << std::endl;
+  else std::cout << "doesn't work" << std::endl;
+
+  if (std::regex_match(text, pattern2)) std::cout << "works" << std::endl;
+  else std::cout << "doesn't work" << std::endl;
+
+  return 0;
+}
