86ea70e8e7e2182bc3f7d2aa6d6118cccca027c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-13 18:41:49 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/7c81d14f2c6129/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct foo {
+    foo() = default;
+    foo(const foo&) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    foo(foo&&) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    foo& operator=(const foo&) { std::cout << __PRETTY_FUNCTION__ << std::endl; return *this; }
+    foo& operator=(foo&&) { std::cout << __PRETTY_FUNCTION__ << std::endl; return *this; }
+    
+    foo& operator+=(int) { return *this; }
+};
+
+foo operator+(const foo& l, int r) {
+    return foo(l) += r;
+}
+
+int main(){
+    foo f;
+    f + 1;
+}
