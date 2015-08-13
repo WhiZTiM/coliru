74cba342a848a79f4d8895aac92a3b2f625bb87a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-13 02:43:59 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/4e89ba78f4e594/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <memory>
+
+struct A {
+  void print() { std::cout << "A" << std::endl; }
+};
+
+struct B : public A {
+  void print() { std::cout << "B" << std::endl; }
+};
+
+int main() {
+  std::shared_ptr<A> ptr_a(new A);
+  std::shared_ptr<B> ptr_b(new B);
+  
+  ptr_a->print();
+  ptr_b->print();
+  ptr_b->A::print();
+}
+    
