3181b9dc9276bb1aa8df60d007d92a85eedceecb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-13 00:08:35 +0000

    Update archive 6b

---
+++ trunk/Archive2/6b/04f743ad98ac1e/cmd.sh
@@ -0,0 +1,9 @@
+##clang++ --version
+##g++-4.8 --version2set -x ;
+# g++-4.9 -std=c++11 -fsanitize=undefined -fsanitize=address -pedantic -Wall -Wabi -Wpadded main.cpp && ./a.out 
+##g++ --version
+##set -x ; g++-4.9 -fsanitize=undefined  -std=c++11 -Wall -Wextra -Wconversion -pedantic  -O3  -pedantic  -pthread -S main.cpp && cat main.s 
+##set -x ; clang++ -std=c++11 -Wall -fsanitize=undefined -pedantic  -O3 main.cpp && ./a.out 
+set -x ; gcc -x c -O0 -std=c99 -Wall -Wconversion -fno-builtin  main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0 -W -Wall -Wextra -Wconversion -pedantic -S main.cpp && cat main.s 
+##set -x ; clang -x c -std=c99 -fsanitize=undefined  -Wall -Wextra -Wconversion -O0  -pedantic main.cpp && ./a.out
