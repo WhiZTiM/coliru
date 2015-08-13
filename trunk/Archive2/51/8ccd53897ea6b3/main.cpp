361d4f4dce21831010f214848b186ff2c7ce3dcc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-13 04:18:07 +0000

    Update archive 51

---
+++ trunk/Archive2/51/8ccd53897ea6b3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+
+using std::shared_ptr;
+
+struct Animal
+{
+    virtual ~Animal(){}
+    virtual void DoSomething(){std::cout << "Animal::DoSomething" << std::endl;}
+    int a=0;
+};
+
+struct Dog : Animal
+{
+    void  DoSomething() override {std::cout << "Dog::DoSomething" << std::endl;}
+    int b = 1;
+};
+
+void Add(shared_ptr<Animal> req)
+{
+    std::cout << "Add" << std::endl;
+}
+
+using animal_ptr =  shared_ptr<Animal>;
+using dog_ptr = shared_ptr<Dog>;
+
+int main()
+{ 
+    dog_ptr    dPtr1 (new Dog());
+    animal_ptr dPtr2 (new Dog());  
+    dPtr1->DoSomething();
+    dPtr2->DoSomething();
+    Add(dPtr1);
+    Add(dPtr2);
+    
+  return 0;
+}
