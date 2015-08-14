c5d1bc6275984d32dea9f3b39f0faf49732ae903
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 21:48:29 +0000

    Update archive 8d

---
+++ trunk/Archive2/8d/d80695ade3325d/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v(2, 1);
+    
+    std::cout << &v << " " << &v[0] << std::endl;
+    
+    v.resize(5);
+    
+    std::cout << &v << " " << &v[0] << std::endl;
+    
+    return 0;
+}
