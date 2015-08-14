f9130fbafa42a1459bc7e8fd311ebb93a247088c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 21:17:54 +0000

    Update archive 1d

---
+++ Archive2/1d/c8b193f48b4700/main.cpp
@@ -0,0 +1,39 @@
+#include <type_traits>
+#include <utility>
+#include <stddef.h>
+
+template <typename CharType, size_t Size>
+class StringT {};
+
+template <typename S, typename C>
+struct IsStringT : std::false_type {};
+
+template <typename C, size_t Size>
+struct IsStringT<StringT<C, Size>, C> : std::true_type {};
+
+template<typename T>
+void GetProperty(T & val)
+{
+    static_assert(IsStringT<T, char>{}, "FAILED");
+}
+
+template<>
+void GetProperty<bool>(bool & val)
+{
+    // Do stuff
+}
+
+template<>
+void GetProperty<int>(int & val)
+{
+    // Do stuff
+}
+
+int main()
+{
+    StringT<char, 512> str1;
+    GetProperty(str1);
+    
+    StringT<char, 1024> str2;
+    GetProperty(str2);
+}
