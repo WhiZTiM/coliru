84441bea6a6aacc1a3ef1234fc391c47f5876720
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 16:05:31 +0000

    Update archive 18

---
+++ Archive2/18/52986c8c60f463/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <cstdint>
+
+auto a = std::numeric_limits<long>::min();
+
+template <typename T, typename U>
+void f(){
+    static_assert(std::is_signed<T>::value, "not bright");
+    
+    U absMin = static_cast<U>(std::numeric_limits<T>::min());
+    std::cerr << absMin << std::endl;
+    std::cerr << -absMin << std::endl;
+}
+
+int main(int ,char**)
+{
+     
+    f<int8_t, uint8_t>(); 
+}
