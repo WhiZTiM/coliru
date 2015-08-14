565b3438deefd6313ee5076aec17abd6a50c98fd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 16:10:35 +0000

    Update archive 36

---
+++ Archive2/36/283ff470103a8d/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+
+template <typename T> struct identity { using type = T; };
+
+template <typename T1, typename T2>
+T2 mapGetByKey(std::map<T1, T2>& map, T1 key, const typename identity<T2>::type& defaultValue = {})
+{
+    auto it = map.find(key);
+
+    if (it != map.end())
+    {
+        return it->second;
+    }
+
+    return defaultValue;
+}
+
+template <typename T1>
+const char* mapGetByKey(std::map<T1, const char*>& map, const T1& key)
+{
+    return mapGetByKey(map, key, "default string");
+}
+
+int main()
+{
+    std::map<int, int> i;
+    std::map<int, const char*> c;
+    
+    std::cout << mapGetByKey(i, 0xCAFE) << std::endl;
+    std::cout << mapGetByKey(c, 0xBEEF) << std::endl;
+}
