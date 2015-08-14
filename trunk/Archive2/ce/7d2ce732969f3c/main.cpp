02a4e3709d6daee9f023adf955056883c7f5a34c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 18:48:53 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/7d2ce732969f3c/main.cpp
@@ -0,0 +1,9 @@
+<parent>
+    <isLocal>true</isLocal>
+    <manufacturer>manufacturername</manufacturer>
+    <modelName>modelname</modelName>
+    <IPAddress>123.45.67</IPAddress>
+    <modelNo>modelnumber</modelNo>
+    <modelUrl>URL to model site</modelUrl>
+    <serialNo>manufacturer's serial number</serialNo>
+</parent>
