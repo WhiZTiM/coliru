5cf32195f91efefeab43ae849cc52a90d6a598b1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 18:37:31 +0000

    Update archive b3

---
+++ Archive2/b3/b1559b8971067b/main.cpp
@@ -0,0 +1,7 @@
+<isLocal>false</isLocal>
+<manufacturer>joe bob's machines</manufacturer>
+<modelName>modelname</modelName>
+<IPAddress>980.123.912.5</IPAddress>
+<modelNo>05348437</modelNo>
+<modelUrl>mysite.com</modelUrl>
+<serialNo>12387ifj2837418</serialNo>
