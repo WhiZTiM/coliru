98bfcfce3a2e202205fc7f764e96f735944679ec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 20:02:51 +0000

    Update archive 88

---
+++ trunk/Archive2/88/192c9e1a44ed0a/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+char* findx(const char* s, const char* x)
+{
+    int count_s = 0;
+
+    while (s[count_s] != 0)
+    {
+        int count_x = 0;
+        while (x[count_x] != 0)
+        {
+            if (s[count_s] == x[count_x])
+            {
+                cout << "Found match " << x[count_x] << " at X[" << count_x
+                << "] with " << s[count_s] << " at S[" << count_s
+                << "]" << endl;
+                return 0;
+                break;
+            }
+            ++count_x;
+        }
+        ++count_s;
+    }
+
+    cout << "Match " << *x << " not found" << endl;
+    return 0;
+}
+
+int main()
+{
+    char* p = new char[]{ "Zorro" };
+    char* p1 = new char[]{ "Zio" };
+    findx(p, p1);
+    delete[] p;
+    delete[] p1;
+    return 0;
+}
