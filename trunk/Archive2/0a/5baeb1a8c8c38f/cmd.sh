5074770958a889dc95f4fe850f54212f51cd3d93
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 13:25:20 +0000

    Update archive 0a

---
+++ trunk/Archive2/0a/5baeb1a8c8c38f/cmd.sh
@@ -0,0 +1,2 @@
+echo -e "OUTPUT FROM TWO DIFFERENT COMPILERS:" && clang++ -std=c++14 -O -Wall -pedantic-errors -pthread main.cpp && ./a.out
+echo -e "\n================================\n" &&     g++ -std=c++14 -O -Wall -pedantic-errors -pthread main.cpp && ./a.out
