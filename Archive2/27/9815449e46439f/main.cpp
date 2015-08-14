c28848417c555df9191eb5615ca0df14191002db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 17:10:39 +0000

    Update archive 27

---
+++ Archive2/27/9815449e46439f/main.cpp
@@ -0,0 +1,35 @@
+#include <memory>
+#include <type_traits>
+
+template <typename...>
+struct voider { using type = void; };
+
+template <typename... Ts>
+using void_t = typename voider<Ts...>::type;
+
+template <typename, typename = void>
+struct is_callable : std::false_type {};
+
+template <typename F, typename... Args>
+struct is_callable<F(Args...), void_t<decltype(std::declval<F>()(std::declval<Args>()...))>> : std::true_type {};
+
+struct Bar {};
+
+template <typename Function>
+void foo(Function function)
+{
+    static_assert(is_callable<Function(std::unique_ptr<Bar>)>{}, "Not callable");
+    
+    auto bar = std::make_unique<Bar>();
+    
+    function(std::move(bar));
+}
+
+void ok(std::unique_ptr<Bar>) {}
+void nok(std::unique_ptr<Bar>&) {}
+
+int main()
+{
+    foo(ok);
+    foo(nok);
+}
