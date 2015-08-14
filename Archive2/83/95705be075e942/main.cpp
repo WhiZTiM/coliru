07d052c6edf60e665769e66e570c4c55cb1e167b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 15:13:19 +0000

    Update archive 83

---
+++ Archive2/83/95705be075e942/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+const int VOIGTR5_[3][3] = {{0, 2, 4}, {2, 1, 3}, {4, 3, -VOIGTR5_[0][0] - VOIGTR5_[0][1]}};
+
+int main() 
+{
+    for (int i=0; i< 4 ; i++)
+    {
+        for (int j=0; j< 4 ; j++)
+        {
+            printf("%d", VOIGTR5_[i][j]);
+            if(j<3)
+                printf("\t");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
