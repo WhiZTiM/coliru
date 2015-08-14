ed0d0ded5cc2429663d15663fc10680643c07c9e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 21:49:26 +0000

    Update archive 95

---
+++ trunk/Archive2/95/2162c624e9b705/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <initializer_list>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    double d = 1.2345;
+    std::printf("%g\n",d);
+    std::printf("%f\n",d);
+    std::printf("%e\n",d);
+    return 0;
+}
