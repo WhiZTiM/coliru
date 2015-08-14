fe1e6c8c0cb8c3fc42e0334bd8fb64d43425b64c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-14 22:05:50 +0000

    Update archive 6e

---
+++ trunk/Archive2/6e/94842db670ad83/main.cpp
@@ -0,0 +1,32 @@
+#include <type_traits>
+#include <utility>
+
+namespace detail
+{
+    template <typename T, std::size_t I, std::size_t J, typename... Ts>
+    constexpr bool has_duplicate = false;
+    
+    template <typename T, std::size_t I, std::size_t J, typename U, typename... Ts>
+    constexpr bool has_duplicate<T, I, J, U, Ts...> = has_duplicate<T, I, J+1, Ts...>;
+    
+    template <typename T, std::size_t I, std::size_t J, typename... Ts>
+    constexpr bool has_duplicate<T, I, J, T, Ts...> = true;
+    
+    template <typename T, std::size_t I, typename... Ts>
+    constexpr bool has_duplicate<T, I, I, T, Ts...> = has_duplicate<T, I, I+1, Ts...>;
+    
+    template <typename S, typename... Ts>
+    constexpr bool no_dupes = false;
+    
+    template <typename... Ts, std::size_t... Is>
+    constexpr bool no_dupes<std::index_sequence<Is...>, Ts...> = !(has_duplicate<Ts, Is, 0, Ts...> || ...);
+}
+
+template <typename... Ts>
+constexpr bool no_dupes = detail::no_dupes<std::index_sequence_for<Ts...>, Ts...>;
+
+int main()
+{
+    static_assert(no_dupes<int, float, char, double>, "!");
+    static_assert(!no_dupes<int, float, char, double, double>, "!");
+}
