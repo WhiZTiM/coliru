f886783588e275a16008786d508898400b99a721
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-15 19:58:06 +0000

    Update archive 99

---
+++ trunk/Archive2/99/d2cd7709bad12f/main.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int FrontStep[12]{
+    90,90,90,90,
+    35,50,66,81,
+    115,122,119,115
+    };
+    
+    
+    typedef int *M[100][4];
+    M Move[12];
+    
+    Move[0][0] = &FrontStep;
+}
