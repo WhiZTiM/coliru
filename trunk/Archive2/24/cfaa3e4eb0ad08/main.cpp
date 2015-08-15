49d514eebd39c34298c30e6ecfc780b9d7053bf6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-15 01:35:06 +0000

    Update archive 24

---
+++ trunk/Archive2/24/cfaa3e4eb0ad08/main.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef std::function<void(const string& hello)> Func;
+
+void Shareify(Func func) {
+    std::shared_ptr<Func> shared_func(&func);
+    
+    shared_func->("hello");
+}
+
+int main() {
+    Shareify(
+        [](const string& hello) {
+            cout << hello << endl;
+        });
+        
+            
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
