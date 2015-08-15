efa997143907a5183acd2a70bdbd1872bc1f4699
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-15 18:26:38 +0000

    Update archive d7

---
+++ Archive2/d7/e5253e2872bf17/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+std::size_t len( const char* cstr )
+{
+    std::size_t n = 0 ;
+    if( cstr != nullptr ) while( *cstr++ ) ++n ;
+    return n ;
+}
+
+char* cat_dot(const char* s1, const char* s2, const char* s3)
+{
+    char* p = new char[ len(s1) + len(s2) + len(s3) + 1 ]; // +1 for the null terminator
+
+    char* temp = p ; // make a copy; we want to return p unmodified
+
+    if(s1) while(*s1) *temp++ = *s1++ ;
+    if(s2) while(*s2) *temp++ = *s2++ ;
+
+    if(s3) while( ( *temp++ = *s3++ ) ) ; // null terminate this time
+
+    return p;
+}
+
+int main()
+{
+    std::cout << cat_dot( "Napoleone", " | ", "Boneparte" ) << '\n'
+              << cat_dot( "Napoleone", "|", "Boneparte" ) << '\n'
+              << cat_dot( "Napoleone", nullptr, "Boneparte" ) << '\n' ;
+              
+    // note: ignoring leaks for brevity          
+}
