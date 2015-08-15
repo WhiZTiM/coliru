0432b7756fe6116db3465d13cd4f1b45a503383a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-15 12:50:26 +0000

    Update archive 3e

---
+++ Archive2/3e/30b5a29bcd3e63/main.cpp
@@ -0,0 +1,17 @@
+#include <memory>
+#include <iostream>
+
+struct Noisy
+{
+    Noisy() { std::cout << "Noisy()" << std::endl; }
+    Noisy(const Noisy&) { std::cout << "Noisy(const Noisy&)" << std::endl; }
+    Noisy(Noisy&&) { std::cout << "Noisy(Noisy&&)" << std::endl; }
+    ~Noisy() { std::cout << "~Noisy()" << std::endl; }
+    Noisy& operator=(const Noisy&) { std::cout << "operator=(const Noisy&)" << std::endl; return *this; }
+    Noisy& operator=(Noisy&&) { std::cout << "operator=(Noisy&&)" << std::endl; return *this; }
+};
+
+int main()
+{
+    std::shared_ptr<Noisy> arr(new Noisy[5], std::default_delete<Noisy[]>{});
+}
