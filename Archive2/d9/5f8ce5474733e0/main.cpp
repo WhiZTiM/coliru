81cbf3f9336858ff95ee39c78e277669e0ef1c11
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-15 09:57:49 +0000

    Update archive d9

---
+++ Archive2/d9/5f8ce5474733e0/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+struct ListNode {
+  int val;
+  ListNode* next;
+};
+
+ListNode* deleteDuplicates(ListNode* head) {
+  if (head == NULL) return NULL;
+  for (ListNode* prev = head, *cur = head->next; prev->next; cur = prev->next)
+  {
+    if (prev->val == cur->val)
+    {
+      prev->next = cur->next;
+      delete cur;
+    }
+    else
+    {
+      prev = cur;
+    }
+  }
+  return head;
+}
+
+int main() {
+
+  ListNode *end = new ListNode{ 2, NULL };
+  ListNode *e2 = new ListNode{ 1, end };
+  ListNode *head = new ListNode{ 1, e2 };
+  head = deleteDuplicates(head);
+
+  ListNode *ptr = head;
+  while(ptr) {
+    std::cout << ptr->val << " -> ";
+    ptr = ptr->next;
+  }
+  std::cout << "NULL";
+
+  return 0;
+}
