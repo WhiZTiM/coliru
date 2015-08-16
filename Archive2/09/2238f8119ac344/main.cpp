61ffd2838612e935c967fbd972047ca210b2376a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-16 13:27:03 +0000

    Update archive 09

---
+++ Archive2/09/2238f8119ac344/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+double v = (180*9.8)/(42*42);  // v should be 1.000000
+
+std::cout.precision(20);
+
+cout << fixed << v << endl;
+
+cout<<asin(v);
+
+    return 0;
+}
