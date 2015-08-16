1da7b338e0414f178a8efead6f02bae76bf890c4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-16 16:27:39 +0000

    Update archive de

---
+++ trunk/Archive2/de/e8183e29487ff3/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <typeinfo>
+#include <functional>
+#include <utility>
+#include <cstddef>
+
+// The stolen function_traits struct...thing
+template<typename T>
+struct function_traits;
+
+template<typename R, typename ...Args>
+struct function_traits<std::function<R(Args...)>>
+{
+    static const size_t nargs = sizeof...(Args);
+
+    using result_type = R;
+
+    template <size_t i>
+    struct arg
+    {
+        using type = typename std::tuple_element<i, std::tuple<Args...>>::type;
+    };
+};
+
+int foo(float x, double d)
+{
+    return int(x);
+}
+
+template <size_t argIdx, typename R, typename... Args>
+auto getArgTypes(std::string& ref) 
+    -> typename std::enable_if<argIdx == 1>::type
+{
+    using fun = function_traits<std::function<R(Args...)> >;
+    ref.append(typeid(typename fun::template arg<0>).name()).append(" ");
+}
+
+template <size_t argIdx, typename R, typename... Args>
+auto getArgTypes(std::string& ref) 
+    -> typename std::enable_if<argIdx != 1>::type
+{
+    using fun = function_traits<std::function<R(Args...)> >;
+    ref.append(typeid(typename fun::template arg<argIdx-1>).name()).append(" ");
+    getArgTypes<argIdx - 1, R, Args...>(ref);
+}
+
+template <typename F, std::size_t... Is>
+void test2(std::index_sequence<Is...>)
+{
+    std::string s;
+    getArgTypes<F::nargs, typename F::result_type, typename F::template arg<Is>...>(s);
+    
+    std::cout << s;
+}
+
+void test()
+{
+    using F = function_traits<std::function<decltype(foo)>>;
+    test2<F>(std::make_index_sequence<F::nargs>{});
+}
+
+int main()
+{
+    test();
+}
