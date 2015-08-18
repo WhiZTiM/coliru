e4d8cead71ac0e96dd5317883f6c219038da7fa6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-18 09:41:26 +0000

    Update archive 02

---
+++ trunk/Archive2/02/fa699289f3b9e0/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main() {
+    vector<int> v = {1,2,3,4,5,6};
+
+    int iterator;
+    
+    cout << ":3" << endl;
+}
