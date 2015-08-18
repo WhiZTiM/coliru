e3ae71173b5a212bb20b35e664da43045b0f0377
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-18 17:00:04 +0000

    Update archive 26

---
+++ trunk/Archive2/26/d584b92193faac/main.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <functional>
+#include <sstream>
+#include <type_traits>
+#include <memory>
+
+struct plus_impl {
+  template<class Self, std::enable_if_t<std::is_class<std::decay_t<Self>>{}>* = nullptr>
+  friend std::decay_t<Self> operator+(int num, Self&& self) {
+    auto tmp = std::forward<Self>(self); // perfect forward a copy
+    tmp += num; // delegate to +=
+    return tmp; // elide return value
+  }
+  template<class Self, std::enable_if_t<std::is_class<std::decay_t<Self>>{}>* = nullptr>
+  friend std::decay_t<Self> operator+(Self&& self, int num) {
+    return num + std::forward<Self>(self); // DRY principle
+  }
+};
+
+template<class T>
+class MyClass :public plus_impl {
+public:
+  MyClass& operator+=(int num){
+      std::cout << "+=" << num << "\n";
+      return *this;
+  } // do actual work here
+};
+
+template<class T>
+struct SubClass : MyClass<T> {
+    SubClass() { std::cout << "SubClass\n"; }
+    ~SubClass() { std::cout << "~SubClass\n"; }
+    SubClass(SubClass&&) { std::cout << "SubClass&&\n"; }
+    SubClass(SubClass const&) { std::cout << "SubClass const&\n"; }
+};
+int main() {
+    {
+        SubClass<int> bob;
+        bob += 1;
+        bob + 2;
+        3 + bob;
+    }
+    {
+        4 + SubClass<int>{};
+    }
+}
