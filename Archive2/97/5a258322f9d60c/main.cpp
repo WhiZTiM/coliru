044dd6d4363b9ff89934cfcea846505ba9179be2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-18 12:18:23 +0000

    Update archive 97

---
+++ Archive2/97/5a258322f9d60c/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <memory>
+ 
+struct Foo { // object to manage
+    Foo() { std::cout << "Foo ctor\n"; }
+    Foo(const Foo&) { std::cout << "Foo copy ctor\n"; }
+    Foo(Foo&&) { std::cout << "Foo move ctor\n"; }
+    ~Foo() { std::cout << "~Foo dtor\n"; }
+};
+
+struct Exception {};
+
+struct InternalPtr {
+    Foo *ptr = nullptr;
+    InternalPtr(Foo *p) : ptr(p) {}
+    InternalPtr() = default;
+    
+    Foo& operator*() const {
+        std::cout << "Checking for a null pointer.." << std::endl;
+        if(ptr == nullptr)
+            throw Exception();
+        return *ptr;
+    }
+    
+    bool operator != (Foo *p) {
+        if(p != ptr)
+            return false;
+        else
+            return true;
+    }
+    void cleanup() {
+        if(ptr != nullptr)
+          delete ptr;
+    }
+};
+ 
+struct D { // deleter
+    using pointer = InternalPtr;
+    D() {};
+    D(const D&) { std::cout << "D copy ctor\n"; }
+    D(D&) { std::cout << "D non-const copy ctor\n";}
+    D(D&&) { std::cout << "D move ctor \n"; }
+    void operator()(InternalPtr& p) const {
+        std::cout << "D is deleting a Foo\n";
+        p.cleanup();
+    };
+};
+ 
+int main()
+{
+    std::unique_ptr<Foo, D> up(nullptr, D()); // deleter is moved
+    
+    try {
+      auto& e = *up;      
+    } catch(Exception&) {
+        std::cout << "null pointer exception detected" << std::endl;
+    }
+
+}
