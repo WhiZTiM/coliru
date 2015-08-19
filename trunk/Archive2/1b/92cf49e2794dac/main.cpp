0110b1be978df4ec0d44e8d9efe4b0dd0f5264ac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-19 13:39:46 +0000

    Update archive 1b

---
+++ trunk/Archive2/1b/92cf49e2794dac/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+#include <numeric>
+using namespace std;
+
+int main(void) {
+    static_assert(sizeof(int) % sizeof(char) == 0, "Don't do this cast");
+	ofstream foo("foo.txt", ios::binary);
+	vector<int> bar(numeric_limits<unsigned char>::max() + 1);
+
+	iota(bar.begin(), bar.end(), 0);
+
+	copy(reinterpret_cast<const char*>(&*bar.data()), reinterpret_cast<const char*>(&*bar.data() + bar.size()), ostreambuf_iterator<char>(foo));
+	foo.close();
+
+	ifstream file_read("foo.txt", ios::binary);
+	vector<decltype(bar)::value_type> output(bar.size());
+
+	copy(istreambuf_iterator<char>(file_read), istreambuf_iterator<char>(), reinterpret_cast<char*>(&*output.data()));
+
+	cout << "First element: " << output.front() << "\nLast element: " << output.back() << "\nAny non-consecutive elements: " << (output.cend() == mismatch(output.cbegin(), prev(output.cend()), next(output.cbegin()), [](auto first1, auto first2) { return first1 + 1 == first2; }).second ? "no\n" : "yes\n");
+
+	return 0;
+}
