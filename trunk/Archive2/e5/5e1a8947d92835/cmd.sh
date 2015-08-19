00d19534f9a312d614167852de52f1e0ee4ffa07
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-19 10:55:08 +0000

    Update archive e5

---
+++ trunk/Archive2/e5/5e1a8947d92835/cmd.sh
@@ -0,0 +1,2 @@
+clang++ --version | grep clang && clang++ -std=c++14 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+echo && g++ --version | grep GCC && g++ -std=c++14 -O3 -Wall -Wextra -pedantic-errors main.cpp  && ./a.out
