a6f380f273dc7f400258d337c76147e4bbb83caf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-19 19:05:15 +0000

    Update archive 88

---
+++ Archive2/88/5a6b694d32d9b5/main.cpp
@@ -0,0 +1,27 @@
+    #include <iostream>
+    #include <regex>
+    #include <string>
+    using namespace std;
+
+    int main()   
+    {
+        regex matcher("a*(needle)b*");
+        smatch findings;
+        string haystack("aaaaaaaaneedlebbbbbbbbbbbbbb");
+    
+        if( regex_match(haystack, findings, matcher) )
+        {
+            // What do I put here to know how the offset of "needle" in the 
+            // string haystack?
+    
+            // This is the position of the entire, which is
+            // always 0 with regex_match, with regex_search
+            cout << "smatch::position - " << findings.position() << endl;
+    
+            // Is this just a string or what? Are there member functions
+            // That can be called?
+            cout << "Needle - " << findings[1] << endl;
+        }
+        
+        return 0;
+    }
