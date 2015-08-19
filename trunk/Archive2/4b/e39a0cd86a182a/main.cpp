8bb47073cc6cfd0bff716041c3432181db1ce178
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-19 20:55:57 +0000

    Update archive 4b

---
+++ trunk/Archive2/4b/e39a0cd86a182a/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/fusion/adapted.hpp>
+#include <boost/fusion/sequence/io/out.hpp>
+#include <boost/fusion/sequence/intrinsic.hpp>
+#include <boost/fusion/view.hpp>
+#include <iostream>
+
+template <typename T, typename U>
+class DoubleTemplate
+{
+};
+
+namespace demo
+{
+    struct adaptee
+    {
+        DoubleTemplate<int, int> dt;
+    };
+}
+
+BOOST_FUSION_ADAPT_STRUCT( demo::adaptee, dt )
+
+int main()
+{
+    demo::adaptee a;
+    a.dt = DoubleTemplate<int, int>();
+ 
+}
