3a77a9201bd695357de928f5f2894fecefd82cfb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-20 07:37:00 +0000

    Update archive 5a

---
+++ trunk/Archive2/5a/342759d045b6d6/cmd.sh
@@ -0,0 +1,2 @@
+clang++ --version | grep clang && clang++ -std=c++14 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors -pthread main.cpp && ./a.out
+echo && g++ --version | grep GCC && g++ -std=c++14 -O3 -Wall -Wextra -pedantic-errors -pthread main.cpp  && ./a.out
