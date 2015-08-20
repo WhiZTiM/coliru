81ff1cbb8668f5e476dd52a9afce862d2c2f6d12
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-20 13:03:53 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/3fd45a0bd7082e/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void f(std::string s)
+{
+    std::cout << "s.data() " << (void*)s.data() << '\n';
+}
+
+std::string operator ""_s(const char* str, size_t len) { return std::string{str, len}; }
+
+std::string s() { return "abcd"; }
+
+int main()
+{
+    std::cout << (void*)("bar"_s.data()) << '\n';
+    std::cout << (void*)("foo"_s.data()) << '\n';
+    std::cout << (void*)("bar"_s.data()) << '\n';
+    f("bar"_s);
+}
