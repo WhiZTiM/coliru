f9f47752151e8349fd035784b94b1a7e5ff322eb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-20 10:25:56 +0000

    Update archive 71

---
+++ Archive2/71/e3d49ef1124ea5/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename T > struct bits_
+{
+    static_assert( std::is_integral<T>::value, "expected an integral type" ) ;
+    using uint_type = typename std::make_unsigned<T>::type; // C++14: std::make_unsigned_t<T>
+    uint_type number ;
+    bool first_digit = true ;
+    explicit bits_( uint_type n ) : number(n) {}
+};
+
+template < typename CHAR_TYPE, typename TRAITS_TYPE, typename T >
+std::basic_ostream<CHAR_TYPE,TRAITS_TYPE>&
+operator<< ( std::basic_ostream<CHAR_TYPE,TRAITS_TYPE>& stm, bits_<T> b )
+{
+    if( b.number == 0 && b.first_digit ) return stm << 0 ;
+    else if( b.number == 1 ) return stm << +(b.number) ;
+    else
+    {
+        b.first_digit = false ;
+        const int last_digit = b.number%2 ;
+        b.number /= 2 ;
+        return stm << b << last_digit ;
+    }
+}
+
+template <typename T > bits_<T> bits( T n ) { return bits_<T>(n) ; }
+
+int main()
+{
+    for( int i : { 0, 1023, 256, 100, 0x01000010, -1 } ) std::cout << bits(i) << '\n' ;
+
+    std::cout << bits( char(-1) ) << '\n' << bits(18LL) << '\n' << bits( -1LL ) << '\n' ;
+    std::wcout << bits( char(-1) ) << L'\n' << bits(18LL) << L'\n' << bits( -1LL ) << L'\n' ;
+}
