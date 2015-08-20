f0746b0b691483ac0d95e0a83977d637eaaa1b8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-20 16:14:39 +0000

    Update archive 0c

---
+++ Archive2/0c/432dd8300a104d/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+    template<typename T>
+    auto foo(T&& t) { std::cout<<"general version"<<std::endl; }
+
+    struct A {};
+    
+    auto foo(A a) { std::cout<<"A"<<std::endl; }
+
+    int main()
+    {
+        A a;
+        const A aconst;
+    
+        foo(a);
+        foo(aconst);
+        foo(std::move(a));
+        foo(std::move(aconst));
+    }
