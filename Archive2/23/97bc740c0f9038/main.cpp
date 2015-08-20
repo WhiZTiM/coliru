711160c00438dbca291053bc8c28bd11962a8f93
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-20 16:51:05 +0000

    Update archive 23

---
+++ Archive2/23/97bc740c0f9038/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+
+class Bar {};
+
+template<typename T, typename std::enable_if<!std::is_same<T, Bar>::value>::type* = nullptr>
+auto foo(T&& t) { std::cout<<"general version"<<std::endl; }
+
+template<typename T, typename std::enable_if<std::is_same<T, Bar>::value>::type* = nullptr>
+auto foo(T&& t) { std::cout<<"Foo version"<<std::endl; }
+    
+int main()
+{
+    foo(25);
+    foo(Bar());
+}
