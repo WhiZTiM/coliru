cebaa0b577f53c5936ec478716bab6f103fb018b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-21 17:02:04 +0000

    Update archive c4

---
+++ trunk/Archive2/c4/78acea41f4f136/main.cpp
@@ -0,0 +1,21 @@
+template<class T>
+class C
+{   
+    int i;
+public:
+    C(int d) : i(d) {}
+    
+    template<class U>
+    void f(C<U> other)
+    {
+        i = static_cast<T>(other.i);
+    }
+};
+
+int main()
+{
+    C<int> ci = 5;
+    C<float> cf = 8.0f;
+    
+    ci.f(cf);
+}
