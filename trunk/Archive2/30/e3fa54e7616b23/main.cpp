2bd76d4b1545996306a3445d73e94bbe93f232be
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-21 04:33:30 +0000

    Update archive 30

---
+++ trunk/Archive2/30/e3fa54e7616b23/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iostream>
+
+struct point2d {
+    int x;
+    int y;
+    
+    point2d operator+(point2d right) {
+        point2d result;
+        result.x = x + right.x;
+        result.y = y + right.y;
+        
+        return (result);
+    }
+};
+
+point2d operator*(int left, point2d right) {
+    point2d result;
+    result.x = left * right.x;
+    result.y = left * right.y;
+    return result;
+}
+
+//Let's "cheat" a little by using our
+//operator above!
+point2d operator*(point2d left, int right) {
+    return(right * left);
+}
+
+int main() {
+    point2d pointA = {1,1};
+    point2d pointB = {3,5};
+    point2d sum = pointA + pointB;
+
+    std::cout << "pointA: (" << pointA.x << "," << pointA.y << ")" << std::endl;
+    std::cout << "pointB: (" << pointB.x << "," << pointB.y << ")" << std::endl;
+    std::cout << "Sum: (" << sum.x << "," << sum.y << ")" << std::endl;
+    
+    //Now this will work.
+    point2d multiplied_int = 2 * sum;
+    
+    std::cout << "2 * sum: (" << multiplied_int.x << "," << multiplied_int.y << ")" << std::endl;
+    
+    return 0;
+}
