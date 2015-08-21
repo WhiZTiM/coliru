ba61e6af297c190b306dc5aa63fd74a620b09da2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-21 06:16:08 +0000

    Update archive 0f

---
+++ Archive2/0f/63ce7762631db1/main.cpp
@@ -0,0 +1,23 @@
+
+
+
+// from boost/asio/detail/impl/strand_service.ipp:
+void strand_service::do_post(implementation_type& impl, operation* op, bool is_continuation)
+{
+    impl->mutex_.lock();
+    if (impl->locked_)
+    {
+        // Some other handler already holds the strand lock. Enqueue for later.
+        impl->waiting_queue_.push(op);
+        impl->mutex_.unlock();
+    }
+    else
+    {
+        // The handler is acquiring the strand lock and so is responsible for
+        // scheduling the strand.
+        impl->locked_ = true;
+        impl->mutex_.unlock();
+        impl->ready_queue_.push(op);
+        io_service_.post_immediate_completion(impl, is_continuation);
+    }
+}
