b9ce9e72192c21f67e0a9c24bb10cb68e42782c2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-21 04:30:52 +0000

    Update archive 23

---
+++ trunk/Archive2/23/62f297bbaa1cec/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+//Definition
+struct point2d {
+    int x;
+    int y;
+};
+
+
+
+
+
+
+
+//This is our operator to add together the X's and Y's for point2d
+point2d operator+(point2d left, point2d right) {
+    point2d result;
+    result.x = left.x + right.x;
+    result.y = left.y + right.y;
+    
+    return result;
+}
+
+
+
+
+int main() {
+    point2d pointA = {1, 2};
+    point2d pointB = {3, 4};
+
+    std::cout << "pointA: (" << pointA.x << "," << pointA.y << ")" << std::endl;
+    std::cout << "pointB: (" << pointB.x << "," << pointB.y << ")" << std::endl;
+    
+    //Let's use the new operator!
+    point2d sumAB = pointA + pointB;
+    
+    std::cout << "pointA + pointB: (" << sumAB.x << "," << sumAB.y << ")" << std::endl;
+    
+    return 0;
+}
