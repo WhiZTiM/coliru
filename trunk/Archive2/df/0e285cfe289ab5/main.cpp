dc8e3d9aecc51a6782c56c4ede38bf99026ce423
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-22 09:31:13 +0000

    Update archive df

---
+++ trunk/Archive2/df/0e285cfe289ab5/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// main int()
+int main()
+{
+    // cout << "Programming is great fun!"'
+    cout << "Programming is great fun!\n"; // semicolon
+
+    // return 0 
+    return 0; // semicolon
+}
