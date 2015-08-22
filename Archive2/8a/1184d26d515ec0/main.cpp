40c101141563680c6b3d2018f78559a66f5bba28
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-22 00:28:25 +0000

    Update archive 8a

---
+++ Archive2/8a/1184d26d515ec0/main.cpp
@@ -0,0 +1,87 @@
+#include <future>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+
+
+template<typename T>
+struct SharedState
+{
+    bool ready_ = false;
+    T result_ = 0;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+};
+
+
+template<typename T>
+struct Future
+{
+    Future(const std::shared_ptr<SharedState<T>>& inSharedState) :
+        mSharedState(inSharedState)
+    {
+    }
+
+    T get()
+    {
+        std::unique_lock<std::mutex> lock(mSharedState->mutex_);
+        while (!mSharedState->ready_)
+        {
+            mSharedState->cond_.wait(lock);
+        }
+        return mSharedState->result_;
+    }
+
+    std::shared_ptr<SharedState<T>> mSharedState;
+};
+
+
+template<typename T>
+struct Promise
+{
+
+    Promise() : mSharedState(std::make_shared<SharedState<T>>()) {}
+
+    void set_value(const T& t)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mSharedState->mutex_);
+            mSharedState->ready_ = true;
+            mSharedState->result_ = t;
+        }
+        mSharedState->cond_.notify_one();
+    }
+
+    void set_value(T&& t)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mSharedState->mutex_);
+            mSharedState->ready_ = true;
+            mSharedState->result_ = std::move(t);
+        }
+        mSharedState->cond_.notify_one();
+    }
+
+    Future<T> get_future()
+    {
+        return Future<T>(mSharedState);
+    }
+
+    std::shared_ptr<SharedState<T>> mSharedState;
+};
+
+
+int main()
+{
+    Promise<int> p;
+    auto fut = p.get_future();
+    std::thread t1([&]
+    {
+        p.set_value(42);
+    });
+
+    std::cout << fut.get() << std::endl;
+
+    t1.join();
+}
