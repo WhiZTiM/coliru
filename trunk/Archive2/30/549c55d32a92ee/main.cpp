db1d113c88454c9ca9a66bed1f495893396c7a29
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-23 14:03:18 +0000

    Update archive 30

---
+++ trunk/Archive2/30/549c55d32a92ee/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <utility>
+#include <tuple>
+#include <cstddef>
+
+struct Noisy
+{
+    Noisy() { std::cout << "Noisy()" << std::endl; }
+    Noisy(const Noisy&) { std::cout << "Noisy(const Noisy&)" << std::endl; }
+    Noisy(Noisy&&) { std::cout << "Noisy(Noisy&&)" << std::endl; }
+    ~Noisy() { std::cout << "~Noisy()" << std::endl; }
+    Noisy& operator=(const Noisy&) { std::cout << "operator=(const Noisy&)" << std::endl; return *this; }
+    Noisy& operator=(Noisy&&) { std::cout << "operator=(Noisy&&)" << std::endl; return *this; }
+};
+
+template <typename T>
+auto explode(T&& t, char)
+{
+    return std::forward_as_tuple(std::forward<T>(t));
+}
+
+template <typename T, int = std::tuple_size<std::decay_t<T>>{}>
+auto explode(T&& t, int);
+
+template <typename T, std::size_t... Is>
+auto explode(T&& t, std::index_sequence<Is...>)
+{
+    return std::tuple_cat(explode(std::get<Is>(std::forward<T>(t)), 0)...);
+}
+
+template <typename T, int = std::tuple_size<std::decay_t<T>>{}>
+auto explode(T&& t, int)
+{
+    return explode(std::forward<T>(t), std::make_index_sequence<std::tuple_size<std::decay_t<T>>{}>{});
+}
+
+template <typename T, std::size_t... Is>
+auto decay_tuple(T&& t, std::index_sequence<Is...>)
+{
+    return std::make_tuple(std::get<Is>(std::forward<T>(t))...);
+}
+
+template <typename T>
+auto decay_tuple(T&& t)
+{
+    return decay_tuple(std::forward<T>(t), std::make_index_sequence<std::tuple_size<std::decay_t<T>>{}>{});
+}
+
+template <std::size_t... Is, typename T>
+auto merge_tuple(T&& t, std::index_sequence<Is...>)
+{
+    return decay_tuple(std::tuple_cat(explode(std::get<Is>(std::forward<T>(t)), 0)...));
+}
+
+template <typename T>
+auto merge_tuple(T&& t)
+{
+    return merge_tuple(std::forward<T>(t), std::make_index_sequence<std::tuple_size<std::decay_t<T>>{}>{});
+}
+
+// Tests:
+
+template <typename T> void TD() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+int main()
+{
+    std::tuple<std::tuple<std::tuple<std::tuple<Noisy, Noisy>>>, std::tuple<std::tuple<Noisy>>> t;
+    
+    auto x = merge_tuple(t);
+    TD<decltype(x)>();
+}
