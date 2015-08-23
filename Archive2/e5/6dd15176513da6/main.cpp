f04b241858d023966d42a3d9201a2baee261ba85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-23 16:39:31 +0000

    Update archive e5

---
+++ Archive2/e5/6dd15176513da6/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template <typename T>
+class MyClass
+{
+public:
+    template <typename C
+            , typename = std::enable_if_t<std::is_class<C>::value>>
+    MyClass(C) { std::cout << "class\n"; }
+    
+    template <typename C
+            , typename = std::enable_if_t<std::is_pointer<C>::value>
+            , typename = void>
+    //        ~~~~~~~~~~~~~~^
+    MyClass(C) { std::cout << "pointer\n"; }
+};
+
+struct S {};
+
+int main()
+{
+    int i{};
+    MyClass<int> a{ &i };
+    MyClass<int> b{ S{} };
+}
