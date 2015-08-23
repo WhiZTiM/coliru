fa4d39e15b846dab98575641b8be84f70642550c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-23 17:41:01 +0000

    Update archive 7f

---
+++ Archive2/7f/6c6ecb8ffad2d5/main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <utility>
+#include <tuple>
+#include <cstddef>
+#include <type_traits>
+
+struct Noisy
+{
+    Noisy() { std::cout << "Noisy()" << std::endl; }
+    Noisy(const Noisy&) { std::cout << "Noisy(const Noisy&)" << std::endl; }
+    Noisy(Noisy&&) { std::cout << "Noisy(Noisy&&)" << std::endl; }
+    ~Noisy() { std::cout << "~Noisy()" << std::endl; }
+    Noisy& operator=(const Noisy&) { std::cout << "operator=(const Noisy&)" << std::endl; return *this; }
+    Noisy& operator=(Noisy&&) { std::cout << "operator=(Noisy&&)" << std::endl; return *this; }
+};
+
+
+
+
+
+
+
+template <typename T>
+auto explode(T&& t, char)
+{
+    return std::forward_as_tuple(std::forward<T>(t));
+}
+
+template <typename T, std::size_t I = std::tuple_size<std::decay_t<T>>{}>
+auto explode(T&& t, int);
+
+template <typename T, std::size_t... Is>
+auto explode(T&& t, std::index_sequence<Is...>)
+{
+    return std::tuple_cat(
+        explode(
+            std::get<Is>(std::forward<T>(t)), 0
+        )...
+    );
+}
+
+template <typename T, std::size_t I>
+auto explode(T&& t, int)
+{
+    return explode(std::forward<T>(t), std::make_index_sequence<I>{});
+}
+
+
+
+/*
+template <typename T, std::size_t... Is>
+auto decay_tuple(T&& t, std::index_sequence<Is...>)
+{
+    return std::make_tuple(std::get<Is>(std::forward<T>(t))...);
+}
+
+template <typename T>
+auto decay_tuple(T&& t)
+{
+    return decay_tuple(std::forward<T>(t), std::make_index_sequence<std::tuple_size<std::decay_t<T>>{}>{});
+}*/
+
+template <typename T, std::size_t... Is>
+auto merge_tuple(T&& t, std::index_sequence<Is...>)
+{
+    return ///decay_tuple(
+        //std::forward_as_tuple(
+        std::tuple_cat(
+            explode(
+                std::get<Is>(std::forward<T>(t)), 0
+            )...
+        );
+    //);
+}
+
+template <typename T>
+auto merge_tuple(T&& t)
+{
+    return merge_tuple(
+        std::forward<T>(t), 
+        std::make_index_sequence<
+            std::tuple_size< std::decay_t<T> >::value
+        >()
+    );
+}
+
+// Tests:
+
+template <typename T> void TD() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+
+template <typename T>
+void test(T&& t)
+{
+     TD<decltype(t)>();
+}
+
+int main()
+{
+    std::tuple<std::tuple<std::tuple<std::tuple<Noisy&, Noisy&>>>, std::tuple<std::tuple<Noisy&>>> t;
+    
+    std::cout << "--------------------------" << std::endl;    
+    auto x = merge_tuple(t);
+    //auto&& x = merge_tuple(std::tuple<std::tuple<std::tuple<std::tuple<Noisy, Noisy>>>, std::tuple<std::tuple<Noisy>>>());
+    /*test(
+        merge_tuple(std::tuple<std::tuple<std::tuple<std::tuple<Noisy, Noisy>>>, std::tuple<std::tuple<Noisy>>>())
+        );*/
+    std::cout << "--------------------------" << std::endl;
+    
+    TD<decltype(x)>();
+    
+    //static_assert(std::is_same<decltype(x), std::tuple<Noisy, Noisy, Noisy>>{}, "!");
+}
