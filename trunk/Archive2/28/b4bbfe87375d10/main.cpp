e4038386fb9f7db1f08d3e4f4807c016c26fdc5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-24 04:07:20 +0000

    Update archive 28

---
+++ trunk/Archive2/28/b4bbfe87375d10/main.cpp
@@ -0,0 +1,21 @@
+template <typename T>
+struct A;
+
+template <typename T>
+constexpr A<T> f();
+
+template <typename T>
+struct A {
+  friend A f<T>();
+ private:
+  constexpr A(){}
+};
+
+template <typename T>
+constexpr A<T> f() { return {}; }
+
+int main() {
+  constexpr auto a  = f<void>();
+
+}
+
