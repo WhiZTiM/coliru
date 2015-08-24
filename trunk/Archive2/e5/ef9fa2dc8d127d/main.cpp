ad6516363dafc75287aef07b254daed39554a527
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-24 13:56:44 +0000

    Update archive e5

---
+++ trunk/Archive2/e5/ef9fa2dc8d127d/main.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+struct Element
+{
+    int a;
+    int b;
+    int c;
+};
+
+bool order_by_a(const Element& lhs, const Element& rhs)
+{
+    return lhs.a < rhs.a;
+}
+
+std::ostream& operator<<(std::ostream& out, const Element& s)
+{
+    return out << '(' << std::setw(2) << s.a <<
+        		 ", " << std::setw(2) << s.b <<
+				 ", " << std::setw(2) << s.c << ')';
+}
+
+Element generate_Element()
+{
+	return {rand(), rand(), rand()};
+}
+
+std::vector<Element> get_array()
+{
+    std::cout << "How many array elements you want?\n";
+    int size;
+    std::cin >> size;
+    std::vector<Element> array(size);
+    std::generate(array.begin(), array.end(), generate_Element);
+    std::sort(array.begin(), array.end(), order_by_a);
+    return array;
+}
+
+int main()
+{
+    srand(time(nullptr));
+    auto array = get_array();
+
+	std::cout << "Array data:\n";
+    for(const auto& v: array)
+        std::cout << v << '\n';
+    std::cout << '\n';
+    
+    array.erase(array.begin() + 2);
+    
+    std::cout << "Array data after erase:\n";
+    for(const auto& v: array)
+        std::cout << v << '\n';
+    std::cout << '\n';
+}
