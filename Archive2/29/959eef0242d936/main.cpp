09d1fafc954a19be427fad7c284e5e63c9296e21
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-24 16:55:38 +0000

    Update archive 29

---
+++ Archive2/29/959eef0242d936/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::vector<int>> create_lists(int no_lists, int no_per_list)
+{
+    int counter = 0;
+    std::vector<std::vector<int>> temp(no_lists, std::vector<int>(no_per_list));
+    for (auto & e : temp)
+        std::generate(e.begin(), e.end(), [&counter](){return counter++;});
+    return temp;
+}
+    
+int main()
+{
+    auto foo = create_lists(3,10);
+    for (const auto & v : foo)
+    {
+        for(const auto & e : v)
+            std::cout << e << " ";
+        std::cout << "\n";
+    }
+}
