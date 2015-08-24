9fa9b9e974ae5c50f991146efea8c5872b1083be
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-08-24 18:54:15 +0000

    Update archive ec

---
+++ Archive2/ec/1a56826c769ef3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main()
+{
+    for (int id = 1; id < 4; ++id) {
+        std::cout << id << ": ";
+        switch (id) {
+            if (1) {
+  case 1: std::cout << "A";
+            } else {
+  case 2: std::cout << "E";
+            }
+          std::cout << "B";
+          std::cout << "C";
+          std::cout << "D";
+          break;
+  case 3: std::cout << "G";
+          break;
+        }
+        std::cout << "\n---\n";
+    }
+    return 0;
+}
